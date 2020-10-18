09bdb3d867fc5bf205c432209e88283fbda9beaf
junho <qkrwnsgh1288@daum.net>
2020-10-19 00:52:41 +0900

    1. add SwapExactAmountIn, SwapExactAmountOut, Error logs 2. add setStore in store.go

--- x/gamm/types/errors.go
+++ x/gamm/types/errors.go
@@ -4,9 +4,10 @@ import sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 // x/gamm module sentinel errors
 var (
-	ErrPoolNotFound = sdkerrors.Register(ModuleName, 2, "pool not found")
-	ErrMathApprox   = sdkerrors.Register(ModuleName, 3, "math approx error")
-	ErrLimitExceed  = sdkerrors.Register(ModuleName, 4, "limit exceeded")
+	ErrPoolNotFound   = sdkerrors.Register(ModuleName, 2, "pool not found")
+	ErrMathApprox     = sdkerrors.Register(ModuleName, 3, "math approx error")
+	ErrLimitExceed    = sdkerrors.Register(ModuleName, 4, "limit exceeded")
+	ErrInvalidRequest = sdkerrors.Register(ModuleName, 5, "bad request")
 
 	ErrNotBound      = sdkerrors.Register(ModuleName, 100, "ERR_NOT_BOUND")
 	ErrMaxInRatio    = sdkerrors.Register(ModuleName, 101, "ERR_MAX_IN_RATIO")
