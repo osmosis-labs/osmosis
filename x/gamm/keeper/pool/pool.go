428f462157334620abb1b3a5ff790b3db6597f48
orngefrost <frostornge@gmail.com>
2020-10-18 20:10:16 +0900

    feat(x/gamm): add naive fields

--- x/gamm/keeper/pool/pool.go
+++ x/gamm/keeper/pool/pool.go
@@ -2,6 +2,13 @@ package pool
 
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
+type Record struct {
+	DenormalizedWeight sdk.Dec
+	Balance            sdk.Int
+}
+
 type Pool struct {
-	Address sdk.AccAddress
+	Address     sdk.AccAddress
+	Records     map[string]Record
+	TotalWeight sdk.Int
 }
