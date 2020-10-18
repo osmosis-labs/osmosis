7a1e41fbf3ffca8bebf275f2ac969b67e51e2b42
orngefrost <frostornge@gmail.com>
2020-10-18 17:37:08 +0900

    refactor(x/gamm): rename Pool to Service

--- x/gamm/keeper/pool/pool.go
+++ x/gamm/keeper/pool/pool.go
@@ -1,72 +1,7 @@
 package pool
 
-import (
-	"github.com/c-osmosis/osmosis/x/gamm/types"
-	"github.com/cosmos/cosmos-sdk/codec"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
-)
+import sdk "github.com/cosmos/cosmos-sdk/types"
 
-type Pool interface {
-	Viewer
-
-	CreatePool(sdk.Context, sdk.AccAddress, sdk.Dec, []types.TokenInfo) error
-	JoinPool(sdk.Context, sdk.AccAddress, sdk.AccAddress, sdk.Int, []types.MaxAmountIn) error
-	ExitPool(sdk.Context, sdk.AccAddress, sdk.AccAddress, sdk.Int, []types.MinAmountOut) error
-}
-
-type pool struct {
-	viewer
-
-	cdc           codec.BinaryMarshaler
-	storeKey      sdk.StoreKey
-	accountKeeper types.AccountKeeper
-	bankKeeper    bankkeeper.Keeper
-}
-
-func NewPool(
-	cdc codec.BinaryMarshaler,
-	storeKey sdk.StoreKey,
-	accountKeeper types.AccountKeeper,
-	bankKeeper bankkeeper.Keeper,
-) Pool {
-	return pool{
-		viewer: viewer{
-			cdc:      cdc,
-			storeKey: storeKey,
-		},
-		cdc:           cdc,
-		storeKey:      storeKey,
-		accountKeeper: accountKeeper,
-		bankKeeper:    bankKeeper,
-	}
-}
-
-func (p pool) CreatePool(
-	ctx sdk.Context,
-	sender sdk.AccAddress,
-	swapFee sdk.Dec,
-	tokenInfo []types.TokenInfo,
-) error {
-	return nil
-}
-
-func (p pool) JoinPool(
-	ctx sdk.Context,
-	sender sdk.AccAddress,
-	targetPool sdk.AccAddress,
-	poolAmountOut sdk.Int,
-	maxAmountsIn []types.MaxAmountIn,
-) error {
-	return nil
-}
-
-func (p pool) ExitPool(
-	ctx sdk.Context,
-	sender sdk.AccAddress,
-	targetPool sdk.AccAddress,
-	poolAmountIn sdk.Int,
-	minAmountsOut []types.MinAmountOut,
-) error {
-	return nil
+type Pool struct {
+	Address sdk.AccAddress
 }
