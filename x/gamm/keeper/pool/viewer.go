3ae1cab686d7c46983467a0199532ffd99a8976d
orngefrost <frostornge@gmail.com>
2020-10-18 17:29:01 +0900

    feat(x/gamm): add implementation of interface Viewer

--- x/gamm/keeper/pool/viewer.go
+++ x/gamm/keeper/pool/viewer.go
@@ -1,3 +1,13 @@
 package pool
 
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 type Viewer interface{}
+
+type viewer struct {
+	cdc      codec.BinaryMarshaler
+	storeKey sdk.StoreKey
+}
