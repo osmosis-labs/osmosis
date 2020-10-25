063e0dbc7d3cfa8936692ac4840da163957aa341
orngefrost <frostornge@gmail.com>
2020-10-25 16:43:48 +0900

    common(x/gamm): fix typo & sort imports

--- x/gamm/keeper/pool/params.go
+++ x/gamm/keeper/pool/params.go
@@ -2,6 +2,6 @@ package pool
 
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
-// TODO: handle this constanct in the param space
+// TODO: handle this constants in the param space
 var maxInRatio, _ = sdk.NewDecFromStr("0.5")
 var maxOutRatio, _ = sdk.NewDecFromStr("0.34")
