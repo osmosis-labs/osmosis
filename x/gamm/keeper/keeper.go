b11b5463762059080f8dc853335879e619347d55
Thunnini <yunjh1994@gmail.com>
2021-01-04 01:51:20 +0900

    feat(x/gamm): add the checker of the module account when creating keeper

--- x/gamm/keeper/keeper.go
+++ x/gamm/keeper/keeper.go
@@ -1,11 +1,24 @@
 package keeper
 
 import (
+	"fmt"
+
 	"github.com/c-osmosis/osmosis/x/gamm/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 )
+
+func permContains(perms []string, perm string) bool {
+	for _, v := range perms {
+		if v == perm {
+			return true
+		}
+	}
+
+	return false
+}
 
 type Keeper struct {
 	storeKey sdk.StoreKey
@@ -17,6 +30,18 @@ type Keeper struct {
 }
 
 func NewKeeper(cdc codec.BinaryMarshaler, storeKey sdk.StoreKey, accountKeeper types.AccountKeeper, bankKeeper bankkeeper.Keeper) Keeper {
+	// Ensure that the module account are set.
+	moduleAddr, perms := accountKeeper.GetModuleAddressAndPermissions(types.ModuleName)
+	if moduleAddr == nil {
+		panic(fmt.Sprintf("%s module account has not been set", types.ModuleName))
+	}
+	if !permContains(perms, authtypes.Minter) {
+		panic(fmt.Sprintf("%s module account should have the minter permission", types.ModuleName))
+	}
+	if !permContains(perms, authtypes.Burner) {
+		panic(fmt.Sprintf("%s module account should have the burner permission", types.ModuleName))
+	}
+
 	return Keeper{
 		storeKey: storeKey,
 		cdc:      cdc,
