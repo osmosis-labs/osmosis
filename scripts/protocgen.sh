c608df1be94ab7f9d100a9bd3e6cd034810dd1c2
antstalepresh <stalepresh121@outlook.com>
2021-01-15 11:51:15 +1000

    simplify proto-generation

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,17 +2,10 @@
 
 set -eo pipefail
 
-protoc_gen_gocosmos() {
-  if ! grep "github.com/gogo/protobuf => github.com/regen-network/protobuf" go.mod &>/dev/null ; then
-    echo -e "\tPlease run this command from somewhere inside the cosmos-sdk folder."
-    return 1
-  fi
-
-  go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
-  go get github.com/cosmos/cosmos-sdk 2>/dev/null
-}
-
-protoc_gen_gocosmos
+# get protoc executions
+go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
+# get cosmos sdk from github
+go get github.com/cosmos/cosmos-sdk 2>/dev/null
 
 # Get the path of the cosmos-sdk repo from go/pkg/mod
 cosmos_sdk_dir=$(go list -f '{{ .Dir }}' -m github.com/cosmos/cosmos-sdk)
