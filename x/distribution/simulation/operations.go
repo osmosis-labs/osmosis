830e1bc5d48cfd353930c080733bf6d3bed3d9f4
ahmedaly113 <ahmedaly113@outlook.com>
2021-03-05 01:28:54 +0800

    fork distribution module from latest sdk, customize proto

---
+++ x/distribution/simulation/operations.go
@@ -0,0 +1,287 @@
+package simulation
+
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/simapp/helpers"
+	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/distribution/keeper"
+	"github.com/cosmos/cosmos-sdk/x/distribution/types"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
+)
+
+// Simulation operation weights constants
+const (
+	OpWeightMsgSetWithdrawAddress          = "op_weight_msg_set_withdraw_address"
+	OpWeightMsgWithdrawDelegationReward    = "op_weight_msg_withdraw_delegation_reward"
+	OpWeightMsgWithdrawValidatorCommission = "op_weight_msg_withdraw_validator_commission"
+	OpWeightMsgFundCommunityPool           = "op_weight_msg_fund_community_pool"
+)
+
+// WeightedOperations returns all the operations from the module with their respective weights
+func WeightedOperations(
+	appParams simtypes.AppParams, cdc codec.JSONMarshaler, ak types.AccountKeeper,
+	bk types.BankKeeper, k keeper.Keeper, sk stakingkeeper.Keeper,
+) simulation.WeightedOperations {
+
+	var weightMsgSetWithdrawAddress int
+	appParams.GetOrGenerate(cdc, OpWeightMsgSetWithdrawAddress, &weightMsgSetWithdrawAddress, nil,
+		func(_ *rand.Rand) {
+			weightMsgSetWithdrawAddress = simappparams.DefaultWeightMsgSetWithdrawAddress
+		},
+	)
+
+	var weightMsgWithdrawDelegationReward int
+	appParams.GetOrGenerate(cdc, OpWeightMsgWithdrawDelegationReward, &weightMsgWithdrawDelegationReward, nil,
+		func(_ *rand.Rand) {
+			weightMsgWithdrawDelegationReward = simappparams.DefaultWeightMsgWithdrawDelegationReward
+		},
+	)
+
+	var weightMsgWithdrawValidatorCommission int
+	appParams.GetOrGenerate(cdc, OpWeightMsgWithdrawValidatorCommission, &weightMsgWithdrawValidatorCommission, nil,
+		func(_ *rand.Rand) {
+			weightMsgWithdrawValidatorCommission = simappparams.DefaultWeightMsgWithdrawValidatorCommission
+		},
+	)
+
+	var weightMsgFundCommunityPool int
+	appParams.GetOrGenerate(cdc, OpWeightMsgFundCommunityPool, &weightMsgFundCommunityPool, nil,
+		func(_ *rand.Rand) {
+			weightMsgFundCommunityPool = simappparams.DefaultWeightMsgFundCommunityPool
+		},
+	)
+
+	return simulation.WeightedOperations{
+		simulation.NewWeightedOperation(
+			weightMsgSetWithdrawAddress,
+			SimulateMsgSetWithdrawAddress(ak, bk, k),
+		),
+		simulation.NewWeightedOperation(
+			weightMsgWithdrawDelegationReward,
+			SimulateMsgWithdrawDelegatorReward(ak, bk, k, sk),
+		),
+		simulation.NewWeightedOperation(
+			weightMsgWithdrawValidatorCommission,
+			SimulateMsgWithdrawValidatorCommission(ak, bk, k, sk),
+		),
+		simulation.NewWeightedOperation(
+			weightMsgFundCommunityPool,
+			SimulateMsgFundCommunityPool(ak, bk, k, sk),
+		),
+	}
+}
+
+// SimulateMsgSetWithdrawAddress generates a MsgSetWithdrawAddress with random values.
+func SimulateMsgSetWithdrawAddress(ak types.AccountKeeper, bk types.BankKeeper, k keeper.Keeper) simtypes.Operation {
+	return func(
+		r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
+	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
+		if !k.GetWithdrawAddrEnabled(ctx) {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgSetWithdrawAddress, "withdrawal is not enabled"), nil, nil
+		}
+
+		simAccount, _ := simtypes.RandomAcc(r, accs)
+		simToAccount, _ := simtypes.RandomAcc(r, accs)
+
+		account := ak.GetAccount(ctx, simAccount.Address)
+		spendable := bk.SpendableCoins(ctx, account.GetAddress())
+
+		fees, err := simtypes.RandomFees(r, ctx, spendable)
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgSetWithdrawAddress, "unable to generate fees"), nil, err
+		}
+
+		msg := types.NewMsgSetWithdrawAddress(simAccount.Address, simToAccount.Address)
+
+		txGen := simappparams.MakeTestEncodingConfig().TxConfig
+		tx, err := helpers.GenTx(
+			txGen,
+			[]sdk.Msg{msg},
+			fees,
+			helpers.DefaultGenTxGas,
+			chainID,
+			[]uint64{account.GetAccountNumber()},
+			[]uint64{account.GetSequence()},
+			simAccount.PrivKey,
+		)
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "unable to generate mock tx"), nil, err
+		}
+
+		_, _, err = app.Deliver(txGen.TxEncoder(), tx)
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "unable to deliver tx"), nil, err
+		}
+
+		return simtypes.NewOperationMsg(msg, true, "", nil), nil, nil
+	}
+}
+
+// SimulateMsgWithdrawDelegatorReward generates a MsgWithdrawDelegatorReward with random values.
+func SimulateMsgWithdrawDelegatorReward(ak types.AccountKeeper, bk types.BankKeeper, k keeper.Keeper, sk stakingkeeper.Keeper) simtypes.Operation {
+	return func(
+		r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
+	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
+		simAccount, _ := simtypes.RandomAcc(r, accs)
+		delegations := sk.GetAllDelegatorDelegations(ctx, simAccount.Address)
+		if len(delegations) == 0 {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgWithdrawDelegatorReward, "number of delegators equal 0"), nil, nil
+		}
+
+		delegation := delegations[r.Intn(len(delegations))]
+
+		validator := sk.Validator(ctx, delegation.GetValidatorAddr())
+		if validator == nil {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgWithdrawDelegatorReward, "validator is nil"), nil, fmt.Errorf("validator %s not found", delegation.GetValidatorAddr())
+		}
+
+		account := ak.GetAccount(ctx, simAccount.Address)
+		spendable := bk.SpendableCoins(ctx, account.GetAddress())
+
+		fees, err := simtypes.RandomFees(r, ctx, spendable)
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgWithdrawDelegatorReward, "unable to generate fees"), nil, err
+		}
+
+		msg := types.NewMsgWithdrawDelegatorReward(simAccount.Address, validator.GetOperator())
+
+		txGen := simappparams.MakeTestEncodingConfig().TxConfig
+		tx, err := helpers.GenTx(
+			txGen,
+			[]sdk.Msg{msg},
+			fees,
+			helpers.DefaultGenTxGas,
+			chainID,
+			[]uint64{account.GetAccountNumber()},
+			[]uint64{account.GetSequence()},
+			simAccount.PrivKey,
+		)
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "unable to generate mock tx"), nil, err
+		}
+
+		_, _, err = app.Deliver(txGen.TxEncoder(), tx)
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "unable to deliver tx"), nil, err
+		}
+
+		return simtypes.NewOperationMsg(msg, true, "", nil), nil, nil
+	}
+}
+
+// SimulateMsgWithdrawValidatorCommission generates a MsgWithdrawValidatorCommission with random values.
+func SimulateMsgWithdrawValidatorCommission(ak types.AccountKeeper, bk types.BankKeeper, k keeper.Keeper, sk stakingkeeper.Keeper) simtypes.Operation {
+	return func(
+		r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
+	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
+
+		validator, ok := stakingkeeper.RandomValidator(r, sk, ctx)
+		if !ok {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgWithdrawValidatorCommission, "random validator is not ok"), nil, nil
+		}
+
+		commission := k.GetValidatorAccumulatedCommission(ctx, validator.GetOperator())
+		if commission.Commission.IsZero() {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgWithdrawValidatorCommission, "validator commission is zero"), nil, nil
+		}
+
+		simAccount, found := simtypes.FindAccount(accs, sdk.AccAddress(validator.GetOperator()))
+		if !found {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgWithdrawValidatorCommission, "could not find account"), nil, fmt.Errorf("validator %s not found", validator.GetOperator())
+		}
+
+		account := ak.GetAccount(ctx, simAccount.Address)
+		spendable := bk.SpendableCoins(ctx, account.GetAddress())
+
+		fees, err := simtypes.RandomFees(r, ctx, spendable)
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgWithdrawValidatorCommission, "unable to generate fees"), nil, err
+		}
+
+		msg := types.NewMsgWithdrawValidatorCommission(validator.GetOperator())
+
+		txGen := simappparams.MakeTestEncodingConfig().TxConfig
+		tx, err := helpers.GenTx(
+			txGen,
+			[]sdk.Msg{msg},
+			fees,
+			helpers.DefaultGenTxGas,
+			chainID,
+			[]uint64{account.GetAccountNumber()},
+			[]uint64{account.GetSequence()},
+			simAccount.PrivKey,
+		)
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "unable to generate mock tx"), nil, err
+		}
+
+		_, _, err = app.Deliver(txGen.TxEncoder(), tx)
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "unable to deliver tx"), nil, err
+		}
+
+		return simtypes.NewOperationMsg(msg, true, "", nil), nil, nil
+	}
+}
+
+// SimulateMsgFundCommunityPool simulates MsgFundCommunityPool execution where
+// a random account sends a random amount of its funds to the community pool.
+func SimulateMsgFundCommunityPool(ak types.AccountKeeper, bk types.BankKeeper, k keeper.Keeper, sk stakingkeeper.Keeper) simtypes.Operation {
+	return func(
+		r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
+	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
+
+		funder, _ := simtypes.RandomAcc(r, accs)
+
+		account := ak.GetAccount(ctx, funder.Address)
+		spendable := bk.SpendableCoins(ctx, account.GetAddress())
+
+		fundAmount := simtypes.RandSubsetCoins(r, spendable)
+		if fundAmount.Empty() {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgFundCommunityPool, "fund amount is empty"), nil, nil
+		}
+
+		var (
+			fees sdk.Coins
+			err  error
+		)
+
+		coins, hasNeg := spendable.SafeSub(fundAmount)
+		if !hasNeg {
+			fees, err = simtypes.RandomFees(r, ctx, coins)
+			if err != nil {
+				return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgFundCommunityPool, "unable to generate fees"), nil, err
+			}
+		}
+
+		msg := types.NewMsgFundCommunityPool(fundAmount, funder.Address)
+		txGen := simappparams.MakeTestEncodingConfig().TxConfig
+		tx, err := helpers.GenTx(
+			txGen,
+			[]sdk.Msg{msg},
+			fees,
+			helpers.DefaultGenTxGas,
+			chainID,
+			[]uint64{account.GetAccountNumber()},
+			[]uint64{account.GetSequence()},
+			funder.PrivKey,
+		)
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "unable to generate mock tx"), nil, err
+		}
+
+		_, _, err = app.Deliver(txGen.TxEncoder(), tx)
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "unable to deliver tx"), nil, err
+		}
+
+		return simtypes.NewOperationMsg(msg, true, "", nil), nil, nil
+	}
+}
---
+++ x/distribution/spec/04_messages.md
@@ -0,0 +1,189 @@
+<!--
+order: 4
+-->
+
+# Messages
+
+## MsgSetWithdrawAddress
+
+By default a withdrawal address is delegator address. If a delegator wants to change it's
+withdrawal address it must send `MsgSetWithdrawAddress`.
+
++++ https://github.com/cosmos/cosmos-sdk/blob/v0.40.0/proto/cosmos/distribution/v1beta1/tx.proto#L29-L37
+
+```go
+
+func (k Keeper) SetWithdrawAddr(ctx sdk.Context, delegatorAddr sdk.AccAddress, withdrawAddr sdk.AccAddress) error 
+	if k.blockedAddrs[withdrawAddr.String()] {
+		fail with "`{withdrawAddr}` is not allowed to receive external funds"
+	}
+
+	if !k.GetWithdrawAddrEnabled(ctx) {
+		fail with `ErrSetWithdrawAddrDisabled`
+	}
+
+	k.SetDelegatorWithdrawAddr(ctx, delegatorAddr, withdrawAddr)
+```
+
+## MsgWithdrawDelegatorReward
+
+under special circumstances a delegator may wish to withdraw rewards from only
+a single validator. 
+
++++ https://github.com/cosmos/cosmos-sdk/blob/v0.40.0/proto/cosmos/distribution/v1beta1/tx.proto#L42-L50
+
+```go
+func WithdrawDelegationReward(delegatorAddr, validatorAddr, withdrawAddr sdk.AccAddress) 
+    height = GetHeight()
+    
+    // get all distribution scenarios
+    pool = staking.GetPool() 
+    feePool = GetFeePool() 
+    delInfo = GetDelegationDistInfo(delegatorAddr,
+                    validatorAddr)
+    valInfo = GetValidatorDistInfo(validatorAddr)
+    validator = GetValidator(validatorAddr)
+
+    feePool, withdraw = delInfo.WithdrawRewards(feePool, valInfo, height, pool.BondedTokens, 
+               validator.Tokens, validator.DelegatorShares, validator.Commission)
+
+    SetFeePool(feePool) 
+    SendCoins(distributionModuleAcc, withdrawAddr, withdraw.TruncateDecimal())
+```
+
+
+### Withdraw Validator Rewards All
+
+When a validator wishes to withdraw their rewards it must send
+array of `MsgWithdrawDelegatorReward`. Note that parts of this transaction logic are also
+triggered each with any change in individual delegations, such as an unbond,
+redelegation, or delegation of additional tokens to a specific validator. This
+transaction withdraws the validators commission fee, as well as any rewards
+earning on their self-delegation.
+
+```go
+
+func WithdrawValidatorRewardsAll(operatorAddr, withdrawAddr sdk.AccAddress)
+
+    height = GetHeight()
+    feePool = GetFeePool() 
+    pool = GetPool() 
+    ValInfo = GetValidatorDistInfo(delegation.ValidatorAddr)
+    validator = GetValidator(delegation.ValidatorAddr)
+
+    // withdraw self-delegation
+    withdraw = GetDelegatorRewardsAll(validator.OperatorAddr, height)
+
+    // withdrawal validator commission rewards
+    feePool, commission = valInfo.WithdrawCommission(feePool, valInfo, height, pool.BondedTokens, 
+               validator.Tokens, validator.Commission)
+    withdraw += commission
+    SetFeePool(feePool) 
+
+    SendCoins(distributionModuleAcc, withdrawAddr, withdraw.TruncateDecimal())
+```
+
+## Common calculations 
+
+### Update total validator accum
+
+The total amount of validator accum must be calculated in order to determine
+the amount of pool tokens which a validator is entitled to at a particular
+block. The accum is always additive to the existing accum. This term is to be
+updated each time rewards are withdrawn from the system. 
+
+```go
+func (g FeePool) UpdateTotalValAccum(height int64, totalBondedTokens Dec) FeePool
+    blocks = height - g.TotalValAccumUpdateHeight
+    g.TotalValAccum += totalDelShares * blocks
+    g.TotalValAccumUpdateHeight = height
+    return g
+```
+
+### Update validator's accums
+
+The total amount of delegator accum must be updated in order to determine the
+amount of pool tokens which each delegator is entitled to, relative to the
+other delegators for that validator. The accum is always additive to
+the existing accum. This term is to be updated each time a
+withdrawal is made from a validator. 
+
+``` go
+func (vi ValidatorDistInfo) UpdateTotalDelAccum(height int64, totalDelShares Dec) ValidatorDistInfo
+    blocks = height - vi.TotalDelAccumUpdateHeight
+    vi.TotalDelAccum += totalDelShares * blocks
+    vi.TotalDelAccumUpdateHeight = height
+    return vi
+```
+
+### FeePool pool to validator pool
+
+Every time a validator or delegator executes a withdrawal or the validator is
+the proposer and receives new tokens, the relevant validator must move tokens
+from the passive global pool to their own pool. It is at this point that the
+commission is withdrawn
+
+```go
+func (vi ValidatorDistInfo) TakeFeePoolRewards(g FeePool, height int64, totalBonded, vdTokens, commissionRate Dec) (
+                                vi ValidatorDistInfo, g FeePool)
+
+    g.UpdateTotalValAccum(height, totalBondedShares)
+    
+    // update the validators pool
+    blocks = height - vi.FeePoolWithdrawalHeight
+    vi.FeePoolWithdrawalHeight = height
+    accum = blocks * vdTokens
+    withdrawalTokens := g.Pool * accum / g.TotalValAccum 
+    commission := withdrawalTokens * commissionRate
+    
+    g.TotalValAccum -= accumm
+    vi.PoolCommission += commission
+    vi.PoolCommissionFree += withdrawalTokens - commission
+    g.Pool -= withdrawalTokens
+
+    return vi, g
+```
+
+
+### Delegation reward withdrawal
+
+For delegations (including validator's self-delegation) all rewards from reward
+pool have already had the validator's commission taken away.
+
+```go
+func (di DelegationDistInfo) WithdrawRewards(g FeePool, vi ValidatorDistInfo,
+    height int64, totalBonded, vdTokens, totalDelShares, commissionRate Dec) (
+    di DelegationDistInfo, g FeePool, withdrawn DecCoins)
+
+    vi.UpdateTotalDelAccum(height, totalDelShares) 
+    g = vi.TakeFeePoolRewards(g, height, totalBonded, vdTokens, commissionRate) 
+    
+    blocks = height - di.WithdrawalHeight
+    di.WithdrawalHeight = height
+    accum = delegatorShares * blocks 
+     
+    withdrawalTokens := vi.Pool * accum / vi.TotalDelAccum
+    vi.TotalDelAccum -= accum
+
+    vi.Pool -= withdrawalTokens
+    vi.TotalDelAccum -= accum
+    return di, g, withdrawalTokens
+
+```
+
+### Validator commission withdrawal
+
+Commission is calculated each time rewards enter into the validator.
+
+```go
+func (vi ValidatorDistInfo) WithdrawCommission(g FeePool, height int64, 
+          totalBonded, vdTokens, commissionRate Dec) (
+          vi ValidatorDistInfo, g FeePool, withdrawn DecCoins)
+
+    g = vi.TakeFeePoolRewards(g, height, totalBonded, vdTokens, commissionRate) 
+    
+    withdrawalTokens := vi.PoolCommission 
+    vi.PoolCommission = 0
+
+    return vi, g, withdrawalTokens
+```
