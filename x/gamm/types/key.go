e8ebf18606f90eb113fc02c3018ea2fe19a87c77
Thunnini <yunjh1994@gmail.com>
2021-04-04 20:10:02 +0900

    Add `GetPoolIdFromShareDenom` function to the gamm module types

--- x/gamm/types/key.go
+++ x/gamm/types/key.go
@@ -2,6 +2,8 @@ package types
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -25,6 +27,20 @@ var (
 
 func GetPoolShareDenom(poolId uint64) string {
 	return fmt.Sprintf("gamm/pool/%d", poolId)
+}
+
+func GetPoolIdFromShareDenom(denom string) (uint64, error) {
+	r, err := regexp.Compile(`(gamm/pool/)([\d]+)$`)
+	if err != nil {
+		return 0, err
+	}
+
+	split := r.FindSubmatch([]byte(denom))
+	if len(split) != 3 {
+		return 0, fmt.Errorf("invalid pool share denom")
+	}
+
+	return strconv.ParseUint(string(split[2]), 10, 64)
 }
 
 func GetKeyPaginationPoolNumbers(poolId uint64) []byte {
--- x/gamm/types/key_test.go
+++ x/gamm/types/key_test.go
@@ -22,3 +22,35 @@ func TestGetPoolShareDenom(t *testing.T) {
 	require.NoError(t, sdk.ValidateDenom(denom))
 	require.Equal(t, "gamm/pool/18446744073709551615", denom)
 }
+
+func TestGetPoolIdFromShareDenom(t *testing.T) {
+	denom := "gamm/pool/1"
+
+	poolId, err := GetPoolIdFromShareDenom(denom)
+	require.NoError(t, err)
+	require.Equal(t, uint64(1), poolId)
+
+	_, err = GetPoolIdFromShareDenom("hello")
+	require.Error(t, err)
+
+	_, err = GetPoolIdFromShareDenom("gamm/pool")
+	require.Error(t, err)
+
+	_, err = GetPoolIdFromShareDenom("gamm/pool/")
+	require.Error(t, err)
+
+	_, err = GetPoolIdFromShareDenom("gamm/pool/hello")
+	require.Error(t, err)
+
+	_, err = GetPoolIdFromShareDenom("gamm/pool//")
+	require.Error(t, err)
+
+	_, err = GetPoolIdFromShareDenom("gamm/pool//1")
+	require.Error(t, err)
+
+	_, err = GetPoolIdFromShareDenom("gamm/pool/1/1")
+	require.Error(t, err)
+
+	_, err = GetPoolIdFromShareDenom("gamm/pool/1/hello")
+	require.Error(t, err)
+}
