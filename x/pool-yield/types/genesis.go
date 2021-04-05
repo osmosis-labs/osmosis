6f9638995481f704fdcc6c996260d3ef8d2d7373
Thunnini <yunjh1994@gmail.com>
2021-04-05 13:28:02 +0900

    Remove "mint_denom" field from the genesis state

--- x/pool-yield/types/genesis.go
+++ x/pool-yield/types/genesis.go
@@ -2,19 +2,14 @@ package types
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
-	"strings"
 	time "time"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 )
 
-func NewGenesisState(mintDenom string, lockableDurations []time.Duration) *GenesisState {
+func NewGenesisState(lockableDurations []time.Duration) *GenesisState {
 	return &GenesisState{
-		MintDenom:         mintDenom,
 		LockableDurations: lockableDurations,
 	}
 }
@@ -22,7 +17,6 @@ func NewGenesisState(mintDenom string, lockableDurations []time.Duration) *Genes
 // DefaultGenesisState gets the raw genesis raw message for testing
 func DefaultGenesisState() *GenesisState {
 	return &GenesisState{
-		MintDenom: "osmo",
 		LockableDurations: []time.Duration{
 			time.Hour,
 			time.Hour * 3,
@@ -46,27 +40,7 @@ func GetGenesisStateFromAppState(cdc codec.JSONMarshaler, appState map[string]js
 // ValidateGenesis validates the provided pool-yield genesis state to ensure the
 // expected invariants holds. (i.e. params in correct bounds)
 func ValidateGenesis(data *GenesisState) error {
-	err := validateMintDenom(data.MintDenom)
-	if err != nil {
-		return err
-	}
 	return validateLockableDurations(data.LockableDurations)
-}
-
-func validateMintDenom(i interface{}) error {
-	v, ok := i.(string)
-	if !ok {
-		return fmt.Errorf("invalid parameter type: %T", i)
-	}
-
-	if strings.TrimSpace(v) == "" {
-		return errors.New("mint denom cannot be blank")
-	}
-	if err := sdk.ValidateDenom(v); err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func validateLockableDurations(i interface{}) error {
