8639c4ebd0f71da5ce100c16969206d124e7ed22
Dev Ojha <ValarDragon@users.noreply.github.com>
2021-05-02 21:52:32 -0500

    Make PoolAccount marshalling marshal weights as decimals (#139)
    * In printing out a pool's deets, convert pool asset weight to decimal
    
    * Move marshalling code to its own file
    
    * Make PoolAsset Marshal differently directly, instead of indirectly
    
    * Change serialization of TotalWeight
    
    * Undo changes to how PoolAsset serializes to JSON, only change its YML serialization
    
    * Make only YML serialization only do truncation
    
    * Add LBP YML serialization test
    
    * Fix merge issue
    
    * Fix second merge conflict
    
    * Fix merge conflict #3
    
    * Make start_time serialization deterministic

--- x/gamm/types/account.go
+++ x/gamm/types/account.go
@@ -1,7 +1,6 @@
 package types
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"sort"
@@ -9,7 +8,6 @@ import (
 	"time"
 
 	"github.com/tendermint/tendermint/crypto"
-	"gopkg.in/yaml.v2"
 
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -462,86 +460,4 @@ func (pa PoolAccount) SetPubKey(pubKey cryptotypes.PubKey) error {
 // SetSequence - Implements AccountI
 func (pa PoolAccount) SetSequence(seq uint64) error {
 	return fmt.Errorf("not supported for pool accounts")
-}
-
-type poolAccountPretty struct {
-	Address            sdk.AccAddress `json:"address" yaml:"address"`
-	PubKey             string         `json:"public_key" yaml:"public_key"`
-	AccountNumber      uint64         `json:"account_number" yaml:"account_number"`
-	Sequence           uint64         `json:"sequence" yaml:"sequence"`
-	Id                 uint64         `json:"id" yaml:"id"`
-	PoolParams         PoolParams     `json:"pool_params" yaml:"pool_params"`
-	FuturePoolGovernor string         `json:"future_pool_governor" yaml:"future_pool_governor"`
-	TotalWeight        sdk.Int        `json:"total_weight" yaml:"total_weight"`
-	TotalShare         sdk.Coin       `json:"total_share" yaml:"total_share"`
-	PoolAssets         []PoolAsset    `json:"pool_assets" yaml:"pool_assets"`
-}
-
-func (pa PoolAccount) String() string {
-	out, _ := pa.MarshalYAML()
-	return out.(string)
-}
-
-// MarshalYAML returns the YAML representation of a PoolAccount.
-func (pa PoolAccount) MarshalYAML() (interface{}, error) {
-	accAddr, err := sdk.AccAddressFromBech32(pa.Address)
-	if err != nil {
-		return nil, err
-	}
-
-	bs, err := yaml.Marshal(poolAccountPretty{
-		Address:            accAddr,
-		PubKey:             "",
-		AccountNumber:      pa.AccountNumber,
-		Id:                 pa.Id,
-		PoolParams:         pa.PoolParams,
-		FuturePoolGovernor: pa.FuturePoolGovernor,
-		TotalWeight:        pa.TotalWeight,
-		TotalShare:         pa.TotalShare,
-		PoolAssets:         pa.PoolAssets,
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return string(bs), nil
-}
-
-// MarshalJSON returns the JSON representation of a PoolAccount.
-func (pa PoolAccount) MarshalJSON() ([]byte, error) {
-	accAddr, err := sdk.AccAddressFromBech32(pa.Address)
-	if err != nil {
-		return nil, err
-	}
-
-	return json.Marshal(poolAccountPretty{
-		Address:            accAddr,
-		PubKey:             "",
-		AccountNumber:      pa.AccountNumber,
-		Id:                 pa.Id,
-		PoolParams:         pa.PoolParams,
-		FuturePoolGovernor: pa.FuturePoolGovernor,
-		TotalWeight:        pa.TotalWeight,
-		TotalShare:         pa.TotalShare,
-		PoolAssets:         pa.PoolAssets,
-	})
-}
-
-// UnmarshalJSON unmarshals raw JSON bytes into a PoolAccount.
-func (pa *PoolAccount) UnmarshalJSON(bz []byte) error {
-	var alias poolAccountPretty
-	if err := json.Unmarshal(bz, &alias); err != nil {
-		return err
-	}
-
-	pa.BaseAccount = authtypes.NewBaseAccount(alias.Address, nil, alias.AccountNumber, alias.Sequence)
-	pa.Id = alias.Id
-	pa.PoolParams = alias.PoolParams
-	pa.FuturePoolGovernor = alias.FuturePoolGovernor
-	pa.TotalWeight = alias.TotalWeight
-	pa.TotalShare = alias.TotalShare
-	pa.PoolAssets = alias.PoolAssets
-
-	return nil
 }
