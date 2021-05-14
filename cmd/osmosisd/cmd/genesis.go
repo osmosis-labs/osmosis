af3ebeff35c98916383ad71ade3933c9dddf55a6
Sunny Aggarwal <sunnya97@protonmail.ch>
2021-05-14 18:49:28 -0400

    finished preparegenesis command

--- cmd/osmosisd/cmd/genesis.go
+++ cmd/osmosisd/cmd/genesis.go
@@ -4,18 +4,28 @@ import (
 	"encoding/json"
 	"fmt"
 
-	claimtypes "github.com/c-osmosis/osmosis/x/claim/types"
-	appparams "github.com/c-osmosos/osmosis/app/params"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/genutil"
-	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
 	"github.com/spf13/cobra"
+
+	claimtypes "github.com/c-osmosis/osmosis/x/claim/types"
+	epochstypes "github.com/c-osmosis/osmosis/x/epochs/types"
+	incentivestypes "github.com/c-osmosis/osmosis/x/incentives/types"
+	minttypes "github.com/c-osmosis/osmosis/x/mint/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
+	distributiontypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
-func PrepareGenesisCmd(defaultNodeHome string) *cobra.Command {
+func PrepareGenesisCmd(defaultNodeHome string, mbm module.BasicManager) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "prepare-genesis",
 		Short: "Prepare a genesis file with initial setup",
@@ -24,7 +34,7 @@ Examples include:
 	- Setting module initial params
 	- Setting denom metadata
 Example:
-	osmosisd validate-genesis
+	osmosisd prepare-genesis
 	- Check input genesis:
 		file is at ~/.gaiad/config/genesis.json
 `,
@@ -32,8 +42,10 @@ Example:
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx := client.GetClientContextFromCmd(cmd)
 			depCdc := clientCtx.JSONMarshaler
+			cdc := depCdc.(codec.Marshaler)
 
-			config.SetRoot(clientCtx.HomeDir)
+			serverCtx := server.GetServerContextFromCmd(cmd)
+			config := serverCtx.Config
 
 			genFile := config.GenesisFile()
 			appState, genDoc, err := genutiltypes.GenesisStateFromGenFile(genFile)
@@ -41,50 +53,123 @@ Example:
 				return fmt.Errorf("failed to unmarshal genesis state: %w", err)
 			}
 
+			genesisParams := TestnetNetworkParams()
+
+			// chain params genesis
+			genDoc.GenesisTime = genesisParams.GenesisTime
+			genDoc.ChainID = genesisParams.ChainID
+
 			// bank module genesis
 			bankGenState := banktypes.GetGenesisStateFromAppState(depCdc, appState)
-
-			osmoMetadata := banktypes.Metadata{
-				Description: fmt.Sprintf("The native token of Osmosis"),
-				DenomUnits: []*banktypes.DenomUnit{
-					{
-						Denom:    appparams.BaseCoinUnit,
-						Exponent: 0,
-						Aliases: []string{
-							"attopoolshare",
-						},
-					},
-					{
-						Denom:    appparams.HumanCoinUnit,
-						Exponent: appparams.Exponent,
-						Aliases:  nil,
-					},
-				},
-				Base:    appparams.BaseCoinUnit,
-				Display: appparams.HumanCoinUnit,
+			bankGenState.DenomMetadata = []banktypes.Metadata{
+				genesisParams.NativeCoinMetadata,
 			}
-
-			// bankGenState.DenomMetadata = banktypes.
-
-			bankGenState.Balances = banktypes.SanitizeGenesisBalances(liquidBalances)
 			bankGenStateBz, err := cdc.MarshalJSON(bankGenState)
 			if err != nil {
 				return fmt.Errorf("failed to marshal bank genesis state: %w", err)
 			}
 			appState[banktypes.ModuleName] = bankGenStateBz
+
+			// staking module genesis
+			stakingGenState := stakingtypes.GetGenesisStateFromAppState(depCdc, appState)
+			stakingGenState.Params = genesisParams.StakingParams
+			stakingGenStateBz, err := cdc.MarshalJSON(stakingGenState)
+			if err != nil {
+				return fmt.Errorf("failed to marshal staking genesis state: %w", err)
+			}
+			appState[stakingtypes.ModuleName] = stakingGenStateBz
+
+			// mint module genesis
+			mintGenState := minttypes.DefaultGenesisState()
+			mintGenState.Params = genesisParams.MintParams
+			mintGenStateBz, err := cdc.MarshalJSON(mintGenState)
+			if err != nil {
+				return fmt.Errorf("failed to marshal mint genesis state: %w", err)
+			}
+			appState[minttypes.ModuleName] = mintGenStateBz
+
+			// distribution module genesis
+			distributionGenState := distributiontypes.DefaultGenesisState()
+			distributionGenState.Params = genesisParams.DistributionParams
+			// TODO Set initial community pool
+			// distributionGenState.FeePool.CommunityPool = sdk.NewDecCoins()
+			distributionGenStateBz, err := cdc.MarshalJSON(distributionGenState)
+			if err != nil {
+				return fmt.Errorf("failed to marshal distribution genesis state: %w", err)
+			}
+			appState[distributiontypes.ModuleName] = distributionGenStateBz
+
+			// gov module genesis
+			govGenState := govtypes.DefaultGenesisState()
+			govGenState.DepositParams = genesisParams.GovParams.DepositParams
+			govGenState.TallyParams = genesisParams.GovParams.TallyParams
+			govGenState.VotingParams = genesisParams.GovParams.VotingParams
+			govGenStateBz, err := cdc.MarshalJSON(govGenState)
+			if err != nil {
+				return fmt.Errorf("failed to marshal gov genesis state: %w", err)
+			}
+			appState[govtypes.ModuleName] = govGenStateBz
+
+			// crisis module genesis
+			crisisGenState := crisistypes.DefaultGenesisState()
+			crisisGenState.ConstantFee = genesisParams.CrisisConstantFee
+			// TODO Set initial community pool
+			// distributionGenState.FeePool.CommunityPool = sdk.NewDecCoins()
+			crisisGenStateBz, err := cdc.MarshalJSON(crisisGenState)
+			if err != nil {
+				return fmt.Errorf("failed to marshal crisis genesis state: %w", err)
+			}
+			appState[crisistypes.ModuleName] = crisisGenStateBz
+
+			// slashing module genesis
+			slashingGenState := slashingtypes.DefaultGenesisState()
+			slashingGenState.Params = genesisParams.SlashingParams
+			slashingGenStateBz, err := cdc.MarshalJSON(slashingGenState)
+			if err != nil {
+				return fmt.Errorf("failed to marshal slashing genesis state: %w", err)
+			}
+			appState[slashingtypes.ModuleName] = slashingGenStateBz
+
+			// incentives module genesis
+			incentivesGenState := incentivestypes.DefaultGenesis()
+			incentivesGenState.Params = genesisParams.IncentivesParams
+			incentivesGenStateBz, err := cdc.MarshalJSON(incentivesGenState)
+			if err != nil {
+				return fmt.Errorf("failed to marshal incentives genesis state: %w", err)
+			}
+			appState[incentivestypes.ModuleName] = incentivesGenStateBz
+
+			// epochs module genesis
+			epochsGenState := epochstypes.DefaultGenesis()
+			epochsGenState.Epochs = genesisParams.Epochs
+			epochsGenStateBz, err := cdc.MarshalJSON(epochsGenState)
+			if err != nil {
+				return fmt.Errorf("failed to marshal epochs genesis state: %w", err)
+			}
+			appState[epochstypes.ModuleName] = epochsGenStateBz
+
 			// claim module genesis
-			claimGenState := claimtypes.DefaultGenesis()
-			claimGenState.ModuleAccountBalance = sdk.NewCoin(sdk.DefaultBondDenom, totalNormalizedOsmoBalance)
-			claimGenState.InitialClaimables = claimBalances
+			claimGenState := claimtypes.GetGenesisStateFromAppState(depCdc, appState)
+			claimGenState.AirdropStartTime = genesisParams.ClaimAirdropStartTime
+			claimGenState.DurationOfDecay = genesisParams.ClaimDurationOfDecay
+			claimGenState.DurationUntilDecay = genesisParams.ClaimDurationUntilDecay
 			claimGenStateBz, err := cdc.MarshalJSON(claimGenState)
 			if err != nil {
 				return fmt.Errorf("failed to marshal claim genesis state: %w", err)
 			}
 			appState[claimtypes.ModuleName] = claimGenStateBz
+
+			// validate genesis state
+			if err = mbm.ValidateGenesis(cdc, clientCtx.TxConfig, appState); err != nil {
+				return fmt.Errorf("error validating genesis file: %s", err.Error())
+			}
+
+			// save genesis
 			appStateJSON, err := json.Marshal(appState)
 			if err != nil {
 				return fmt.Errorf("failed to marshal application genesis state: %w", err)
 			}
+
 			genDoc.AppState = appStateJSON
 			err = genutil.ExportGenesisFile(genDoc, genFile)
 			return err
---
+++ cmd/osmosisd/cmd/genesisparams.go
@@ -0,0 +1,119 @@
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	distributiontypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+
+	appparams "github.com/c-osmosis/osmosis/app/params"
+	epochstypes "github.com/c-osmosis/osmosis/x/epochs/types"
+	incentivestypes "github.com/c-osmosis/osmosis/x/incentives/types"
+	minttypes "github.com/c-osmosis/osmosis/x/mint/types"
+)
+
+type NetworkParams struct {
+	TotalSupply        sdk.Int
+	ChainID            string
+	GenesisTime        time.Time
+	NativeCoinMetadata banktypes.Metadata
+
+	StakingParams      stakingtypes.Params
+	MintParams         minttypes.Params
+	DistributionParams distributiontypes.Params
+	GovParams          govtypes.Params
+
+	CrisisConstantFee sdk.Coin
+
+	SlashingParams   slashingtypes.Params
+	IncentivesParams incentivestypes.Params
+
+	Epochs []epochstypes.EpochInfo
+
+	ClaimAirdropStartTime   time.Time
+	ClaimDurationUntilDecay time.Duration
+	ClaimDurationOfDecay    time.Duration
+}
+
+func TestnetNetworkParams() NetworkParams {
+	testnetNetworkParams := NetworkParams{}
+
+	testnetNetworkParams.TotalSupply = sdk.NewIntWithDecimal(1, 9) // 1 Billion
+	testnetNetworkParams.ChainID = "osmo-testnet-thanatos"
+	testnetNetworkParams.GenesisTime = time.Now()
+	testnetNetworkParams.NativeCoinMetadata = banktypes.Metadata{
+		Description: fmt.Sprintf("The native token of Osmosis"),
+		DenomUnits: []*banktypes.DenomUnit{
+			{
+				Denom:    appparams.BaseCoinUnit,
+				Exponent: 0,
+				Aliases: []string{
+					fmt.Sprintf("u%s", appparams.HumanCoinUnit),
+				},
+			},
+			{
+				Denom:    appparams.HumanCoinUnit,
+				Exponent: appparams.OsmoExponent,
+				Aliases:  nil,
+			},
+		},
+		Base:    appparams.BaseCoinUnit,
+		Display: appparams.HumanCoinUnit,
+	}
+
+	testnetNetworkParams.StakingParams = stakingtypes.DefaultParams()
+	testnetNetworkParams.StakingParams.UnbondingTime = time.Hour * 24 * 7 * 2 // 2 weeks
+	testnetNetworkParams.StakingParams.MaxValidators = 100
+	testnetNetworkParams.StakingParams.BondDenom = testnetNetworkParams.NativeCoinMetadata.Base
+
+	testnetNetworkParams.MintParams = minttypes.DefaultParams()
+	testnetNetworkParams.MintParams.EpochDuration = time.Hour * 24 * 7                                                      // 1 week
+	testnetNetworkParams.MintParams.GenesisEpochProvisions = sdk.NewDecFromInt(testnetNetworkParams.TotalSupply.QuoRaw(10)) // 10% of final supply
+	testnetNetworkParams.MintParams.MintDenom = testnetNetworkParams.NativeCoinMetadata.Base
+	testnetNetworkParams.MintParams.ReductionFactor = sdk.NewDecWithPrec(5, 1) // 0.5
+	testnetNetworkParams.MintParams.ReductionPeriodInEpochs = 52 * 3           // 3 years
+
+	testnetNetworkParams.DistributionParams = distributiontypes.DefaultParams()
+	testnetNetworkParams.DistributionParams.BaseProposerReward = sdk.MustNewDecFromStr("0.01")
+	testnetNetworkParams.DistributionParams.BonusProposerReward = sdk.MustNewDecFromStr("0")
+	testnetNetworkParams.DistributionParams.CommunityTax = sdk.MustNewDecFromStr("0")
+	testnetNetworkParams.DistributionParams.WithdrawAddrEnabled = true
+
+	testnetNetworkParams.GovParams = govtypes.DefaultParams()
+	testnetNetworkParams.GovParams.DepositParams.MaxDepositPeriod = time.Hour * 24 * 7 // 1 week
+	testnetNetworkParams.GovParams.DepositParams.MinDeposit = sdk.NewCoins(sdk.NewCoin(
+		testnetNetworkParams.NativeCoinMetadata.Base,
+		testnetNetworkParams.TotalSupply.QuoRaw(1_000_000), // 1 millionth of supply
+	))
+	testnetNetworkParams.GovParams.TallyParams.Quorum = sdk.MustNewDecFromStr("0.25") // 25%
+	testnetNetworkParams.GovParams.VotingParams.VotingPeriod = time.Hour * 24 * 7     // 1 week
+
+	testnetNetworkParams.CrisisConstantFee = sdk.NewCoin(
+		testnetNetworkParams.NativeCoinMetadata.Base,
+		testnetNetworkParams.TotalSupply.QuoRaw(100_000), // 1/100,000 of total supply
+	)
+
+	testnetNetworkParams.SlashingParams = slashingtypes.DefaultParams()
+	testnetNetworkParams.SlashingParams.SignedBlocksWindow = int64(10000)
+
+	testnetNetworkParams.Epochs = epochstypes.DefaultGenesis().Epochs
+
+	for _, epoch := range testnetNetworkParams.Epochs {
+		epoch.StartTime = testnetNetworkParams.GenesisTime
+	}
+
+	testnetNetworkParams.IncentivesParams = incentivestypes.DefaultParams()
+	testnetNetworkParams.IncentivesParams.DistrEpochIdentifier = "weekly"
+
+	testnetNetworkParams.ClaimAirdropStartTime = testnetNetworkParams.GenesisTime
+	testnetNetworkParams.ClaimDurationUntilDecay = time.Hour * 24 * 7 * 12 // 12 weeks
+	testnetNetworkParams.ClaimDurationOfDecay = time.Hour * 24 * 7 * 12    // 12 weeks
+
+	return testnetNetworkParams
+}
