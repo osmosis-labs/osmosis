df06d3647abf4dfbb2ba50138cae71e8d4ea3903
Sunny Aggarwal <sunnya97@protonmail.ch>
2021-05-18 17:19:23 -0400

    fix CLI/REST tests

--- x/mint/client/cli/cli_test.go
+++ x/mint/client/cli/cli_test.go
@@ -51,12 +51,13 @@ func (s *IntegrationTestSuite) TestGetCmdQueryParams() {
 		{
 			"json output",
 			[]string{fmt.Sprintf("--%s=1", flags.FlagHeight), fmt.Sprintf("--%s=json", tmcli.OutputFlag)},
-			`{"mint_denom":"stake","genesis_epoch_provisions":"5000000.000000000000000000","epoch_identifier":"weekly","reduction_period_in_epochs":"156","reduction_factor":"0.500000000000000000","distribution_proportions":{"staking":"0.500000000000000000","pool_incentives":"0.300000000000000000","developer_rewards":"0.200000000000000000"}}`,
+			`{"mint_denom":"stake","genesis_epoch_provisions":"5000000.000000000000000000","epoch_identifier":"weekly","reduction_period_in_epochs":"156","reduction_factor":"0.500000000000000000","distribution_proportions":{"staking":"0.500000000000000000","pool_incentives":"0.300000000000000000","developer_rewards":"0.200000000000000000"},"developer_rewards_receiver":""}`,
 		},
 		{
 			"text output",
 			[]string{fmt.Sprintf("--%s=1", flags.FlagHeight), fmt.Sprintf("--%s=text", tmcli.OutputFlag)},
-			`distribution_proportions:
+			`developer_rewards_receiver: ""
+distribution_proportions:
   developer_rewards: "0.200000000000000000"
   pool_incentives: "0.300000000000000000"
   staking: "0.500000000000000000"
