8461383d7a886128ce895efd751430045638acf8
ValarDragon <dojha12@gmail.com>
2021-05-28 18:22:17 -0700

    Rename & document accumulation methods

--- store/tree.go
+++ store/tree.go
@@ -17,6 +17,8 @@ import (
 // Branches have m sized key index slice. Each key index represents
 // the starting index of the child node's index(inclusive), and the
 // ending index of the previous node of the child node's index(exclusive).
+// TODO: We should abstract out the leaves of this tree to allow more data aside from
+// the accumulation value to go there.
 type Tree struct {
 	store store.KVStore
 	m     uint8
@@ -26,6 +28,23 @@ func NewTree(store store.KVStore, m uint8) Tree {
 	tree := Tree{store, m}
 	tree.Set(nil, sdk.ZeroInt())
 	return tree
+}
+
+func (t Tree) Set(key []byte, acc sdk.Int) {
+	node := t.nodeGet(0, key)
+	node.setLeaf(acc)
+
+	node.parent().push(child{key, acc})
+}
+
+func (t Tree) Remove(key []byte) {
+	node := t.nodeGet(0, key)
+	if !node.exists() {
+		return
+	}
+	parent := node.parent()
+	node.delete()
+	parent.pull(key)
 }
 
 // node is pointer to a specific node inside the tree
@@ -321,6 +340,7 @@ func (t Tree) root() *node {
 	}
 }
 
+// Get returns the (sdk.Int) accumulation value at a given leaf.
 func (t Tree) Get(key []byte) (res sdk.Int) {
 	keybz := t.leafKey(key)
 	if !t.store.Has(keybz) {
@@ -387,38 +407,31 @@ func (t Tree) ReverseIterator(begin, end []byte) store.Iterator {
 	return t.nodeReverseIterator(0, begin, end)
 }
 
-func (t Tree) Set(key []byte, acc sdk.Int) {
-	node := t.nodeGet(0, key)
-	node.setLeaf(acc)
-
-	node.parent().push(child{key, acc})
-}
-
-func (t Tree) Remove(key []byte) {
-	node := t.nodeGet(0, key)
-	if !node.exists() {
-		return
-	}
-	parent := node.parent()
-	node.delete()
-	parent.pull(key)
-}
-
-func (node *node) accSplit(key []byte) (left sdk.Int, exact sdk.Int, right sdk.Int) {
+// accumulationSplit returns the accumulated value for all of the following:
+// left: all leaves under nodePointer with key < provided key
+// exact: leaf with key = provided key
+// right: all leaves under nodePointer with key > provided key
+// Note that the equalities here are _exclusive_.
+func (node *node) accumulationSplit(key []byte) (left sdk.Int, exact sdk.Int, right sdk.Int) {
 	left, exact, right = sdk.ZeroInt(), sdk.ZeroInt(), sdk.ZeroInt()
+	// If the current node is a leaf node, then ...
 	if node.level == 0 {
-		var err error
+		accumulatedValue := sdk.ZeroInt()
 		bz := node.tree.store.Get(node.tree.leafKey(node.key))
-		switch bytes.Compare(node.key, key) {
-		case -1:
-			err = json.Unmarshal(bz, &left)
-		case 0:
-			err = json.Unmarshal(bz, &exact)
-		case 1:
-			err = json.Unmarshal(bz, &right)
-		}
+		err := json.Unmarshal(bz, &accumulatedValue)
 		if err != nil {
 			panic(err)
+		}
+		// Check if the leaf key is to the left of the input key,
+		// if so this value is on the left. Similar for the other cases.
+		// Recall that all of the output arguments default to 0, if unset internally.
+		switch bytes.Compare(node.key, key) {
+		case -1:
+			left = accumulatedValue
+		case 0:
+			exact = accumulatedValue
+		case 1:
+			right = accumulatedValue
 		}
 		return
 	}
@@ -428,28 +441,42 @@ func (node *node) accSplit(key []byte) (left sdk.Int, exact sdk.Int, right sdk.I
 	if !match {
 		idx--
 	}
-	left, exact, right = node.tree.nodeGet(node.level-1, children[idx].Index).accSplit(key)
+	left, exact, right = node.tree.nodeGet(node.level-1, children[idx].Index).accumulationSplit(key)
 	left = left.Add(children[:idx].accumulate())
 	right = right.Add(children[idx+1:].accumulate())
 	return
 }
 
-func (t Tree) SplitAcc(key []byte) (sdk.Int, sdk.Int, sdk.Int) {
-	return t.root().accSplit(key)
+// TotalAccumulatedValue returns the sum of the weights for all leaves
+func (t Tree) TotalAccumulatedValue() sdk.Int {
+	return t.SubsetAccumulation(nil, nil)
 }
 
-func (t Tree) SliceAcc(start []byte, end []byte) sdk.Int {
+// Prefix sum returns the total weight of all leaves with keys <= to the provided key.
+func (t Tree) PrefixSum(key []byte) sdk.Int {
+	return t.SubsetAccumulation(nil, key)
+}
+
+// SubsetAccumulation returns the total value of all leaves with keys
+// between start and end (inclusive of both ends)
+// if start is nil, it is the beginning of the tree.
+// if end is nil, it is the end of the tree.
+func (t Tree) SubsetAccumulation(start []byte, end []byte) sdk.Int {
 	if start == nil {
-		left, exact, _ := t.root().accSplit(end)
+		left, exact, _ := t.root().accumulationSplit(end)
 		return left.Add(exact)
 	}
 	if end == nil {
-		_, exact, right := t.root().accSplit(start)
+		_, exact, right := t.root().accumulationSplit(start)
 		return exact.Add(right)
 	}
-	_, leftexact, leftrest := t.root().accSplit(start)
-	_, _, rightest := t.root().accSplit(end)
+	_, leftexact, leftrest := t.root().accumulationSplit(start)
+	_, _, rightest := t.root().accumulationSplit(end)
 	return leftexact.Add(leftrest).Sub(rightest)
+}
+
+func (t Tree) SplitAcc(key []byte) (sdk.Int, sdk.Int, sdk.Int) {
+	return t.root().accumulationSplit(key)
 }
 
 func (node *node) visualize(depth int, acc sdk.Int) {
