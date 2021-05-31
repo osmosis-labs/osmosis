21f1d9d47e6edc95a25bfb31896da6c6c0b86cf2
ValarDragon <dojha12@gmail.com>
2021-05-31 13:27:06 -0500

    Add some docs specifying the pool parameters

---
+++ x/incentives/keeper/bench_test.go
@@ -0,0 +1,11 @@
+package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (suite *KeeperTestSuite) setupBenchmarkState(ctx *sdk.Context) {
+
+	// suite.Require().NoError(err)
+	// return potID, pot
+}
