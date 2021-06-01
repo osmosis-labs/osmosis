9024981919a62c4a4c423d9b6d9f23b8bdeef46d
Thunnini <yunjh1994@gmail.com>
2021-06-01 23:18:53 +0900

    Add some tests

--- x/claim/keeper/claim_test.go
+++ x/claim/keeper/claim_test.go
@@ -1,6 +1,8 @@
 package keeper_test
 
 import (
+	"time"
+
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
@@ -8,6 +10,106 @@ import (
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/osmosis-labs/osmosis/x/claim/types"
 )
+
+func (suite *KeeperTestSuite) TestHookOfUnclaimableAccount() {
+	suite.SetupTest()
+
+	pub1 := secp256k1.GenPrivKey().PubKey()
+	addr1 := sdk.AccAddress(pub1.Address())
+	suite.app.AccountKeeper.SetAccount(suite.ctx, authtypes.NewBaseAccount(addr1, nil, 0, 0))
+
+	claim, err := suite.app.ClaimKeeper.GetClaimRecord(suite.ctx, addr1)
+	suite.NoError(err)
+	suite.Equal(types.ClaimRecord{}, claim)
+
+	suite.app.ClaimKeeper.AfterSwap(suite.ctx, addr1)
+
+	balances := suite.app.BankKeeper.GetAllBalances(suite.ctx, addr1)
+	suite.Equal(sdk.Coins{}, balances)
+}
+
+func (suite *KeeperTestSuite) TestHookBeforeAirdropStart() {
+	suite.SetupTest()
+
+	airdropStartTime := time.Now().Add(time.Hour)
+
+	err := suite.app.ClaimKeeper.SetParams(suite.ctx, types.Params{
+		AirdropStartTime:   airdropStartTime,
+		DurationUntilDecay: time.Hour,
+		DurationOfDecay:    time.Hour * 4,
+	})
+
+	pub1 := secp256k1.GenPrivKey().PubKey()
+	addr1 := sdk.AccAddress(pub1.Address())
+
+	claimRecords := []types.ClaimRecord{
+		{
+			Address:                addr1.String(),
+			InitialClaimableAmount: sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 1000)),
+			ActionCompleted:        []bool{false, false, false, false},
+		},
+	}
+	suite.app.AccountKeeper.SetAccount(suite.ctx, authtypes.NewBaseAccount(addr1, nil, 0, 0))
+
+	err = suite.app.ClaimKeeper.SetClaimRecords(suite.ctx, claimRecords)
+	suite.Require().NoError(err)
+
+	coins, err := suite.app.ClaimKeeper.GetUserTotalClaimable(suite.ctx, addr1)
+	suite.NoError(err)
+	suite.Equal(claimRecords[0].InitialClaimableAmount, coins)
+
+	coins, err = suite.app.ClaimKeeper.GetClaimableAmountForAction(suite.ctx, addr1, types.ActionSwap)
+	suite.NoError(err)
+	// Now, it is before starting air drop, so this value should return the empty coins
+	suite.True(coins.Empty())
+
+	suite.app.ClaimKeeper.AfterSwap(suite.ctx, addr1)
+	balances := suite.app.BankKeeper.GetAllBalances(suite.ctx, addr1)
+	// Now, it is before starting air drop, so claim module should not send the balances to the user after swap.
+	suite.True(balances.Empty())
+
+	suite.app.ClaimKeeper.AfterSwap(suite.ctx.WithBlockTime(airdropStartTime), addr1)
+	balances = suite.app.BankKeeper.GetAllBalances(suite.ctx, addr1)
+	// Now, it is the time for air drop, so claim module should send the balances to the user after swap.
+	suite.Equal(claimRecords[0].InitialClaimableAmount.AmountOf(sdk.DefaultBondDenom).Quo(sdk.NewInt(4)), balances.AmountOf(sdk.DefaultBondDenom))
+}
+
+func (suite *KeeperTestSuite) TestDuplicatedActionNotWithdrawRepeatedly() {
+	suite.SetupTest()
+
+	pub1 := secp256k1.GenPrivKey().PubKey()
+	addr1 := sdk.AccAddress(pub1.Address())
+
+	claimRecords := []types.ClaimRecord{
+		{
+			Address:                addr1.String(),
+			InitialClaimableAmount: sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 1000)),
+			ActionCompleted:        []bool{false, false, false, false},
+		},
+	}
+	suite.app.AccountKeeper.SetAccount(suite.ctx, authtypes.NewBaseAccount(addr1, nil, 0, 0))
+
+	err := suite.app.ClaimKeeper.SetClaimRecords(suite.ctx, claimRecords)
+	suite.Require().NoError(err)
+
+	coins1, err := suite.app.ClaimKeeper.GetUserTotalClaimable(suite.ctx, addr1)
+	suite.Require().NoError(err)
+	suite.Require().Equal(coins1, claimRecords[0].InitialClaimableAmount)
+
+	suite.app.ClaimKeeper.AfterSwap(suite.ctx, addr1)
+	claim, err := suite.app.ClaimKeeper.GetClaimRecord(suite.ctx, addr1)
+	suite.NoError(err)
+	suite.True(claim.ActionCompleted[types.ActionSwap])
+	claimedCoins := suite.app.BankKeeper.GetAllBalances(suite.ctx, addr1)
+	suite.Require().Equal(claimedCoins.AmountOf(sdk.DefaultBondDenom), claimRecords[0].InitialClaimableAmount.AmountOf(sdk.DefaultBondDenom).Quo(sdk.NewInt(4)))
+
+	suite.app.ClaimKeeper.AfterSwap(suite.ctx, addr1)
+	claim, err = suite.app.ClaimKeeper.GetClaimRecord(suite.ctx, addr1)
+	suite.NoError(err)
+	suite.True(claim.ActionCompleted[types.ActionSwap])
+	claimedCoins = suite.app.BankKeeper.GetAllBalances(suite.ctx, addr1)
+	suite.Require().Equal(claimedCoins.AmountOf(sdk.DefaultBondDenom), claimRecords[0].InitialClaimableAmount.AmountOf(sdk.DefaultBondDenom).Quo(sdk.NewInt(4)))
+}
 
 func (suite *KeeperTestSuite) TestDelegationAutoWithdrawAndDelegateMore() {
 	suite.SetupTest()
@@ -50,7 +152,8 @@ func (suite *KeeperTestSuite) TestDelegationAutoWithdrawAndDelegateMore() {
 
 	validator, _ = validator.AddTokensFromDel(sdk.TokensFromConsensusPower(1))
 	delAmount := sdk.TokensFromConsensusPower(1)
-	suite.app.BankKeeper.SetBalance(suite.ctx, addr2, sdk.NewCoin(sdk.DefaultBondDenom, delAmount))
+	err = suite.app.BankKeeper.SetBalance(suite.ctx, addr2, sdk.NewCoin(sdk.DefaultBondDenom, delAmount))
+	suite.NoError(err)
 	_, err = suite.app.StakingKeeper.Delegate(suite.ctx, addr2, delAmount, stakingtypes.Unbonded, validator, true)
 	suite.NoError(err)
 
@@ -151,4 +254,91 @@ func (suite *KeeperTestSuite) TestAirdropFlow() {
 	coins2, err = suite.app.ClaimKeeper.GetUserTotalClaimable(suite.ctx, addr2)
 	suite.Require().NoError(err)
 	suite.Require().Equal(coins2, sdk.Coins{})
+}
+
+func (suite *KeeperTestSuite) TestClaimOfDecayed() {
+	airdropStartTime := time.Now()
+	durationUntilDecay := time.Hour
+	durationOfDecay := time.Hour * 4
+
+	pub1 := secp256k1.GenPrivKey().PubKey()
+	addr1 := sdk.AccAddress(pub1.Address())
+
+	claimRecords := []types.ClaimRecord{
+		{
+			Address:                addr1.String(),
+			InitialClaimableAmount: sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 1000)),
+			ActionCompleted:        []bool{false, false, false, false},
+		},
+	}
+
+	tests := []struct {
+		fn func()
+	}{
+		{
+			fn: func() {
+				ctx := suite.ctx.WithBlockTime(airdropStartTime)
+				coins, err := suite.app.ClaimKeeper.GetClaimableAmountForAction(ctx, addr1, types.ActionSwap)
+				suite.NoError(err)
+				suite.Equal(claimRecords[0].InitialClaimableAmount.AmountOf(sdk.DefaultBondDenom).Quo(sdk.NewInt(4)).String(), coins.AmountOf(sdk.DefaultBondDenom).String())
+
+				suite.app.ClaimKeeper.AfterSwap(ctx, addr1)
+				coins = suite.app.BankKeeper.GetAllBalances(ctx, addr1)
+				suite.Equal(claimRecords[0].InitialClaimableAmount.AmountOf(sdk.DefaultBondDenom).Quo(sdk.NewInt(4)).String(), coins.AmountOf(sdk.DefaultBondDenom).String())
+			},
+		},
+		{
+			fn: func() {
+				ctx := suite.ctx.WithBlockTime(airdropStartTime.Add(durationUntilDecay))
+				coins, err := suite.app.ClaimKeeper.GetClaimableAmountForAction(ctx, addr1, types.ActionSwap)
+				suite.NoError(err)
+				suite.Equal(claimRecords[0].InitialClaimableAmount.AmountOf(sdk.DefaultBondDenom).Quo(sdk.NewInt(4)).String(), coins.AmountOf(sdk.DefaultBondDenom).String())
+
+				suite.app.ClaimKeeper.AfterSwap(ctx, addr1)
+				coins = suite.app.BankKeeper.GetAllBalances(ctx, addr1)
+				suite.Equal(claimRecords[0].InitialClaimableAmount.AmountOf(sdk.DefaultBondDenom).Quo(sdk.NewInt(4)).String(), coins.AmountOf(sdk.DefaultBondDenom).String())
+			},
+		},
+		{
+			fn: func() {
+				ctx := suite.ctx.WithBlockTime(airdropStartTime.Add(durationUntilDecay).Add(durationOfDecay / 2))
+				coins, err := suite.app.ClaimKeeper.GetClaimableAmountForAction(ctx, addr1, types.ActionSwap)
+				suite.NoError(err)
+				suite.Equal(claimRecords[0].InitialClaimableAmount.AmountOf(sdk.DefaultBondDenom).Quo(sdk.NewInt(8)).String(), coins.AmountOf(sdk.DefaultBondDenom).String())
+
+				suite.app.ClaimKeeper.AfterSwap(ctx, addr1)
+				coins = suite.app.BankKeeper.GetAllBalances(ctx, addr1)
+				suite.Equal(claimRecords[0].InitialClaimableAmount.AmountOf(sdk.DefaultBondDenom).Quo(sdk.NewInt(8)).String(), coins.AmountOf(sdk.DefaultBondDenom).String())
+			},
+		},
+		{
+			fn: func() {
+				ctx := suite.ctx.WithBlockTime(airdropStartTime.Add(durationUntilDecay).Add(durationOfDecay))
+				coins, err := suite.app.ClaimKeeper.GetClaimableAmountForAction(ctx, addr1, types.ActionSwap)
+				suite.NoError(err)
+				suite.True(coins.Empty())
+
+				suite.app.ClaimKeeper.AfterSwap(ctx, addr1)
+				coins = suite.app.BankKeeper.GetAllBalances(ctx, addr1)
+				suite.True(coins.Empty())
+			},
+		},
+	}
+
+	for _, test := range tests {
+		suite.SetupTest()
+
+		err := suite.app.ClaimKeeper.SetParams(suite.ctx, types.Params{
+			AirdropStartTime:   airdropStartTime,
+			DurationUntilDecay: durationUntilDecay,
+			DurationOfDecay:    durationOfDecay,
+		})
+		suite.NoError(err)
+
+		suite.app.AccountKeeper.SetAccount(suite.ctx, authtypes.NewBaseAccount(addr1, nil, 0, 0))
+		err = suite.app.ClaimKeeper.SetClaimRecords(suite.ctx, claimRecords)
+		suite.Require().NoError(err)
+
+		test.fn()
+	}
 }
