4963fb4ffb296b721a527e95370d5ab4bf64cb63
antstalepresh <36045227+antstalepresh@users.noreply.github.com>
2021-06-02 06:41:57 +1000

    Add test for no lock pot distribution (#229)
    * add test for no lock pot distribution
    
    * TestNoLockNonPerpetualPotDistribution

--- x/incentives/keeper/pot_test.go
+++ x/incentives/keeper/pot_test.go
@@ -331,3 +331,93 @@ func (suite *KeeperTestSuite) TestPerpetualPotOperations() {
 	rewardsEst = suite.app.IncentivesKeeper.GetRewardsEst(suite.ctx, lockOwner, []lockuptypes.PeriodLock{}, 100)
 	suite.Require().Equal(sdk.Coins(nil), rewardsEst)
 }
+
+func (suite *KeeperTestSuite) TestNoLockPerpetualPotDistribution() {
+	// test for module get pots
+	suite.SetupTest()
+
+	// setup no lock perpetual pot
+	coins := sdk.Coins{sdk.NewInt64Coin("stake", 10)}
+	potID, _, _, startTime := suite.SetupNewPot(true, coins)
+
+	// check pots
+	pots := suite.app.IncentivesKeeper.GetNotFinishedPots(suite.ctx)
+	suite.Require().Len(pots, 1)
+	expectedPot := types.Pot{
+		Id:          potID,
+		IsPerpetual: true,
+		DistributeTo: lockuptypes.QueryCondition{
+			LockQueryType: lockuptypes.ByDuration,
+			Denom:         "lptoken",
+			Duration:      time.Second,
+		},
+		Coins:             coins,
+		NumEpochsPaidOver: 1,
+		FilledEpochs:      0,
+		DistributedCoins:  sdk.Coins{},
+		StartTime:         startTime,
+	}
+	suite.Require().Equal(pots[0].String(), expectedPot.String())
+
+	// start distribution
+	suite.ctx = suite.ctx.WithBlockTime(startTime)
+	pot, err := suite.app.IncentivesKeeper.GetPotByID(suite.ctx, potID)
+	suite.Require().NoError(err)
+	err = suite.app.IncentivesKeeper.BeginDistribution(suite.ctx, *pot)
+	suite.Require().NoError(err)
+
+	// distribute coins to stakers, since it's perpetual distribute everything on single distribution
+	distrCoins, err := suite.app.IncentivesKeeper.Distribute(suite.ctx, *pot)
+	suite.Require().NoError(err)
+	suite.Require().Equal(distrCoins, sdk.Coins(nil))
+
+	// check state is same after distribution
+	pots = suite.app.IncentivesKeeper.GetNotFinishedPots(suite.ctx)
+	suite.Require().Len(pots, 1)
+	suite.Require().Equal(pots[0].String(), expectedPot.String())
+}
+
+func (suite *KeeperTestSuite) TestNoLockNonPerpetualPotDistribution() {
+	// test for module get pots
+	suite.SetupTest()
+
+	// setup no lock non-perpetual pot
+	coins := sdk.Coins{sdk.NewInt64Coin("stake", 10)}
+	potID, _, _, startTime := suite.SetupNewPot(false, coins)
+
+	// check pots
+	pots := suite.app.IncentivesKeeper.GetNotFinishedPots(suite.ctx)
+	suite.Require().Len(pots, 1)
+	expectedPot := types.Pot{
+		Id:          potID,
+		IsPerpetual: false,
+		DistributeTo: lockuptypes.QueryCondition{
+			LockQueryType: lockuptypes.ByDuration,
+			Denom:         "lptoken",
+			Duration:      time.Second,
+		},
+		Coins:             coins,
+		NumEpochsPaidOver: 2,
+		FilledEpochs:      0,
+		DistributedCoins:  sdk.Coins{},
+		StartTime:         startTime,
+	}
+	suite.Require().Equal(pots[0].String(), expectedPot.String())
+
+	// start distribution
+	suite.ctx = suite.ctx.WithBlockTime(startTime)
+	pot, err := suite.app.IncentivesKeeper.GetPotByID(suite.ctx, potID)
+	suite.Require().NoError(err)
+	err = suite.app.IncentivesKeeper.BeginDistribution(suite.ctx, *pot)
+	suite.Require().NoError(err)
+
+	// distribute coins to stakers, since it's perpetual distribute everything on single distribution
+	distrCoins, err := suite.app.IncentivesKeeper.Distribute(suite.ctx, *pot)
+	suite.Require().NoError(err)
+	suite.Require().Equal(distrCoins, sdk.Coins(nil))
+
+	// check state is same after distribution
+	pots = suite.app.IncentivesKeeper.GetNotFinishedPots(suite.ctx)
+	suite.Require().Len(pots, 1)
+	suite.Require().Equal(pots[0].String(), expectedPot.String())
+}
