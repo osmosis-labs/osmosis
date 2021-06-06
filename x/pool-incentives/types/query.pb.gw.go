63a6638af1a2d3fdcb40c0b136374cc81c87aeb8
Sunny Aggarwal <sunny@sikka.tech>
2021-06-06 02:45:11 -0400

    removed nullability and reran make proto (#249)

--- x/pool-incentives/types/query.pb.gw.go
+++ x/pool-incentives/types/query.pb.gw.go
@@ -408,7 +408,7 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 }
 
 var (
-	pattern_Query_GaugeIds_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"osmosis", "pool-incentives", "v1beta1", "pot-ids", "pool_id"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_GaugeIds_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"osmosis", "pool-incentives", "v1beta1", "gauge-ids", "pool_id"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_Query_DistrInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"osmosis", "pool-incentives", "v1beta1", "distr_info"}, "", runtime.AssumeColonVerbOpt(true)))
 
