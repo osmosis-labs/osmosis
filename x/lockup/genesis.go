d1fcc2d2f39c4d69e4ae01f8c6430b7ede83e54d
antstalepresh <stalepresh121@outlook.com>
2021-06-23 19:41:20 +1000

    add genesis export for lockup module

--- x/lockup/genesis.go
+++ x/lockup/genesis.go
@@ -9,9 +9,20 @@ import (
 // InitGenesis initializes the capability module's state from a provided genesis
 // state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
+	for _, lock := range genState.Locks {
+		if err := k.ResetLock(ctx, lock); err != nil {
+			panic(err)
+		}
+	}
 }
 
 // ExportGenesis returns the capability module's exported genesis.
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
-	return types.DefaultGenesis()
+	locks, err := k.GetPeriodLocks(ctx)
+	if err != nil {
+		panic(err)
+	}
+	return &types.GenesisState{
+		Locks: locks,
+	}
 }
