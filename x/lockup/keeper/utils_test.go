c83730f105e9e194c6caf5f52e419779db5b948f
antstalepresh <36045227+antstalepresh@users.noreply.github.com>
2021-06-24 02:45:05 +1000

    resolve lock tokens gas growth (#336)

--- x/lockup/keeper/utils_test.go
+++ x/lockup/keeper/utils_test.go
@@ -23,30 +23,6 @@ func TestCombineKeys(t *testing.T) {
 	require.Equal(t, key[4], key3[0])
 }
 
-func TestFindIndex(t *testing.T) {
-	IDs := []uint64{1, 2, 3, 4, 5}
-	require.Equal(t, findIndex(IDs, 1), 0)
-	require.Equal(t, findIndex(IDs, 3), 2)
-	require.Equal(t, findIndex(IDs, 5), 4)
-	require.Equal(t, findIndex(IDs, 6), -1)
-}
-
-func TestRemoveValue(t *testing.T) {
-	IDs := []uint64{1, 2, 3, 4, 5}
-	IDs, index1 := removeValue(IDs, 5)
-	require.Len(t, IDs, 4)
-	require.Equal(t, index1, 4)
-	IDs, index2 := removeValue(IDs, 3)
-	require.Len(t, IDs, 3)
-	require.Equal(t, index2, 2)
-	IDs, index3 := removeValue(IDs, 1)
-	require.Len(t, IDs, 2)
-	require.Equal(t, index3, 0)
-	IDs, index4 := removeValue(IDs, 6)
-	require.Len(t, IDs, 2)
-	require.Equal(t, index4, -1)
-}
-
 func TestGetTimeKey(t *testing.T) {
 	now := time.Now()
 	timeKey := getTimeKey(now)
