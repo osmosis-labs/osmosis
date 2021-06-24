bdd1f2218720025c4b8c3a5af28973654839d97a
antstalepresh <stalepresh121@outlook.com>
2021-06-24 22:41:33 +1000

    update startnode script

--- startnode.sh
+++ startnode.sh
@@ -10,4 +10,6 @@ osmosisd add-genesis-account $(osmosisd keys show validator -a --keyring-backend
 osmosisd gentx validator 500000000stake --keyring-backend=test --home=$HOME/.osmosisd --chain-id=testing
 osmosisd collect-gentxs --home=$HOME/.osmosisd
 
+cat $HOME/.osmosisd/config/genesis.json | jq '.app_state["gov"]["voting_params"]["voting_period"]="10s"' > $HOME/.osmosisd/config/tmp_genesis.json && mv $HOME/.osmosisd/config/tmp_genesis.json $HOME/.osmosisd/config/genesis.json
+
 osmosisd start --home=$HOME/.osmosisd
