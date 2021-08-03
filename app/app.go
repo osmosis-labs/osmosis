d0a4fc6e38b4dbccb22857e918993605d8dbcb0e
Dev Ojha <ValarDragon@users.noreply.github.com>
2021-08-02 22:51:49 -0500

    Fork in BeginBlock code (#395)
    * Add x/incentives cli docs (#332)
    
    * add README
    
    * modify create gauge tx
    
    * modify example
    
    * modify examples
    
    * fix cli
    
    * Apply suggestions from code review
    
    Co-authored-by: Dev Ojha <ValarDragon@users.noreply.github.com>
    
    * rm epoch duration
    
    * Apply suggestions from code review
    
    * apply review
    
    Co-authored-by: Dev Ojha <ValarDragon@users.noreply.github.com>
    
    * Fix CLI bug with start-time being blank
    
    * fork to fix gov min deposit
    
    Co-authored-by: mconcat <monoidconcat@gmail.com>
    Co-authored-by: Unity Chaos <unitylchaos@protonmail.com>

--- app/app.go
+++ app/app.go
@@ -558,6 +558,7 @@ func (app *OsmosisApp) Name() string { return app.BaseApp.Name() }
 
 // BeginBlocker application updates every begin block
 func (app *OsmosisApp) BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock) abci.ResponseBeginBlock {
+	forks(ctx, app)
 	return app.mm.BeginBlock(ctx, req)
 }
 
