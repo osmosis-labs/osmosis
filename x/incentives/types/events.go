af6cb98774ea86f5a9dd7f76d5bb62ce6fbf1a15
antstalepresh <36045227+antstalepresh@users.noreply.github.com>
2021-08-18 14:33:43 +1000

    add distribution event (#415)

--- x/incentives/types/events.go
+++ x/incentives/types/events.go
@@ -2,8 +2,12 @@ package types
 
 // event types
 const (
-	TypeEvtCreateGauge = "create_gauge"
-	TypeEvtAddToGauge  = "add_to_gauge"
+	TypeEvtCreateGauge  = "create_gauge"
+	TypeEvtAddToGauge   = "add_to_gauge"
+	TypeEvtDistribution = "distribution"
 
-	AttributeGaugeID = "gauge_id"
+	AttributeGaugeID  = "gauge_id"
+	AttributeLockID   = "lock_id"
+	AttributeReceiver = "receiver"
+	AttributeAmount   = "amount"
 )
