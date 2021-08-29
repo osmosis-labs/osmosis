6d082da01c355a899ba0b293c79993b71dafea74
Dev Ojha <ValarDragon@users.noreply.github.com>
2021-08-29 14:15:44 -0400

    Switch to SDK branch that lowers epoch time (#451)
    * Switch to SDK branch that eliminates epoch time
    
    * Improve benchmark accuracy
    
    * Sync more things

--- app/test_helpers.go
+++ app/test_helpers.go
@@ -2,8 +2,10 @@ package app
 
 import (
 	"encoding/json"
+	"os"
 
 	"github.com/cosmos/cosmos-sdk/simapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/libs/log"
 	dbm "github.com/tendermint/tm-db"
@@ -30,4 +32,40 @@ func Setup(isCheckTx bool) *OsmosisApp {
 	}
 
 	return app
+}
+
+// SetupTestingAppWithLevelDb initializes a new OsmosisApp intended for testing,
+// with LevelDB as a db
+func SetupTestingAppWithLevelDb(isCheckTx bool) (app *OsmosisApp, cleanupFn func()) {
+	dir := "osmosis_testing"
+	db, err := sdk.NewLevelDB("osmosis_leveldb_testing", dir)
+	if err != nil {
+		panic(err)
+	}
+	app = NewOsmosisApp(log.NewNopLogger(), db, nil, true, map[int64]bool{}, DefaultNodeHome, 5, MakeEncodingConfig(), simapp.EmptyAppOptions{})
+	if !isCheckTx {
+		genesisState := NewDefaultGenesisState()
+		stateBytes, err := json.MarshalIndent(genesisState, "", " ")
+		if err != nil {
+			panic(err)
+		}
+
+		app.InitChain(
+			abci.RequestInitChain{
+				Validators:      []abci.ValidatorUpdate{},
+				ConsensusParams: simapp.DefaultConsensusParams,
+				AppStateBytes:   stateBytes,
+			},
+		)
+	}
+
+	cleanupFn = func() {
+		db.Close()
+		err = os.RemoveAll(dir)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return
 }
