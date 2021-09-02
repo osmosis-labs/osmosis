bbde208019ba1ff976608c35fc4dd02fb1efec42
Jacob Gadikian <jacobgadikian@gmail.com>
2021-09-03 06:33:49 +0700

    Update statesync.sh

--- scripts/statesync.sh
+++ scripts/statesync.sh
@@ -1,12 +1,7 @@
 
 #!/bin/bash
-# Based on the work of Joe (Chorus-One) for Microtick - https://github.com/microtick/bounties/tree/main/statesync
-# You need config in two peers (avoid seed servers) this values in app.toml:
-#     [state-sync]
-#     snapshot-interval = 1000
-#     snapshot-keep-recent = 10
-# Pruning should be fine tuned also, for this testings is set to nothing
-#     pruning = "nothing"
+# microtick and bitcanna contributed significantly here.
+
 
 set -e
 
@@ -15,24 +10,24 @@ export GOPATH=~/go
 export PATH=$PATH:~/go/bin
 go install ./...
 
+
+# MAKE HOME FOLDER AND GET GENESIS
 osmosisd init test 
 wget -O $HOME/.osmosisd/config/genesis.json $GENESIS 
   
 
 INTERVAL=1500
 
+# GET TRUST HASH AND TRUST HEIGHT
+
 LATEST_HEIGHT=$(curl -s $RPC1:$RPC_PORT1/block | jq -r .result.block.header.height);
 BLOCK_HEIGHT=$(($LATEST_HEIGHT-$INTERVAL)) 
 TRUST_HASH=$(curl -s "$RPC1:$RPC_PORT1/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
 
-NODE1_ID=$(curl -s "$RPC1:$RPC_PORT1/status" | jq -r .result.node_info.id)
-NODE2_ID=$(curl -s "$RPC2:$RPC_PORT2/status" | jq -r .result.node_info.id)
-#NODE3_ID=$(curl -s "$RPC3:$RPC_PORT3/status" | jq -r .result.node_info.id)
 
+# TELL USER WHAT WE ARE DOING
 echo "TRUST HEIGHT: $BLOCK_HEIGHT"
 echo "TRUST HASH: $TRUST_HASH"
-echo "NODE ONE: $NODE1_ID@$NODE1_IP:$P2P_PORT1"
-echo "NODE TWO: $NODE2_ID@$NODE2_IP:$P2P_PORT2"
 
 
 # export state sync vars
@@ -41,5 +36,5 @@ export OSMOSISD_STATESYNC_RPC_SERVERS="162.55.132.230:2001,162.55.132.230:2001"
 export OSMOSISD_STATESYNC_TRUST_HEIGHT=$BLOCK_HEIGHT
 export OSMOSISD_STATESYNC_TRUST_HASH=$TRUST_HASH
 
-./osmosisd unsafe-reset-all
-./osmosisd start
+osmosisd unsafe-reset-all
+osmosisd start
