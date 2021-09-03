1cfc56f5bf6d27db7cdc1c34c15515ed1b66bdfc
antstalepresh <stalepresh121@outlook.com>
2021-09-03 19:46:08 +1000

    TestHandleUpdateHrpIbcChannelProposal

--- x/bech32ibc/keeper/gov_test.go
+++ x/bech32ibc/keeper/gov_test.go
@@ -1,3 +1,50 @@
 package keeper_test
 
+import (
+	channeltypes "github.com/cosmos/cosmos-sdk/x/ibc/core/04-channel/types"
+	"github.com/osmosis-labs/osmosis/x/bech32ibc/types"
+)
+
 // TODO: write test for HandleUpdateHrpIbcChannelProposal
+
+func (suite *KeeperTestSuite) TestHandleUpdateHrpIbcChannelProposal() {
+	suite.SetupTest()
+
+	// check genesis hrp ibc records
+	hrpIbcRecords := suite.app.Bech32IBCKeeper.GetHrpIbcRecords(suite.ctx)
+	suite.Require().Len(hrpIbcRecords, 0)
+
+	// check valid channel existence correctly
+	err := suite.app.Bech32IBCKeeper.HandleUpdateHrpIbcChannelProposal(suite.ctx, &types.UpdateHrpIbcChannelProposal{
+		Title:         "update hrp ibc channel",
+		Description:   "update hrp ibc channel",
+		Hrp:           "akash",
+		SourceChannel: "channel-1",
+	})
+	suite.Require().Error(err)
+
+	hrpIbcRecords = suite.app.Bech32IBCKeeper.GetHrpIbcRecords(suite.ctx)
+	suite.Require().Len(hrpIbcRecords, 0)
+
+	// create channel and try
+	suite.app.IBCKeeper.ChannelKeeper.SetChannel(suite.ctx, "transfer", "channel-1", channeltypes.Channel{
+		State:    1,
+		Ordering: 1,
+		Counterparty: channeltypes.Counterparty{
+			PortId:    "transfer",
+			ChannelId: "channel-1",
+		},
+		ConnectionHops: []string{},
+		Version:        "ics20",
+	})
+	err = suite.app.Bech32IBCKeeper.HandleUpdateHrpIbcChannelProposal(suite.ctx, &types.UpdateHrpIbcChannelProposal{
+		Title:         "update hrp ibc channel",
+		Description:   "update hrp ibc channel",
+		Hrp:           "akash",
+		SourceChannel: "channel-1",
+	})
+	suite.Require().NoError(err)
+
+	hrpIbcRecords = suite.app.Bech32IBCKeeper.GetHrpIbcRecords(suite.ctx)
+	suite.Require().Len(hrpIbcRecords, 1)
+}
