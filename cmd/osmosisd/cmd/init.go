cf858dabe0ddbf6d4a827d6c7620f8de424dd82b
Brian Fritton <bfritton@gmail.com>
2021-09-08 16:16:56 -0500

    Update init.go
    Change to max outbound peers for validator node as requested by Valardragon and proposed by FONDSMATIVE and dff | mp20

--- cmd/osmosisd/cmd/init.go
+++ cmd/osmosisd/cmd/init.go
@@ -97,7 +97,7 @@ func InitCmd(mbm module.BasicManager, defaultNodeHome string) *cobra.Command {
 			//Override default settings in config.toml
 			config.P2P.Seeds = strings.Join(seeds[:],",")
 			config.P2P.MaxNumInboundPeers = 150
-			config.P2P.MaxNumOutboundPeers = 40
+			config.P2P.MaxNumOutboundPeers = 100
 			config.Mempool.Size = 10000
 			config.StateSync.TrustPeriod = 112 * time.Hour
 			config.FastSync.Version = "v0"
