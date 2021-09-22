9f20553a8e1be2bfc656289a982e44cba441c777
Jacob Gadikian <jacobgadikian@gmail.com>
2021-09-23 01:47:05 +0700

    Update archivenode.bash

--- scripts/archivenode.bash
+++ scripts/archivenode.bash
@@ -2,7 +2,7 @@
 git checkout v1.0.1
 make install
 osmosisd init archive
-cp contrib/osmoarchive.service /etc/systemd/system
+wget -O ~/.osmosisd/config/genesis.json https://github.com/osmosis-labs/networks/raw/main/osmosis-1/genesis.json
 osmosisd start --pruning nothing --p2p.seed_nodes 085f62d67bbf9c501e8ac84d4533440a1eef6c45@95.217.196.54:26656,f515a8599b40f0e84dfad935ba414674ab11a668@osmosis.blockpane.com:26656
 git checkout v3.1.0
 make install
