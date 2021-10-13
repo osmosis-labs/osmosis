dd1e4eeaf5370e9338a0eb38e6b5811ccd862341
Dev Ojha <ValarDragon@users.noreply.github.com>
2021-10-13 10:36:12 -0500

    Linear time improvements to epoch time (#517)

--- x/incentives/keeper/gauge.go
+++ x/incentives/keeper/gauge.go
@@ -368,7 +368,8 @@ func (k Keeper) distributeInternal(
 			denomLockAmt := lock.Coins.AmountOfNoDenomValidation(gauge.DistributeTo.Denom)
 			amt := coin.Amount.Mul(denomLockAmt).Quo(lockSum.Mul(sdk.NewInt(int64(remainEpochs))))
 			if amt.IsPositive() {
-				distrCoins = distrCoins.Add(sdk.NewCoin(coin.Denom, amt))
+				newlyDistributedCoin := sdk.Coin{Denom: coin.Denom, Amount: amt}
+				distrCoins = distrCoins.Add(newlyDistributedCoin)
 			}
 		}
 		distrCoins = distrCoins.Sort()
