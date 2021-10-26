6031c2f0d46f301ca6519ed56c5f939302a02574
Sunny Aggarwal <sunny@osmosis.team>
2021-10-26 13:57:39 +0100

    Update x/txfees/module.go
    Co-authored-by: Dev Ojha <ValarDragon@users.noreply.github.com>

--- x/txfees/module.go
+++ x/txfees/module.go
@@ -79,6 +79,7 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
+	//nolint:errcheck
 	types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
 }
 
