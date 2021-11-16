88f47299e6eb937f1508e891af841aa7e760b9f7
mattverse <mattpark1028@gmail.com>
2021-11-16 15:41:46 +0900

    Add gas test for join pool

--- x/gamm/keeper/gas_test.go
+++ x/gamm/keeper/gas_test.go
@@ -1,13 +1,17 @@
 package keeper_test
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/osmosis-labs/osmosis/x/gamm/types"
 )
 
 var (
-	defaultAddr       sdk.AccAddress = sdk.AccAddress([]byte("addr1---------------"))
-	defaultCoins      sdk.Coins      = sdk.Coins{sdk.NewInt64Coin("stake", 10)}
-	minShareOutAmount sdk.Int        = sdk.OneInt()
+	defaultAddr  sdk.AccAddress = sdk.AccAddress([]byte("addr1---------------"))
+	defaultCoins sdk.Coins      = sdk.Coins{}
+	// defaultCoins      sdk.Coins      = sdk.Coins{sdk.NewInt64Coin("stake", 10)}
+	minShareOutAmount sdk.Int = types.OneShare.MulRaw(50)
 )
 
 func (suite *KeeperTestSuite) measureJoinPoolGas(
@@ -22,6 +26,8 @@ func (suite *KeeperTestSuite) measureJoinPoolGas(
 	return spentNow
 }
 
+// measureAvgAndMaxJoinPoolGas iterates JoinPool over designated amount of times
+// to acquire average gas spent
 func (suite *KeeperTestSuite) measureAvgAndMaxJoinPoolGas(
 	numIterations int,
 	addr sdk.AccAddress,
@@ -43,37 +49,34 @@ func (suite *KeeperTestSuite) measureAvgAndMaxJoinPoolGas(
 
 // This maintains hard coded gas test vector changes,
 // so we can easily track changes
-// func (suite *KeeperTestSuite) TestJoinPoolGas() {
-// 	suite.SetupTest()
+func (suite *KeeperTestSuite) TestJoinPoolGas() {
+	suite.SetupTest()
+	poolId := suite.preparePool()
 
-// 	poolIDFn := func(int) uint64 { return 1 }
-// 	minShareOutAmountFn := func(int) sdk.Int { return minShareOutAmount }
-// 	maxCoinsFn := func(int) sdk.Coins { return defaultCoins }
-// 	totalNumJoins := 10000
+	poolIDFn := func(int) uint64 { return poolId }
+	minShareOutAmountFn := func(int) sdk.Int { return minShareOutAmount }
+	maxCoinsFn := func(int) sdk.Coins { return defaultCoins }
+	startAveragingAt := 1000
+	totalNumJoins := 10000
 
-// 	suite.preparePool()
-// 	firstJoinGas := suite.measureJoinPoolGas(defaultAddr, defaultCoins, time.Second)
-// 	suite.Assert().Equal(73686, int(firstJoinGas))
+	err := suite.app.BankKeeper.AddCoins(suite.ctx, defaultAddr, sdk.NewCoins(
+		sdk.NewCoin("uosmo", sdk.NewInt(10000000000000)),
+		sdk.NewCoin("foo", sdk.NewInt(10000000000000000)),
+		sdk.NewCoin("bar", sdk.NewInt(10000000000000000)),
+		sdk.NewCoin("baz", sdk.NewInt(10000000000000000)),
+	))
+	suite.Require().NoError(err)
 
-// 	avgGas, maxGas := suite.measureAvgAndMaxJoinPoolGas(totalNumJoins, defaultAddr, poolIDFn, minShareOutAmountFn, maxCoinsFn)
-// 	fmt.Printf("test deets: total locks created %d, begin average at %d\n", totalNumLocks, startAveragingAt)
-// 	suite.Assert().Equal(64028, int(avgGas), "average gas / lock")
-// 	suite.Assert().Equal(64118, int(maxGas), "max gas / lock")
-// }
+	firstJoinGas := suite.measureJoinPoolGas(defaultAddr, poolId, types.OneShare.MulRaw(50), defaultCoins)
+	suite.Assert().Equal(76608, int(firstJoinGas))
 
-// func (suite *KeeperTestSuite) TestJoinPoolDistinctDenomGas() {
-// 	suite.SetupTest()
+	for i := 1; i < startAveragingAt; i++ {
+		err := suite.app.GAMMKeeper.JoinPool(suite.ctx, defaultAddr, poolId, types.OneShare.MulRaw(50), sdk.Coins{})
+		suite.Require().NoError(err)
+	}
 
-// 	coinsFn := func(int) sdk.Coins { return defaultCoins }
-// 	durFn := func(i int) time.Duration { return time.Duration(i+1) * time.Second }
-// 	totalNumLocks := 10000
-
-// 	suite.LockTokens(defaultAddr, defaultCoins, time.Second)
-// 	firstLockGasAmount := suite.ctx.GasMeter().GasConsumed()
-// 	suite.Assert().Equal(firstLockGasAmount, uint64(73686))
-
-// 	avgGas, maxGas := suite.measureAvgAndMaxLockGas(totalNumLocks, defaultAddr, coinsFn, durFn)
-// 	fmt.Printf("test deets: total locks created %d\n", totalNumLocks)
-// 	suite.Assert().EqualValues(110729, int(avgGas), "average gas / lock")
-// 	suite.Assert().EqualValues(231313, int(maxGas), "max gas / lock")
-// }
+	avgGas, maxGas := suite.measureAvgAndMaxJoinPoolGas(totalNumJoins, defaultAddr, poolIDFn, minShareOutAmountFn, maxCoinsFn)
+	fmt.Printf("test deets: total %d of pools joined, begin average at %d\n", totalNumJoins, startAveragingAt)
+	suite.Assert().Equal(71065, int(avgGas), "average gas / join pool")
+	suite.Assert().Equal(71164, int(maxGas), "max gas / join pool")
+}
