814d03122f71ed0a8d2290ce6c1245cc2b4e710b
Jacob Gadikian <jacobgadikian@gmail.com>
2021-11-28 19:01:30 +0700

    Update init.go

--- cmd/osmosisd/cmd/init.go
+++ cmd/osmosisd/cmd/init.go
@@ -87,6 +87,7 @@ func InitCmd(mbm module.BasicManager, defaultNodeHome string) *cobra.Command {
 			seeds := []string{
 				"63aba59a7da5197c0fbcdc13e760d7561791dca8@162.55.132.230:2000",         // Notional
 				"f515a8599b40f0e84dfad935ba414674ab11a668@osmosis.blockpane.com:26656", // [ block pane ]
+				"6bcdbcfd5d2c6ba58460f10dbcfde58278212833@osmosis.artifact-staking.io:26656", // Artifact Staking
 			}
 
 			//Override default settings in config.toml
