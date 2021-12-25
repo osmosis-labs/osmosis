10d223436dc7c2f68122ac1b2d3596ab23f7ab69
Sunny Aggarwal <sunny@osmosis.team>
2021-12-25 01:39:36 -0500

    Update x/tokenfactory/keeper/keeper.go
    Co-authored-by: Dev Ojha <ValarDragon@users.noreply.github.com>

--- x/tokenfactory/keeper/keeper.go
+++ x/tokenfactory/keeper/keeper.go
@@ -63,6 +63,9 @@ func (k Keeper) GetCreatorsPrefixStore(ctx sdk.Context) sdk.KVStore {
 // }
 
 // CreateModuleAccount creates a module account with minting and burning capabilities
+// This account isn't intended to store any coins, 
+// it purely mints and burns them on behalf of the admin of respective denoms, 
+// and sends to the relevant address.
 func (k Keeper) CreateModuleAccount(ctx sdk.Context) {
 	moduleAcc := authtypes.NewEmptyModuleAccount(types.ModuleName, authtypes.Minter, authtypes.Burner)
 	k.accountKeeper.SetModuleAccount(ctx, moduleAcc)
