fef68d4b99aa2d23280f45324b1130e6faaf80ad
Robert Zaremba <robert@zaremba.ch>
2022-01-16 21:26:29 +0100

    dummy query sever

---
+++ x/osmolbp/api/proto_desc.go
@@ -0,0 +1,8 @@
+package api
+
+import grpc "google.golang.org/grpc"
+
+// MsgServiceDesc return ServiceDesc for Msg server
+func MsgServiceDesc() *grpc.ServiceDesc {
+	return &_Msg_serviceDesc
+}
---
+++ x/osmolbp/keeper/query_server.go
@@ -0,0 +1,11 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/osmosis-labs/osmosis/x/osmolbp/api"
+)
+
+func (k Keeper) LBPs(ctx context.Context, q *api.QueryLBPs) (*api.QueryLBPsResponse, error) {
+	return nil, nil
+}
