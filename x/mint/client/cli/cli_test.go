b70ef2fd7b84890e15a2ee37776d79de360a537c
faddat <jacobgadikian@gmail.com>
2022-01-23 01:57:15 +0000

    I think this is correct, but am not certain.  Found an old import, and updated the go module paths in the .proto files

--- x/mint/client/cli/cli_test.go
+++ x/mint/client/cli/cli_test.go
@@ -1,3 +1,4 @@
+//go:build norace
 // +build norace
 
 package cli_test
@@ -13,8 +14,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
 	"github.com/cosmos/cosmos-sdk/testutil/network"
-	"github.com/osmosis-labs/osmosis/app"
-	"github.com/osmosis-labs/osmosis/x/mint/client/cli"
+	"github.com/osmosis-labs/osmosis/v4/app"
+	"github.com/osmosis-labs/osmosis/v4/x/mint/client/cli"
 )
 
 type IntegrationTestSuite struct {
