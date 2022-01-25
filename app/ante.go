f689c46beb508b0c75d577737edfd841699eebb8
Hleb Albau <hleb.albau@gmail.com>
2022-01-25 02:10:08 +0100

    Add separate min gas price option for txes with high gas wanted (#777)

--- app/ante.go
+++ app/ante.go
@@ -1,11 +1,8 @@
 package app
 
 import (
-	"fmt"
-
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	"github.com/cosmos/cosmos-sdk/x/auth/signing"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
@@ -28,14 +25,11 @@ func NewAnteHandler(
 	signModeHandler signing.SignModeHandler,
 	channelKeeper channelkeeper.Keeper,
 ) sdk.AnteHandler {
-	mempoolFeeDecorator := txfeeskeeper.NewMempoolFeeDecorator(*txFeesKeeper)
-	// Optional if anyone else is using this repo. Primarily of impact for Osmosis.
-	// TODO: Abstract this better
-	mempoolFeeDecorator.SetArbMinGasFee(parseArbGasFromConfig(appOpts))
+	mempoolFeeOptions := txfeestypes.NewMempoolFeeOptions(appOpts)
+	mempoolFeeDecorator := txfeeskeeper.NewMempoolFeeDecorator(*txFeesKeeper, mempoolFeeOptions)
 	return sdk.ChainAnteDecorators(
 		ante.NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
 		ante.NewRejectExtensionOptionsDecorator(),
-		NewMempoolMaxGasPerTxDecorator(),
 		// Use Mempool Fee Decorator from our txfees module instead of default one from auth
 		// https://github.com/cosmos/cosmos-sdk/blob/master/x/auth/middleware/fee.go#L34
 		mempoolFeeDecorator,
@@ -51,58 +45,4 @@ func NewAnteHandler(
 		ante.NewIncrementSequenceDecorator(ak),
 		ibcante.NewAnteDecorator(channelKeeper),
 	)
-}
-
-// TODO: Abstract this function better. We likely need a parse `osmosis-mempool` config section.
-func parseArbGasFromConfig(appOpts servertypes.AppOptions) sdk.Dec {
-	arbMinFeeInterface := appOpts.Get("osmosis-mempool.arbitrage-min-gas-fee")
-	arbMinFee := txfeeskeeper.DefaultArbMinGasFee
-	if arbMinFeeInterface != nil {
-		arbMinFeeStr, ok := arbMinFeeInterface.(string)
-		if !ok {
-			panic("invalidly configured osmosis-mempool.arbitrage-min-gas-fee")
-		}
-		var err error
-		// pre-pend 0 to allow the config to start with a decimal, e.g. ".01"
-		arbMinFee, err = sdk.NewDecFromStr("0" + arbMinFeeStr)
-		if err != nil {
-			panic(fmt.Errorf("invalidly configured osmosis-mempool.arbitrage-min-gas-fee, err= %v", err))
-		}
-	}
-	return arbMinFee
-}
-
-// NewMempoolMaxGasPerTxDecorator will check if the transaction's gas
-// is greater than the local validator's max_gas_wanted_per_tx.
-// TODO: max_gas_per_tx is hardcoded here, should move to being defined in app.toml.
-// If gas_wanted is too high, decorator returns error and tx is rejected from mempool.
-// Note this only applies when ctx.CheckTx = true
-// If gas is sufficiently low or not CheckTx, then call next AnteHandler
-// CONTRACT: Tx must implement FeeTx to use MempoolMaxGasPerTxDecorator
-type MempoolMaxGasPerTxDecorator struct{}
-
-func NewMempoolMaxGasPerTxDecorator() MempoolMaxGasPerTxDecorator {
-	return MempoolMaxGasPerTxDecorator{}
-}
-
-func (mgd MempoolMaxGasPerTxDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
-	feeTx, ok := tx.(sdk.FeeTx)
-	if !ok {
-		return ctx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "Tx must be a FeeTx")
-	}
-
-	gas := feeTx.GetGas()
-	// maximum gas wanted per tx set to 25M
-	max_gas_wanted_per_tx := uint64(25000000)
-
-	// Ensure that the provided gas is less than the maximum gas per tx.
-	// if this is a CheckTx. This is only for local mempool purposes, and thus
-	// is only ran on check tx.
-	if ctx.IsCheckTx() && !simulate {
-		if gas > max_gas_wanted_per_tx {
-			return ctx, sdkerrors.Wrapf(sdkerrors.ErrOutOfGas, "Too much gas wanted: %d, maximum is 25,000,000", gas)
-		}
-	}
-
-	return next(ctx, tx, simulate)
 }
