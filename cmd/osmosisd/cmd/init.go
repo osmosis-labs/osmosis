ca8a441361317bfa264291904ff74098986e59d7
Jacob Gadikian <jacobgadikian@gmail.com>
2022-02-01 17:29:16 +0700

    Update init.go

--- cmd/osmosisd/cmd/init.go
+++ cmd/osmosisd/cmd/init.go
@@ -84,7 +84,10 @@ func InitCmd(mbm module.BasicManager, defaultNodeHome string) *cobra.Command {
 			//An easy way to run a lightweight seed node is to use tenderseed: github.com/binaryholdings/tenderseed
 
 			seeds := []string{
-				"63aba59a7da5197c0fbcdc13e760d7561791dca8@162.55.132.230:2000",               // Notional
+				"21d7539792ee2e0d650b199bf742c56ae0cf499e@162.55.132.230:2000",               // Notional
+				"295b417f995073d09ff4c6c141bd138a7f7b5922@65.21.141.212:2000", 		      // Notional
+				"ec4d3571bf709ab78df61716e47b5ac03d077a1a@65.108.43.26:2000",		      // Notional
+				"4cb8e1e089bdf44741b32638591944dc15b7cce3@65.108.73.18:2000",		      // Notional
 				"f515a8599b40f0e84dfad935ba414674ab11a668@osmosis.blockpane.com:26656",       // [ block pane ]
 				"6bcdbcfd5d2c6ba58460f10dbcfde58278212833@osmosis.artifact-staking.io:26656", // Artifact Staking
 			}
