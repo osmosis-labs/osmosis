eca99e6d6b93e3a1b7ba3ff056af21d5de1cbec9
mconcat <monoidconcat@gmail.com>
2022-02-10 19:36:11 -0800

    add synthlock invariant in progress
    add synthlock invariant

---
+++ x/lockup/keeper/invariants.go
@@ -0,0 +1,34 @@
+package keeper
+
+// DONTCOVER
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/osmosis-labs/osmosis/x/gamm/types"
+)
+
+// RegisterInvariants registers all governance invariants
+func RegisterInvariants(ir sdk.InvariantRegistry, keeper Keeper) {
+	ir.RegisterRoute(types.ModuleName, "synthetic-lockup-invariant", SyntheticLockupInvariant(keeper))
+}
+
+func SyntheticLockupInvariant(keeper Keeper) sdk.Invariant {
+	return func(ctx sdk.Context) (string, bool) {
+		synthlocks := keeper.GetAllSyntheticLockups(ctx)
+		for _, synthlock := range synthlocks {
+			baselock, err := keeper.GetLockByID(ctx, synthlock.UnderlyingLockId)
+			if err != nil {
+				panic(err)
+			}
+			if !baselock.Coins.IsAllGT(synthlock.Coins) {
+				return sdk.FormatInvariant(types.ModuleName, "synthetic-lockup-invariant",
+					fmt.Sprintf("\tSynthetic lock token amount %s\n\tUnderlying lock ID: %d, token amount %s\n",
+						synthlock.Coins, baselock.ID, baselock.Coins)), true
+			}
+		}
+		return sdk.FormatInvariant(types.ModuleName, "synthetic-lockup-invariant", "All synthetic lockup invariant passed"), false
+	}
+}
