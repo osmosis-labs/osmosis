95220bbdd379b2a74717e966d712c34c6ba6a087
ValarDragon <dojha12@gmail.com>
2022-02-15 12:34:21 -0800

    Comment out handler

--- x/superfluid/handler.go
+++ x/superfluid/handler.go
@@ -22,9 +22,9 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 		case *types.MsgSuperfluidUndelegate:
 			res, err := msgServer.SuperfluidUndelegate(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
-		case *types.MsgSuperfluidRedelegate:
-			res, err := msgServer.SuperfluidRedelegate(sdk.WrapSDKContext(ctx), msg)
-			return sdk.WrapServiceResult(ctx, res, err)
+		// case *types.MsgSuperfluidRedelegate:
+		// 	res, err := msgServer.SuperfluidRedelegate(sdk.WrapSDKContext(ctx), msg)
+		// return sdk.WrapServiceResult(ctx, res, err)
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
