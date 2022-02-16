332509223f5d691e463b94268ddd6315aa44bc50
Jacob <jacobgadikian@gmail.com>
2022-02-16 08:04:51 +0700

    State sync now works properly with rocksdb.  Building with rocksdb now uses the ldflags.

--- scripts/statesync-rocks.bash
+++ scripts/statesync-rocks.bash
@@ -14,19 +14,21 @@ export PATH=$PATH:~/go/bin
 # Use if building on a mac
 # export CGO_CFLAGS="-I/opt/homebrew/Cellar/rocksdb/6.27.3/include"
 # export CGO_LDFLAGS="-L/opt/homebrew/Cellar/rocksdb/6.27.3/lib -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy -llz4 -lzstd -L/opt/homebrew/Cellar/snappy/1.1.9/lib -L/opt/homebrew/Cellar/lz4/1.9.3/lib/ -L /opt/homebrew/Cellar/zstd/1.5.1/lib/"
-go install ./...
-
+go install -ldflags '-w -s -X github.com/cosmos/cosmos-sdk/types.DBBackend=rocksdb' -tags rocksdb ./...
 
 # MAKE HOME FOLDER AND GET GENESIS
-osmosisd init test
+osmosisd init osmosis-rocks
 wget -O ~/.osmosisd/config/genesis.json https://cloudflare-ipfs.com/ipfs/QmXRvBT3hgoXwwPqbK6a2sXUuArGM8wPyo1ybskyyUwUxs
+
+# this will let tendermint know that we want rocks
+sed -i 's/goleveldb/rocksdb/' ~/.osmosisd/config/config.toml
 
 
 # Uncomment if resyncing a server
 # osmosisd unsafe-reset-all --home /osmosis/osmosis
 
 
-# Set State Sync Snapshot Interval
+
 INTERVAL=1500
 
 # GET TRUST HASH AND TRUST HEIGHT
@@ -47,8 +49,12 @@ export OSMOSISD_STATESYNC_RPC_SERVERS="162.55.132.230:2001,162.55.132.230:2001"
 export OSMOSISD_STATESYNC_TRUST_HEIGHT=$BLOCK_HEIGHT
 export OSMOSISD_STATESYNC_TRUST_HASH=$TRUST_HASH
 
+# Rockdb won't make this folder, so we make it 
+mkdir -p ~/.osmosisd/data/snapshots/metadata.db
+
 # THIS WILL FAIL BECAUSE THE APP VERSION IS CORRECTLY SET IN OSMOSIS
-osmosisd start 
+osmosisd start --db_backend rocksdb 
+
 
 # --db_path /tmp/osmosisd/db --db_rocksdb_options "compression=kNoCompression" --db_rocksdb_options "compaction_style=kCompactionStyleLevel" --db_rocksdb_options "level_compaction_dynamic_level_bytes=true" --db_rocksdb_options "num_levels=4" --db_rocksdb_options "max_bytes_for_level_base=104857600" --db_rocksdb_options "max_bytes_for_level_multiplier=1" --db_rocksdb_options "max_background_compactions=4" --db_rocksdb_options "max_background_flushes=4" --db_rocksdb_options "write_buffer_size=104857600" --db_rocksdb_options "target_file_size_base=104857600" --db_rocksdb_options "target_file_size_multiplier=1" --db_rocksdb_options "max_write_buffer_number=4" --db_rocksdb_options "min_write_buffer_number_to_merge=2" --db_rocksdb_options "max_grandparent_overlap_factor=10" --db_rocksdb_options "max_bytes_for_level_multiplier_additional=[4,4,4,4]" --db_rocksdb_options "compaction_pri=kOldestSmallestSeqFirst" --db_rocksdb_options "compaction_options_fifo={max_table_files_size=104857600}" --db_rocksdb_options "compaction_options_universal={size_ratio=1, min_merge_width=2, max_merge_width=2, max_size_amplification_percent=20}" --db_rocksdb_options "compaction_options_level_base={compression=kNoCompression, filter_policy=kNoFilter}" --db_rocksdb_options "compaction_options_level_base={compression=kSnappyCompression, filter_policy=
 
@@ -60,4 +66,4 @@ go install -tags rocksdb ./...
 tendermint set-app-version 1 --home ~/.osmosisd
 
 # THERE, NOW IT'S SYNCED AND YOU CAN PLAY
-osmosisd start
+osmosisd start --db_backend rocksdb
