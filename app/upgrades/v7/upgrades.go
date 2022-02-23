dd09fae8bd3df9f60e431bc2f945f5dc8ea56633
Matt, Park <45252226+mattverse@users.noreply.github.com>
2022-02-22 20:18:05 -0800

    Add superfluid migrations code (#952)
    * Add superfluid migrations code
    
    * Add AddNewSuperfluidAsset

--- app/upgrades/v7/upgrades.go
+++ app/upgrades/v7/upgrades.go
@@ -9,12 +9,18 @@ import (
 	"github.com/CosmWasm/wasmd/x/wasm"
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 
+	epochskeeper "github.com/osmosis-labs/osmosis/v7/x/epochs/keeper"
 	lockupkeeper "github.com/osmosis-labs/osmosis/v7/x/lockup/keeper"
 	mintkeeper "github.com/osmosis-labs/osmosis/v7/x/mint/keeper"
+
+	superfluidkeeper "github.com/osmosis-labs/osmosis/v7/x/superfluid/keeper"
+	superfluidtypes "github.com/osmosis-labs/osmosis/v7/x/superfluid/types"
 )
 
 func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator,
 	wasmKeeper *wasm.Keeper,
+	superfluidKeeper *superfluidkeeper.Keeper,
+	epochsKeeper *epochskeeper.Keeper,
 	lockupKeeper *lockupkeeper.Keeper,
 	mintKeeper *mintkeeper.Keeper,
 	accountKeeper *authkeeper.AccountKeeper,
@@ -44,6 +50,14 @@ func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator,
 			ctx, *lockupKeeper, accountKeeper,
 			lockupkeeper.BaselineDurations, lockupkeeper.HourDuration,
 		)
+
+		ctx.Logger().Info("Migration for superfluid staking")
+
+		superfluidAsset := superfluidtypes.SuperfluidAsset{
+			Denom:     "gamm/pool/1",
+			AssetType: superfluidtypes.SuperfluidAssetTypeLPShare,
+		}
+		superfluidKeeper.AddNewSuperfluidAsset(ctx, superfluidAsset)
 
 		// Set the supply offset from the developer vesting account
 		mintkeeper.SetInitialSupplyOffsetDuringMigration(ctx, *mintKeeper)
