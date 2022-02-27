691bcbdfabde9b27edf6bdb31d68de5160232a3f
ValarDragon <dojha12@gmail.com>
2022-02-27 13:15:19 -0800

    Add panic guard, bug fix

--- osmoutils/cache_ctx.go
+++ osmoutils/cache_ctx.go
@@ -1,6 +1,9 @@
 package osmoutils
 
 import (
+	"errors"
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -8,10 +11,17 @@ import (
 // drop the state machine change and log the error.
 // If there is no error, proceeds as normal (but with some slowdown due to SDK store weirdness)
 // Try to avoid usage of iterators in f.
-func ApplyFuncIfNoError(ctx sdk.Context, f func(ctx sdk.Context) error) error {
+func ApplyFuncIfNoError(ctx sdk.Context, f func(ctx sdk.Context) error) (err error) {
+	// Add a panic safeguard
+	defer func() {
+		if recovErr := recover(); recovErr != nil {
+			fmt.Println(recovErr)
+			err = errors.New("panic occured during execution")
+		}
+	}()
 	// makes a new cache context, which all state changes get wrapped inside of.
 	cacheCtx, write := ctx.CacheContext()
-	err := f(cacheCtx)
+	err = f(cacheCtx)
 	if err != nil {
 		ctx.Logger().Error(err.Error())
 	} else {
