36f9755a29e2dcfacd2dd7b778e76becb61079e5
Jacob Gadikian <jacobgadikian@gmail.com>
2022-03-01 08:52:55 +0700

    flatten NewOsmosisApp

--- app/app.go
+++ app/app.go
@@ -207,9 +207,17 @@ func init() {
 
 // NewOsmosis returns a reference to an initialized Osmosis.
 func NewOsmosisApp(
-	logger log.Logger, db dbm.DB, traceStore io.Writer, loadLatest bool, skipUpgradeHeights map[int64]bool,
-	homePath string, invCheckPeriod uint, encodingConfig appparams.EncodingConfig, appOpts servertypes.AppOptions,
-	wasmEnabledProposals []wasm.ProposalType, wasmOpts []wasm.Option,
+	logger log.Logger,
+	db dbm.DB,
+	traceStore io.Writer,
+	loadLatest bool,
+	skipUpgradeHeights map[int64]bool,
+	homePath string,
+	invCheckPeriod uint,
+	encodingConfig appparams.EncodingConfig,
+	appOpts servertypes.AppOptions,
+	wasmEnabledProposals []wasm.ProposalType,
+	wasmOpts []wasm.Option,
 	baseAppOptions ...func(*baseapp.BaseApp),
 ) *OsmosisApp {
 
