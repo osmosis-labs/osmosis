2d3bb29bc87ad911d2bc6a4a4b86d2e893e330ca
Mauro Lacy <mauro@lacy.com.es>
2022-03-11 13:28:42 +0100

    Wasm queries and message types

---
+++ app/wasm/bindings/query.go
@@ -0,0 +1,38 @@
+package cosmwasm
+
+// OsmosisQuery contains osmosis custom queries.
+// See https://github.com/confio/osmosis-bindings/blob/main/packages/bindings/src/query.rs
+type OsmosisQuery struct {
+	/// Given a subdenom minted by a contract via `OsmosisMsg::MintTokens`,
+	/// returns the full denom as used by `BankMsg::Send`.
+	FullDenom *FullDenom `json:"full_denom,omitempty"`
+	/// For a given pool ID, list all tokens traded on it with current liquidity (spot).
+	/// As well as the total number of LP shares and their denom.
+	PoolState *PoolState `json:"pool_state,omitempty"`
+	/// Return current spot price swapping In for Out on given pool ID.
+	/// Warning: this can easily be manipulated via sandwich attacks, do not use as price oracle.
+	/// We will add TWAP for more robust price feed.
+	SpotPrice *SpotPrice `json:"spot_price,omitempty"`
+	/// Return current spot price swapping In for Out on given pool ID.
+	EstimatePrice *EstimatePrice `json:"estimate_price,omitempty"`
+}
+
+type FullDenom struct {
+	Contract string `json:"contract"`
+	SubDenom string `json:"sub_denom"`
+}
+
+type PoolState struct {
+	PoolId int `json:"id"`
+}
+
+type SpotPrice struct {
+	Swap        Swap `json:"swap"`
+	WithSwapFee bool `json:"with_swap_fee"`
+}
+
+type EstimatePrice struct {
+	First  Swap
+	Route  []Step
+	Amount SwapAmount
+}
---
+++ app/wasm/bindings/types.go
@@ -0,0 +1,32 @@
+package cosmwasm
+
+type Swap struct {
+	PoolId   int    `json:"pool_id"`
+	DenomIn  string `json:"denom_in"`
+	DenomOut string `json:"denom_out"`
+}
+
+type Step struct {
+	PoolId   int    `json:"pool_id"`
+	DenomOut string `json:"denom_out"`
+}
+
+type SwapAmount struct {
+	In  string `json:"in,omitempty"`
+	Out string `json:"out,omitempty"`
+}
+
+type SwapAmountWithLimit struct {
+	ExactIn  *ExactIn  `json:"exact_in,omitempty"`
+	ExactOut *ExactOut `json:"exact_out,omitempty"`
+}
+
+type ExactIn struct {
+	Input     string `json:"input"`
+	MinOutput string `json:"min_output"`
+}
+
+type ExactOut struct {
+	MaxInput string `json:"max_input"`
+	Output   string `json:"output"`
+}
