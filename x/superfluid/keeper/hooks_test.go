00b4b2c37c9e22899cbffedc90c876357a56bd9d
Matt, Park <45252226+mattverse@users.noreply.github.com>
2022-03-14 13:15:55 +0900

    Test improvisation for Superfluid (#1070)
    * add utility for SetupGammPoolsAndSuperfluidAssets
    
    * remove hardcoded gamm pool denoms
    
    * fix for main branch ctx => Ctx, app => App in tests
    
    * Test Changes
    
    * Add testing packages
    
    * Delete fmt
    
    * Fix lint
    
    * Remove multiple pacakges for apptesting
    
    * Add parameter for reward allocation
    
    * Improvements on test
    
    Co-authored-by: antstalepresh <stalepresh121@outlook.com>

--- x/superfluid/keeper/hooks_test.go
+++ x/superfluid/keeper/hooks_test.go
@@ -1,10 +1,8 @@
 package keeper_test
 
 import (
-	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-	"github.com/tendermint/tendermint/crypto/ed25519"
 )
 
 func (suite *KeeperTestSuite) TestSuperfluidAfterEpochEnd() {
@@ -18,7 +16,7 @@ func (suite *KeeperTestSuite) TestSuperfluidAfterEpochEnd() {
 			"happy path with single validator and delegator",
 			[]stakingtypes.BondStatus{stakingtypes.Bonded},
 			[]superfluidDelegation{{0, 0, 0, 1000000}},
-			sdk.Coins{},
+			sdk.Coins{{Amount: sdk.NewInt(999990), Denom: "stake"}},
 		},
 	}
 
@@ -27,34 +25,39 @@ func (suite *KeeperTestSuite) TestSuperfluidAfterEpochEnd() {
 			suite.SetupTest()
 			valAddrs := suite.SetupValidators(tc.validatorStats)
 
+			// we create two additional pools: total three pools, 10 gauges
 			denoms, poolIds := suite.SetupGammPoolsAndSuperfluidAssets([]sdk.Dec{sdk.NewDec(20), sdk.NewDec(20)})
 
 			// Generate delegator addresses
 			delAddrs := CreateRandomAccounts(1)
-			intermediaryAccs, _ := suite.SetupSuperfluidDelegations(delAddrs, valAddrs, tc.superDelegations, denoms)
+			intermediaryAccs, locks := suite.SetupSuperfluidDelegations(delAddrs, valAddrs, tc.superDelegations, denoms)
 			suite.checkIntermediaryAccountDelegations(intermediaryAccs)
 
-			// gamm swap operation before refresh
-			acc1 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+			// run swap and set spot price
 			pool, err := suite.App.GAMMKeeper.GetPool(suite.Ctx, poolIds[0])
 			suite.Require().NoError(err)
 			poolAssets := pool.GetAllPoolAssets()
-
-			coins := sdk.Coins{sdk.NewInt64Coin(poolAssets[1].Token.Denom, 100000000000000)}
-			err = simapp.FundAccount(suite.App.BankKeeper, suite.Ctx, acc1, coins)
-			suite.Require().NoError(err)
-			_, _, err = suite.App.GAMMKeeper.SwapExactAmountOut(
-				suite.Ctx, acc1,
-				poolIds[0], poolAssets[1].Token.Denom, poolAssets[1].Token.Amount,
-				sdk.NewCoin(poolAssets[0].Token.Denom, poolAssets[0].Token.Amount.Quo(sdk.NewInt(4))))
-			suite.Require().NoError(err)
+			suite.SwapAndSetSpotPrice(poolIds[0], poolAssets[1], poolAssets[0])
 
 			// run epoch actions
 			suite.BeginNewBlock(true)
 
 			// check lptoken twap value set
-			newEpochTwap := suite.App.SuperfluidKeeper.GetOsmoEquivalentMultiplier(suite.Ctx, denoms[0])
-			suite.Require().Equal(newEpochTwap.String(), "15.000000000000000000")
+			newEpochMultiplier := suite.App.SuperfluidKeeper.GetOsmoEquivalentMultiplier(suite.Ctx, denoms[0])
+			suite.Require().Equal(newEpochMultiplier, sdk.NewDec(15))
+
+			// check gauge creation in new block
+			intermediaryAccAddr := suite.App.SuperfluidKeeper.GetLockIdIntermediaryAccountConnection(suite.Ctx, locks[0].ID)
+			intermediaryAcc := suite.App.SuperfluidKeeper.GetIntermediaryAccount(suite.Ctx, intermediaryAccAddr)
+			gauge, err := suite.App.IncentivesKeeper.GetGaugeByID(suite.Ctx, intermediaryAcc.GaugeId)
+
+			suite.Require().NoError(err)
+			suite.Require().Equal(gauge.Id, intermediaryAcc.GaugeId)
+			suite.Require().Equal(gauge.IsPerpetual, true)
+			suite.Require().Equal(gauge.Coins, tc.expRewards)
+			suite.Require().Equal(gauge.NumEpochsPaidOver, uint64(1))
+			suite.Require().Equal(gauge.FilledEpochs, uint64(1))
+			suite.Require().Equal(gauge.DistributedCoins, tc.expRewards)
 
 			// check delegation changes
 			for _, acc := range intermediaryAccs {
@@ -63,9 +66,9 @@ func (suite *KeeperTestSuite) TestSuperfluidAfterEpochEnd() {
 				delegation, found := suite.App.StakingKeeper.GetDelegation(suite.Ctx, acc.GetAccAddress(), valAddr)
 				suite.Require().True(found)
 				suite.Require().Equal(sdk.NewDec(7500000), delegation.Shares)
-				// TODO: Check reward distribution
-				// suite.Require().NotEqual(sdk.Coins{}, )
 			}
+			balance := suite.App.BankKeeper.GetAllBalances(suite.Ctx, delAddrs[0])
+			suite.Require().Equal(tc.expRewards, balance)
 		})
 	}
 }
