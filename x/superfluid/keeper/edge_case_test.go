110fc14951c376dc6664d5adc1c704fc1db789f7
antstalepresh <stalepresh121@outlook.com>
2022-03-22 00:25:44 +1000

    add one more todo for Dev's suggestion

--- x/superfluid/keeper/edge_case_test.go
+++ x/superfluid/keeper/edge_case_test.go
@@ -1,3 +1,4 @@
 package keeper_test
 
 // TODO: try creating a normal lockup when superfluid lockup for same denom exists
+// TODO: try the case if a validator becomes jailed, or double sign slashes (Dev suggestion)
