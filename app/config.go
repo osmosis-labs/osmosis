63633b87bce29dff5d3185351783df383ba54fab
Roman <34196718+p0mvn@users.noreply.github.com>
2022-03-28 08:16:05 -0700

    Database stability improvements (#1154)
    * update sdk and iavl with database stability improvements
    
    * changelog
    
    * fmt
    
    * upgrade iavl and sdk with smaller fast node cache size
    
    Co-authored-by: Roman <roman@osmosis.team>

--- app/config.go
+++ app/config.go
@@ -12,8 +12,8 @@ import (
 	"github.com/osmosis-labs/osmosis/v7/app/params"
 	dbm "github.com/tendermint/tm-db"
 
+	pruningTypes "github.com/cosmos/cosmos-sdk/pruning/types"
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 )
@@ -39,7 +39,7 @@ func DefaultConfig() network.Config {
 		AccountTokens:     sdk.TokensFromConsensusPower(1000, sdk.DefaultPowerReduction),
 		StakingTokens:     sdk.TokensFromConsensusPower(500, sdk.DefaultPowerReduction),
 		BondedTokens:      sdk.TokensFromConsensusPower(100, sdk.DefaultPowerReduction),
-		PruningStrategy:   storetypes.PruningOptionNothing,
+		PruningStrategy:   pruningTypes.PruningOptionNothing,
 		CleanupDir:        true,
 		SigningAlgo:       string(hd.Secp256k1Type),
 		KeyringOptions:    []keyring.Option{},
