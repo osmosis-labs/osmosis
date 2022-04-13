193f83299859e2e9da972558d9d5086346ed5fdb
alpo <yukseloglua@berkeley.edu>
2022-04-13 09:19:43 -0700

    run linter

--- app/apptesting/test_suite.go
+++ app/apptesting/test_suite.go
@@ -222,12 +222,12 @@ func (keeperTestHelper *KeeperTestHelper) LockTokens(addr sdk.AccAddress, coins 
 }
 
 func (keeperTestHelper *KeeperTestHelper) BuildTx(
-  txBuilder client.TxBuilder,
-  msgs []sdk.Msg,
-  sigV2 signing.SignatureV2,
-  memo string, txFee sdk.Coins,
-  gasLimit uint64,
-) (authsigning.Tx) {
+	txBuilder client.TxBuilder,
+	msgs []sdk.Msg,
+	sigV2 signing.SignatureV2,
+	memo string, txFee sdk.Coins,
+	gasLimit uint64,
+) authsigning.Tx {
 	err := txBuilder.SetMsgs(msgs[0])
 	keeperTestHelper.Require().NoError(err)
 
