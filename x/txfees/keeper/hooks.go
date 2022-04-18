ab34ca3d91179e714af8865f1901882d4fc2568c
alpo <yukseloglua@berkeley.edu>
2022-04-18 08:33:41 -0700

    remove panic errors at epoch end and remove potential for unbounded denom loop

--- x/txfees/keeper/hooks.go
+++ x/txfees/keeper/hooks.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	"github.com/osmosis-labs/osmosis/v7/osmoutils"
 	epochstypes "github.com/osmosis-labs/osmosis/v7/x/epochs/types"
 	txfeestypes "github.com/osmosis-labs/osmosis/v7/x/txfees/types"
 )
@@ -12,35 +13,37 @@ func (k Keeper) BeforeEpochStart(ctx sdk.Context, epochIdentifier string, epochN
 // at the end of each epoch, swap all non-OSMO fees into OSMO and transfer to fee module account
 func (k Keeper) AfterEpochEnd(ctx sdk.Context, epochIdentifier string, epochNumber int64) {
 	nonNativeFeeAddr := k.accountKeeper.GetModuleAddress(txfeestypes.NonNativeFeeCollectorName)
-	nonNativeBalances := k.bankKeeper.GetAllBalances(ctx, nonNativeFeeAddr)
+	// nonNativeBalances := k.bankKeeper.GetAllBalances(ctx, nonNativeFeeAddr)
 	baseDenom, _ := k.GetBaseDenom(ctx)
+	feeTokens := k.GetFeeTokens(ctx)
 
-	for _, coin := range nonNativeBalances {
+	for _, coin := range feeTokens {
 		if coin.Denom == baseDenom {
 			continue
 		}
+		coinBalance := k.bankKeeper.GetBalance(ctx, nonNativeFeeAddr, coin.Denom)
 		feetoken, err := k.GetFeeToken(ctx, coin.Denom)
 		if err != nil {
-			panic(err)
+			break
 		}
 
 		// We allow full slippage. Theres not really an effective way to bound slippage until TWAP's land,
 		// but even then the point is a bit moot.
 		// The only thing that could be done is a costly griefing attack to reduce the amount of osmo given as tx fees.
 		// However the idea of the txfees FeeToken gating is that the pool is sufficiently liquid for that base token.
-		_, err = k.gammKeeper.SwapExactAmountIn(ctx, nonNativeFeeAddr, feetoken.PoolID, coin, baseDenom, sdk.ZeroInt())
+		_, err = k.gammKeeper.SwapExactAmountIn(ctx, nonNativeFeeAddr, feetoken.PoolID, coinBalance, baseDenom, sdk.ZeroInt())
 		if err != nil {
-			panic(err)
+			break
 		}
 	}
 
 	// Get all of the txfee payout denom in the module account
 	nonNativeCoins := sdk.NewCoins(k.bankKeeper.GetBalance(ctx, nonNativeFeeAddr, baseDenom))
 
-	err := k.bankKeeper.SendCoinsFromModuleToModule(ctx, txfeestypes.NonNativeFeeCollectorName, txfeestypes.FeeCollectorName, nonNativeCoins)
-	if err != nil {
-		panic(err)
-	}
+	_ = osmoutils.ApplyFuncIfNoError(ctx, func(cacheCtx sdk.Context) error {
+		err := k.bankKeeper.SendCoinsFromModuleToModule(ctx, txfeestypes.NonNativeFeeCollectorName, txfeestypes.FeeCollectorName, nonNativeCoins)
+		return err
+	})
 }
 
 // Hooks wrapper struct for incentives keeper
