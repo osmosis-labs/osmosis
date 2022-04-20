b70c678df390108429afa9eed831d21203db225f
romelukaku <gundamaster5@gmail.com>
2022-04-20 07:57:14 +0700

    use apptesting.KeeperTestHelper SetupTestApp() in txfees SetupTest()

--- x/txfees/keeper/keeper_test.go
+++ x/txfees/keeper/keeper_test.go
@@ -2,27 +2,18 @@ package keeper_test
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/suite"
 	"github.com/tendermint/tendermint/crypto/ed25519"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
-	"github.com/cosmos/cosmos-sdk/baseapp"
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	"github.com/osmosis-labs/osmosis/v7/app"
 	"github.com/osmosis-labs/osmosis/v7/app/apptesting"
-	"github.com/osmosis-labs/osmosis/v7/x/txfees/keeper"
 	"github.com/osmosis-labs/osmosis/v7/x/txfees/types"
 )
 
 type KeeperTestSuite struct {
 	apptesting.KeeperTestHelper
-
-	clientCtx client.Context
 
 	queryClient types.QueryClient
 }
@@ -34,21 +25,12 @@ var (
 )
 
 func (suite *KeeperTestSuite) SetupTest(isCheckTx bool) {
-	app := app.Setup(isCheckTx)
-	ctx := app.BaseApp.NewContext(isCheckTx, tmproto.Header{Height: 1, ChainID: "osmosis-1", Time: time.Now().UTC()})
-
-	queryHelper := baseapp.NewQueryServerTestHelper(ctx, app.InterfaceRegistry())
-	types.RegisterQueryServer(queryHelper, keeper.NewQuerier(*app.TxFeesKeeper))
-	queryClient := types.NewQueryClient(queryHelper)
-
-	suite.App = app
-	suite.Ctx = ctx
-
-	suite.queryClient = queryClient
+	suite.SetupTestApp()
+	suite.queryClient = types.NewQueryClient(suite.QueryHelper)
 
 	// Mint some assets to the accounts.
 	for _, acc := range []sdk.AccAddress{acc1, acc2, acc3} {
-		err := simapp.FundAccount(suite.App.BankKeeper, suite.Ctx, acc,
+		suite.FundAcc(acc,
 			sdk.NewCoins(
 				sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(10000000000)),
 				sdk.NewCoin("uosmo", sdk.NewInt(100000000000000000)), // Needed for pool creation fee
@@ -56,9 +38,6 @@ func (suite *KeeperTestSuite) SetupTest(isCheckTx bool) {
 				sdk.NewCoin("foo", sdk.NewInt(10000000)),
 				sdk.NewCoin("bar", sdk.NewInt(10000000)),
 			))
-		if err != nil {
-			panic(err)
-		}
 	}
 }
 
