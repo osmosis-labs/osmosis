060b5739d11d60ddd47240e57a80f89c624a0c87
Dev Ojha <dojha@berkeley.edu>
2022-04-20 21:12:04 -0500

    WIP panic recovery

--- x/epochs/types/hooks.go
+++ x/epochs/types/hooks.go
@@ -1,6 +1,10 @@
 package types
 
 import (
+	fmt "fmt"
+	"runtime"
+	"runtime/debug"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -32,4 +36,31 @@ func (h MultiEpochHooks) BeforeEpochStart(ctx sdk.Context, epochIdentifier strin
 	for i := range h {
 		h[i].BeforeEpochStart(ctx, epochIdentifier, epochNumber)
 	}
+}
+
+func panicCatchingEpochHook(
+	ctx sdk.Context,
+	hookFn func(ctx sdk.Context, epochIdentifier string, epochNumber int64),
+	epochIdentifier string,
+	epochNumber int64) {
+
+	defer func() {
+		if recovErr := recover(); recovErr != nil {
+			fmt.Println("Recovering from panic:", recovErr)
+			fmt.Println("Stack Trace:")
+			debug.PrintStack()
+			switch e := recovErr.(type) {
+			case string:
+				ctx.Logger().Error("Recovering from panicrecovered (string) panic:", e)
+			case runtime.Error:
+				fmt.Println("recovered (runtime.Error) panic:", e.Error())
+			case error:
+				fmt.Println("recovered (error) panic:", e.Error())
+			default:
+				fmt.Println("recovered (default) panic:", e)
+			}
+			fmt.Println(string(debug.Stack()))
+			return
+		}
+	}()
 }
