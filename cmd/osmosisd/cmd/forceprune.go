cc22dd3273cabfd3a9fcfc9f4dc27e25de92acec
Jacob Gadikian <jacobgadikian@gmail.com>
2022-04-25 20:01:08 +0700

    fix forceprune

--- cmd/osmosisd/cmd/forceprune.go
+++ cmd/osmosisd/cmd/forceprune.go
@@ -3,39 +3,22 @@ package cmd
 // DONTCOVER
 
 import (
-<<<<<<< HEAD
-	"github.com/spf13/cobra"
-	"fmt"
-	"strconv"
-	
-=======
 	"fmt"
 	"os/exec"
 	"strconv"
 
 	"github.com/spf13/cobra"
 
->>>>>>> origin/v7.x
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/opt"
 	"github.com/syndtr/goleveldb/leveldb/util"
 	tmstore "github.com/tendermint/tendermint/store"
 	tmdb "github.com/tendermint/tm-db"
 
-<<<<<<< HEAD
-	"os/exec"
-
-=======
->>>>>>> origin/v7.x
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/tendermint/tendermint/config"
 )
 
-<<<<<<< HEAD
-var fheight = "full_height"
-var mheight = "min_height"
-const batchMaxSize = 1000
-=======
 const (
 	batchMaxSize      = 1000
 	kValidators       = "validatorsKey:"
@@ -46,34 +29,12 @@ const (
 	defaultFullHeight = "188000"
 	defaultMinHeight  = "1000"
 )
->>>>>>> origin/v7.x
 
 // get cmd to convert any bech32 address to an osmo prefix.
 func forceprune() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "forceprune",
 		Short: "Example osmosisd forceprune -f 188000 -m 1000, which would keep blockchain and state data of last 188000 blocks (approximately 2 weeks) and ABCI responses of last 1000 blocks.",
-<<<<<<< HEAD
-		Long: "Forceprune options prunes and compacts blockstore.db and state.db. One needs to shut down chain before running forceprune. By default it keeps last 188000 blocks (approximately 2 weeks of data) blockstore and state db (validator and consensus information) and 1000 blocks of abci responses from state.db. Everything beyond these heights in blockstore and state.db is pruned. ABCI Responses are stored in index db and so redundant especially if one is running pruned nodes. As a result we are removing ABCI data from state.db aggressively by default. One can override height for blockstore.db and state.db by using -f option and for abci response by using -m option. Example osmosisd forceprune -f 188000 -m 1000.",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			full_height_flag, err := cmd.Flags().GetString(fheight)
-			if err != nil {
-				return err
-			}
-			
-			min_height_flag, err := cmd.Flags().GetString(mheight)
-			if err != nil {
-				return err
-			}
-			
-			clientCtx := client.GetClientContextFromCmd(cmd)
-			conf := config.DefaultConfig()
-			db_path := string(clientCtx.HomeDir) + "/" + conf.DBPath
-			
-			cmdr := exec.Command("osmosisd", "status")
-			err = cmdr.Run()
-			
-=======
 		Long:  "Forceprune options prunes and compacts blockstore.db and state.db. One needs to shut down chain before running forceprune. By default it keeps last 188000 blocks (approximately 2 weeks of data) blockstore and state db (validator and consensus information) and 1000 blocks of abci responses from state.db. Everything beyond these heights in blockstore and state.db is pruned. ABCI Responses are stored in index db and so redundant especially if one is running pruned nodes. As a result we are removing ABCI data from state.db aggressively by default. One can override height for blockstore.db and state.db by using -f option and for abci response by using -m option. Example osmosisd forceprune -f 188000 -m 1000.",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			fullHeightFlag, err := cmd.Flags().GetString(fullHeight)
@@ -93,91 +54,10 @@ func forceprune() *cobra.Command {
 			cmdr := exec.Command("osmosisd", "status")
 			err = cmdr.Run()
 
->>>>>>> origin/v7.x
 			if err == nil {
 				// continue only if throws errror
 				return nil
 			}
-<<<<<<< HEAD
-			
-			full_height, err := strconv.ParseInt(full_height_flag, 10, 64)
-			if err != nil {
-				return err
-			}
-			
-			min_height, err := strconv.ParseInt(min_height_flag, 10, 64)
-      			if err != nil {
-				return err
-			}
-			
-			opts := opt.Options{
-				DisableSeeksCompaction: true,
-			}
-			
-			db_bs, err := tmdb.NewGoLevelDBWithOpts("blockstore", db_path, &opts)
-			if err != nil {
-				return err
-			}
-			
-			bs := tmstore.NewBlockStore(db_bs)
-			start_height := bs.Base()
-			current_height := bs.Height()
-			
-			fmt.Println("Pruning Block Store ...")
-			prunedBlocks, err := bs.PruneBlocks(current_height - full_height)
-			if err != nil {
-				return err
-			}
-			fmt.Println("Pruned Block Store ...", prunedBlocks)
-			db_bs.Close()
-			
-			fmt.Println("Compacting Block Store ...")
-			
-			db, err := leveldb.OpenFile(db_path+"/blockstore.db", &opts)
-			defer db.Close()
-			if err != nil {
-				return err
-			}
-			if err = db.CompactRange(*util.BytesPrefix([]byte{})); err != nil {
-				return err
-			}
-			
-			db, err = leveldb.OpenFile(db_path+"/state.db", &opts)
-			if err != nil {
-				return err
-			}
-			stateDBKeys := []string{"validatorsKey:", "consensusParamsKey:", "abciResponsesKey:"}
-			fmt.Println("Pruning State Store ...")
-			for i, s := range stateDBKeys {
-				fmt.Println(i, s)
-				
-				retain_height := int64(0)
-				if s == "abciResponsesKey:" {
-					retain_height = current_height - min_height
-				} else {
-					retain_height = current_height - full_height
-				}
-				
-				batch := new(leveldb.Batch)
-				curBatchSize := uint64(0)
-				
-				fmt.Println(start_height, current_height, retain_height)
-				
-				for c := start_height; c < retain_height; c++ {
-					batch.Delete([]byte(s + strconv.FormatInt(c, 10)))
-					curBatchSize++
-					
-					if curBatchSize%batchMaxSize == 0 && curBatchSize > 0 {
-						err := db.Write(batch, nil)
-						if err != nil {
-							return err
-						}
-						batch.Reset()
-						batch = new(leveldb.Batch)
-					}
-				}
-				
-=======
 
 			fullHeight, err := strconv.ParseInt(fullHeightFlag, 10, 64)
 			if err != nil {
@@ -291,29 +171,11 @@ func forcepruneStateStore(dbPath string, startHeight, currentHeight, minHeight, 
 			curBatchSize++
 
 			if curBatchSize%batchMaxSize == 0 && curBatchSize > 0 {
->>>>>>> origin/v7.x
 				err := db.Write(batch, nil)
 				if err != nil {
 					return err
 				}
 				batch.Reset()
-<<<<<<< HEAD
-			}
-			
-			fmt.Println("Compacting State Store ...")
-			if err = db.CompactRange(*util.BytesPrefix([]byte{})); err != nil {
-				return err
-			}
-			fmt.Println("Done ...")
-			
-			return nil
-		},
-	}
-	
-	cmd.Flags().StringP(fheight, "f", "188000", "Full height to chop to")
-	cmd.Flags().StringP(mheight, "m", "1000", "Min height for ABCI to chop to")
-	return cmd
-=======
 				batch = new(leveldb.Batch)
 			}
 		}
@@ -330,5 +192,4 @@ func forcepruneStateStore(dbPath string, startHeight, currentHeight, minHeight, 
 		return err
 	}
 	return nil
->>>>>>> origin/v7.x
 }
