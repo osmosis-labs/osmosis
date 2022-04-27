98a1f6eae2c264300e8b2752896f0df3d9b6e15f
alpo <62043214+AlpinYukseloglu@users.noreply.github.com>
2022-04-27 16:31:31 -0700

    Stableswap: Createpool logic (#1312)
    * add CreatePool logic
    
    * formatting
    
    * add relevant function to the message server
    
    * add stableswap to relevant register functions to module.go
    
    * Add error check in x/gamm/keeper/msg_server.go
    
    Co-authored-by: Roman <roman@osmosis.team>
    
    * set initial pool liquidity for CreatePool
    
    * formatting
    
    * add NewStableswapPool function
    
    * update with suggestions from review
    
    Co-authored-by: Roman <roman@osmosis.team>

--- x/gamm/keeper/msg_server.go
+++ x/gamm/keeper/msg_server.go
@@ -7,6 +7,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/osmosis-labs/osmosis/v7/x/gamm/pool-models/balancer"
+	"github.com/osmosis-labs/osmosis/v7/x/gamm/pool-models/stableswap"
 	"github.com/osmosis-labs/osmosis/v7/x/gamm/types"
 )
 
@@ -26,14 +27,29 @@ func NewBalancerMsgServerImpl(keeper *Keeper) balancer.MsgServer {
 	}
 }
 
+func NewStableswapMsgServerImpl(keeper *Keeper) stableswap.MsgServer {
+	return &msgServer{
+		keeper: keeper,
+	}
+}
+
 var (
-	_ types.MsgServer    = msgServer{}
-	_ balancer.MsgServer = msgServer{}
+	_ types.MsgServer      = msgServer{}
+	_ balancer.MsgServer   = msgServer{}
+	_ stableswap.MsgServer = msgServer{}
 )
 
 func (server msgServer) CreateBalancerPool(goCtx context.Context, msg *balancer.MsgCreateBalancerPool) (*balancer.MsgCreateBalancerPoolResponse, error) {
 	poolId, err := server.CreatePool(goCtx, msg)
 	return &balancer.MsgCreateBalancerPoolResponse{PoolID: poolId}, err
+}
+
+func (server msgServer) CreateStableswapPool(goCtx context.Context, msg *stableswap.MsgCreateStableswapPool) (*stableswap.MsgCreateStableswapPoolResponse, error) {
+	poolId, err := server.CreatePool(goCtx, msg)
+	if err != nil {
+		return nil, err
+	}
+	return &stableswap.MsgCreateStableswapPoolResponse{PoolID: poolId}, nil
 }
 
 func (server msgServer) CreatePool(goCtx context.Context, msg types.CreatePoolMsg) (poolId uint64, err error) {
