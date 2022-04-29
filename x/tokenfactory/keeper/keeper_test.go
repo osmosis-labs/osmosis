cd529cfb4cb1fa54e246a99d3ed0bf19c13ab171
Sunny Aggarwal <sunnya97@protonmail.ch>
2022-04-29 00:44:08 -0400

    add back query

--- x/tokenfactory/keeper/keeper_test.go
+++ x/tokenfactory/keeper/keeper_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/suite"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
@@ -28,9 +29,9 @@ func (suite *KeeperTestSuite) SetupTest() {
 
 	suite.app.TokenFactoryKeeper.CreateModuleAccount(suite.ctx)
 
-	// queryHelper := baseapp.NewQueryServerTestHelper(suite.ctx, suite.app.InterfaceRegistry())
-	// types.RegisterQueryServer(queryHelper, suite.app.TokenFactoryKeeper)
-	// suite.queryClient = types.NewQueryClient(queryHelper)
+	queryHelper := baseapp.NewQueryServerTestHelper(suite.ctx, suite.app.InterfaceRegistry())
+	types.RegisterQueryServer(queryHelper, suite.app.TokenFactoryKeeper)
+	suite.queryClient = types.NewQueryClient(queryHelper)
 }
 
 func (suite *KeeperTestSuite) SetupTestWithLevelDb() {
--- x/tokenfactory/module.go
+++ x/tokenfactory/module.go
@@ -141,7 +141,7 @@ func (am AppModule) LegacyQuerierHandler(legacyQuerierCdc *codec.LegacyAmino) sd
 // module-specific GRPC queries.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
-	// types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
+	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 }
 
 // RegisterInvariants registers the capability module's invariants.
