ebe002c4d1df65a65c2378d52771ff5997a6460c
Adam Tucker <adamleetucker@outlook.com>
2022-05-02 21:19:26 +0000

    delete to readd

--- tests/e2e/scripts/osmosis_upgrade.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-osmosisd tx gov submit-proposal software-upgrade v8 --title "v8 upgrade" --description "v8 upgrade proposal" --upgrade-height 75 --upgrade-info "" --chain-id $1 --from val -b block --yes --keyring-backend test
-osmosisd tx gov deposit 1 10000000stake --from val --chain-id $1 -b block --yes --keyring-backend test
-osmosisd tx gov vote 1 yes --from val --chain-id $1 -b block --yes --keyring-backend test
