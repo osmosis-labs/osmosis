b71507c04b970780013b00b2420290245a238604
Dev Ojha <ValarDragon@users.noreply.github.com>
2022-05-04 00:00:52 -0500

    Add structs for Upgrades and Forks structs (#1379)
    Closes: #1375 
    
    ## What is the purpose of the change
    
    Simplifies a lot of the upgrades management logic / boilerplate. The goal is that every new upgrade should from the app.go level, just require a one line update. And all the complexity would otherwise be held to within each upgrades module.
    
    This PR is marked as draft until Changelog update + docs update to the `app/upgrades` README.
    
    ## Brief change log
    
    * Introduces an Upgrade and Fork struct
    * Adapt existing upgrades to use it
    
    ## Testing and Verifying
    
    This should be covered by the existing test for the v4 upgrade, which tests that upgrade handler execution happens.
    
    ## Documentation and Release Note
    
      - Does this pull request introduce a new feature or user-facing behavior changes? no
      - Is a relevant changelog entry added to the `Unreleased` section in `CHANGELOG.md`? Needs to be Done
      - How is the feature or change documented? (not applicable   /   specification (`x/<module>/spec/`)  /  [Osmosis docs repo](https://github.com/osmosis-labs/docs)   /   not documented)

---
+++ app/upgrades/types.go
@@ -0,0 +1,40 @@
+package upgrades
+
+import (
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	store "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	"github.com/osmosis-labs/osmosis/v7/app/keepers"
+)
+
+// Upgrade defines a struct containing necessary fields that a SoftwareUpgradeProposal
+// must have written, in order for the state migration to go smoothly.
+// An upgrade must implement this struct, and then set it in the app.go.
+// The app.go will then define the handler.
+type Upgrade struct {
+	// Upgrade version name, for the upgrade handler, e.g. `v7`
+	UpgradeName string
+
+	// Function that creates an upgrade handler
+	CreateUpgradeHandler func(mm *module.Manager, configurator module.Configurator, keepers *keepers.AppKeepers) upgradetypes.UpgradeHandler
+	// Store upgrades, should be used for any new modules introduced, new modules deleted, or store names renamed.
+	StoreUpgrades store.StoreUpgrades
+}
+
+// Fork defines a struct containing the requisite fields for a non-software upgrade proposal
+// Hard Fork at a given height to implement.
+// There is one time code that can be added for the start of the Fork, in `BeginForkLogic`.
+// Any other change in the code should be height-gated, if the goal is to have old and new binaries
+// to be compatible prior to the upgrade height.
+type Fork struct {
+	// Upgrade version name, for the upgrade handler, e.g. `v7`
+	UpgradeName string
+	// height the upgrade occurs at
+	UpgradeHeight int64
+
+	// Function that runs some custom state transition code at the beginning of a fork.
+	BeginForkLogic func(ctx sdk.Context, keepers *keepers.AppKeepers)
+}
