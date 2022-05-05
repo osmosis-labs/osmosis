1ec3ed997de806ba175da407a7b31f49de02d895
Brendan Wong <35351983+LunrEclipse@users.noreply.github.com>
2022-05-04 17:01:38 -0700

    Mapping for tokens
    Created a Mapping of Token Denomination (String) to Array of Poolids containing that token for arg team

--- x/arbitrage-solver/keeper/endblock.go
+++ x/arbitrage-solver/keeper/endblock.go
@@ -4,6 +4,34 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 
 func (k Keeper) EndBlockLogic(ctx sdk.Context) error {
 	// Run all end block logic we want here
+	tokenToPool := make(map[string] []uint64)
+
+	for i := 0; i < 100; i++ {
+		pool, poolErr := k.gammKeeper.GetPool(ctx, uint64(i))
+		if poolErr != nil {
+			return poolErr
+		}
+		assetsInPool := k.gammKeeper.GetTotalLiquidity(ctx)
+
+		curToken := assetsInPool.GetDenomByIndex(0)
+		if tokenToPool[curToken] == nil {
+			tokenToPool[curToken] = []uint64{uint64(i)}
+		} else {
+			curArray := tokenToPool[curToken]
+			curArray = append(curArray, uint64(i))
+			tokenToPool[curToken] = curArray
+		}
+
+		curToken = assetsInPool.GetDenomByIndex(1)
+		if tokenToPool[curToken] == nil {
+			tokenToPool[curToken] = []uint64{uint64(i)}
+		} else {
+			curArray := tokenToPool[curToken]
+			curArray = append(curArray, uint64(i))
+			tokenToPool[curToken] = curArray
+		}
+		_ = pool
+	}
 
 	// Lets say I want to:
 	// * swap 1 osmo through pool 1, to atom
@@ -44,6 +72,7 @@ func (k Keeper) EndBlockLogic(ctx sdk.Context) error {
 
 	// Hack to get around the golang required variables
 	_ = priceDifference
+	_ = tokenToPool
 
 	return nil
 }
