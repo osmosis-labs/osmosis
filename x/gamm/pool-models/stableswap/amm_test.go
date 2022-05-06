b07f76350181edaee6e76d815c17ac04f9166617
alpo <62043214+AlpinYukseloglu@users.noreply.github.com>
2022-05-06 10:20:10 -0700

    Update x/gamm/pool-models/stableswap/amm_test.go
    Co-authored-by: Dev Ojha <ValarDragon@users.noreply.github.com>

--- x/gamm/pool-models/stableswap/amm_test.go
+++ x/gamm/pool-models/stableswap/amm_test.go
@@ -49,7 +49,7 @@ func TestCFMMInvariantTwoAssets(t *testing.T) {
 		k1 := cfmmConstant(test.xReserve.Sub(xOut), test.yReserve.Add(test.yIn))
 		decApproxEq(t, k0, k1, kErrTolerance)
 
-		// using multi-asset cfmm
+		// using multi-asset cfmm (should be equivalent with u = 1, w = 0)
 		k2 := cfmmConstantMulti(test.xReserve, test.yReserve, sdk.OneDec(), sdk.ZeroDec())
 		xOut2 := solveCfmmMulti(test.xReserve, test.yReserve, sdk.ZeroDec(), test.yIn)
 		fmt.Println(xOut2)
