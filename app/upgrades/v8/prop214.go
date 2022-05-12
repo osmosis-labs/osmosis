4d9933bd90e756edc46e1e35c415f81f59a5c215
mattverse <mattpark1028@gmail.com>
2022-05-12 19:12:58 +0900

    Change swap fee via upgrade handler

---
+++ app/upgrades/v8/prop214.go
@@ -0,0 +1,25 @@
+package v8
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	gammkeeper "github.com/osmosis-labs/osmosis/v7/x/gamm/keeper"
+	"github.com/osmosis-labs/osmosis/v7/x/gamm/pool-models/balancer"
+)
+
+func Prop214(ctx sdk.Context, gamm *gammkeeper.Keeper) {
+	poolId := 1
+	pool, err := gamm.GetPoolAndPoke(ctx, uint64(poolId))
+	if err != nil {
+		panic(err)
+	}
+
+	balancerPool, ok := pool.(*balancer.Pool)
+	if !ok {
+		panic(ok)
+	}
+
+	balancerPool.PoolParams.SwapFee = sdk.MustNewDecFromStr("0.002")
+
+	gamm.SetPool(ctx, balancerPool)
+}
