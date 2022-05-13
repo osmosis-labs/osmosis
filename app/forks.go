100b57cc80f1095a5572e3af6b0aaf85c5f6d864
mattverse <mattpark1028@gmail.com>
2022-05-13 21:38:48 +0900

    Add v8 fork logic

--- app/forks.go
+++ app/forks.go
@@ -2,8 +2,10 @@ package app
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	v3 "github.com/osmosis-labs/osmosis/v7/app/upgrades/v3"
 	v6 "github.com/osmosis-labs/osmosis/v7/app/upgrades/v6"
+	v8 "github.com/osmosis-labs/osmosis/v7/app/upgrades/v8"
 )
 
 // BeginBlockForks is intended to be ran in
@@ -13,6 +15,8 @@ func BeginBlockForks(ctx sdk.Context, app *OsmosisApp) {
 		v3.RunForkLogic(ctx, app.GovKeeper, app.StakingKeeper)
 	case v6.UpgradeHeight:
 		v6.RunForkLogic(ctx)
+	case v8.UpgradeHeight:
+		v8.RunForkLogic(ctx, app.SuperfluidKeeper)
 	default:
 		// do nothing
 		return
--- app/upgrades/v8/constants.go
+++ app/upgrades/v8/constants.go
@@ -2,9 +2,10 @@ package v8
 
 const (
 	// UpgradeName defines the on-chain upgrade name for the Osmosis v3 upgrade.
-	UpgradeName = "v3"
+	UpgradeName = "v8"
 
 	// UpgradeHeight defines the block height at which the Osmosis v3 upgrade is
 	// triggered.
-	UpgradeHeight = 712_000
+	// TODO: Choose upgrade height
+	UpgradeHeight = 100_000
 )
