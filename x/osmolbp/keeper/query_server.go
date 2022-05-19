97a894293b41e1f52a902438949600fc4b514f3d
Robert Zaremba <robert@zaremba.ch>
2022-05-19 13:11:53 +0200

    osmolbp dummy query implementation

--- x/osmolbp/keeper/query_server.go
+++ x/osmolbp/keeper/query_server.go
@@ -9,3 +9,11 @@ import (
 func (k Keeper) LBPs(ctx context.Context, q *api.QueryLBPs) (*api.QueryLBPsResponse, error) {
 	return nil, nil
 }
+
+func (k Keeper) LBP(ctx context.Context, q *api.QueryLBP) (*api.QueryLBPResponse, error) {
+	return nil, nil
+}
+
+func (k Keeper) UserPosition(ctx context.Context, q *api.QueryLBP) (*api.QueryLBPResponse, error) {
+	return nil, nil
+}
