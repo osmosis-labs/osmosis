f8c9898484c3c7be45ee11be26e9f2ce180badb3
Adam Tucker <adamleetucker@outlook.com>
2022-05-19 13:45:23 -0500

    Update tx.go

--- x/txfees/client/cli/tx.go
+++ x/txfees/client/cli/tx.go
@@ -8,6 +8,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/gov/client/cli"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
