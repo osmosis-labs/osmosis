bbdf356dda9d6b4e8ead44e129de8dfbc821fee6
romelukaku <gundamaster5@gmail.com>
2022-05-20 01:29:47 +0700

    some change in lp_test

--- x/gamm/pool-models/internal/cfmm_common/lp_test.go
+++ x/gamm/pool-models/internal/cfmm_common/lp_test.go
@@ -2,6 +2,7 @@ package cfmm_common_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
@@ -13,105 +14,125 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// a helper function used to multiply coins
+func mulCoins(coins sdk.Coins, multiplier sdk.Dec) sdk.Coins {
+	outCoins := sdk.Coins{}
+	for _, coin := range coins {
+		outCoin := sdk.NewCoin(coin.Denom, multiplier.MulInt(coin.Amount).TruncateInt())
+		if !outCoin.Amount.IsZero() {
+			outCoins = append(outCoins, outCoin)
+		}
+	}
+	return outCoins
+}
+
 func TestCalcExitPool(t *testing.T) {
 	var (
-		two_asset_pool = &stableswap.Pool{
-			TotalShares:   sdk.NewCoin(gammtypes.GetPoolShareDenom(1), gammtypes.InitPoolSharesSupply),
-			PoolLiquidity: sdk.NewCoins(sdk.NewInt64Coin("foo", 1000000000), sdk.NewInt64Coin("bar", 1000000000)),
-		}
+		emptyContext = sdk.Context{}
 
-		three_asset_pool = &balancer.Pool{
-			TotalShares: sdk.NewCoin(gammtypes.GetPoolShareDenom(1), sdk.NewIntFromUint64(6000000000)),
-			PoolAssets: []balancer.PoolAsset{
-				{Token: sdk.NewInt64Coin("foo", 2000000000)},
-				{Token: sdk.NewInt64Coin("bar", 3000000000)},
-				{Token: sdk.NewInt64Coin("baz", 4000000000)},
+		// create these pools used for testing
+		twoAssetPool, err1 = stableswap.NewStableswapPool(
+			1,
+			stableswap.PoolParams{ExitFee: sdk.ZeroDec()},
+			sdk.NewCoins(sdk.NewInt64Coin("foo", 1000000000),
+				sdk.NewInt64Coin("bar", 1000000000)),
+			"",
+			time.Now(),
+		)
+		threeAssetPool, err2 = balancer.NewBalancerPool(
+			1,
+			balancer.PoolParams{SwapFee: sdk.ZeroDec(), ExitFee: sdk.ZeroDec()},
+			[]balancer.PoolAsset{
+				{Token: sdk.NewInt64Coin("foo", 2000000000), Weight: sdk.NewIntFromUint64(5)},
+				{Token: sdk.NewInt64Coin("bar", 3000000000), Weight: sdk.NewIntFromUint64(5)},
+				{Token: sdk.NewInt64Coin("baz", 4000000000), Weight: sdk.NewIntFromUint64(5)},
 			},
-		}
+			"",
+			time.Now(),
+		)
+		twoAssetPoolWithExitFee, err3 = stableswap.NewStableswapPool(
+			1,
+			stableswap.PoolParams{ExitFee: sdk.MustNewDecFromStr("0.0001")},
+			sdk.NewCoins(sdk.NewInt64Coin("foo", 1000000000),
+				sdk.NewInt64Coin("bar", 1000000000)),
+			"",
+			time.Now(),
+		)
+		threeAssetPoolWithExitFee, err4 = balancer.NewBalancerPool(
+			1,
+			balancer.PoolParams{SwapFee: sdk.ZeroDec(), ExitFee: sdk.MustNewDecFromStr("0.0002")},
+			[]balancer.PoolAsset{
+				{Token: sdk.NewInt64Coin("foo", 2000000000), Weight: sdk.NewIntFromUint64(5)},
+				{Token: sdk.NewInt64Coin("bar", 3000000000), Weight: sdk.NewIntFromUint64(5)},
+				{Token: sdk.NewInt64Coin("baz", 4000000000), Weight: sdk.NewIntFromUint64(5)},
+			},
+			"",
+			time.Now(),
+		)
 	)
+
+	// make sure there're no error creating those pools
+	require.NoError(t, err1)
+	require.NoError(t, err2)
+	require.NoError(t, err3)
+	require.NoError(t, err4)
 
 	tests := []struct {
 		name          string
 		pool          gammtypes.PoolI
 		exitingShares sdk.Int
-		exitFee       sdk.Dec
-		expExitCoins  sdk.Coins
 		expError      bool
 	}{
 		{
-			name:          "two-asset pool, zero exiting shares",
-			pool:          two_asset_pool,
-			exitingShares: sdk.NewIntFromUint64(0),
-			exitFee:       sdk.ZeroDec(),
-			expExitCoins:  sdk.Coins{},
-			expError:      false,
-		},
-		{
-			name:          "three-asset pool, zero exiting shares",
-			pool:          three_asset_pool,
-			exitingShares: sdk.NewIntFromUint64(0),
-			exitFee:       sdk.ZeroDec(),
-			expExitCoins:  sdk.Coins{},
-			expError:      false,
-		},
-		{
 			name:          "two-asset pool, exiting shares grater than total shares",
-			pool:          two_asset_pool,
-			exitingShares: gammtypes.InitPoolSharesSupply.AddRaw(1),
-			exitFee:       sdk.ZeroDec(),
-			expExitCoins:  sdk.Coins{},
+			pool:          &twoAssetPool,
+			exitingShares: twoAssetPool.GetTotalShares().AddRaw(1),
 			expError:      true,
 		},
 		{
 			name:          "three-asset pool, exiting shares grater than total shares",
-			pool:          three_asset_pool,
-			exitingShares: sdk.NewIntFromUint64(7000000000),
-			exitFee:       sdk.ZeroDec(),
-			expExitCoins:  sdk.Coins{},
+			pool:          &threeAssetPool,
+			exitingShares: threeAssetPool.GetTotalShares().AddRaw(1),
 			expError:      true,
 		},
 		{
 			name:          "two-asset pool, valid exiting shares",
-			pool:          two_asset_pool,
-			exitingShares: gammtypes.InitPoolSharesSupply.QuoRaw(2),
-			exitFee:       sdk.ZeroDec(),
-			expExitCoins:  sdk.Coins{sdk.NewInt64Coin("foo", 500000000), sdk.NewInt64Coin("bar", 500000000)},
+			pool:          &twoAssetPool,
+			exitingShares: twoAssetPool.GetTotalShares().QuoRaw(2),
 			expError:      false,
 		},
 		{
 			name:          "three-asset pool, valid exiting shares",
-			pool:          three_asset_pool,
-			exitingShares: sdk.NewIntFromUint64(3000000000),
-			exitFee:       sdk.ZeroDec(),
-			expExitCoins:  sdk.Coins{sdk.NewInt64Coin("foo", 1000000000), sdk.NewInt64Coin("bar", 1500000000), sdk.NewInt64Coin("baz", 2000000000)},
+			pool:          &threeAssetPool,
+			exitingShares: sdk.NewIntFromUint64(3000000000000),
 			expError:      false,
 		},
 		{
 			name:          "two-asset pool with exit fee, valid exiting shares",
-			pool:          two_asset_pool,
-			exitingShares: gammtypes.InitPoolSharesSupply.QuoRaw(2),
-			exitFee:       sdk.MustNewDecFromStr("0.0002"),
-			expExitCoins:  sdk.Coins{sdk.NewInt64Coin("foo", 499900000), sdk.NewInt64Coin("bar", 499900000)},
+			pool:          &twoAssetPoolWithExitFee,
+			exitingShares: twoAssetPoolWithExitFee.GetTotalShares().QuoRaw(2),
 			expError:      false,
 		},
 		{
 			name:          "three-asset pool with exit fee, valid exiting shares",
-			pool:          three_asset_pool,
-			exitingShares: sdk.NewIntFromUint64(3000000000),
-			exitFee:       sdk.MustNewDecFromStr("0.0001"),
-			expExitCoins:  sdk.Coins{sdk.NewInt64Coin("foo", 999900000), sdk.NewInt64Coin("bar", 1499850000), sdk.NewInt64Coin("baz", 1999800000)},
+			pool:          &threeAssetPoolWithExitFee,
+			exitingShares: sdk.NewIntFromUint64(7000000000000),
 			expError:      false,
 		},
 	}
 
 	for _, test := range tests {
-		// using empty context since it won't be used
-		exitCoins, err := cfmm_common.CalcExitPool(sdk.Context{}, test.pool, test.exitingShares, test.exitFee)
+		// using empty context since the context won't be used anyway
+		exitFee := test.pool.GetExitFee(emptyContext)
+		exitCoins, err := cfmm_common.CalcExitPool(emptyContext, test.pool, test.exitingShares, exitFee)
 		if test.expError {
 			require.Error(t, err, "test: %v", test.name)
 		} else {
 			require.NoError(t, err, "test: %v", test.name)
-			require.Equal(t, test.expExitCoins.Sort().String(), exitCoins.Sort().String(), "test: %v", test.name)
+
+			// exitCoins = ( (1 - exitFee) * exitingShares / poolTotalShares ) * poolTotalLiquidity
+			expExitCoins := mulCoins(test.pool.GetTotalPoolLiquidity(emptyContext), (sdk.OneDec().Sub(exitFee)).MulInt(test.exitingShares).QuoInt(test.pool.GetTotalShares()))
+			require.Equal(t, expExitCoins.Sort().String(), exitCoins.Sort().String(), "test: %v", test.name)
 		}
 	}
 }
