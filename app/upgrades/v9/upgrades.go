c7844cde5745b7e144313958930e6974bc52ba74
Dev Ojha <dojha@berkeley.edu>
2022-05-24 03:33:36 +0200

    Fill in all the message types

--- app/upgrades/v9/upgrades.go
+++ app/upgrades/v9/upgrades.go
@@ -3,7 +3,14 @@ package v9
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	gammtypes "github.com/osmosis-labs/osmosis/v7/x/gamm/types"
 
 	ica "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts"
 	icacontrollertypes "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/controller/types"
@@ -30,8 +37,30 @@ func CreateUpgradeHandler(
 
 		// create ICS27 Host submodule params
 		hostParams := icahosttypes.Params{
-			HostEnabled:   true,
-			AllowMessages: []string{"/cosmos.bank.v1beta1.MsgSend"},
+			HostEnabled: true,
+			AllowMessages: []string{
+				sdk.MsgTypeURL(&banktypes.MsgSend{}),
+				sdk.MsgTypeURL(&stakingtypes.MsgDelegate{}),
+				sdk.MsgTypeURL(&stakingtypes.MsgBeginRedelegate{}),
+				sdk.MsgTypeURL(&stakingtypes.MsgCreateValidator{}),
+				sdk.MsgTypeURL(&stakingtypes.MsgEditValidator{}),
+				sdk.MsgTypeURL(&distrtypes.MsgWithdrawDelegatorReward{}),
+				sdk.MsgTypeURL(&distrtypes.MsgSetWithdrawAddress{}),
+				sdk.MsgTypeURL(&distrtypes.MsgWithdrawValidatorCommission{}),
+				sdk.MsgTypeURL(&distrtypes.MsgFundCommunityPool{}),
+				sdk.MsgTypeURL(&govtypes.MsgVote{}),
+				sdk.MsgTypeURL(&authz.MsgExec{}),
+				sdk.MsgTypeURL(&authz.MsgGrant{}),
+				sdk.MsgTypeURL(&authz.MsgRevoke{}),
+				sdk.MsgTypeURL(&gammtypes.MsgJoinPool{}),
+				sdk.MsgTypeURL(&gammtypes.MsgExitPool{}),
+				sdk.MsgTypeURL(&gammtypes.MsgSwapExactAmountIn{}),
+				sdk.MsgTypeURL(&gammtypes.MsgSwapExactAmountOut{}),
+				sdk.MsgTypeURL(&gammtypes.MsgJoinSwapExternAmountIn{}),
+				sdk.MsgTypeURL(&gammtypes.MsgJoinSwapShareAmountOut{}),
+				sdk.MsgTypeURL(&gammtypes.MsgExitSwapExternAmountOut{}),
+				sdk.MsgTypeURL(&gammtypes.MsgExitSwapShareAmountIn{}),
+			},
 		}
 
 		// initialize ICS27 module
