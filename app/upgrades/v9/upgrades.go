39621ddcba5d4350ef5b6cc6ad67539b1d5ddccd
Roman <ackhtariev@gmail.com>
2022-05-24 19:31:01 -0400

    update upgrade handler to set the app version

--- app/upgrades/v9/upgrades.go
+++ app/upgrades/v9/upgrades.go
@@ -8,12 +8,19 @@ import (
 	"github.com/osmosis-labs/osmosis/v7/app/keepers"
 )
 
+const preUpgradeAppVersion = 8
+
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
 	keepers *keepers.AppKeepers,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		// We set the app version to pre-upgrade because it will be incremented by one
+		// After the upgrade is applied by the handler.
+		if err := keepers.UpgradeKeeper.SetAppVersion(ctx, preUpgradeAppVersion); err != nil {
+			return nil, err
+		}
 		ExecuteProp214(ctx, keepers.GAMMKeeper)
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
