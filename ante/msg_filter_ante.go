25ed80c65c29fb9ada6e4a7ee68be8afdd08db83
Dev Ojha <dojha@berkeley.edu>
2022-05-24 19:15:38 +0200

    Add msg_filter_ante_handler

---
+++ ante/msg_filter_ante.go
@@ -0,0 +1,35 @@
+package ante
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	ibcchanneltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
+)
+
+// MsgFilterDecorator defines an AnteHandler decorator for the v8 upgrade that
+// provide height-gated message filtering acceptance.
+type MsgFilterDecorator struct{}
+
+// AnteHandle performs an AnteHandler check that returns an error if the tx contains a message
+// that is blocked.
+// Right now, we block MsgTimeoutOnClose due to incorrect behavior that could occur if a packet is re-enabled.
+func (mfd MsgFilterDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
+	if hasInvalidMsgs(tx.GetMsgs()) {
+		currHeight := ctx.BlockHeight()
+		return ctx, fmt.Errorf("tx contains unsupported message types at height %d", currHeight)
+	}
+
+	return next(ctx, tx, simulate)
+}
+
+func hasInvalidMsgs(msgs []sdk.Msg) bool {
+	for _, msg := range msgs {
+		switch msg.(type) {
+		case *ibcchanneltypes.MsgTimeoutOnClose:
+			return true
+		}
+	}
+
+	return false
+}
---
+++ ante/msg_filter_ante_test.go
@@ -0,0 +1,70 @@
+package ante_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/stretchr/testify/require"
+
+	ibcchanneltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
+
+	"github.com/osmosis-labs/osmosis/v7/ante"
+	"github.com/osmosis-labs/osmosis/v7/app"
+	v8 "github.com/osmosis-labs/osmosis/v7/app/upgrades/v8"
+)
+
+func noOpAnteDecorator() sdk.AnteHandler {
+	return func(ctx sdk.Context, _ sdk.Tx, _ bool) (sdk.Context, error) {
+		return ctx, nil
+	}
+}
+
+func TestMsgFilterDecorator(t *testing.T) {
+	handler := ante.MsgFilterDecorator{}
+	txCfg := app.MakeEncodingConfig().TxConfig
+
+	addr1 := sdk.AccAddress([]byte("addr1_______________"))
+	addr2 := sdk.AccAddress([]byte("addr2_______________"))
+
+	testCases := []struct {
+		name      string
+		ctx       sdk.Context
+		msgs      []sdk.Msg
+		expectErr bool
+	}{
+		{
+			name: "valid tx",
+			ctx:  sdk.Context{}.WithBlockHeight(v8.UpgradeHeight - 1),
+			msgs: []sdk.Msg{
+				banktypes.NewMsgSend(addr1, addr2, sdk.NewCoins(sdk.NewInt64Coin("foo", 5))),
+			},
+			expectErr: false,
+		},
+		{
+			name: "invalid tx",
+			ctx:  sdk.Context{}.WithBlockHeight(v8.UpgradeHeight),
+			msgs: []sdk.Msg{
+				banktypes.NewMsgSend(addr1, addr2, sdk.NewCoins(sdk.NewInt64Coin("foo", 5))),
+				&ibcchanneltypes.MsgTimeoutOnClose{},
+			},
+			expectErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			txBuilder := txCfg.NewTxBuilder()
+			require.NoError(t, txBuilder.SetMsgs(tc.msgs...))
+
+			_, err := handler.AnteHandle(tc.ctx, txBuilder.GetTx(), false, noOpAnteDecorator())
+			if tc.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
