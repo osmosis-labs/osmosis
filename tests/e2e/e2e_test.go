9488f2b59a8501d8dbe90fc47214b93563b7047c
Roman <roman@osmosis.team>
2022-05-26 19:05:04 -0400

    TestCreatePoolPostUpgrade

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -4,13 +4,14 @@ import (
 	"github.com/osmosis-labs/osmosis/v7/tests/e2e/chain"
 )
 
-func (s *IntegrationTestSuite) TestCreatePool() {
+func (s *IntegrationTestSuite) TestCreatePoolPostUpgrade() {
 	if s.skipUpgrade {
 		s.T().Skip()
 	}
 
 	chainA := s.configurer.GetChainConfig(0).GetChain()
 	s.configurer.CreatePool(chainA.ChainMeta.Id, 0, "pool2A.json")
+	s.configurer.CreatePool(chainA.ChainMeta.Id, 0, "pool2B.json")
 }
 
 func (s *IntegrationTestSuite) TestIBCTokenTransfer() {
