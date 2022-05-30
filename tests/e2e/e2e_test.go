2fdfeaefd2ee4e4ced268bbb25b9c9be0681c7f7
Adam Tucker <adam@osmosis.team>
2022-05-30 15:41:51 -0500

    tests: superfluid delegator voting (#1586)
    * change to osmo no stake
    
    * currently working
    
    * superfluid vote overwrite test
    
    * change docker file
    
    * refactor: e2e setup to be more extensible for state sync (#1565)
    
    Closes: #XXX
    
    This PR refactors e2e test setup to have an abstraction `chainConfig` that has information about which validators should not be run during initialization. This is a first step toward testing state-sync as we want to postpone running some nodes so that we can test nodes "catching up".
    
    Additionally, this PR extracts a separate package `docker` for managing and storing all information related to docker images.
    
    - create a `chainConfig` struct to encapsulate all configurations related to chains in a single abstraction
       * remove global variables `propHeightA`, `propHeightB`, `votingPeriodA`, `votingPeriodB`. Instead, these are part of the `chainConfig` struct
       * `skipRunValidatorIndexes` - this is needed to skip certain validators from being run during setup so that we can test state-sync post-initialization and upgrade
    - remove code duplication for every chain. Instead, always loop over `chainConfig`s so that if we add more chains later, the setup would work out of the box
    
    - tested running both with and without upgrade
    
      - Does this pull request introduce a new feature or user-facing behavior changes? no
      - Is a relevant changelog entry added to the `Unreleased` section in `CHANGELOG.md`? no
      - How is the feature or change documented? not applicable
    
    * add extract oper address
    
    * extract val addr
    
    * new line
    
    * delete no longer needed script
    
    * Delete .bash_history
    
    * add helper functions to declutter
    
    * remove majority boilerplate
    
    * move docker file back to distroless
    
    * no longer use bash to add account
    
    * remove debugging print statements
    
    * Delete .bash_history
    
    * remove extra line in docker file
    
    * Apply suggestions from code review
    
    Co-authored-by: Roman <roman@osmosis.team>
    
    * code review suggestions
    
    * further validator abstractions
    
    Co-authored-by: Roman <roman@osmosis.team>

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -1,14 +1,77 @@
 package e2e
 
 import (
+	"fmt"
+	"strconv"
+	"time"
+
 	"github.com/osmosis-labs/osmosis/v7/tests/e2e/chain"
 )
 
 func (s *IntegrationTestSuite) TestIBCTokenTransfer() {
-	chainA := s.chainConfigs[0].chain
-	chainB := s.chainConfigs[1].chain
-
-	// compare coins of reciever pre and post IBC send
+	chainA := s.chainConfigs[0]
+	chainB := s.chainConfigs[1]
+	// compare coins of receiver pre and post IBC send
 	// diff should only be the amount sent
-	s.sendIBC(chainA, chainB, chainB.Validators[0].PublicAddress, chain.OsmoToken)
+	s.sendIBC(chainA, chainB, chainB.validators[0].validator.PublicAddress, chain.OsmoToken)
+}
+
+func (s *IntegrationTestSuite) TestSuperfluidVoting() {
+	chainA := s.chainConfigs[0]
+	s.submitSuperfluidProposal(chainA, "gamm/pool/1")
+	s.depositProposal(chainA)
+	s.voteProposal(chainA)
+	walletAddr := s.createWallet(chainA, 0, "wallet")
+	// send gamm tokens to validator's other wallet (non self-delegation wallet)
+	s.sendTx(chainA, 0, "100000000000000000000gamm/pool/1", chainA.validators[0].validator.PublicAddress, walletAddr)
+	// lock tokens from validator 0 on chain A
+	s.lockTokens(chainA, 0, "100000000000000000000gamm/pool/1", "240s", "wallet")
+	// superfluid delegate from validator 0 non self-delegation wallet to validator 1 on chain A
+	s.superfluidDelegate(chainA, chainA.validators[1].operatorAddress, "wallet")
+	// create a text prop, deposit and vote yes
+	s.submitTextProposal(chainA, "superfluid vote overwrite test")
+	s.depositProposal(chainA)
+	s.voteProposal(chainA)
+	// set delegator vote to no
+	s.voteNoProposal(chainA, 0, "wallet")
+
+	chainAAPIEndpoint := fmt.Sprintf("http://%s", s.valResources[chainA.meta.Id][0].GetHostPort("1317/tcp"))
+	sfProposalNumber := strconv.Itoa(chainA.latestProposalNumber)
+	s.Require().Eventually(
+		func() bool {
+			noTotal, yesTotal, noWithVetoTotal, abstainTotal, err := s.queryPropTally(chainAAPIEndpoint, sfProposalNumber)
+			if err != nil {
+				return false
+			}
+			if abstainTotal.Int64()+noTotal.Int64()+noWithVetoTotal.Int64()+yesTotal.Int64() <= 0 {
+				return false
+			}
+			return true
+		},
+		1*time.Minute,
+		time.Second,
+		"Osmosis node failed to retrieve prop tally",
+	)
+	noTotal, _, _, _, _ := s.queryPropTally(chainAAPIEndpoint, sfProposalNumber)
+	noTotalFinal, err := strconv.Atoi(noTotal.String())
+	s.Require().NoError(err)
+
+	s.Require().Eventually(
+		func() bool {
+			intAccountBalance, err := s.queryIntermediaryAccount(chainA, chainAAPIEndpoint, "gamm/pool/1", chainA.validators[1].operatorAddress)
+			s.Require().NoError(err)
+			if err != nil {
+				return false
+			}
+			if noTotalFinal != intAccountBalance {
+				fmt.Printf("noTotalFinal %v does not match intAccountBalance %v", noTotalFinal, intAccountBalance)
+				return false
+			}
+			return true
+		},
+		1*time.Minute,
+		time.Second,
+		"superfluid delegation vote overwrite not working as expected",
+	)
+
 }
