9da60dee485955579497dc6ac3f2048e7fea268e
vuong <nguyenvuong1122000@gmail.com>
2022-06-06 23:26:28 +0700

    for mint test

--- x/tokenfactory/keeper/admins_test.go
+++ x/tokenfactory/keeper/admins_test.go
@@ -1,6 +1,8 @@
 package keeper_test
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/osmosis-labs/osmosis/v7/x/tokenfactory/keeper"
@@ -71,4 +73,61 @@ func (suite *KeeperTestSuite) TestAdminMsgs() {
 	})
 	suite.Require().NoError(err)
 	suite.Require().Equal("", queryRes.AuthorityMetadata.Admin)
+}
+
+func (suite *KeeperTestSuite) TestMintDenom() {
+	suite.SetupTest()
+	var (
+		denom     string
+		msgServer types.MsgServer
+		addr0bal  = int64(0)
+	)
+
+	// Create a denom
+	msgServer = keeper.NewMsgServerImpl(*suite.App.TokenFactoryKeeper)
+	res, err := msgServer.CreateDenom(sdk.WrapSDKContext(suite.Ctx), types.NewMsgCreateDenom(suite.TestAccs[0].String(), "bitcoin"))
+	suite.Require().NoError(err)
+	denom = res.GetNewTokenDenom()
+
+	for _, tc := range []struct {
+		desc      string
+		amount    int64
+		mintDenom string
+		valid     bool
+		admin     string
+	}{
+		{
+			desc:      "success case",
+			amount:    10,
+			mintDenom: denom,
+			valid:     true,
+			admin:     suite.TestAccs[0].String(),
+		},
+		{
+			desc:      "denom does not exist",
+			amount:    10,
+			mintDenom: "factory/osmo1t7egva48prqmzl59x5ngv4zx0dtrwewc9m7z44/evmos",
+			valid:     false,
+			admin:     suite.TestAccs[0].String(),
+		},
+		{
+			desc:      "mint is not by the admin",
+			amount:    10,
+			mintDenom: denom,
+			valid:     false,
+			admin:     suite.TestAccs[1].String(),
+		},
+	} {
+		suite.Run(fmt.Sprintf("Case %s", tc.desc), func() {
+			// Test minting to admins own account
+			_, err := msgServer.Mint(sdk.WrapSDKContext(suite.Ctx), types.NewMsgMint(tc.admin, sdk.NewInt64Coin(tc.mintDenom, 10)))
+			addr0bal += 10
+			if tc.valid {
+				suite.Require().NoError(err)
+				suite.Require().True(suite.App.BankKeeper.GetBalance(suite.Ctx, suite.TestAccs[0], denom).Amount.Int64() == addr0bal, suite.App.BankKeeper.GetBalance(suite.Ctx, suite.TestAccs[0], denom))
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
 }
