ce320a64e3ecf44c3115cd78e1b4b45a946d587a
Bradley Lostak <bradleycl98@gmail.com>
2022-06-06 19:00:34 -0700

    add support msg join swap share amount out

--- x/gamm/types/msg_swap_sybil.go
+++ x/gamm/types/msg_swap_sybil.go
@@ -6,7 +6,7 @@ import (
 
 // Sybil Resistant Fee Swap Msg defines a simple interface for getting
 // - poolIds on a swap msg route
-// - token to apply the sybil resistanct fees to
+// - token to apply the sybil resistant fees to
 type SybilResistantFeeSwap interface {
 	TokenDenomsOnPath() []string
 	PoolIdOnPath() []uint64
@@ -19,11 +19,19 @@ var (
 	_ SybilResistantFeeSwap = MsgJoinSwapExternAmountIn{}
 )
 
+func (msg MsgSwapExactAmountOut) PoolIdOnPath() []uint64 {
+	ids := make([]uint64, 0, len(msg.Routes))
+	for i := 0; i < len(msg.Routes); i++ {
+		ids = append(ids, msg.Routes[i].PoolId)
+	}
+	return ids
+}
+
 func (msg MsgSwapExactAmountOut) GetTokenToFee() sdk.Coin {
 	return msg.GetTokenOut()
 }
 
-func (msg MsgSwapExactAmountOut) PoolIdOnPath() []uint64 {
+func (msg MsgSwapExactAmountIn) PoolIdOnPath() []uint64 {
 	ids := make([]uint64, 0, len(msg.Routes))
 	for i := 0; i < len(msg.Routes); i++ {
 		ids = append(ids, msg.Routes[i].PoolId)
@@ -35,24 +43,30 @@ func (msg MsgSwapExactAmountIn) GetTokenToFee() sdk.Coin {
 	return msg.GetTokenIn()
 }
 
-func (msg MsgSwapExactAmountIn) PoolIdOnPath() []uint64 {
-	ids := make([]uint64, 0, len(msg.Routes))
-	for i := 0; i < len(msg.Routes); i++ {
-		ids = append(ids, msg.Routes[i].PoolId)
-	}
-	return ids
+func (msg MsgJoinSwapExternAmountIn) PoolIdOnPath() (path []uint64) {
+	path[0] = msg.PoolId
+	return path
 }
 
 func (msg MsgJoinSwapExternAmountIn) GetTokenToFee() sdk.Coin {
 	return msg.TokenIn
 }
 
-func (msg MsgJoinSwapExternAmountIn) PoolIdOnPath() (path []uint64) {
+func (msg MsgJoinSwapExternAmountIn) TokenDenomsOnPath() (denom []string) {
+	denom[0] = msg.TokenIn.Denom
+	return denom
+}
+
+func (msg MsgJoinSwapShareAmountOut) GetTokenToFee() sdk.Coin {
+	return sdk.NewCoin(msg.TokenInDenom, msg.TokenInMaxAmount)
+}
+
+func (msg MsgJoinSwapShareAmountOut) PoolIdOnPath() (path []uint64) {
 	path[0] = msg.PoolId
 	return path
 }
 
-func (msg MsgJoinSwapExternAmountIn) TokenDenomsOnPath() (denom []string) {
-	denom[0] = msg.TokenIn.Denom
+func (msg MsgJoinSwapShareAmountOut) TokenDenomsOnPath() (denom []string) {
+	denom[0] = msg.TokenInDenom
 	return denom
 }
