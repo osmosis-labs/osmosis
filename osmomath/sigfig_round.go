3941f55100b524ae3c7e37102825352dbe9c63cc
Dev Ojha <dojha@berkeley.edu>
2022-06-07 17:22:44 -0500

    Address PR comment

--- osmomath/sigfig_round.go
+++ osmomath/sigfig_round.go
@@ -6,7 +6,7 @@ var pointOne = sdk.OneDec().QuoInt64(10)
 
 func SigFigRound(d sdk.Dec, tenToSigFig sdk.Int) sdk.Dec {
 	// for d > .1, we do round(d * 10^sigfig) / 10^sigfig
-	// for k, where 10^k*d > 1 && 10^{k-1}*d < .1, we do:
+	// for k, where 10^k*d > .1 && 10^{k-1}*d < .1, we do:
 	// (round(10^k * d * 10^sigfig) / (10^sigfig * 10^k)
 	// take note of floor div, vs normal div
 	k := uint64(0)
@@ -17,10 +17,8 @@ func SigFigRound(d sdk.Dec, tenToSigFig sdk.Int) sdk.Dec {
 	// d * 10^k * 10^sigfig
 	dkSigFig := dTimesK.MulInt(tenToSigFig)
 	numerator := dkSigFig.RoundInt().ToDec()
-	denominator := tenToSigFig
-	if k != 0 {
-		tenToK := sdk.NewInt(10).ToDec().Power(k)
-		denominator = denominator.Mul(tenToK.TruncateInt())
-	}
+
+	tenToK := sdk.NewInt(10).ToDec().Power(k)
+	denominator := tenToSigFig.Mul(tenToK.TruncateInt())
 	return numerator.QuoInt(denominator)
 }
