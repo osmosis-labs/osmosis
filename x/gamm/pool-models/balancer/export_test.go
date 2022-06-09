5bea39e965dfaafe8e023944e546a4c538a54cdf
Roman <ackhtariev@gmail.com>
2022-06-09 16:26:49 -0400

    remove TestCalcSingleAssetJoin

--- x/gamm/pool-models/balancer/export_test.go
+++ x/gamm/pool-models/balancer/export_test.go
@@ -12,10 +12,6 @@ var (
 	UpdateIntermediaryPoolAssets        = updateIntermediaryPoolAssets
 )
 
-func (p *Pool) CalcSingleAssetJoin(tokenIn sdk.Coin, swapFee sdk.Dec, tokenInPoolAsset PoolAsset, totalShares sdk.Int) (numShares sdk.Int, err error) {
-	return p.calcSingleAssetJoin(tokenIn, swapFee, tokenInPoolAsset, totalShares)
-}
-
 func (p *Pool) CalcJoinSingleAssetTokensIn(tokensIn sdk.Coins, totalSharesSoFar sdk.Int, poolAssetsByDenom map[string]PoolAsset, swapFee sdk.Dec) (sdk.Int, sdk.Coins, error) {
 	return p.calcJoinSingleAssetTokensIn(tokensIn, totalSharesSoFar, poolAssetsByDenom, swapFee)
 }
