0cf3e6e149ad719faf16a165de0cca0dd9b0286b
Roman <ackhtariev@gmail.com>
2022-06-09 16:22:41 -0400

    test: calcJoinSingleAssetTokensIn; validated by Wolfram

--- x/gamm/pool-models/balancer/export_test.go
+++ x/gamm/pool-models/balancer/export_test.go
@@ -1,6 +1,12 @@
 package balancer
 
+import sdk "github.com/cosmos/cosmos-sdk/types"
+
 var (
 	CalcPoolSharesOutGivenSingleAssetIn = calcPoolSharesOutGivenSingleAssetIn
 	CalcSingleAssetInGivenPoolSharesOut = calcSingleAssetInGivenPoolSharesOut
 )
+
+func (p *Pool) CalcSingleAssetJoin(tokenIn sdk.Coin, swapFee sdk.Dec, tokenInPoolAsset PoolAsset, totalShares sdk.Int) (numShares sdk.Int, err error) {
+	return p.calcSingleAssetJoin(tokenIn, swapFee, tokenInPoolAsset, totalShares)
+}
