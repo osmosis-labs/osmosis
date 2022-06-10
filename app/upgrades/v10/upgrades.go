bf2325175870bc69eb8bf2b8d36f1a195c8ada92
Dev Ojha <dojha@berkeley.edu>
2022-06-09 23:47:52 -0500

    Try adding test

--- app/upgrades/v10/upgrades.go
+++ app/upgrades/v10/upgrades.go
@@ -16,17 +16,19 @@ func CreateUpgradeHandler(
 	keepers *keepers.AppKeepers,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		// TODO{Irregular state change}
+		AgreedUponIrregularStateChange(ctx, keepers)
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
 }
 
-// Validators by choosing this binary and through explicit off-chain signalling
-// have chosen the approach of doing an irregular state change.
-// The change being, transfering all liquid funds from consenting addresses to
-// to the recovery address.
-// This is done with cryptographic approval of this action.
-func AgreedUponIrregularStateChange(ctx sdk.Context, keepers *keepers.AppKeepers) {
+type TransferFromAddress struct {
+	Addr sdk.AccAddress
+	// proof string
+}
+
+var TransferFromAddresses = []TransferFromAddress{}
+
+func init() {
 	// (1) osmo1hq8tlgq0kqz9e56532zghdhz7g8gtjymdltqer
 	// firestake address
 	// https://www.mintscan.io/cosmos/txs/8841476422795A83014F55C2C8915E21635D02011B3BA0CA92020893F5ED30DA
@@ -34,7 +36,7 @@ func AgreedUponIrregularStateChange(ctx sdk.Context, keepers *keepers.AppKeepers
 	if err != nil {
 		panic(err)
 	}
-	ForceTransferAllTokens(ctx, keepers, addr, RecoveryAddress)
+	TransferFromAddresses = append(TransferFromAddresses, TransferFromAddress{addr})
 	// (2) osmo1v44mqmhvtn8cw373xv0hw6npddccnr70lqsk9s
 	// firestake address
 	// https://www.mintscan.io/cosmos/txs/D6187E67AAA4A16A414D41D8FADD35B1B082F50A548989C7FD9B083D2CAEB0C0
@@ -42,21 +44,32 @@ func AgreedUponIrregularStateChange(ctx sdk.Context, keepers *keepers.AppKeepers
 	if err != nil {
 		panic(err)
 	}
-	ForceTransferAllTokens(ctx, keepers, addr, RecoveryAddress)
+	TransferFromAddresses = append(TransferFromAddresses, TransferFromAddress{addr})
 	// (3) osmo10t26acjmemggsahq6uvyucm4tj3z0mhz23ljh2
 	// https://www.mintscan.io/cosmos/txs/0F536D1FAB700363B8A3EE47431BA7E7D80A40F55976C9B80A9F6984C8D0198A
 	addr, err = sdk.AccAddressFromBech32("osmo10t26acjmemggsahq6uvyucm4tj3z0mhz23ljh2")
 	if err != nil {
 		panic(err)
 	}
-	ForceTransferAllTokens(ctx, keepers, addr, RecoveryAddress)
+	TransferFromAddresses = append(TransferFromAddresses, TransferFromAddress{addr})
 	// (4) osmo18qx59wy8s3ytax3e0akna934e86mw776vlzjtq
 	// https://www.mintscan.io/cosmos/txs/0929B33C9F6368F10652D63218DC0E6B5AF8B0F986D041900AFDFC1B5EAC040D
 	addr, err = sdk.AccAddressFromBech32("osmo18qx59wy8s3ytax3e0akna934e86mw776vlzjtq")
 	if err != nil {
 		panic(err)
 	}
-	ForceTransferAllTokens(ctx, keepers, addr, RecoveryAddress)
+	TransferFromAddresses = append(TransferFromAddresses, TransferFromAddress{addr})
+}
+
+// Validators by choosing this binary and through explicit off-chain signalling
+// have chosen the approach of doing an irregular state change.
+// The change being, transferring all liquid funds from consenting addresses to
+// to the recovery address.
+// This is done with cryptographic approval of this action.
+func AgreedUponIrregularStateChange(ctx sdk.Context, keepers *keepers.AppKeepers) {
+	for _, addr := range TransferFromAddresses {
+		ForceTransferAllTokens(ctx, keepers, addr.Addr, RecoveryAddress)
+	}
 }
 
 // ForceTransferAllTokens from address `from` to address `to`.
