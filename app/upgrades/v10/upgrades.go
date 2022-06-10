99cfcbf435e9af8f37202eacbfa95effafffa54b
Dev Ojha <dojha@berkeley.edu>
2022-06-09 20:19:33 -0500

    Add v10 forking logic

--- app/upgrades/v10/upgrades.go
+++ app/upgrades/v10/upgrades.go
@@ -1,4 +1,4 @@
-package v9
+package v10
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -16,19 +16,7 @@ func CreateUpgradeHandler(
 	keepers *keepers.AppKeepers,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		// TODO: This upgrade is blocked on https://github.com/cosmos/cosmos-sdk/pull/11800
-
-		// Set the max_age_num_blocks in the evidence params to reflect the 14 day
-		// unbonding period.
-		//
-		// Ref: https://github.com/osmosis-labs/osmosis/issues/1160
-		cp := bpm.GetConsensusParams(ctx)
-		if cp != nil && cp.Evidence != nil {
-			evParams := cp.Evidence
-			evParams.MaxAgeNumBlocks = 186_092
-
-			bpm.StoreConsensusParams(ctx, cp)
-		}
+		// TODO{Irregular state change}
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
 }
