a3e86970f6b40f3094708480e055d42641f469d9
Dev Ojha <dojha@berkeley.edu>
2022-06-09 23:15:28 -0500

    Add logic for transition

--- app/upgrades/v10/constants.go
+++ app/upgrades/v10/constants.go
@@ -4,6 +4,8 @@ import (
 	"github.com/osmosis-labs/osmosis/v7/app/upgrades"
 
 	store "github.com/cosmos/cosmos-sdk/store/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // Last executed block on the v9 code was 4713064.
@@ -14,6 +16,16 @@ const ForkHeight = 4713065
 // UpgradeName defines the on-chain upgrade name for the Osmosis v9-fork for recovery.
 // This is not called v10, due to this bug that would require a state migration to fix:
 const UpgradeName = "v10"
+
+// RecoveryAddress that the irregular state change transfers to.
+// TODO: Derive it, currently set to 'distribution' module account.
+var RecoveryAddress, recoveryAddressErr = sdk.AccAddressFromBech32("osmo1jv65s3grqf6v6jl3dp4t6c9t9rk99cd80yhvld")
+
+func init() {
+	if recoveryAddressErr != nil {
+		panic("recovery address decoding failure")
+	}
+}
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
--- app/upgrades/v10/upgrades.go
+++ app/upgrades/v10/upgrades.go
@@ -20,3 +20,53 @@ func CreateUpgradeHandler(
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
 }
+
+// Validators by choosing this binary and through explicit off-chain signalling
+// have chosen the approach of doing an irregular state change.
+// The change being, transfering all liquid funds from consenting addresses to
+// to the recovery address.
+// This is done with cryptographic approval of this action.
+func AgreedUponIrregularStateChange(ctx sdk.Context, keepers *keepers.AppKeepers) {
+	// (1) osmo1hq8tlgq0kqz9e56532zghdhz7g8gtjymdltqer
+	// firestake address
+	// https://www.mintscan.io/cosmos/txs/8841476422795A83014F55C2C8915E21635D02011B3BA0CA92020893F5ED30DA
+	addr, err := sdk.AccAddressFromBech32("osmo1hq8tlgq0kqz9e56532zghdhz7g8gtjymdltqer")
+	if err != nil {
+		panic(err)
+	}
+	ForceTransferAllTokens(ctx, keepers, addr, RecoveryAddress)
+	// (2) osmo1v44mqmhvtn8cw373xv0hw6npddccnr70lqsk9s
+	// firestake address
+	// https://www.mintscan.io/cosmos/txs/D6187E67AAA4A16A414D41D8FADD35B1B082F50A548989C7FD9B083D2CAEB0C0
+	addr, err = sdk.AccAddressFromBech32("osmo1v44mqmhvtn8cw373xv0hw6npddccnr70lqsk9s")
+	if err != nil {
+		panic(err)
+	}
+	ForceTransferAllTokens(ctx, keepers, addr, RecoveryAddress)
+	// (3) osmo10t26acjmemggsahq6uvyucm4tj3z0mhz23ljh2
+	// https://www.mintscan.io/cosmos/txs/0F536D1FAB700363B8A3EE47431BA7E7D80A40F55976C9B80A9F6984C8D0198A
+	addr, err = sdk.AccAddressFromBech32("osmo10t26acjmemggsahq6uvyucm4tj3z0mhz23ljh2")
+	if err != nil {
+		panic(err)
+	}
+	ForceTransferAllTokens(ctx, keepers, addr, RecoveryAddress)
+	// (4) osmo18qx59wy8s3ytax3e0akna934e86mw776vlzjtq
+	// https://www.mintscan.io/cosmos/txs/0929B33C9F6368F10652D63218DC0E6B5AF8B0F986D041900AFDFC1B5EAC040D
+	addr, err = sdk.AccAddressFromBech32("osmo18qx59wy8s3ytax3e0akna934e86mw776vlzjtq")
+	if err != nil {
+		panic(err)
+	}
+	ForceTransferAllTokens(ctx, keepers, addr, RecoveryAddress)
+}
+
+// ForceTransferAllTokens from address `from` to address `to`.
+// Assumes neither of `from` or `to` are a module account, from is non-nil.
+func ForceTransferAllTokens(ctx sdk.Context, keepers *keepers.AppKeepers, from sdk.AccAddress, to sdk.AccAddress) {
+	balances := keepers.BankKeeper.GetAllBalances(ctx, from)
+	err := keepers.BankKeeper.SendCoins(ctx, from, to, balances)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func VerifyTx() {}
