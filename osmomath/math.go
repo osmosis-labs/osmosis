9a63bb1ac930e0acfbffc29b8a96add70aacd439
Matt, Park <45252226+mattverse@users.noreply.github.com>
2022-06-10 13:28:35 -0500

    Update osmomath/math.go
    Co-authored-by: Dev Ojha <ValarDragon@users.noreply.github.com>

--- osmomath/math.go
+++ osmomath/math.go
@@ -81,7 +81,7 @@ func Pow(base sdk.Dec, exp sdk.Dec) sdk.Dec {
 }
 
 // Contract: 0 < base <= 2
-// 0 < exp < 1.
+// 0 <= exp < 1.
 func PowApprox(base sdk.Dec, exp sdk.Dec, precision sdk.Dec) sdk.Dec {
 	if exp.IsZero() {
 		return sdk.OneDec()
