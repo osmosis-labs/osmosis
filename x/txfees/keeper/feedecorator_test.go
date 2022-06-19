2a00eee5ee1338b64df99021785ac05d9a237144
Bradley Lostak <bradleycl98@gmail.com>
2022-06-18 23:54:47 -0700

    add test get min gas price for tx - still need to add more test cases

--- x/txfees/keeper/feedecorator_test.go
+++ x/txfees/keeper/feedecorator_test.go
@@ -1,6 +1,8 @@
 package keeper_test
 
 import (
+	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
+
 	clienttx "github.com/cosmos/cosmos-sdk/client/tx"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	"github.com/cosmos/cosmos-sdk/simapp"
@@ -9,6 +11,8 @@ import (
 
 	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
 
+	appParams "github.com/osmosis-labs/osmosis/v7/app/params"
+	gammtypes "github.com/osmosis-labs/osmosis/v7/x/gamm/types"
 	"github.com/osmosis-labs/osmosis/v7/x/txfees/keeper"
 	txKeeper "github.com/osmosis-labs/osmosis/v7/x/txfees/keeper"
 	"github.com/osmosis-labs/osmosis/v7/x/txfees/types"
@@ -364,5 +368,128 @@ func (suite *KeeperTestSuite) TestIsSufficientFee() {
 			suite.Require().Error(err, "test: %s", test.name)
 		}
 	}
+}
 
+func (suite *KeeperTestSuite) TestGetMinBaseGasPriceForTxExactAmountIn() {
+	appParams.SetAddressPrefixes()
+	pk1 := ed25519.GenPrivKey().PubKey()
+	addr1 := sdk.AccAddress(pk1.Address()).String()
+	//	invalidAddr := sdk.AccAddress("invalid")
+
+	createMsg := func(after func(msg gammtypes.MsgSwapExactAmountIn) gammtypes.MsgSwapExactAmountIn) gammtypes.MsgSwapExactAmountIn {
+		properMsg := gammtypes.MsgSwapExactAmountIn{
+			Sender: addr1,
+			Routes: []gammtypes.SwapAmountInRoute{{
+				PoolId:        1,
+				TokenOutDenom: "test1",
+			}, {
+				PoolId:        2,
+				TokenOutDenom: "test2",
+			}},
+			TokenIn:           sdk.NewCoin("test", sdk.NewInt(100)),
+			TokenOutMinAmount: sdk.NewInt(200),
+		}
+
+		return after(properMsg)
+	}
+
+	msg := createMsg(func(msg gammtypes.MsgSwapExactAmountIn) gammtypes.MsgSwapExactAmountIn {
+		// Do nothing
+		return msg
+	})
+
+	suite.Require().Equal(msg.Route(), gammtypes.RouterKey)
+	suite.Require().Equal(msg.Type(), "swap_exact_amount_in")
+	signersSybilFee := msg.GetSigners()
+	suite.Require().Equal(len(signersSybilFee), 1)
+	suite.Require().Equal(signersSybilFee[0].String(), addr1)
+
+	createSybil := func(after func(s txKeeper.Sybil) txKeeper.Sybil) txKeeper.Sybil {
+		properSybil := txKeeper.Sybil{
+			GasPrice: sdk.MustNewDecFromStr("1"),
+			FeesPaid: sdk.NewCoin("test", sdk.NewInt(1)),
+		}
+
+		return after(properSybil)
+	}
+
+	sybilStruct := createSybil(func(s txKeeper.Sybil) txKeeper.Sybil {
+		// do nothing
+		return s
+	})
+
+	suite.Require().Equal(sdk.MustNewDecFromStr("1"), sybilStruct.GasPrice)
+	suite.Require().Equal(sdk.NewCoin("test", sdk.NewInt(1)), sybilStruct.FeesPaid)
+
+	baseDenom, err := suite.App.TxFeesKeeper.GetBaseDenom(suite.Ctx)
+	suite.Require().NoError(err, "base denom %v error %v for test get min gas price", baseDenom, err)
+	mempoolFeeOpts := types.NewDefaultMempoolFeeOptions()
+	mempoolFeeOpts.MinGasPriceForHighGasTx = sdk.MustNewDecFromStr("0.0025")
+
+	tests := []struct {
+		name           string
+		msg            gammtypes.MsgSwapExactAmountIn
+		txFee          sdk.Coin
+		expectGasPrice sdk.Dec
+		expectFeesPaid sdk.Coin
+		gasRequested   uint64
+		expectPass     bool
+	}{
+		{
+			name: "msg does not need sybil fees",
+			msg: createMsg(func(msg gammtypes.MsgSwapExactAmountIn) gammtypes.MsgSwapExactAmountIn {
+				// Do nothing
+				return msg
+			}),
+			txFee:          sdk.NewCoin(baseDenom, sdk.NewInt(10000)),
+			expectGasPrice: suite.Ctx.MinGasPrices().AmountOf(baseDenom),
+			expectFeesPaid: sdk.NewCoin(baseDenom, sdk.ZeroInt()),
+			expectPass:     true,
+		},
+	}
+
+	for _, test := range tests {
+		suite.SetupTest(false)
+
+		// TxBuilder components reset for every test case
+		txBuilder := suite.clientCtx.TxConfig.NewTxBuilder()
+		priv0, _, addr0 := testdata.KeyTestPubAddr()
+		acc1 := suite.App.AccountKeeper.NewAccountWithAddress(suite.Ctx, addr0)
+		suite.App.AccountKeeper.SetAccount(suite.Ctx, acc1)
+		msgs := []sdk.Msg{&test.msg}
+		privs, accNums, accSeqs := []cryptotypes.PrivKey{priv0}, []uint64{0}, []uint64{0}
+		signerData := authsigning.SignerData{
+			ChainID:       suite.Ctx.ChainID(),
+			AccountNumber: accNums[0],
+			Sequence:      accSeqs[0],
+		}
+
+		gasLimit := test.gasRequested
+		sigV2, _ := clienttx.SignWithPrivKey(
+			1,
+			signerData,
+			txBuilder,
+			privs[0],
+			suite.clientCtx.TxConfig,
+			accSeqs[0],
+		)
+
+		// build transaction with swap extern amount out msg
+		tx := suite.BuildTx(txBuilder, msgs, sigV2, "", sdk.NewCoins(test.txFee), gasLimit)
+
+		// create mempool fee decorator
+		mfd := keeper.NewMempoolFeeDecorator(*suite.App.TxFeesKeeper, mempoolFeeOpts)
+
+		// get sybil fee struct
+		sybil, err := mfd.GetMinBaseGasPriceForTx(suite.Ctx, baseDenom, tx)
+
+		// check if sybil fee struct is as expected
+		if test.expectPass {
+			suite.Require().Equal(sybil.GasPrice, test.expectGasPrice)
+			suite.Require().Equal(sybil.FeesPaid, test.expectFeesPaid)
+			suite.Require().NoError(err, "test: %s", test.name)
+		} else {
+			suite.Require().Error(err, "test: %s", test.name)
+		}
+	}
 }
