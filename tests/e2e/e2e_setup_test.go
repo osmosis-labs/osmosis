83959f53b701baea885a1c522e3582d81f8ddb39
Roman <ackhtariev@gmail.com>
2022-06-19 18:02:22 -0400

    remove more redundant structs

--- tests/e2e/e2e_setup_test.go
+++ tests/e2e/e2e_setup_test.go
@@ -6,23 +6,9 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/stretchr/testify/suite"
-
-	"github.com/osmosis-labs/osmosis/v7/tests/e2e/chain"
 	configurer "github.com/osmosis-labs/osmosis/v7/tests/e2e/configurer"
+	"github.com/stretchr/testify/suite"
 )
-
-type chainConfig struct {
-	// voting period is number of blocks it takes to deposit, 1.2 seconds per validator to vote on the prop, and a buffer.
-	votingPeriod float32
-	// upgrade proposal height for chain.
-	propHeight int
-	// Indexes of the validators to skip from running during initialization.
-	// This is needed for testing functionality like state-sync where we would
-	// like to start a node during tests post-initialization.
-	skipRunValidatorIndexes map[int]struct{}
-	chain                   *chain.Chain
-}
 
 const (
 	// Environment variable name to skip the upgrade tests
