eb2f118f09e0bd463a1800e2f8dfd1010a0f93c4
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-06-24 18:21:40 -0400

    updates

--- x/gamm/pool-models/balancer/pool.go
+++ x/gamm/pool-models/balancer/pool.go
@@ -2,10 +2,10 @@ package balancer
 
 import (
 	"errors"
-	fmt "fmt"
+	"fmt"
 	"sort"
 	"strings"
-	time "time"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
