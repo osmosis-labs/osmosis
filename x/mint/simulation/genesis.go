5001b8489ed6bbcb82956f3b0841a8be50d0e179
Roman <ackhtariev@gmail.com>
2022-06-25 16:02:50 -0400

    randomize mint gen state simulation

--- x/mint/simulation/genesis.go
+++ x/mint/simulation/genesis.go
@@ -5,6 +5,7 @@ package simulation
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 
 	"github.com/osmosis-labs/osmosis/v7/x/mint/types"
 
@@ -12,39 +13,185 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 )
 
+// Simulation parameter constants.
+const (
+	epochProvisionsKey                        = "epoch_provisions"
+	epochIdentifierKey                        = "epoch_identifier"
+	reductionFactorKey                        = "reduction_factor"
+	reductionPeriodInEpochsKey                = "reduction_period_in_epochs"
+	stakingDistributionProportionKey          = "staking_distribution_proportion"
+	poolIncentivesDistributionProportionKey   = "pool_incentives_distribution_proportion"
+	developerRewardsDistributionProportionKey = "developer_rewards_distribution_proportion"
+	communityPoolDistributionProportionKey    = "community_pool_distribution_proportion"
+	weightedDevRewardReceiversKey             = "weighted_dev_reward_receivers"
+	mintingRewardsDistributionStartEpochKey   = "minting_rewards_distribution_start_epoch"
+	reductionStartedEpochKey                  = "reduction_started_epoch"
+
+	maxInt64 = int(^uint(0) >> 1)
+)
+
+var (
+	epochIdentifierOptions = []string{"day", "week"}
+)
+
 // RandomizedGenState generates a random GenesisState for mint.
 func RandomizedGenState(simState *module.SimulationState) {
-	// minter
+	var epochProvisions sdk.Dec
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, epochProvisionsKey, &epochProvisions, simState.Rand,
+		func(r *rand.Rand) { epochProvisions = genEpochProvisions(r) },
+	)
 
-	// var maxRewardPerEpoch sdk.Dec
-	// simState.AppParams.GetOrGenerate(
-	// 	simState.Cdc, MaxRewardPerEpoch, &maxRewardPerEpoch, simState.Rand,
-	// 	func(r *rand.Rand) { maxRewardPerEpoch = GenMaxRewardPerEpoch(r) },
-	// )
+	var epochIdentifier string
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, epochIdentifierKey, &epochIdentifier, simState.Rand,
+		func(r *rand.Rand) { epochIdentifier = genEpochIdentifier(r) },
+	)
 
-	// var minRewardPerEpoch sdk.Dec
-	// simState.AppParams.GetOrGenerate(
-	// 	simState.Cdc, MinRewardPerEpoch, &minRewardPerEpoch, simState.Rand,
-	// 	func(r *rand.Rand) { minRewardPerEpoch = GenMinRewardPerEpoch(r) },
-	// )
-	// Leaving as sample code
+	var reductionFactor sdk.Dec
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, reductionFactorKey, &reductionFactor, simState.Rand,
+		func(r *rand.Rand) { reductionFactor = genReductionFactor(r) },
+	)
+
+	var reductionPeriodInEpochs int64
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, reductionPeriodInEpochsKey, &reductionPeriodInEpochs, simState.Rand,
+		func(r *rand.Rand) { reductionPeriodInEpochs = genReductionPeriodInEpochs(r) },
+	)
+
+	randomDisitributionProportions := genProportionsAddingUpToOne(simState.Rand, 4)
+
+	var stakingDistributionProportion sdk.Dec
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, stakingDistributionProportionKey, &stakingDistributionProportion, simState.Rand,
+		func(r *rand.Rand) { stakingDistributionProportion = randomDisitributionProportions[0] },
+	)
+
+	var poolIncentivesDistributionProportion sdk.Dec
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, poolIncentivesDistributionProportionKey, &poolIncentivesDistributionProportion, simState.Rand,
+		func(r *rand.Rand) { poolIncentivesDistributionProportion = randomDisitributionProportions[1] },
+	)
+
+	var developerRewardsDistributionProportion sdk.Dec
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, developerRewardsDistributionProportionKey, &developerRewardsDistributionProportion, simState.Rand,
+		func(r *rand.Rand) { developerRewardsDistributionProportion = randomDisitributionProportions[2] },
+	)
+
+	var communityPoolDistributionProportion sdk.Dec
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, communityPoolDistributionProportionKey, &communityPoolDistributionProportion, simState.Rand,
+		func(r *rand.Rand) { communityPoolDistributionProportion = randomDisitributionProportions[3] },
+	)
+
+	var weightedDevRewardReceivers []types.WeightedAddress
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, weightedDevRewardReceiversKey, &weightedDevRewardReceivers, simState.Rand,
+		func(r *rand.Rand) {
+			addressCount := r.Intn(5)
+			randomDevRewardProportions := genProportionsAddingUpToOne(simState.Rand, addressCount)
+
+			for i := 0; i < addressCount; i++ {
+				weightedDevRewardReceivers = append(weightedDevRewardReceivers, types.WeightedAddress{
+					Address: fmt.Sprintf("address_%d", i),
+					Weight:  randomDevRewardProportions[i],
+				})
+			}
+		},
+	)
+
+	var mintintRewardsDistributionStartEpoch int64
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, mintingRewardsDistributionStartEpochKey, &mintintRewardsDistributionStartEpoch, simState.Rand,
+		func(r *rand.Rand) { mintintRewardsDistributionStartEpoch = genMintintRewardsDistributionStartEpoch(r) },
+	)
+
+	var reductionStartedEpoch int64
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, reductionStartedEpochKey, &reductionStartedEpoch, simState.Rand,
+		func(r *rand.Rand) { reductionStartedEpoch = genReductionStartedEpoch(r) },
+	)
 
 	mintDenom := sdk.DefaultBondDenom
-	epochProvisions := sdk.NewDec(500000) // TODO: Randomize this
-	params := types.NewParams(mintDenom, epochProvisions, "week", sdk.NewDecWithPrec(5, 1), 156, types.DistributionProportions{
-		Staking:          sdk.NewDecWithPrec(4, 1), // 0.4
-		PoolIncentives:   sdk.NewDecWithPrec(3, 1), // 0.3
-		DeveloperRewards: sdk.NewDecWithPrec(2, 1), // 0.2
-		CommunityPool:    sdk.NewDecWithPrec(1, 1), // 0.1
-	}, []types.WeightedAddress{}, 0)
+	params := types.NewParams(
+		mintDenom,
+		epochProvisions,
+		epochIdentifier,
+		reductionFactor,
+		reductionPeriodInEpochs,
+		types.DistributionProportions{
+			Staking:          stakingDistributionProportion,
+			PoolIncentives:   poolIncentivesDistributionProportion,
+			DeveloperRewards: developerRewardsDistributionProportion,
+			CommunityPool:    communityPoolDistributionProportion,
+		},
+		weightedDevRewardReceivers,
+		mintintRewardsDistributionStartEpoch)
 
-	mintGenesis := types.NewGenesisState(types.InitialMinter(), params, 0)
+	minter := types.NewMinter(epochProvisions)
+
+	mintGenesis := types.NewGenesisState(minter, params, reductionStartedEpoch)
 
 	bz, err := json.MarshalIndent(&mintGenesis, "", " ")
 	if err != nil {
 		panic(err)
 	}
-	// TODO: Do some randomization later
-	fmt.Printf("Selected deterministically generated minting parameters:\n%s\n", bz)
+	fmt.Printf("Selected pseudo-randomly generated minting parameters:\n%s\n", bz)
 	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(mintGenesis)
+}
+
+func genEpochProvisions(r *rand.Rand) sdk.Dec {
+	return sdk.NewDec(int64(r.Intn(maxInt64)))
+}
+
+func genEpochIdentifier(r *rand.Rand) string {
+	return epochIdentifierOptions[rand.Intn(len(epochIdentifierOptions))]
+}
+
+func genReductionFactor(r *rand.Rand) sdk.Dec {
+	return sdk.NewDecWithPrec(int64(r.Intn(10)), 1)
+}
+
+func genReductionPeriodInEpochs(r *rand.Rand) int64 {
+	return int64(r.Intn(maxInt64))
+}
+
+func genStakingDistributionProportion(r *rand.Rand) sdk.Dec {
+	return sdk.NewDecWithPrec(int64(r.Intn(5)), 1)
+}
+
+func genPoolIncentivesDistributionProportion(r *rand.Rand, limitRatio sdk.Dec) sdk.Dec {
+	return sdk.NewDecWithPrec(int64(r.Intn(int(limitRatio.MulInt64(10).TruncateInt64()))), 1)
+}
+
+func genDeveloperRewardsDistributionProportion(r *rand.Rand, limitRatio sdk.Dec) sdk.Dec {
+	return sdk.NewDecWithPrec(int64(r.Intn(int(limitRatio.MulInt64(10).TruncateInt64()))), 1)
+}
+
+// genProportionsAddingUpToOne reurns a slice with numberOfProportions that add up to 1.
+func genProportionsAddingUpToOne(r *rand.Rand, numberOfProportions int) []sdk.Dec {
+	proportions := make([]sdk.Dec, numberOfProportions)
+
+	// We start by estimating the first proportion with a limit of 1.
+	// Then, subtract the first proportion from 1 to esimate
+	// the remaining ratio to be used as upper bound for next randomization.
+	// Next, repeat the randomization process for the remaining proportions.
+	remainingRatio := sdk.OneDec()
+	for i := 0; i < numberOfProportions-1; i++ {
+		nextProportion := sdk.NewDecWithPrec(int64(r.Intn(int(remainingRatio.MulInt64(10).TruncateInt64()))), 1)
+		proportions[i] = nextProportion
+		remainingRatio = remainingRatio.Sub(nextProportion)
+	}
+	proportions[numberOfProportions-1] = remainingRatio
+	return proportions
+}
+
+func genMintintRewardsDistributionStartEpoch(r *rand.Rand) int64 {
+	return int64(r.Intn(maxInt64))
+}
+
+func genReductionStartedEpoch(r *rand.Rand) int64 {
+	return int64(r.Intn(maxInt64))
 }
--- x/mint/simulation/genesis_test.go
+++ x/mint/simulation/genesis_test.go
@@ -2,6 +2,7 @@ package simulation_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"math/rand"
 	"testing"
 
@@ -12,6 +13,7 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 )
@@ -22,7 +24,7 @@ func TestRandomizedGenState(t *testing.T) {
 	interfaceRegistry := codectypes.NewInterfaceRegistry()
 	cdc := codec.NewProtoCodec(interfaceRegistry)
 
-	s := rand.NewSource(1)
+	s := rand.NewSource(5)
 	r := rand.New(s)
 
 	simState := module.SimulationState{
@@ -40,10 +42,97 @@ func TestRandomizedGenState(t *testing.T) {
 	var mintGenesis types.GenesisState
 	simState.Cdc.MustUnmarshalJSON(simState.GenState[types.ModuleName], &mintGenesis)
 
-	require.Equal(t, "stake", mintGenesis.Params.MintDenom)
-	require.Equal(t, "0stake", mintGenesis.Minter.EpochProvision(mintGenesis.Params).String())
-	require.Equal(t, "0.000000000000000000", mintGenesis.Minter.NextEpochProvisions(mintGenesis.Params).String())
-	require.Equal(t, "0.000000000000000000", mintGenesis.Minter.EpochProvisions.String())
+	const (
+		expectedEpochProvisionsStr      = "7913048388940673156"
+		expectedEpochIdentifier         = "week"
+		expectedReductionFactorStr      = "0.6"
+		expectedReductionPeriodInEpochs = int64(9171281239991390334)
+
+		expectedStakingDistributionProportion          = "0.8"
+		expectedPoolIncentivesDistributionProportion   = "0"
+		expectedDeveloperRewardsDistributionProportion = "0.1"
+		expectedCommunityPoolDistributionProportion    = "0.1"
+
+		expectedMintintRewardsDistributionStartEpoch = int64(3800660657951843153)
+
+		expectedReductionStartedEpoch = int64(1187669907065521396)
+
+		expectedNextEpochProvisionsStr = "3956524194470336578"
+		expectedDenom                  = sdk.DefaultBondDenom
+	)
+
+	var expectedWeightedAddresses []types.WeightedAddress = []types.WeightedAddress{
+		{
+			Address: "address_0",
+			Weight:  sdk.NewDecWithPrec(4, 1),
+		},
+		{
+			Address: "address_1",
+			Weight:  sdk.NewDecWithPrec(0, 1),
+		},
+		{
+			Address: "address_2",
+			Weight:  sdk.NewDecWithPrec(2, 1),
+		},
+		{
+			Address: "address_3",
+			Weight:  sdk.NewDecWithPrec(4, 1),
+		},
+	}
+
+	// Epoch provisions from Minter.
+	epochProvisionsDec, err := sdk.NewDecFromStr(expectedEpochProvisionsStr)
+	require.NoError(t, err)
+	require.Equal(t, epochProvisionsDec, mintGenesis.Minter.EpochProvisions)
+
+	// Epoch identifier.
+	require.Equal(t, expectedEpochIdentifier, mintGenesis.Params.EpochIdentifier)
+
+	// Reduction factor.
+	reductionFactorDec, err := sdk.NewDecFromStr(expectedReductionFactorStr)
+	require.NoError(t, err)
+	require.Equal(t, reductionFactorDec, mintGenesis.Params.ReductionFactor)
+
+	// Reduction perion in epochs.
+	require.Equal(t, expectedReductionPeriodInEpochs, mintGenesis.Params.ReductionPeriodInEpochs)
+
+	// Staking rewards distribution proportion.
+	stakingDistributionProportionDec, err := sdk.NewDecFromStr(expectedStakingDistributionProportion)
+	require.NoError(t, err)
+	require.Equal(t, stakingDistributionProportionDec, mintGenesis.Params.DistributionProportions.Staking)
+
+	// Pool incentives distribution proportion.
+	poolIncentivesDistributionProportionDec, err := sdk.NewDecFromStr(expectedPoolIncentivesDistributionProportion)
+	require.NoError(t, err)
+	require.Equal(t, poolIncentivesDistributionProportionDec, mintGenesis.Params.DistributionProportions.PoolIncentives)
+
+	// Developer rewards distribution proportion.
+	developerRewardsDistributionProportionDec, err := sdk.NewDecFromStr(expectedDeveloperRewardsDistributionProportion)
+	require.NoError(t, err)
+	require.Equal(t, developerRewardsDistributionProportionDec, mintGenesis.Params.DistributionProportions.DeveloperRewards)
+
+	// Community pool distribution proportion.
+	communityPoolDistributionProportionDec, err := sdk.NewDecFromStr(expectedCommunityPoolDistributionProportion)
+	require.NoError(t, err)
+	require.Equal(t, communityPoolDistributionProportionDec, mintGenesis.Params.DistributionProportions.CommunityPool)
+
+	// Weighted developer rewards receivers.
+	require.Equal(t, expectedWeightedAddresses, mintGenesis.Params.WeightedDeveloperRewardsReceivers)
+
+	// Minting rewards distribution start epoch
+	require.Equal(t, expectedMintintRewardsDistributionStartEpoch, mintGenesis.Params.MintingRewardsDistributionStartEpoch)
+
+	// Reduction started epoch.
+	require.Equal(t, expectedReductionStartedEpoch, mintGenesis.ReductionStartedEpoch)
+
+	// Next epoch provisions.
+	nextEpochProvisionsDec := epochProvisionsDec.Mul(reductionFactorDec)
+	require.NoError(t, err)
+	require.Equal(t, nextEpochProvisionsDec, mintGenesis.Minter.NextEpochProvisions(mintGenesis.Params))
+
+	// Denom and Epoch provisions from Params.
+	require.Equal(t, expectedDenom, mintGenesis.Params.MintDenom)
+	require.Equal(t, fmt.Sprintf("%s%s", expectedEpochProvisionsStr, expectedDenom), mintGenesis.Minter.EpochProvision(mintGenesis.Params).String())
 }
 
 // TestRandomizedGenState_Invalid tests abnormal scenarios of applying RandomizedGenState.
