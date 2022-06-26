ff0aa521213698f50aba04c53583c6f376a5c186
Roman <ackhtariev@gmail.com>
2022-06-26 14:18:34 -0400

    test: x/mint thirdening

--- x/mint/keeper/hooks_test.go
+++ x/mint/keeper/hooks_test.go
@@ -4,12 +4,13 @@ import (
 	"testing"
 	"time"
 
-	osmoapp "github.com/osmosis-labs/osmosis/v7/app"
-	lockuptypes "github.com/osmosis-labs/osmosis/v7/x/lockup/types"
-	"github.com/osmosis-labs/osmosis/v7/x/mint/types"
 	"github.com/stretchr/testify/require"
 	abci "github.com/tendermint/tendermint/abci/types"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
+	osmoapp "github.com/osmosis-labs/osmosis/v7/app"
+	lockuptypes "github.com/osmosis-labs/osmosis/v7/x/lockup/types"
+	"github.com/osmosis-labs/osmosis/v7/x/mint/types"
 
 	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -217,6 +218,134 @@ func TestEndOfEpochNoDistributionWhenIsNotYetStartTime(t *testing.T) {
 	// halven period should be set to mintParams.MintingRewardsDistributionStartEpoch
 	lastHalvenPeriod := app.MintKeeper.GetLastHalvenEpochNum(ctx)
 	require.Equal(t, lastHalvenPeriod, mintParams.MintingRewardsDistributionStartEpoch)
+}
+
+func TestAfterEpochEnd_FirstYearThirdening_RealParameters(t *testing.T) {
+	const (
+		reductionPeriodInEpochs                    = 365
+		mintingRewardsDistributionStartEpoch       = 1
+		thirdeningEpoch                      int64 = 365 + 1
+
+		genesisEpochProvisions = "821917808219.178082191780821917"
+		epochIdentifier        = "day"
+
+		// Actual value taken from mainnet for sanity checking calculations.
+		mainnetThirdenedProvisions = "547945205479.452055068493150684"
+	)
+
+	app := osmoapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+
+	genesisEpochProvisionsDec, err := sdk.NewDecFromStr(genesisEpochProvisions)
+	require.NoError(t, err)
+
+	// params := types.DefaultParams()
+	// From: https://github.com/osmosis-labs/networks/raw/main/osmosis-1/genesis.json
+	mintParams := types.Params{
+		MintDenom:               "stake",
+		GenesisEpochProvisions:  genesisEpochProvisionsDec,
+		EpochIdentifier:         epochIdentifier,
+		ReductionPeriodInEpochs: reductionPeriodInEpochs,
+		ReductionFactor:         sdk.NewDec(2).Quo(sdk.NewDec(3)),
+		DistributionProportions: types.DistributionProportions{
+			Staking:          sdk.NewDecWithPrec(25, 2),
+			PoolIncentives:   sdk.NewDecWithPrec(45, 2),
+			DeveloperRewards: sdk.NewDecWithPrec(25, 2),
+			CommunityPool:    sdk.NewDecWithPrec(05, 2),
+		},
+		WeightedDeveloperRewardsReceivers: []types.WeightedAddress{
+			{
+				Address: "osmo14kjcwdwcqsujkdt8n5qwpd8x8ty2rys5rjrdjj",
+				Weight:  sdk.NewDecWithPrec(2887, 4),
+			},
+			{
+				Address: "osmo1gw445ta0aqn26suz2rg3tkqfpxnq2hs224d7gq",
+				Weight:  sdk.NewDecWithPrec(229, 3),
+			},
+			{
+				Address: "osmo13lt0hzc6u3htsk7z5rs6vuurmgg4hh2ecgxqkf",
+				Weight:  sdk.NewDecWithPrec(1625, 4),
+			},
+			{
+				Address: "osmo1kvc3he93ygc0us3ycslwlv2gdqry4ta73vk9hu",
+				Weight:  sdk.NewDecWithPrec(109, 3),
+			},
+			{
+				Address: "osmo19qgldlsk7hdv3ddtwwpvzff30pxqe9phq9evxf",
+				Weight:  sdk.NewDecWithPrec(995, 3).Quo(sdk.NewDec(10)), // 0.0995
+			},
+			{
+				Address: "osmo19fs55cx4594een7qr8tglrjtt5h9jrxg458htd",
+				Weight:  sdk.NewDecWithPrec(6, 1).Quo(sdk.NewDec(10)), // 0.06
+			},
+			{
+				Address: "osmo1ssp6px3fs3kwreles3ft6c07mfvj89a544yj9k",
+				Weight:  sdk.NewDecWithPrec(15, 2).Quo(sdk.NewDec(10)), // 0.015
+			},
+			{
+				Address: "osmo1c5yu8498yzqte9cmfv5zcgtl07lhpjrj0skqdx",
+				Weight:  sdk.NewDecWithPrec(1, 1).Quo(sdk.NewDec(10)), // 0.01
+			},
+			{
+				Address: "osmo1yhj3r9t9vw7qgeg22cehfzj7enwgklw5k5v7lj",
+				Weight:  sdk.NewDecWithPrec(75, 2).Quo(sdk.NewDec(100)), // 0.0075
+			},
+			{
+				Address: "osmo18nzmtyn5vy5y45dmcdnta8askldyvehx66lqgm",
+				Weight:  sdk.NewDecWithPrec(7, 1).Quo(sdk.NewDec(100)), // 0.007
+			},
+			{
+				Address: "osmo1z2x9z58cg96ujvhvu6ga07yv9edq2mvkxpgwmc",
+				Weight:  sdk.NewDecWithPrec(5, 1).Quo(sdk.NewDec(100)), // 0.005
+			},
+			{
+				Address: "osmo1tvf3373skua8e6480eyy38avv8mw3hnt8jcxg9",
+				Weight:  sdk.NewDecWithPrec(25, 2).Quo(sdk.NewDec(100)), // 0.0025
+			},
+			{
+				Address: "osmo1zs0txy03pv5crj2rvty8wemd3zhrka2ne8u05n",
+				Weight:  sdk.NewDecWithPrec(25, 2).Quo(sdk.NewDec(100)), // 0.0025
+			},
+			{
+				Address: "osmo1djgf9p53n7m5a55hcn6gg0cm5mue4r5g3fadee",
+				Weight:  sdk.NewDecWithPrec(1, 1).Quo(sdk.NewDec(100)), // 0.001
+			},
+			{
+				Address: "osmo1488zldkrn8xcjh3z40v2mexq7d088qkna8ceze",
+				Weight:  sdk.NewDecWithPrec(8, 1).Quo(sdk.NewDec(1000)), // 0.0008
+			},
+		},
+		MintingRewardsDistributionStartEpoch: mintingRewardsDistributionStartEpoch,
+	}
+
+	sumOfWeights := sdk.ZeroDec()
+	// Ensure developer reward receivers add up to 1.
+	for _, w := range mintParams.WeightedDeveloperRewardsReceivers {
+		sumOfWeights = sumOfWeights.Add(w.Weight)
+	}
+	require.Equal(t, sdk.OneDec(), sumOfWeights)
+
+	app.MintKeeper.SetParams(ctx, mintParams)
+	app.MintKeeper.SetLastHalvenEpochNum(ctx, 0)
+
+	app.MintKeeper.SetMinter(ctx, types.Minter{
+		EpochProvisions: genesisEpochProvisionsDec,
+	})
+
+	for i := int64(0); i < thirdeningEpoch; i++ {
+		app.MintKeeper.AfterEpochEnd(ctx, "day", i)
+		require.Equal(t, genesisEpochProvisions, app.MintKeeper.GetMinter(ctx).EpochProvisions.String())
+	}
+
+	// This end of epoch should trigger thirderning.
+	app.MintKeeper.AfterEpochEnd(ctx, "day", thirdeningEpoch)
+
+	require.Equal(t, thirdeningEpoch, app.MintKeeper.GetLastHalvenEpochNum(ctx))
+
+	expectedThirdenedProvisions := mintParams.ReductionFactor.Mul(genesisEpochProvisionsDec)
+	// Sanity check with the actual value on mainnet.
+	require.Equal(t, mainnetThirdenedProvisions, expectedThirdenedProvisions.String())
+	require.Equal(t, expectedThirdenedProvisions, app.MintKeeper.GetMinter(ctx).EpochProvisions)
 }
 
 func setupGaugeForLPIncentives(t *testing.T, app *osmoapp.OsmosisApp, ctx sdk.Context) {
