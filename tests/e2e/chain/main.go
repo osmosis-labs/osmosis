ce460bc620c638ebc86075aa0a714e783e176f89
Adam Tucker <adam@osmosis.team>
2022-06-27 12:45:56 -0700

    v10 chain init (#1881)

--- tests/e2e/chain/main.go
+++ tests/e2e/chain/main.go
@@ -2,12 +2,12 @@ package chain
 
 import "time"
 
-func Init(id, dataDir string, validatorConfigs []*ValidatorConfig, votingPeriod time.Duration) (*Chain, error) {
+func Init(id, dataDir string, validatorConfigs []*ValidatorConfig, votingPeriod time.Duration, forkHeight int) (*Chain, error) {
 	chain, err := new(id, dataDir)
 	if err != nil {
 		return nil, err
 	}
-	if err := initNodes(chain, len(validatorConfigs)); err != nil {
+	if err := initNodes(chain, len(validatorConfigs), forkHeight); err != nil {
 		return nil, err
 	}
 	if err := initGenesis(chain, votingPeriod); err != nil {
