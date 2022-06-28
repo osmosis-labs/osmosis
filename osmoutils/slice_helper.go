d7c3f6b0ead68c0b59518850fe2ccfc62740d407
Dev Ojha <ValarDragon@users.noreply.github.com>
2022-06-28 17:55:38 -0500

    Update osmoutils/slice_helper.go
    Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>

--- osmoutils/slice_helper.go
+++ osmoutils/slice_helper.go
@@ -6,6 +6,7 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+// SortSlice sorts a slice of type T elements that implement constraints.Ordered.
 func SortSlice[T constraints.Ordered](s []T) {
 	sort.Slice(s, func(i, j int) bool {
 		return s[i] < s[j]
