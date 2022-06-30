9c05ecff936db67d71da1d71f97c809492a69615
Xiangan He <xiangan9@gmail.com>
2022-06-30 19:35:55 -0400

    working protobuf generation fix

--- contrib/scripts/check-generated.sh
+++ contrib/scripts/check-generated.sh
@@ -2,16 +2,13 @@ set -euo pipefail
 # Install buf and gogo tools, so that differences that arise from
 # toolchain differences are also caught.
 readonly tools="$(mktemp -d)"
-export PATH="${PATH}:${tools}/bin"
-export GOBIN="${tools}/bin"
+go install github.com/bufbuild/buf/cmd/buf
 go install github.com/gogo/protobuf/protoc-gen-gogofaster@latest
 
 #Specificially ignore all differences in go.mod / go.sum.
-echo "Generating Protobuf files"
-go run github.com/bufbuild/buf/cmd/buf generate
-mv ./proto/tendermint/abci/types.pb.go ./abci/types/
+make proto-gen
 
-if ! git diff --stat --exit-code ; then
+if ! git diff --stat --exit-code . ':(exclude)*.mod' ':(exclude)*.sum'; then
     echo ">> ERROR:"
     echo ">>"
     echo ">> Protobuf generated code requires update (either tools or .proto files may have changed)."
