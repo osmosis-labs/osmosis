e07a87b60d1eb4c93e222e67a0dc9be80f9ae574
Xiangan He <xiangan9@gmail.com>
2022-06-30 09:52:05 -0400

    moving to check-generated, specifying only checks for generated dep files

---
+++ contrib/scripts/check-generated.sh
@@ -0,0 +1,21 @@
+set -euo pipefail
+# Install buf and gogo tools, so that differences that arise from
+# toolchain differences are also caught.
+readonly tools="$(mktemp -d)"
+export PATH="${PATH}:${tools}/bin"
+export GOBIN="${tools}/bin"
+go install github.com/gogo/protobuf/protoc-gen-gogofaster@latest
+
+#Specificially ignore all differences in go.mod / go.sum.
+echo "Generating Protobuf files"
+go run github.com/bufbuild/buf/cmd/buf generate
+mv ./proto/tendermint/abci/types.pb.go ./abci/types/
+
+if ! git diff --stat --exit-code ; then
+    echo ">> ERROR:"
+    echo ">>"
+    echo ">> Protobuf generated code requires update (either tools or .proto files may have changed)."
+    echo ">> Ensure your tools are up-to-date, re-run 'make proto-gen' and update this PR."
+    echo ">>"
+    exit 1
+fi
