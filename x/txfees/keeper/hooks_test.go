fe70456343c2f9165866541027a53cd60ee85bad
stackman27 <sis1001@berkeley.edu>
2022-07-01 01:09:24 -0700

    addressed matts comments

--- x/txfees/keeper/hooks_test.go
+++ x/txfees/keeper/hooks_test.go
@@ -38,12 +38,13 @@ func (suite *KeeperTestSuite) TestTxFeesAfterEpochEnd() {
 	_, ustPool := suite.preparePool(ust)
 
 	tests := []struct {
-		name      string
-		coin      sdk.Coin
-		baseDenom string
-		denom     string
-		poolType  gammtypes.PoolI
-		swapFee   sdk.Dec
+		name       string
+		coin       sdk.Coin
+		baseDenom  string
+		denom      string
+		poolType   gammtypes.PoolI
+		swapFee    sdk.Dec
+		expectPass bool
 	}{
 		{
 			name:      "TxFees AfterEpochEnd for uion",
@@ -72,13 +73,9 @@ func (suite *KeeperTestSuite) TestTxFeesAfterEpochEnd() {
 	}
 
 	var finalOutputAmount = sdk.NewInt(0)
-	moduleAddrFee := suite.App.AccountKeeper.GetModuleAddress(types.FeeCollectorName)
-	moduleAddrNonNativeFee := suite.App.AccountKeeper.GetModuleAddress(types.NonNativeFeeCollectorName)
-
-	params := suite.App.IncentivesKeeper.GetParams(suite.Ctx)
-	futureCtx := suite.Ctx.WithBlockTime(time.Now().Add(time.Minute))
 
 	for _, tc := range tests {
+		// Get the output amount in osmos denom
 		expectedOutput, err := tc.poolType.CalcOutAmtGivenIn(suite.Ctx,
 			sdk.Coins{sdk.Coin{Denom: tc.denom, Amount: tc.coin.Amount}},
 			tc.baseDenom,
@@ -86,17 +83,25 @@ func (suite *KeeperTestSuite) TestTxFeesAfterEpochEnd() {
 		suite.Require().NoError(err)
 
 		finalOutputAmount = finalOutputAmount.Add(expectedOutput.Amount)
+
+		// Deposit some fee amount (non-native-denom) to the fee module account
 		_, _, addr0 := testdata.KeyTestPubAddr()
 		simapp.FundAccount(suite.App.BankKeeper, suite.Ctx, addr0, sdk.Coins{tc.coin})
 		suite.App.BankKeeper.SendCoinsFromAccountToModule(suite.Ctx, addr0, types.NonNativeFeeCollectorName, sdk.Coins{tc.coin})
 
-		suite.Require().True(suite.App.BankKeeper.HasBalance(suite.Ctx, moduleAddrNonNativeFee, tc.coin))
+		// End of epoch, so all the non-osmo fee amount should be swapped to osmo and transfer to fee module account
+		params := suite.App.IncentivesKeeper.GetParams(suite.Ctx)
+		futureCtx := suite.Ctx.WithBlockTime(time.Now().Add(time.Minute))
+		suite.App.TxFeesKeeper.AfterEpochEnd(futureCtx, params.DistrEpochIdentifier, int64(1))
 
-		suite.App.EpochsKeeper.AfterEpochEnd(futureCtx, params.DistrEpochIdentifier, int64(1))
+		// check the balance of the native-basedenom in module
+		moduleAddrFee := suite.App.AccountKeeper.GetModuleAddress(types.FeeCollectorName)
 		moduleBaseDenomBalance := suite.App.BankKeeper.GetBalance(suite.Ctx, moduleAddrFee, tc.baseDenom)
+
+		// checks the balance of the non-native denom in module account (should be empty)
+		moduleAddrNonNativeFee := suite.App.AccountKeeper.GetModuleAddress(types.NonNativeFeeCollectorName)
+
 		suite.Require().Empty(suite.App.BankKeeper.GetAllBalances(suite.Ctx, moduleAddrNonNativeFee))
-		// Tried to make use Equal() to check the exact same value but moduleBaseDenomBalance.Amount seems to be incrementing
-		// by 2000000 per test hmm
-		suite.Require().True(moduleBaseDenomBalance.Amount.GTE(finalOutputAmount))
+		suite.Require().Equal(moduleBaseDenomBalance.Amount, finalOutputAmount)
 	}
 }
