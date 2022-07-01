dbb92cd33e589e71d0599ba7521d1514fd83b3ec
Xiangan He <xiangan9@gmail.com>
2022-06-30 20:54:47 -0400

    reverting

--- contrib/scripts/check-generated.sh
+++ contrib/scripts/check-generated.sh
@@ -1,18 +1,20 @@
+#!/usr/bin/env bash
+
 set -euo pipefail
 # Install buf and gogo tools, so that differences that arise from
 # toolchain differences are also caught.
 readonly tools="$(mktemp -d)"
-go install github.com/bufbuild/buf/cmd/buf
-go install github.com/gogo/protobuf/protoc-gen-gogofaster@latest
+go get github.com/bufbuild/buf/cmd/buf
+go get github.com/gogo/protobuf/protoc-gen-gogofaster@latest
+
+make proto-all
 
 #Specificially ignore all differences in go.mod / go.sum.
-make proto-gen
-
 if ! git diff --stat --exit-code . ':(exclude)*.mod' ':(exclude)*.sum'; then
     echo ">> ERROR:"
     echo ">>"
     echo ">> Protobuf generated code requires update (either tools or .proto files may have changed)."
-    echo ">> Ensure your tools are up-to-date, re-run 'make proto-gen' and update this PR."
+    echo ">> Ensure your tools are up-to-date, re-run 'make proto-all' and update this PR."
     echo ">>"
     exit 1
 fi
