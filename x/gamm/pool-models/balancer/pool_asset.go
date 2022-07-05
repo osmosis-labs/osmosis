e2e9ae05e4064b152d9a1d9bc5fc72a7823b5414
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2022-07-05 17:58:00 +0700

    clean up & check weight

--- x/gamm/pool-models/balancer/pool_asset.go
+++ x/gamm/pool-models/balancer/pool_asset.go
@@ -12,23 +12,22 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// Validates a pool asset, to check if it has a valid weight.
-func (asset PoolAsset) ValidateWeight() error {
+type poolAssetPretty struct {
+	Token  sdk.Coin `json:"token" yaml:"token"`
+	Weight sdk.Dec  `json:"weight" yaml:"weight"`
+}
+
+// validates a pool asset, to check if it has a valid weight.
+func (asset PoolAsset) validateWeight() error {
 	if asset.Weight.LTE(sdk.ZeroInt()) {
 		return fmt.Errorf("a token's weight in the pool must be greater than 0")
 	}
 
-	// TODO: Choose a value that is too large for weights
-	// if asset.Weight >= (1 << 32) {
-	// 	return fmt.Errorf("a token's weight in the pool must be less than 2^32")
-	// }
+	if asset.Weight.GTE(sdk.NewInt(1 << 32)) {
+		return fmt.Errorf("a token's weight in the pool must be less than 2^32")
+	}
 
 	return nil
-}
-
-type poolAssetPretty struct {
-	Token  sdk.Coin `json:"token" yaml:"token"`
-	Weight sdk.Dec  `json:"weight" yaml:"weight"`
 }
 
 func (asset PoolAsset) prettify() poolAssetPretty {
@@ -37,14 +36,6 @@ func (asset PoolAsset) prettify() poolAssetPretty {
 		Token:  asset.Token,
 	}
 }
-
-// D: at name
-// func (asset poolAssetPretty) uglify() PoolAsset {
-// 	return PoolAsset{
-// 		Weight: asset.Weight.MulInt64(GuaranteedWeightPrecision).RoundInt(),
-// 		Token:  asset.Token,
-// 	}
-// }
 
 // MarshalYAML returns the YAML representation of a PoolAsset.
 // This is assumed to not be called on a stand-alone instance, so it removes the first marshalled line.
@@ -57,18 +48,18 @@ func (pa PoolAsset) MarshalYAML() (interface{}, error) {
 	return s, nil
 }
 
-// SortPoolAssetsOutOfPlaceByDenom sorts pool assets in place, by weight
+// sortPoolAssetsOutOfPlaceByDenom sorts pool assets in place, by weight
 // Doesn't deep copy the underlying weights, but it does place the assets
 // into a new slice.
-func SortPoolAssetsOutOfPlaceByDenom(assets []PoolAsset) []PoolAsset {
+func sortPoolAssetsOutOfPlaceByDenom(assets []PoolAsset) []PoolAsset {
 	assets_copy := make([]PoolAsset, len(assets))
 	copy(assets_copy, assets)
-	SortPoolAssetsByDenom(assets_copy)
+	sortPoolAssetsByDenom(assets_copy)
 	return assets_copy
 }
 
-// SortPoolAssetsByDenom sorts pool assets in place, by weight.
-func SortPoolAssetsByDenom(assets []PoolAsset) {
+// sortPoolAssetsByDenom sorts pool assets in place, by weight.
+func sortPoolAssetsByDenom(assets []PoolAsset) {
 	sort.Slice(assets, func(i, j int) bool {
 		PoolAssetA := assets[i]
 		PoolAssetB := assets[j]
@@ -77,13 +68,12 @@ func SortPoolAssetsByDenom(assets []PoolAsset) {
 	})
 }
 
-func ValidateUserSpecifiedPoolAssets(assets []PoolAsset) error {
+func validateUserSpecifiedPoolAssets(assets []PoolAsset) error {
 	// The pool must be swapping between at least two assets
 	if len(assets) < 2 {
 		return types.ErrTooFewPoolAssets
 	}
 
-	// TODO: Add the limit of binding token to the pool params?
 	if len(assets) > 8 {
 		return sdkerrors.Wrapf(types.ErrTooManyPoolAssets, "%d", len(assets))
 	}
@@ -106,8 +96,8 @@ func ValidateUserSpecifiedPoolAssets(assets []PoolAsset) error {
 	return nil
 }
 
-// PoolAssetsCoins returns all the coins corresponding to a slice of pool assets.
-func PoolAssetsCoins(assets []PoolAsset) sdk.Coins {
+// poolAssetsCoins returns all the coins corresponding to a slice of pool assets.
+func poolAssetsCoins(assets []PoolAsset) sdk.Coins {
 	coins := sdk.Coins{}
 	for _, asset := range assets {
 		coins = coins.Add(asset.Token)
@@ -115,7 +105,7 @@ func PoolAssetsCoins(assets []PoolAsset) sdk.Coins {
 	return coins
 }
 
-func GetPoolAssetByDenom(assets []PoolAsset, denom string) (PoolAsset, bool) {
+func getPoolAssetByDenom(assets []PoolAsset, denom string) (PoolAsset, bool) {
 	for _, asset := range assets {
 		if asset.Token.Denom == denom {
 			return asset, true
