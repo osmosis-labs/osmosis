aff5befbab622a0099e5934057846f6a481f3302
Dev Ojha <dojha@berkeley.edu>
2022-07-09 23:13:39 -0500

    Add more to genesis test that would've caught this

--- x/tokenfactory/keeper/genesis_test.go
+++ x/tokenfactory/keeper/genesis_test.go
@@ -3,6 +3,7 @@ package keeper_test
 import (
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
@@ -24,6 +25,12 @@ func TestGenesis(t *testing.T) {
 				},
 			},
 			{
+				Denom: "factory/osmo1t7egva48prqmzl59x5ngv4zx0dtrwewc9m7z44/diff-admin",
+				AuthorityMetadata: types.DenomAuthorityMetadata{
+					Admin: "osmo15czt5nhlnvayqq37xun9s9yus0d6y26dw9xnzn",
+				},
+			},
+			{
 				Denom: "factory/osmo1t7egva48prqmzl59x5ngv4zx0dtrwewc9m7z44/litecoin",
 				AuthorityMetadata: types.DenomAuthorityMetadata{
 					Admin: "osmo1t7egva48prqmzl59x5ngv4zx0dtrwewc9m7z44",
@@ -34,6 +41,7 @@ func TestGenesis(t *testing.T) {
 	app := simapp.Setup(false)
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
 
+	app.TokenFactoryKeeper.SetParams(ctx, types.Params{DenomCreationFee: sdk.Coins{sdk.NewInt64Coin("uosmo", 100)}})
 	app.TokenFactoryKeeper.InitGenesis(ctx, genesisState)
 	exportedGenesis := app.TokenFactoryKeeper.ExportGenesis(ctx)
 	require.NotNil(t, exportedGenesis)
