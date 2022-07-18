4727818095151d33350e7e483866526356662b25
Dev Ojha <dojha@berkeley.edu>
2022-07-18 14:56:22 -0500

    Reduce token factory complexity

--- x/tokenfactory/simulation/sim_msgs.go
+++ x/tokenfactory/simulation/sim_msgs.go
@@ -5,6 +5,7 @@ import (
 
 	legacysimulationtype "github.com/cosmos/cosmos-sdk/types/simulation"
 
+	"github.com/osmosis-labs/osmosis/v10/osmoutils"
 	simulation "github.com/osmosis-labs/osmosis/v10/simulation/types"
 	"github.com/osmosis-labs/osmosis/v10/x/tokenfactory/keeper"
 	"github.com/osmosis-labs/osmosis/v10/x/tokenfactory/types"
@@ -14,53 +15,37 @@ import (
 
 // RandomMsgCreateDenom creates a random tokenfactory denom that is no greater than 44 alphanumeric characters
 func RandomMsgCreateDenom(k keeper.Keeper, sim *simulation.SimCtx, ctx sdk.Context) (*types.MsgCreateDenom, error) {
-	// select a pseudo-random simulation account
-	sender := sim.RandomSimAccount()
-
-	subdenom := sim.RandStringOfLength(44)
-
 	return &types.MsgCreateDenom{
-		Sender:   sender.Address.String(),
-		Subdenom: subdenom,
+		Sender:   sim.RandomSimAccount().Address.String(),
+		Subdenom: sim.RandStringOfLength(types.MaxSubdenomLength),
 	}, nil
 }
 
 func RandomMsgMintDenom(k keeper.Keeper, sim *simulation.SimCtx, ctx sdk.Context) (*types.MsgMint, error) {
-	var tokenFactoryMint sdk.Coin
-	acc, senderExists := sim.RandomSimAccountWithConstraint(accountHasTokenFactoryDenomConstraint(k, ctx))
+	acc, senderExists := sim.RandomSimAccountWithConstraint(accountCreatedTokenFactoryDenom(k, ctx))
 	if !senderExists {
 		return nil, errors.New("no addr has created a tokenfactory coin")
 	}
+	// Pick denom
 	store := k.GetCreatorPrefixStore(ctx, acc.Address.String())
+	denoms := osmoutils.GatherAllKeysFromStore(store)
+	denom := simulation.RandSelect(sim, denoms...)
 
-	iterator := store.Iterator(nil, nil)
-	defer iterator.Close()
-
-	denoms := []string{}
-	for ; iterator.Valid(); iterator.Next() {
-		denoms = append(denoms, string(iterator.Key()))
-	}
-	randIndex := simulation.RandLTBound(sim, len(denoms))
-	denom := denoms[randIndex]
-	tokenFactoryMint.Denom = denom
-	tokenFactoryMint.Amount = sdk.NewInt(int64(sim.RandIntBetween(0, 1000000000000)))
+	// TODO: Replace with an improved rand exponential coin
+	mintAmount := sim.RandPositiveInt(sdk.NewIntFromUint64(1000_000000))
 	return &types.MsgMint{
 		Sender: acc.Address.String(),
-		Amount: tokenFactoryMint,
+		Amount: sdk.NewCoin(denom, mintAmount),
 	}, nil
 }
 
-func accountHasTokenFactoryDenomConstraint(k keeper.Keeper, ctx sdk.Context) simulation.SimAccountConstraint {
+// TODO: We are going to need to index the owner of an account as well, rather than creator
+// to simulate admin changes
+func accountCreatedTokenFactoryDenom(k keeper.Keeper, ctx sdk.Context) simulation.SimAccountConstraint {
 	return func(acc legacysimulationtype.Account) bool {
 		store := k.GetCreatorPrefixStore(ctx, acc.Address.String())
-
 		iterator := store.Iterator(nil, nil)
 		defer iterator.Close()
-
-		denoms := []string{}
-		for ; iterator.Valid(); iterator.Next() {
-			denoms = append(denoms, string(iterator.Key()))
-		}
-		return len(denoms) != 0
+		return iterator.Valid()
 	}
 }
