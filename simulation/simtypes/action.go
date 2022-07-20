84dce39240e19cb3c3f8c126de66bbe069998d78
Adam Tucker <adam@osmosis.team>
2022-07-19 20:50:22 -0500

    simulator: frequency management (#2156)
    * simulator weight management
    
    * Move action selection out of executor into types
    
    * linter changes
    
    * Update simulation/simtypes/weight.go
    
    Co-authored-by: Roman <roman@osmosis.team>
    
    * change from Roman review
    
    Co-authored-by: Roman <roman@osmosis.team>

--- simulation/simtypes/action.go
+++ simulation/simtypes/action.go
@@ -2,6 +2,7 @@ package simtypes
 
 import (
 	"fmt"
+	"math/rand"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/simulation"
@@ -11,29 +12,33 @@ import (
 
 // Action represents a simulator action.
 // The details of this struct are internal,
-// we currently plan on maintaining 3 constructors for it.
-// * ActionFromWeightedOperation - for legacy simulator compatibility
-// * ActionFromMsg - An easy API to go from creating a message via simctx to something simulator can deal with
-// * ActionFromDependentMessages - API for defining a series of messages that depend on one another, and should satisfy
-//   some properties post-execution.
+// we currently plan on maintaining 2 constructors for it.
+// * weightedOperationAction - for legacy simulator compatibility
+// * msgBasedAction - An easy API to go from creating a message via simctx to something simulator can deal with
 type Action interface {
 	Name() string
-	// I envision this weight being provided from a config.
+	// I envision this frequency being provided from a config.
 	// Module providers can optionally provide a default from an enum,
 	// but this should not be the default.
-	// TODO: Maybe we make a "WithWeight" fn?
-	Weight() Weight
+	Frequency() Frequency
 	Execute(*SimCtx, sdk.Context) (
 		OperationMsg simulation.OperationMsg, futureOps []simulation.FutureOperation, err error)
+	WithFrequency(w Frequency) Action
 }
+
+type selectActionFn func(r *rand.Rand) Action
 
 type weightedOperationAction struct {
 	moduleName string
+	frequency  Frequency
 	op         simulation.WeightedOperation
 }
 
-func (a weightedOperationAction) Name() string   { return fmt.Sprintf("%s: weighted_op", a.moduleName) }
-func (a weightedOperationAction) Weight() Weight { return Weight(a.op.Weight()) }
+func (a weightedOperationAction) WithFrequency(w Frequency) Action { a.frequency = w; return a }
+func (a weightedOperationAction) Name() string                     { return fmt.Sprintf("%s: weighted_op", a.moduleName) }
+func (a weightedOperationAction) Frequency() Frequency {
+	return Frequency(mapFrequencyFromInt(a.op.Weight()))
+}
 func (a weightedOperationAction) Execute(sim *SimCtx, ctx sdk.Context) (
 	simulation.OperationMsg, []simulation.FutureOperation, error,
 ) {
@@ -62,7 +67,7 @@ func NewKeeperlessMsgBasedAction[M sdk.Msg](actionName string, msgGenerator func
 	// TODO: Also do we even want this? Isn't the goal to write simulation event hooks based on Name
 	// var sampleMsg M
 	// msgName := osmoutils.GetType(sampleMsg)
-	return msgBasedAction{name: actionName, weight: Normal, msgGenerator: wrappedMsgGen}
+	return msgBasedAction{name: actionName, frequency: Common, msgGenerator: wrappedMsgGen}
 }
 
 func CurryMsgGenerator[K interface{}, M sdk.Msg](k K, f func(K, *SimCtx, sdk.Context) (M, error)) func(*SimCtx, sdk.Context) (M, error) {
@@ -81,17 +86,13 @@ func NewMsgBasedAction[K interface{}, M sdk.Msg](actionName string, k K, f func(
 
 type msgBasedAction struct {
 	name         string
-	weight       Weight
+	frequency    Frequency
 	msgGenerator func(sim *SimCtx, ctx sdk.Context) (sdk.Msg, error)
 }
 
-func (m msgBasedAction) WithWeight(weight Weight) msgBasedAction {
-	m.weight = weight
-	return m
-}
-
-func (m msgBasedAction) Name() string   { return m.name }
-func (m msgBasedAction) Weight() Weight { return m.weight }
+func (m msgBasedAction) WithFrequency(w Frequency) Action { m.frequency = w; return m }
+func (m msgBasedAction) Name() string                     { return m.name }
+func (m msgBasedAction) Frequency() Frequency             { return m.frequency }
 func (m msgBasedAction) Execute(sim *SimCtx, ctx sdk.Context) (
 	OperationMsg simulation.OperationMsg, futureOps []simulation.FutureOperation, err error,
 ) {
@@ -104,4 +105,31 @@ func (m msgBasedAction) Execute(sim *SimCtx, ctx sdk.Context) (
 		return simulation.NoOpMsg(m.name, m.name, fmt.Sprintf("unable to build tx due to: %v", err)), nil, err
 	}
 	return sim.deliverTx(tx, msg, m.name)
+}
+
+func totalFrequency(actions []Action) int {
+	totalFrequency := 0
+	for _, action := range actions {
+		totalFrequency += mapFrequencyFromString(action.Frequency())
+	}
+
+	return totalFrequency
+}
+
+func GetSelectActionFn(actions []Action) selectActionFn {
+	totalOpFrequency := totalFrequency(actions)
+
+	return func(r *rand.Rand) Action {
+		x := r.Intn(totalOpFrequency)
+		// TODO: Change to an accum list approach
+		for i := 0; i < len(actions); i++ {
+			if x <= mapFrequencyFromString(actions[i].Frequency()) {
+				return actions[i]
+			}
+
+			x -= mapFrequencyFromString(actions[i].Frequency())
+		}
+		// shouldn't happen
+		return actions[0]
+	}
 }
