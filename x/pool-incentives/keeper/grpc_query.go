371e6d16759d940642a3211b4cba28a9dc468f43
Xiangan He <xiangan9@gmail.com>
2022-07-29 08:54:34 -0400

    finished: additional docs

--- x/pool-incentives/keeper/grpc_query.go
+++ x/pool-incentives/keeper/grpc_query.go
@@ -25,6 +25,7 @@ func NewQuerier(k Keeper) Querier {
 	return Querier{Keeper: k}
 }
 
+// LockableDurations iterates over all gauges,returns gaugeIDs.
 func (q Querier) GaugeIds(ctx context.Context, req *types.QueryGaugeIdsRequest) (*types.QueryGaugeIdsResponse, error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "empty request")
@@ -61,16 +62,19 @@ func (q Querier) GaugeIds(ctx context.Context, req *types.QueryGaugeIdsRequest) 
 	return &types.QueryGaugeIdsResponse{GaugeIdsWithDuration: gaugeIdsWithDuration}, nil
 }
 
+// LockableDurations iterates over all gauges, returns distribution info.
 func (q Querier) DistrInfo(ctx context.Context, _ *types.QueryDistrInfoRequest) (*types.QueryDistrInfoResponse, error) {
 	sdkCtx := sdk.UnwrapSDKContext(ctx)
 	return &types.QueryDistrInfoResponse{DistrInfo: q.Keeper.GetDistrInfo(sdkCtx)}, nil
 }
 
+// Params iterates over all gauges, returns lock params.
 func (q Querier) Params(ctx context.Context, _ *types.QueryParamsRequest) (*types.QueryParamsResponse, error) {
 	sdkCtx := sdk.UnwrapSDKContext(ctx)
 	return &types.QueryParamsResponse{Params: q.Keeper.GetParams(sdkCtx)}, nil
 }
 
+// LockableDurations iterates over all gauges, returns gauges' lock duration
 func (q Querier) LockableDurations(ctx context.Context, _ *types.QueryLockableDurationsRequest) (*types.QueryLockableDurationsResponse, error) {
 	sdkCtx := sdk.UnwrapSDKContext(ctx)
 	return &types.QueryLockableDurationsResponse{LockableDurations: q.Keeper.GetLockableDurations(sdkCtx)}, nil
