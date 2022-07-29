e8b4fca9398c0806df978bdcae16304ff933303e
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2022-07-29 16:10:28 +0700

    refactor

--- x/superfluid/keeper/msg_server_test.go
+++ x/superfluid/keeper/msg_server_test.go
@@ -60,6 +60,140 @@ func (suite *KeeperTestSuite) TestMsgSuperfluidDelegate() {
 	}
 }
 
+func (suite *KeeperTestSuite) TestMsgSuperfluidUndelegate() {
+	type param struct {
+		coinsToLock         sdk.Coins
+		lockOwner           sdk.AccAddress
+		duration            time.Duration
+		coinsInOwnerAddress sdk.Coins
+	}
+
+	tests := []struct {
+		name       string
+		param      param
+		expectPass bool
+	}{
+		{
+			name: "superfluid undelegation for not superfluid delegated lockup",
+			param: param{
+				coinsToLock:         sdk.Coins{sdk.NewInt64Coin("stake", 10)},       // setup wallet
+				lockOwner:           sdk.AccAddress([]byte("addr1---------------")), // setup wallet
+				duration:            time.Second,
+				coinsInOwnerAddress: sdk.Coins{sdk.NewInt64Coin("stake", 10)},
+			},
+			expectPass: false,
+		},
+	}
+
+	for _, test := range tests {
+		suite.SetupTest()
+
+		suite.FundAcc(test.param.lockOwner, test.param.coinsInOwnerAddress)
+
+		lockupMsgServer := lockupkeeper.NewMsgServerImpl(suite.App.LockupKeeper)
+		c := sdk.WrapSDKContext(suite.Ctx)
+		resp, err := lockupMsgServer.LockTokens(c, lockuptypes.NewMsgLockTokens(test.param.lockOwner, test.param.duration, test.param.coinsToLock))
+
+		msgServer := keeper.NewMsgServerImpl(suite.App.SuperfluidKeeper)
+		_, err = msgServer.SuperfluidUndelegate(c, types.NewMsgSuperfluidUndelegate(test.param.lockOwner, resp.ID))
+
+		if test.expectPass {
+			suite.Require().NoError(err)
+		} else {
+			suite.Require().Error(err)
+		}
+	}
+}
+
+func (suite *KeeperTestSuite) TestMsgSuperfluidUnbondLock() {
+	type param struct {
+		coinsToLock         sdk.Coins
+		lockOwner           sdk.AccAddress
+		duration            time.Duration
+		coinsInOwnerAddress sdk.Coins
+	}
+
+	tests := []struct {
+		name       string
+		param      param
+		expectPass bool
+	}{
+		{
+			name: "superfluid unbond lock that is not superfluid lockup",
+			param: param{
+				coinsToLock:         sdk.Coins{sdk.NewInt64Coin("stake", 10)},       // setup wallet
+				lockOwner:           sdk.AccAddress([]byte("addr1---------------")), // setup wallet
+				duration:            time.Second,
+				coinsInOwnerAddress: sdk.Coins{sdk.NewInt64Coin("stake", 10)},
+			},
+			expectPass: false,
+		},
+	}
+
+	for _, test := range tests {
+		suite.SetupTest()
+
+		suite.FundAcc(test.param.lockOwner, test.param.coinsInOwnerAddress)
+
+		lockupMsgServer := lockupkeeper.NewMsgServerImpl(suite.App.LockupKeeper)
+		c := sdk.WrapSDKContext(suite.Ctx)
+		resp, err := lockupMsgServer.LockTokens(c, lockuptypes.NewMsgLockTokens(test.param.lockOwner, test.param.duration, test.param.coinsToLock))
+
+		msgServer := keeper.NewMsgServerImpl(suite.App.SuperfluidKeeper)
+		_, err = msgServer.SuperfluidUnbondLock(c, types.NewMsgSuperfluidUnbondLock(test.param.lockOwner, resp.ID))
+
+		if test.expectPass {
+			suite.Require().NoError(err)
+		} else {
+			suite.Require().Error(err)
+		}
+	}
+}
+
+func (suite *KeeperTestSuite) TestMsgLockAndSuperfluidDelegate() {
+	type param struct {
+		coinsToLock         sdk.Coins
+		lockOwner           sdk.AccAddress
+		duration            time.Duration
+		coinsInOwnerAddress sdk.Coins
+	}
+
+	tests := []struct {
+		name       string
+		param      param
+		expectPass bool
+	}{
+		{
+			name: "superfluid lock and superfluid delegate for not allowed asset",
+			param: param{
+				coinsToLock:         sdk.Coins{sdk.NewInt64Coin("stake", 10)},       // setup wallet
+				lockOwner:           sdk.AccAddress([]byte("addr1---------------")), // setup wallet
+				duration:            time.Second,
+				coinsInOwnerAddress: sdk.Coins{sdk.NewInt64Coin("stake", 10)},
+			},
+			expectPass: false,
+		},
+	}
+
+	for _, test := range tests {
+		suite.SetupTest()
+
+		suite.FundAcc(test.param.lockOwner, test.param.coinsInOwnerAddress)
+
+		c := sdk.WrapSDKContext(suite.Ctx)
+		valAddrs := suite.SetupValidators([]stakingtypes.BondStatus{stakingtypes.Bonded})
+
+		msgServer := keeper.NewMsgServerImpl(suite.App.SuperfluidKeeper)
+		_, err := msgServer.LockAndSuperfluidDelegate(c, types.NewMsgLockAndSuperfluidDelegate(test.param.lockOwner, test.param.coinsToLock, valAddrs[0]))
+
+		if test.expectPass {
+			suite.Require().NoError(err)
+		} else {
+			suite.Require().Error(err)
+		}
+	}
+}
+
 // TestMsgSuperfluidDelegate_Event tests that events are correctly emitted
 // when calling SuperfluidDelegate.
 func (suite *KeeperTestSuite) TestMsgSuperfluidDelegate_Event() {
@@ -110,51 +244,6 @@ func (suite *KeeperTestSuite) TestMsgSuperfluidDelegate_Event() {
 		if test.expectPass {
 			suite.Require().NoError(err)
 			assertEventEmitted(suite, suite.Ctx, types.TypeEvtSuperfluidDelegate, 1)
-		} else {
-			suite.Require().Error(err)
-		}
-	}
-}
-
-func (suite *KeeperTestSuite) TestMsgSuperfluidUndelegate() {
-	type param struct {
-		coinsToLock         sdk.Coins
-		lockOwner           sdk.AccAddress
-		duration            time.Duration
-		coinsInOwnerAddress sdk.Coins
-	}
-
-	tests := []struct {
-		name       string
-		param      param
-		expectPass bool
-	}{
-		{
-			name: "superfluid undelegation for not superfluid delegated lockup",
-			param: param{
-				coinsToLock:         sdk.Coins{sdk.NewInt64Coin("stake", 10)},       // setup wallet
-				lockOwner:           sdk.AccAddress([]byte("addr1---------------")), // setup wallet
-				duration:            time.Second,
-				coinsInOwnerAddress: sdk.Coins{sdk.NewInt64Coin("stake", 10)},
-			},
-			expectPass: false,
-		},
-	}
-
-	for _, test := range tests {
-		suite.SetupTest()
-
-		suite.FundAcc(test.param.lockOwner, test.param.coinsInOwnerAddress)
-
-		lockupMsgServer := lockupkeeper.NewMsgServerImpl(suite.App.LockupKeeper)
-		c := sdk.WrapSDKContext(suite.Ctx)
-		resp, err := lockupMsgServer.LockTokens(c, lockuptypes.NewMsgLockTokens(test.param.lockOwner, test.param.duration, test.param.coinsToLock))
-
-		msgServer := keeper.NewMsgServerImpl(suite.App.SuperfluidKeeper)
-		_, err = msgServer.SuperfluidUndelegate(c, types.NewMsgSuperfluidUndelegate(test.param.lockOwner, resp.ID))
-
-		if test.expectPass {
-			suite.Require().NoError(err)
 		} else {
 			suite.Require().Error(err)
 		}
@@ -220,51 +309,6 @@ func (suite *KeeperTestSuite) TestMsgSuperfluidUndelegate_Event() {
 	}
 }
 
-func (suite *KeeperTestSuite) TestMsgSuperfluidUnbondLock() {
-	type param struct {
-		coinsToLock         sdk.Coins
-		lockOwner           sdk.AccAddress
-		duration            time.Duration
-		coinsInOwnerAddress sdk.Coins
-	}
-
-	tests := []struct {
-		name       string
-		param      param
-		expectPass bool
-	}{
-		{
-			name: "superfluid unbond lock that is not superfluid lockup",
-			param: param{
-				coinsToLock:         sdk.Coins{sdk.NewInt64Coin("stake", 10)},       // setup wallet
-				lockOwner:           sdk.AccAddress([]byte("addr1---------------")), // setup wallet
-				duration:            time.Second,
-				coinsInOwnerAddress: sdk.Coins{sdk.NewInt64Coin("stake", 10)},
-			},
-			expectPass: false,
-		},
-	}
-
-	for _, test := range tests {
-		suite.SetupTest()
-
-		suite.FundAcc(test.param.lockOwner, test.param.coinsInOwnerAddress)
-
-		lockupMsgServer := lockupkeeper.NewMsgServerImpl(suite.App.LockupKeeper)
-		c := sdk.WrapSDKContext(suite.Ctx)
-		resp, err := lockupMsgServer.LockTokens(c, lockuptypes.NewMsgLockTokens(test.param.lockOwner, test.param.duration, test.param.coinsToLock))
-
-		msgServer := keeper.NewMsgServerImpl(suite.App.SuperfluidKeeper)
-		_, err = msgServer.SuperfluidUnbondLock(c, types.NewMsgSuperfluidUnbondLock(test.param.lockOwner, resp.ID))
-
-		if test.expectPass {
-			suite.Require().NoError(err)
-		} else {
-			suite.Require().Error(err)
-		}
-	}
-}
-
 // TestMsgSuperfluidUnbondLock_Event tests that events are correctly emitted
 // when calling SuperfluidUnbondLock.
 func (suite *KeeperTestSuite) TestMsgSuperfluidUnbondLock_Event() {
@@ -323,50 +367,6 @@ func (suite *KeeperTestSuite) TestMsgUnPoolWhitelistedPool_Event() {
 		_, err := msgServer.UnPoolWhitelistedPool(sdk.WrapSDKContext(suite.Ctx), types.NewMsgUnPoolWhitelistedPool(sender, poolId))
 		suite.Require().NoError(err)
 		assertEventEmitted(suite, suite.Ctx, types.TypeEvtUnpoolId, 1)
-	}
-}
-
-func (suite *KeeperTestSuite) TestMsgLockAndSuperfluidDelegate() {
-	type param struct {
-		coinsToLock         sdk.Coins
-		lockOwner           sdk.AccAddress
-		duration            time.Duration
-		coinsInOwnerAddress sdk.Coins
-	}
-
-	tests := []struct {
-		name       string
-		param      param
-		expectPass bool
-	}{
-		{
-			name: "superfluid lock and superfluid delegate for not allowed asset",
-			param: param{
-				coinsToLock:         sdk.Coins{sdk.NewInt64Coin("stake", 10)},       // setup wallet
-				lockOwner:           sdk.AccAddress([]byte("addr1---------------")), // setup wallet
-				duration:            time.Second,
-				coinsInOwnerAddress: sdk.Coins{sdk.NewInt64Coin("stake", 10)},
-			},
-			expectPass: false,
-		},
-	}
-
-	for _, test := range tests {
-		suite.SetupTest()
-
-		suite.FundAcc(test.param.lockOwner, test.param.coinsInOwnerAddress)
-
-		c := sdk.WrapSDKContext(suite.Ctx)
-		valAddrs := suite.SetupValidators([]stakingtypes.BondStatus{stakingtypes.Bonded})
-
-		msgServer := keeper.NewMsgServerImpl(suite.App.SuperfluidKeeper)
-		_, err := msgServer.LockAndSuperfluidDelegate(c, types.NewMsgLockAndSuperfluidDelegate(test.param.lockOwner, test.param.coinsToLock, valAddrs[0]))
-
-		if test.expectPass {
-			suite.Require().NoError(err)
-		} else {
-			suite.Require().Error(err)
-		}
 	}
 }
 
