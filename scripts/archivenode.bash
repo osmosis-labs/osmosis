183a4f9057f1957a21b604deaf690455c3c78e4e
Jacob Gadikian <jacobgadikian@gmail.com>
2022-08-02 22:56:15 +0700

    Update archivenode.bash

--- scripts/archivenode.bash
+++ scripts/archivenode.bash
@@ -32,7 +32,7 @@ go mod tidy
 go install -ldflags '-w -s -X github.com/cosmos/cosmos-sdk/types.DBBackend=pebbledb' -tags pebbledb ./...
 osmosisd init speedrun
 wget -O ~/.osmosisd/config/genesis.json https://github.com/osmosis-labs/networks/raw/main/osmosis-1/genesis.json
-time -o howlong -a osmosisd start --db_backend pebbledb
+osmosisd start --db_backend pebbledb
 git reset --hard
 
 # VERSION FOUR
@@ -41,7 +41,7 @@ git checkout v4.x
 go mod edit -replace github.com/tendermint/tm-db=github.com/notional-labs/tm-db@136c7b6
 go mod tidy
 go install -ldflags '-w -s -X github.com/cosmos/cosmos-sdk/types.DBBackend=pebbledb' -tags pebbledb ./...
-time -o howlong -a osmosisd start --db_backend pebbledb
+osmosisd start --db_backend pebbledb
 git reset --hard
 
 
@@ -51,7 +51,7 @@ git checkout v6.x
 go mod edit -replace github.com/tendermint/tm-db=github.com/notional-labs/tm-db@136c7b6
 go mod tidy
 go install -ldflags '-w -s -X github.com/cosmos/cosmos-sdk/types.DBBackend=pebbledb' -tags pebbledb ./...
-time -o howlong -a osmosisd start --db_backend pebbledb
+osmosisd start --db_backend pebbledb
 git reset --hard
 
 
@@ -62,7 +62,7 @@ git checkout v7.x
 go mod edit -replace github.com/tendermint/tm-db=github.com/notional-labs/tm-db@136c7b6
 go mod tidy
 go install -ldflags '-w -s -X github.com/cosmos/cosmos-sdk/types.DBBackend=pebbledb' -tags pebbledb ./...
-time -o howlong -a osmosisd start --db_backend pebbledb
+osmosisd start --db_backend pebbledb
 git reset --hard
 
 
@@ -73,7 +73,7 @@ git checkout v8.x
 go mod edit -replace github.com/tendermint/tm-db=github.com/notional-labs/tm-db@136c7b6
 go mod tidy
 go install -ldflags '-w -s -X github.com/cosmos/cosmos-sdk/types.DBBackend=pebbledb' -tags pebbledb ./...
-time -o howlong -a osmosisd start --db_backend pebbledb
+osmosisd start --db_backend pebbledb
 git reset --hard
 
 
@@ -83,7 +83,7 @@ git checkout v9.x
 go mod edit -replace github.com/tendermint/tm-db=github.com/notional-labs/tm-db@136c7b6
 go mod tidy
 go install -ldflags '-w -s -X github.com/cosmos/cosmos-sdk/types.DBBackend=pebbledb' -tags pebbledb ./...time osmosisd start --db_backend pebbledb
-time -o howlong -a osmosisd start --db_backend pebbledb
+osmosisd start --db_backend pebbledb
 git reset --hard
 
 
@@ -93,5 +93,5 @@ git checkout v10.x
 go mod edit -replace github.com/tendermint/tm-db=github.com/notional-labs/tm-db@136c7b6
 go mod tidy
 go install -ldflags '-w -s -X github.com/cosmos/cosmos-sdk/types.DBBackend=pebbledb' -tags pebbledb ./...
-time -o howlong -a osmosisd start --db_backend pebbledb
+osmosisd start --db_backend pebbledb
 
