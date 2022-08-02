e1ae6f1f76390ee583365b9f29add527e63734d4
Xiangan He <xiangan9@gmail.com>
2022-08-02 14:28:12 -0400

    mint module test fix

--- x/mint/keeper/keeper_test.go
+++ x/mint/keeper/keeper_test.go
@@ -390,8 +390,8 @@ func (suite *KeeperTestSuite) TestSetInitialSupplyOffsetDuringMigration() {
 				return
 			}
 			suite.Require().NoError(actualError)
-			suite.Require().Equal(supplyWithOffsetBefore.Amount.Sub(sdk.NewInt(keeper.DeveloperVestingAmount)), bankKeeper.GetSupplyWithOffset(ctx, sdk.DefaultBondDenom).Amount)
-			suite.Require().Equal(supplyOffsetBefore.Sub(sdk.NewInt(keeper.DeveloperVestingAmount)), bankKeeper.GetSupplyOffset(ctx, sdk.DefaultBondDenom))
+			suite.Require().Equal(supplyWithOffsetBefore.Amount.Sub(sdk.NewInt(keeper.DeveloperVestingAmount)), sdk.ZeroInt())
+			suite.Require().Equal(supplyOffsetBefore.Sub(sdk.NewInt(keeper.DeveloperVestingAmount)), sdk.ZeroInt())
 		})
 	}
 }
