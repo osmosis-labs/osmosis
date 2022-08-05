11344a665d6001b779f9f25ab94f06eec88f59db
vuong <nguyenvuong1122000@gmail.com>
2022-08-05 15:34:53 +0700

    superfluid

--- x/superfluid/types/msg_test.go
+++ x/superfluid/types/msg_test.go
@@ -1,18 +1,14 @@
-package types
+package types_test
 
 import (
-	"encoding/json"
 	"testing"
-	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	authzcodec "github.com/cosmos/cosmos-sdk/x/authz/codec"
 
+	"github.com/osmosis-labs/osmosis/v10/app/apptesting"
 	appParams "github.com/osmosis-labs/osmosis/v10/app/params"
+	"github.com/osmosis-labs/osmosis/v10/x/superfluid/types"
 
-	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
-	"github.com/cosmos/cosmos-sdk/x/authz"
-	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/crypto/ed25519"
 )
 
@@ -22,7 +18,6 @@ func TestAuthzMsg(t *testing.T) {
 	pk1 := ed25519.GenPrivKey().PubKey()
 	addr1 := sdk.AccAddress(pk1.Address()).String()
 	coin := sdk.NewCoin("stake", sdk.NewInt(1))
-	someDate := time.Date(1, 1, 1, 1, 1, 1, 1, time.UTC)
 
 	const (
 		mockGranter string = "cosmos1abc"
@@ -35,7 +30,7 @@ func TestAuthzMsg(t *testing.T) {
 	}{
 		{
 			name: "MsgLockAndSuperfluidDelegate",
-			msg: &MsgLockAndSuperfluidDelegate{
+			msg: &types.MsgLockAndSuperfluidDelegate{
 				Sender:  addr1,
 				Coins:   sdk.NewCoins(coin),
 				ValAddr: "valoper1xyz",
@@ -43,7 +38,7 @@ func TestAuthzMsg(t *testing.T) {
 		},
 		{
 			name: "MsgSuperfluidDelegate",
-			msg: &MsgSuperfluidDelegate{
+			msg: &types.MsgSuperfluidDelegate{
 				Sender:  addr1,
 				LockId:  1,
 				ValAddr: "valoper1xyz",
@@ -51,21 +46,21 @@ func TestAuthzMsg(t *testing.T) {
 		},
 		{
 			name: "MsgSuperfluidUnbondLock",
-			msg: &MsgSuperfluidUnbondLock{
+			msg: &types.MsgSuperfluidUnbondLock{
 				Sender: addr1,
 				LockId: 1,
 			},
 		},
 		{
 			name: "MsgSuperfluidUndelegate",
-			msg: &MsgSuperfluidUndelegate{
+			msg: &types.MsgSuperfluidUndelegate{
 				Sender: addr1,
 				LockId: 1,
 			},
 		},
 		{
 			name: "MsgUnPoolWhitelistedPool",
-			msg: &MsgUnPoolWhitelistedPool{
+			msg: &types.MsgUnPoolWhitelistedPool{
 				Sender: addr1,
 				PoolId: 1,
 			},
@@ -73,35 +68,7 @@ func TestAuthzMsg(t *testing.T) {
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			var (
-				mockMsgGrant  authz.MsgGrant
-				mockMsgRevoke authz.MsgRevoke
-				mockMsgExec   authz.MsgExec
-			)
-
-			// Authz: Grant Msg
-			typeURL := sdk.MsgTypeURL(tc.msg)
-			grant, err := authz.NewGrant(someDate, authz.NewGenericAuthorization(typeURL), someDate.Add(time.Hour))
-			require.NoError(t, err)
-
-			msgGrant := authz.MsgGrant{Granter: mockGranter, Grantee: mockGrantee, Grant: grant}
-			msgGrantBytes := json.RawMessage(sdk.MustSortJSON(authzcodec.ModuleCdc.MustMarshalJSON(&msgGrant)))
-			err = authzcodec.ModuleCdc.UnmarshalJSON(msgGrantBytes, &mockMsgGrant)
-			require.NoError(t, err)
-
-			// Authz: Revoke Msg
-			msgRevoke := authz.MsgRevoke{Granter: mockGranter, Grantee: mockGrantee, MsgTypeUrl: typeURL}
-			msgRevokeByte := json.RawMessage(sdk.MustSortJSON(authzcodec.ModuleCdc.MustMarshalJSON(&msgRevoke)))
-			err = authzcodec.ModuleCdc.UnmarshalJSON(msgRevokeByte, &mockMsgRevoke)
-			require.NoError(t, err)
-
-			// Authz: Exec Msg
-			msgAny, _ := cdctypes.NewAnyWithValue(tc.msg)
-			msgExec := authz.MsgExec{Grantee: mockGrantee, Msgs: []*cdctypes.Any{msgAny}}
-			execMsgByte := json.RawMessage(sdk.MustSortJSON(authzcodec.ModuleCdc.MustMarshalJSON(&msgExec)))
-			err = authzcodec.ModuleCdc.UnmarshalJSON(execMsgByte, &mockMsgExec)
-			require.NoError(t, err)
-			require.Equal(t, msgExec.Msgs[0].Value, mockMsgExec.Msgs[0].Value)
+			apptesting.TestMessageAuthzSerialization(t, tc.msg)
 		})
 	}
 }
