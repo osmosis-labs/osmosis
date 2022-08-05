1ad2e65869c5b35108513bd8d4a53d3b2e0a25fd
vuong <nguyenvuong1122000@gmail.com>
2022-08-05 15:36:32 +0700

    token factory

--- x/tokenfactory/types/msgs_test.go
+++ x/tokenfactory/types/msgs_test.go
@@ -1,18 +1,14 @@
-package types
+package types_test
 
 import (
-	"encoding/json"
 	"testing"
-	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	"github.com/osmosis-labs/osmosis/v10/app/apptesting"
 	appParams "github.com/osmosis-labs/osmosis/v10/app/params"
+	"github.com/osmosis-labs/osmosis/v10/x/tokenfactory/types"
 
-	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
-	"github.com/cosmos/cosmos-sdk/x/authz"
-	authzcodec "github.com/cosmos/cosmos-sdk/x/authz/codec"
-	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/crypto/ed25519"
 )
 
@@ -22,7 +18,6 @@ func TestAuthzMsg(t *testing.T) {
 	pk1 := ed25519.GenPrivKey().PubKey()
 	addr1 := sdk.AccAddress(pk1.Address()).String()
 	coin := sdk.NewCoin("denom", sdk.NewInt(1))
-	someDate := time.Date(1, 1, 1, 1, 1, 1, 1, time.UTC)
 
 	const (
 		mockGranter string = "cosmos1abc"
@@ -35,28 +30,28 @@ func TestAuthzMsg(t *testing.T) {
 	}{
 		{
 			name: "MsgCreateDenom",
-			msg: &MsgCreateDenom{
+			msg: &types.MsgCreateDenom{
 				Sender:   addr1,
 				Subdenom: "valoper1xyz",
 			},
 		},
 		{
 			name: "MsgBurn",
-			msg: &MsgBurn{
+			msg: &types.MsgBurn{
 				Sender: addr1,
 				Amount: coin,
 			},
 		},
 		{
 			name: "MsgMint",
-			msg: &MsgMint{
+			msg: &types.MsgMint{
 				Sender: addr1,
 				Amount: coin,
 			},
 		},
 		{
 			name: "MsgChangeAdmin",
-			msg: &MsgChangeAdmin{
+			msg: &types.MsgChangeAdmin{
 				Sender:   addr1,
 				Denom:    "denom",
 				NewAdmin: "osmo1q8tq5qhrhw6t970egemuuwywhlhpnmdmts6xnu",
@@ -65,36 +60,7 @@ func TestAuthzMsg(t *testing.T) {
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			var (
-				mockMsgGrant  authz.MsgGrant
-				mockMsgRevoke authz.MsgRevoke
-				mockMsgExec   authz.MsgExec
-			)
-
-			// Authz: Grant Msg
-			typeURL := sdk.MsgTypeURL(tc.msg)
-			grant, err := authz.NewGrant(someDate, authz.NewGenericAuthorization(typeURL), someDate.Add(time.Hour))
-			require.NoError(t, err)
-
-			msgGrant := authz.MsgGrant{Granter: mockGranter, Grantee: mockGrantee, Grant: grant}
-			msgGrantBytes := json.RawMessage(sdk.MustSortJSON(authzcodec.ModuleCdc.MustMarshalJSON(&msgGrant)))
-			err = authzcodec.ModuleCdc.UnmarshalJSON(msgGrantBytes, &mockMsgGrant)
-			require.NoError(t, err)
-
-			// Authz: Revoke Msg
-			msgRevoke := authz.MsgRevoke{Granter: mockGranter, Grantee: mockGrantee, MsgTypeUrl: typeURL}
-			msgRevokeByte := json.RawMessage(sdk.MustSortJSON(authzcodec.ModuleCdc.MustMarshalJSON(&msgRevoke)))
-			err = authzcodec.ModuleCdc.UnmarshalJSON(msgRevokeByte, &mockMsgRevoke)
-			require.NoError(t, err)
-
-			// Authz: Exec Msg
-			msgAny, _ := cdctypes.NewAnyWithValue(tc.msg)
-			msgExec := authz.MsgExec{Grantee: mockGrantee, Msgs: []*cdctypes.Any{msgAny}}
-			execMsgByte := json.RawMessage(sdk.MustSortJSON(authzcodec.ModuleCdc.MustMarshalJSON(&msgExec)))
-			err = authzcodec.ModuleCdc.UnmarshalJSON(execMsgByte, &mockMsgExec)
-			require.NoError(t, err)
-			require.Equal(t, msgExec.Msgs[0].Value, mockMsgExec.Msgs[0].Value)
-
+			apptesting.TestMessageAuthzSerialization(t, tc.msg)
 		})
 	}
 }
