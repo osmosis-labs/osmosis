e4476c378ff6792faaa5ab9ada21add198e79271
khanh-notional <50263489+catShaark@users.noreply.github.com>
2022-08-05 21:45:24 +0700

    Update x/gamm/pool-models/stableswap/pool.go
    Co-authored-by: Dev Ojha <ValarDragon@users.noreply.github.com>

--- x/gamm/pool-models/stableswap/pool.go
+++ x/gamm/pool-models/stableswap/pool.go
@@ -24,7 +24,7 @@ func NewStableswapPool(poolId uint64, stableswapPoolParams PoolParams, initialLi
 	if len(scalingFactors) == 0 {
 		scalingFactors = []uint64{1, 1}
 	} else if scalingFactors[0] == 0 || scalingFactors[1] == 0 {
-		scalingFactors = []uint64{1, 1}
+		return Pooll{}, types.ErrInvalidStableswapScalingFactors
 	}
 
 	pool := Pool{
