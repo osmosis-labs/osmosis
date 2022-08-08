945a34838bc0e0752846a566ca2ccf76b2d12277
Xiangan He <76530366+xBalbinus@users.noreply.github.com>
2022-08-08 08:01:15 -0400

    Update x/pool-incentives/types/expected_keepers.go
    Co-authored-by: Adam Tucker <adam@osmosis.team>

--- x/pool-incentives/types/expected_keepers.go
+++ x/pool-incentives/types/expected_keepers.go
@@ -20,7 +20,7 @@ type AccountKeeper interface {
 	GetModuleAccount(ctx sdk.Context, name string) authtypes.ModuleAccountI
 }
 
-// The BankKeeper sends tokens across modules and is able to get account balances.
+// BankKeeper sends tokens across modules and is able to get account balances.
 type BankKeeper interface {
 	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
 
