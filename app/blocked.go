ba2098e455d7e3172a48a2ba02fac2e557b8f58d
Jacob Gadikian <jacobgadikian@gmail.com>
2022-08-09 13:50:25 +0700

    format blocked.go

--- app/blocked.go
+++ app/blocked.go
@@ -17,7 +17,7 @@ func (app *OsmosisApp) BlockedAddrs() map[string]bool {
 	// We block all OFAC-blocked ETH addresses from receiving tokens as well
 	// The list is sourced from: https://www.treasury.gov/ofac/downloads/sanctions/1.0/sdn_advanced.xml
 	// List updated with tornado cash addresses Aug 9 2022
-	// we should ensure that each build of osmosis contains all blocked addresses in this file.  
+	// we should ensure that each build of osmosis contains all blocked addresses in this file.
 	ofacRawEthAddrs := []string{
 		"0x7F367cC41522cE07553e823bf3be79A889DEbe1B",
 		"0xd882cfc20f52f2599d84b8e8d58c7fb62cfe344b",
@@ -44,7 +44,7 @@ func (app *OsmosisApp) BlockedAddrs() map[string]bool {
 		"0xc455f7fd3e0e12afd51fba5c106909934d8a0e4a",
 		"0xfec8a60023265364d066a1212fde3930f6ae8da7",
 		"0x8589427373D6D84E98730D7795D8f6f8731FDA16",
-		"0x722122dF12D4e14e13Ac3b6895a86e84145b6967", 
+		"0x722122dF12D4e14e13Ac3b6895a86e84145b6967",
 		"0xDD4c48C0B24039969fC16D1cdF626eaB821d3384",
 		"0xd90e2f925DA726b50C4Ed8D0Fb90Ad053324F31b",
 		"0xd96f2B1c14Db8458374d9Aca76E26c3D18364307",
@@ -55,12 +55,12 @@ func (app *OsmosisApp) BlockedAddrs() map[string]bool {
 		"0xFD8610d20aA15b7B2E3Be39B396a1bC3516c7144",
 		"0xF60dD140cFf0706bAE9Cd734Ac3ae76AD9eBC32A",
 		"0x22aaA7720ddd5388A3c0A3333430953C68f1849b",
-		"0xBA214C1c1928a32Bffe790263E38B4Af9bFCD659", 
+		"0xBA214C1c1928a32Bffe790263E38B4Af9bFCD659",
 		"0xb1C8094B234DcE6e03f10a5b673c1d8C69739A00",
 		"0x527653eA119F3E6a1F5BD18fbF4714081D7B31ce",
 		"0x58E8dCC13BE9780fC42E8723D8EaD4CF46943dF2",
 		"0xD691F27f38B395864Ea86CfC7253969B409c362d",
-		"0xaEaaC358560e11f52454D997AAFF2c5731B6f8a6", 
+		"0xaEaaC358560e11f52454D997AAFF2c5731B6f8a6",
 		"0x1356c899D8C9467C7f71C195612F8A395aBf2f0a",
 		"0xA60C772958a3eD56c1F15dD055bA37AC8e523a0D",
 		"0x169AD27A470D064DEDE56a2D3ff727986b15D52B",
@@ -77,7 +77,7 @@ func (app *OsmosisApp) BlockedAddrs() map[string]bool {
 		"0xD21be7248e0197Ee08E0c20D4a96DEBdaC3D20Af",
 		"0x610B717796ad172B316836AC95a2ffad065CeaB4",
 		"0x178169B423a011fff22B9e3F3abeA13414dDD0F1",
-		"0xbB93e510BbCD0B7beb5A853875f9eC60275CF498", 
+		"0xbB93e510BbCD0B7beb5A853875f9eC60275CF498",
 		"0x2717c5e28cf931547B621a5dddb772Ab6A35B701",
 		"0x03893a7c7463AE47D46bc7f091665f1893656003",
 		"0xCa0840578f57fE71599D29375e16783424023357",
@@ -87,7 +87,7 @@ func (app *OsmosisApp) BlockedAddrs() map[string]bool {
 		"0xDD4c48C0B24039969fC16D1cdF626eaB821d3384",
 		"0xd90e2f925DA726b50C4Ed8D0Fb90Ad053324F31b",
 		"0xd96f2B1c14Db8458374d9Aca76E26c3D18364307",
-		"0x4736dCf1b7A3d580672CcE6E7c65cd5cc9cFBa9D", 
+		"0x4736dCf1b7A3d580672CcE6E7c65cd5cc9cFBa9D",
 	}
 	for _, addr := range ofacRawEthAddrs {
 		blockedAddrs[addr] = true
@@ -96,4 +96,3 @@ func (app *OsmosisApp) BlockedAddrs() map[string]bool {
 
 	return blockedAddrs
 }
-
