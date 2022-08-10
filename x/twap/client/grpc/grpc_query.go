6b83205743b5c08857b629a2b87f6ecf6aab572d
Dev Ojha <dojha@berkeley.edu>
2022-08-10 10:20:10 -0500

    Add a GRPC query for TWAP

---
+++ x/twap/client/grpc/grpc_query.go
@@ -0,0 +1,38 @@
+package grpc
+
+import (
+	context "context"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
+
+	"github.com/osmosis-labs/osmosis/v10/x/twap"
+)
+
+// This file should evolve to being code gen'd, off of `proto/twap/v1beta/query.yml`
+
+type Querier struct {
+	K twap.Keeper
+}
+
+var _ QueryServer = Querier{}
+
+func (q Querier) GetArithmeticTwap(grpcCtx context.Context,
+	req *GetArithmeticTwapRequest,
+) (*GetArithmeticTwapResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+	if (req.EndTime == nil || *req.EndTime == time.Time{}) {
+		*req.EndTime = time.Now()
+	}
+
+	ctx := sdk.UnwrapSDKContext(grpcCtx)
+	twap, err := q.K.GetArithmeticTwap(ctx, req.PoolId, req.BaseAsset, req.QuoteAsset, req.StartTime, *req.EndTime)
+	if err != nil {
+		return nil, err
+	}
+	return &GetArithmeticTwapResponse{twap}, nil
+}
