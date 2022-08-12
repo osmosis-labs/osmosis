5d55217b6528fca6d8fdad7721971bcd458a65a3
Roman <ackhtariev@gmail.com>
2022-08-12 13:27:47 -0400

    fix(ci): import paths script to work with query gen

--- scripts/replace_import_paths.sh
+++ scripts/replace_import_paths.sh
@@ -17,23 +17,17 @@ replace_paths() {
     sed -i "s/github.com\/osmosis-labs\/osmosis\/v${version_to_replace}/github.com\/osmosis-labs\/osmosis\/v${NEXT_MAJOR_VERSION}/g" ${file}
 }
 
-echo "Replacing import paths in Go files"
-for mod in $modules;
-do
-    for file in $mod/*; do
-        if [ -f "${file}" ]; then
-            replace_paths $file
-        fi
-    done
-done
+echo "Replacing import paths in all files"
+pwd
 
-echo "Replacing import paths in proto files"
-for file in $(find proto/osmosis -type f -name "*.proto"); do
-    replace_paths $file
-done
+files=$(find ./ -type f -and -not \( -path './vendor*' -or -path './.git*' -or -name '*.md' \))
 
-# protocgen.sh
-replace_paths "scripts/protocgen.sh"
+echo "Updating all files"
+for file in $files; do
+    if [ -f "${file}" ]; then
+        replace_paths ${file}
+    fi
+done
 
 echo "Updating go.mod and vendoring"
 # go.mod
@@ -41,6 +35,7 @@ replace_paths "go.mod"
 go mod vendor >/dev/null
 
 echo "running make proto-gen"
-# ensure protos match generated Go files
+# ensure that generated files are updated.
 # N.B.: This must be run after go mod vendor.
 make proto-gen >/dev/null
+make run-querygen >/dev/null
