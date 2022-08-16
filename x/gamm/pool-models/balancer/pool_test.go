5a2e5a77a213dd6e1810b3a70b6faa4635622863
Richard Liu <rliu@nightwatchapp.com>
2022-08-15 19:39:28 -0700

    x/gamm: Add PoolI mocks and test for inactive pools (#2165)
    * add mock_pool.go interface
    
    * add mock_pool.go interface
    
    * make sure to increment
    
    * table driven + add active pool for control
    
    * cleanup error checks
    
    * Apply suggestions from code review
    
    Co-authored-by: Matt, Park <45252226+mattverse@users.noreply.github.com>
    
    * add commentsg
    
    * pr comments
    
    * fix typo
    
    * fix test failures
    
    Co-authored-by: Matt, Park <45252226+mattverse@users.noreply.github.com>
    Co-authored-by: Adam Tucker <adam@osmosis.team>

--- x/gamm/pool-models/balancer/pool_test.go
+++ x/gamm/pool-models/balancer/pool_test.go
@@ -1195,7 +1195,6 @@ func TestBalancerPoolPokeTokenWeights(t *testing.T) {
 
 // This test (currently trivially) checks to make sure that `IsActive` returns true for balancer pools.
 // This is mainly to make sure that if IsActive is ever used as an emergency switch, it is not accidentally left off for any (or all) pools.
-// TODO: create a test with mocks to make sure IsActive works as intended when flipped for specific pools/all pools
 func TestIsActive(t *testing.T) {
 	tests := map[string]struct {
 		expectedIsActive bool
