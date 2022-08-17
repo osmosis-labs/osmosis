7734c20cc1d23a9d3cdea3f9b8d4ee13ca1a20c6
Adam Tucker <adam@osmosis.team>
2022-08-17 17:26:20 -0500

    address Roman's comment

--- scripts/mutation-test.sh
+++ scripts/mutation-test.sh
@@ -8,7 +8,8 @@ DISABLED_MUTATORS='branch/*'
 # Only consider the following:
 # * go files in types, keeper, or module root directories
 # * ignore test and Protobuf files
-MUTATION_SOURCES=$(find ./x -type f \( -path '*/keeper/*' -or -path '*/types/*' -or -path '*' \) \( -name '*.go' -and -not -name '*_test.go' -and -not -name '*pb*' \))
+MUTATION_SOURCES=$(find ./x -type f \( -path '*/keeper/*' -or -path '*/types/*' \) \( -name '*.go' -and -not -name '*_test.go' -and -not -name '*pb*' \))
+MUTATION_SOURCES+=$(find ./x -maxdepth 2 -type f \( -name '*.go' -and -not -name '*_test.go' -and -not -name '*pb*' \))
 
 # Filter on a module-by-module basis as provided by input
 arg_len=$#
@@ -28,22 +29,23 @@ MUTATION_SOURCES=$(echo "$MUTATION_SOURCES" | grep "$MODULE_FORMAT")
 
 # Collect multiple lines into a single line to be fed into go-mutesting
 MUTATION_SOURCES=$(echo $MUTATION_SOURCES | tr '\n' ' ')
+echo $MUTATION_SOURCES
 
-echo "running mutation tests for the following module(s): $MODULE_NAMES"
-OUTPUT=$(go run github.com/osmosis-labs/go-mutesting/cmd/go-mutesting --disable=$DISABLED_MUTATORS $MUTATION_SOURCES)
+# echo "running mutation tests for the following module(s): $MODULE_NAMES"
+# OUTPUT=$(go run github.com/osmosis-labs/go-mutesting/cmd/go-mutesting --disable=$DISABLED_MUTATORS $MUTATION_SOURCES)
 
-# Fetch the final result output and the overall mutation testing score
-RESULT=$(echo "$OUTPUT" | grep 'The mutation score')
-SCORE=$(echo "$RESULT" | grep -Eo '[[:digit:]]\.[[:digit:]]+')
+# # Fetch the final result output and the overall mutation testing score
+# RESULT=$(echo "$OUTPUT" | grep 'The mutation score')
+# SCORE=$(echo "$RESULT" | grep -Eo '[[:digit:]]\.[[:digit:]]+')
 
-echo "writing mutation test result to mutation_test_result.txt"
-echo "$OUTPUT" > mutation_test_result.txt
+# echo "writing mutation test result to mutation_test_result.txt"
+# echo "$OUTPUT" > mutation_test_result.txt
 
-# Print the mutation score breakdown
-echo $RESULT
+# # Print the mutation score breakdown
+# echo $RESULT
 
-# Return a non-zero exit code if the score is below 75%
-if (( $(echo "$SCORE < 0.75" |bc -l) )); then
-  echo "Mutation testing score below desired level ($SCORE < 0.75)"
-  exit 1
-fi
+# # Return a non-zero exit code if the score is below 75%
+# if (( $(echo "$SCORE < 0.75" |bc -l) )); then
+#   echo "Mutation testing score below desired level ($SCORE < 0.75)"
+#   exit 1
+# fi
