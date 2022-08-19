afbc10fff217e7e14c59bd0dc345a351ec7b359f
Roman <roman@osmosis.team>
2022-08-19 13:24:50 -0400

    feat(twap): init and export genesis (#2451)
    * fix(twap/upgrade): set TWAP parameters
    
    * feat(twap/appwiring): register parameters correctly; import and export genesis
    
    * Update x/twap/twapmodule/module.go
    
    * move genesis logic to keeper
    
    * ensure correct order in init and export genesis
    
    * lint
    
    * validate genesis state and twap record
    
    * genesis fix
    
    * update genesis errors
    
    * Update x/twap/keeper.go
    
    * Update x/twap/keeper.go
    
    * Update x/twap/keeper.go
    
    * Update x/twap/types/genesis.go
    
    * Update x/twap/twapmodule/module.go
    
    * Update x/twap/types/export_test.go
    
    * genesis merge conflict

--- x/twap/keeper_test.go
+++ x/twap/keeper_test.go
@@ -1,6 +1,7 @@
 package twap_test
 
 import (
+	"sort"
 	"testing"
 	"time"
 
@@ -8,6 +9,7 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"github.com/osmosis-labs/osmosis/v11/app/apptesting"
+	"github.com/osmosis-labs/osmosis/v11/app/apptesting/osmoassert"
 	"github.com/osmosis-labs/osmosis/v11/x/twap"
 	"github.com/osmosis-labs/osmosis/v11/x/twap/types"
 )
@@ -36,6 +38,239 @@ func (s *TestSuite) SetupTest() {
 	s.Setup()
 	s.twapkeeper = s.App.TwapKeeper
 	s.Ctx = s.Ctx.WithBlockTime(baseTime)
+}
+
+var (
+	basicParams = types.NewParams("week")
+
+	mostRecentRecordPoolOne = types.TwapRecord{
+		PoolId:                      basePoolId,
+		Asset0Denom:                 denom0,
+		Asset1Denom:                 denom1,
+		Height:                      3,
+		Time:                        tPlusOne.Add(time.Second),
+		P0LastSpotPrice:             sdk.OneDec(),
+		P1LastSpotPrice:             sdk.OneDec(),
+		P0ArithmeticTwapAccumulator: sdk.OneDec(),
+		P1ArithmeticTwapAccumulator: sdk.OneDec(),
+	}
+
+	basicCustomGenesis = types.NewGenesisState(
+		basicParams,
+		[]types.TwapRecord{
+			mostRecentRecordPoolOne,
+		})
+
+	increasingOrderByTimeRecordsPoolOne = types.NewGenesisState(
+		basicParams,
+		[]types.TwapRecord{
+			{
+				PoolId:                      basePoolId,
+				Asset0Denom:                 denom0,
+				Asset1Denom:                 denom1,
+				Height:                      1,
+				Time:                        baseTime,
+				P0LastSpotPrice:             sdk.OneDec(),
+				P1LastSpotPrice:             sdk.OneDec(),
+				P0ArithmeticTwapAccumulator: sdk.OneDec(),
+				P1ArithmeticTwapAccumulator: sdk.OneDec(),
+			},
+			{
+				PoolId:                      basePoolId,
+				Asset0Denom:                 denom0,
+				Asset1Denom:                 denom1,
+				Height:                      2,
+				Time:                        tPlusOne,
+				P0LastSpotPrice:             sdk.OneDec(),
+				P1LastSpotPrice:             sdk.OneDec(),
+				P0ArithmeticTwapAccumulator: sdk.OneDec(),
+				P1ArithmeticTwapAccumulator: sdk.OneDec(),
+			},
+			mostRecentRecordPoolOne,
+		})
+
+	mostRecentRecordPoolTwo = types.TwapRecord{
+		PoolId:                      basePoolId,
+		Asset0Denom:                 denom0,
+		Asset1Denom:                 denom2,
+		Height:                      1,
+		Time:                        tPlusOne.Add(time.Second),
+		P0LastSpotPrice:             sdk.OneDec(),
+		P1LastSpotPrice:             sdk.OneDec(),
+		P0ArithmeticTwapAccumulator: sdk.OneDec(),
+		P1ArithmeticTwapAccumulator: sdk.OneDec(),
+	}
+
+	decreasingOrderByTimeRecordsPoolTwo = types.NewGenesisState(
+		basicParams,
+		[]types.TwapRecord{
+			mostRecentRecordPoolTwo,
+			{
+				PoolId:                      basePoolId,
+				Asset0Denom:                 denom0,
+				Asset1Denom:                 denom2,
+				Height:                      2,
+				Time:                        tPlusOne,
+				P0LastSpotPrice:             sdk.OneDec(),
+				P1LastSpotPrice:             sdk.OneDec(),
+				P0ArithmeticTwapAccumulator: sdk.OneDec(),
+				P1ArithmeticTwapAccumulator: sdk.OneDec(),
+			},
+			{
+				PoolId:                      basePoolId,
+				Asset0Denom:                 denom0,
+				Asset1Denom:                 denom2,
+				Height:                      3,
+				Time:                        baseTime,
+				P0LastSpotPrice:             sdk.OneDec(),
+				P1LastSpotPrice:             sdk.OneDec(),
+				P0ArithmeticTwapAccumulator: sdk.OneDec(),
+				P1ArithmeticTwapAccumulator: sdk.OneDec(),
+			},
+		})
+
+	bothPoolsGenesis = types.NewGenesisState(
+		basicParams,
+		append(increasingOrderByTimeRecordsPoolOne.Twaps, decreasingOrderByTimeRecordsPoolTwo.Twaps...),
+	)
+)
+
+// TestTWAPInitGenesis tests that genesis is initialized correctly
+// with different parameters and state.
+// Asserts that the most recent records are set correctly.
+func (suite *TestSuite) TestTwapInitGenesis() {
+	testCases := map[string]struct {
+		twapGenesis *types.GenesisState
+
+		expectPanic bool
+
+		expectedMostRecentRecord []types.TwapRecord
+	}{
+		"default genesis - success": {
+			twapGenesis: types.DefaultGenesis(),
+		},
+		"custom valid genesis; success": {
+			twapGenesis: basicCustomGenesis,
+
+			expectedMostRecentRecord: []types.TwapRecord{
+				mostRecentRecordPoolOne,
+			},
+		},
+		"custom valid multi record; increasing; success": {
+			twapGenesis: increasingOrderByTimeRecordsPoolOne,
+
+			expectedMostRecentRecord: []types.TwapRecord{
+				mostRecentRecordPoolOne,
+			},
+		},
+		"custom valid multi record; decreasing (sorted internally); success": {
+			twapGenesis: decreasingOrderByTimeRecordsPoolTwo,
+
+			expectedMostRecentRecord: []types.TwapRecord{
+				mostRecentRecordPoolTwo,
+			},
+		},
+		"custom valid multi record and multi pool; success": {
+			twapGenesis: bothPoolsGenesis,
+
+			expectedMostRecentRecord: []types.TwapRecord{
+				mostRecentRecordPoolTwo, mostRecentRecordPoolOne,
+			},
+		},
+		"custom invalid genesis - error": {
+			twapGenesis: types.NewGenesisState(
+				types.NewParams("week"),
+				[]types.TwapRecord{
+					{
+						PoolId:                      0, // invalid
+						Asset0Denom:                 "test1",
+						Asset1Denom:                 "test2",
+						Height:                      1,
+						Time:                        baseTime,
+						P0LastSpotPrice:             sdk.OneDec(),
+						P1LastSpotPrice:             sdk.OneDec(),
+						P0ArithmeticTwapAccumulator: sdk.OneDec(),
+						P1ArithmeticTwapAccumulator: sdk.OneDec(),
+					},
+				}),
+
+			expectPanic: true,
+		},
+	}
+
+	for name, tc := range testCases {
+		suite.Run(name, func() {
+			suite.Setup()
+			// Setup.
+			ctx := suite.Ctx
+			twapKeeper := suite.App.TwapKeeper
+
+			// Test.
+			osmoassert.ConditionalPanic(suite.T(), tc.expectPanic, func() { twapKeeper.InitGenesis(ctx, tc.twapGenesis) })
+			if tc.expectPanic {
+				return
+			}
+
+			// Assertions.
+
+			// Parameters were set.
+			suite.Require().Equal(tc.twapGenesis.Params, twapKeeper.GetParams(ctx))
+
+			for _, expectedMostRecentRecord := range tc.expectedMostRecentRecord {
+				record, err := twapKeeper.GetMostRecentRecordStoreRepresentation(ctx, expectedMostRecentRecord.PoolId, expectedMostRecentRecord.Asset0Denom, expectedMostRecentRecord.Asset1Denom)
+				suite.Require().NoError(err)
+				suite.Require().Equal(expectedMostRecentRecord, record)
+			}
+		})
+	}
+}
+
+// TestTWAPExportGenesis tests that genesis is exported correctly.
+// It first initializes genesis to the expected value. Then, attempts
+// to export it. Lastly, compares exported to the expected.
+func (suite *TestSuite) TestTWAPExportGenesis() {
+	testCases := map[string]struct {
+		expectedGenesis *types.GenesisState
+	}{
+		"default genesis": {
+			expectedGenesis: types.DefaultGenesis(),
+		},
+		"custom genesis": {
+			expectedGenesis: basicCustomGenesis,
+		},
+		"custom multi-record; increasing": {
+			expectedGenesis: increasingOrderByTimeRecordsPoolOne,
+		},
+		"custom multi-record; decreasing": {
+			expectedGenesis: decreasingOrderByTimeRecordsPoolTwo,
+		},
+	}
+
+	for name, tc := range testCases {
+		suite.Run(name, func() {
+			suite.Setup()
+			// Setup.
+			app := suite.App
+			ctx := suite.Ctx
+			twapKeeper := app.TwapKeeper
+
+			twapKeeper.InitGenesis(ctx, tc.expectedGenesis)
+
+			// Test.
+			actualGenesis := twapKeeper.ExportGenesis(ctx)
+
+			// Assertions.
+			suite.Require().Equal(tc.expectedGenesis.Params, actualGenesis.Params)
+
+			// Sort expected by time. This is done because the exported genesis returns
+			// recors in ascending order by time.
+			sort.Slice(tc.expectedGenesis.Twaps, func(i, j int) bool {
+				return tc.expectedGenesis.Twaps[i].Time.Before(tc.expectedGenesis.Twaps[j].Time)
+			})
+
+			suite.Require().Equal(tc.expectedGenesis.Twaps, actualGenesis.Twaps)
+		})
+	}
 }
 
 // sets up a new two asset pool, with spot price 1
