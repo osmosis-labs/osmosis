f541e4d124d5f338f8ef9246612b1e19f1d50a6b
Adam Tucker <adam@osmosis.team>
2022-08-20 09:23:01 -0500

    separate join pool test

--- x/twap/listeners_test.go
+++ x/twap/listeners_test.go
@@ -34,13 +34,59 @@ func (s *TestSuite) TestSwapTriggeringTrackPoolId() {
 	s.Require().Equal([]uint64{poolId}, s.twapkeeper.GetChangedPools(s.Ctx))
 }
 
-// TestJoinSwapAndEndBlockTriggeringSave tests that if we:
+// TestSwapAndEndBlockTriggeringSave tests that if we:
 // * create a pool in block 1
-// * join that pool and swap in block 2
+// * swap in block 2
 // then after block 2 end block, we have saved records for the pool,
 // for both block 1 & 2, with distinct spot prices in their records, and accumulators incremented.
 // TODO: Abstract this to be more table driven, and test more pool / block setups.
-func (s *TestSuite) TestJoinSwapAndEndBlockTriggeringSave() {
+func (s *TestSuite) TestSwapAndEndBlockTriggeringSave() {
+	s.Ctx = s.Ctx.WithBlockTime(baseTime)
+	poolId := s.PrepareUni2PoolWithAssets(defaultUniV2Coins[0], defaultUniV2Coins[1])
+	expectedHistoricalTwap, err := types.NewTwapRecord(s.App.GAMMKeeper, s.Ctx, poolId, denom0, denom1)
+	s.Require().NoError(err)
+
+	s.EndBlock()
+	s.Commit() // clear transient store
+	// Now on a clean state after a create pool
+	s.Require().Equal(baseTime.Add(time.Second), s.Ctx.BlockTime())
+	s.RunBasicSwap(poolId)
+
+	// accumulators are default right here
+	expectedLatestTwapUpToAccum, err := types.NewTwapRecord(s.App.GAMMKeeper, s.Ctx, poolId, denom0, denom1)
+	s.Require().NoError(err)
+	// ensure different spot prices
+	s.Require().NotEqual(expectedHistoricalTwap.P0LastSpotPrice, expectedLatestTwapUpToAccum.P0LastSpotPrice)
+	s.Require().NotEqual(expectedHistoricalTwap.P1LastSpotPrice, expectedLatestTwapUpToAccum.P1LastSpotPrice)
+
+	s.EndBlock()
+
+	// check records
+	historicalOldTwap, err := s.twapkeeper.GetRecordAtOrBeforeTime(s.Ctx, poolId, baseTime, denom0, denom1)
+	s.Require().NoError(err)
+	s.Require().Equal(expectedHistoricalTwap, historicalOldTwap)
+
+	latestTwap, err := s.twapkeeper.GetMostRecentRecordStoreRepresentation(s.Ctx, poolId, denom0, denom1)
+	s.Require().NoError(err)
+	s.Require().Equal(latestTwap.P0LastSpotPrice, expectedLatestTwapUpToAccum.P0LastSpotPrice)
+	s.Require().Equal(latestTwap.P1LastSpotPrice, expectedLatestTwapUpToAccum.P1LastSpotPrice)
+
+	latestTwap2, err := s.twapkeeper.GetRecordAtOrBeforeTime(s.Ctx, poolId, s.Ctx.BlockTime(), denom0, denom1)
+	s.Require().NoError(err)
+	s.Require().Equal(latestTwap, latestTwap2)
+
+	// check accumulators incremented - we test details of correct increment in logic
+	s.Require().True(latestTwap.P0ArithmeticTwapAccumulator.GT(historicalOldTwap.P0ArithmeticTwapAccumulator))
+	s.Require().True(latestTwap.P1ArithmeticTwapAccumulator.GT(historicalOldTwap.P1ArithmeticTwapAccumulator))
+}
+
+// TestJoinSwapAndEndBlockTriggeringSave tests that if we:
+// * create a pool in block 1
+// * join that pool in block 2
+// then after block 2 end block, we have saved records for the pool,
+// for both block 1 & 2, with distinct spot prices in their records, and accumulators incremented.
+// TODO: Abstract this to be more table driven, and test more pool / block setups.
+func (s *TestSuite) TestJoinAndEndBlockTriggeringSave() {
 	s.Ctx = s.Ctx.WithBlockTime(baseTime)
 	poolId := s.PrepareUni2PoolWithAssets(defaultUniV2Coins[0], defaultUniV2Coins[1])
 	expectedHistoricalTwap, err := types.NewTwapRecord(s.App.GAMMKeeper, s.Ctx, poolId, denom0, denom1)
@@ -51,14 +97,10 @@ func (s *TestSuite) TestJoinSwapAndEndBlockTriggeringSave() {
 	// Now on a clean state after a create pool
 	s.Require().Equal(baseTime.Add(time.Second), s.Ctx.BlockTime())
 	s.RunBasicJoin(poolId)
-	s.RunBasicSwap(poolId)
 
 	// accumulators are default right here
 	expectedLatestTwapUpToAccum, err := types.NewTwapRecord(s.App.GAMMKeeper, s.Ctx, poolId, denom0, denom1)
 	s.Require().NoError(err)
-	// ensure different spot prices
-	s.Require().NotEqual(expectedHistoricalTwap.P0LastSpotPrice, expectedLatestTwapUpToAccum.P0LastSpotPrice)
-	s.Require().NotEqual(expectedHistoricalTwap.P1LastSpotPrice, expectedLatestTwapUpToAccum.P1LastSpotPrice)
 
 	s.EndBlock()
 
