118875d12d05314987bc3251bb8236fcc1bf93a5
Adam Tucker <adam@osmosis.team>
2022-08-22 19:15:52 -0500

    only test message emission

--- x/tokenfactory/keeper/msg_server_test.go
+++ x/tokenfactory/keeper/msg_server_test.go
@@ -3,8 +3,6 @@ package keeper_test
 import (
 	"fmt"
 
-	"github.com/stretchr/testify/suite"
-
 	"github.com/osmosis-labs/osmosis/v11/x/tokenfactory/keeper"
 	"github.com/osmosis-labs/osmosis/v11/x/tokenfactory/types"
 
@@ -12,15 +10,8 @@ import (
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
-var _ = suite.TestingSuite(nil)
-
-// TestMintDenom ensures the following properties of the MintMessage:
-// * Noone can mint tokens for a denom that doesn't exist
-// * Only the admin of a denom can mint tokens for it
-// * The admin of a denom can mint tokens for it
+// TestMintDenomMsg tests TypeMsgMint message is emitted on a successful mint
 func (suite *KeeperTestSuite) TestMintDenomMsg() {
-	var addr0bal int64
-
 	// Create a denom
 	suite.CreateDefaultDenom()
 
@@ -40,13 +31,6 @@ func (suite *KeeperTestSuite) TestMintDenomMsg() {
 			valid:     false,
 		},
 		{
-			desc:      "mint is not by the admin",
-			amount:    10,
-			mintDenom: suite.defaultDenom,
-			admin:     suite.TestAccs[1].String(),
-			valid:     false,
-		},
-		{
 			desc:                  "success case",
 			amount:                10,
 			mintDenom:             suite.defaultDenom,
@@ -58,28 +42,19 @@ func (suite *KeeperTestSuite) TestMintDenomMsg() {
 		suite.Run(fmt.Sprintf("Case %s", tc.desc), func() {
 			ctx := suite.Ctx.WithEventManager(sdk.NewEventManager())
 			suite.Require().Equal(0, len(ctx.EventManager().Events()))
-			// Test minting to admins own account
-			response, err := suite.msgServer.Mint(sdk.WrapSDKContext(ctx), types.NewMsgMint(tc.admin, sdk.NewInt64Coin(tc.mintDenom, 10)))
-			if tc.valid {
-				addr0bal += 10
-				suite.Require().NoError(err)
-				suite.Require().NotNil(response)
-				suite.Require().Equal(suite.App.BankKeeper.GetBalance(ctx, suite.TestAccs[0], suite.defaultDenom).Amount.Int64(), addr0bal, suite.App.BankKeeper.GetBalance(ctx, suite.TestAccs[0], suite.defaultDenom))
-			} else {
-				suite.Require().Error(err)
-
-			}
+			// Test mint message
+			suite.msgServer.Mint(sdk.WrapSDKContext(ctx), types.NewMsgMint(tc.admin, sdk.NewInt64Coin(tc.mintDenom, 10)))
+			// Ensure current number and type of event is emitted
 			suite.AssertEventEmitted(ctx, types.TypeMsgMint, tc.expectedMessageEvents)
 		})
 	}
 }
 
+// TestBurnDenomMsg tests TypeMsgBurn message is emitted on a successful burn
 func (suite *KeeperTestSuite) TestBurnDenomMsg() {
 	var addr0bal int64
-
 	// Create a denom.
 	suite.CreateDefaultDenom()
-
 	// mint 10 default token for testAcc[0]
 	suite.msgServer.Mint(sdk.WrapSDKContext(suite.Ctx), types.NewMsgMint(suite.TestAccs[0].String(), sdk.NewInt64Coin(suite.defaultDenom, 10)))
 	addr0bal += 10
@@ -94,28 +69,12 @@ func (suite *KeeperTestSuite) TestBurnDenomMsg() {
 	}{
 		{
 			desc:      "denom does not exist",
-			amount:    10,
 			burnDenom: "factory/osmo1t7egva48prqmzl59x5ngv4zx0dtrwewc9m7z44/evmos",
 			admin:     suite.TestAccs[0].String(),
 			valid:     false,
 		},
 		{
-			desc:      "burn is not by the admin",
-			amount:    10,
-			burnDenom: suite.defaultDenom,
-			admin:     suite.TestAccs[1].String(),
-			valid:     false,
-		},
-		{
-			desc:      "burn amount is bigger than minted amount",
-			amount:    1000,
-			burnDenom: suite.defaultDenom,
-			admin:     suite.TestAccs[1].String(),
-			valid:     false,
-		},
-		{
 			desc:                  "success case",
-			amount:                10,
 			burnDenom:             suite.defaultDenom,
 			admin:                 suite.TestAccs[0].String(),
 			valid:                 true,
@@ -125,22 +84,15 @@ func (suite *KeeperTestSuite) TestBurnDenomMsg() {
 		suite.Run(fmt.Sprintf("Case %s", tc.desc), func() {
 			ctx := suite.Ctx.WithEventManager(sdk.NewEventManager())
 			suite.Require().Equal(0, len(ctx.EventManager().Events()))
-			// Test minting to admins own account
-			response, err := suite.msgServer.Burn(sdk.WrapSDKContext(ctx), types.NewMsgBurn(tc.admin, sdk.NewInt64Coin(tc.burnDenom, 10)))
-			if tc.valid {
-				addr0bal -= 10
-				suite.Require().NoError(err)
-				suite.Require().NotNil(response)
-				suite.Require().True(suite.App.BankKeeper.GetBalance(ctx, suite.TestAccs[0], suite.defaultDenom).Amount.Int64() == addr0bal, suite.App.BankKeeper.GetBalance(ctx, suite.TestAccs[0], suite.defaultDenom))
-			} else {
-				suite.Require().Error(err)
-				suite.Require().True(suite.App.BankKeeper.GetBalance(ctx, suite.TestAccs[0], suite.defaultDenom).Amount.Int64() == addr0bal, suite.App.BankKeeper.GetBalance(ctx, suite.TestAccs[0], suite.defaultDenom))
-			}
+			// Test burn message
+			suite.msgServer.Burn(sdk.WrapSDKContext(ctx), types.NewMsgBurn(tc.admin, sdk.NewInt64Coin(tc.burnDenom, 10)))
+			// Ensure current number and type of event is emitted
 			suite.AssertEventEmitted(ctx, types.TypeMsgBurn, tc.expectedMessageEvents)
 		})
 	}
 }
 
+// TestCreateDenomMsg tests TypeMsgCreateDenom message is emitted on a successful denom creation
 func (suite *KeeperTestSuite) TestCreateDenomMsg() {
 	defaultDenomCreationFee := types.Params{DenomCreationFee: sdk.NewCoins(sdk.NewCoin("uosmo", sdk.NewInt(50000000)))}
 	for _, tc := range []struct {
@@ -170,35 +122,15 @@ func (suite *KeeperTestSuite) TestCreateDenomMsg() {
 			suite.Require().Equal(0, len(ctx.EventManager().Events()))
 			// Set denom creation fee in params
 			keeper.Keeper.SetParams(*suite.App.TokenFactoryKeeper, ctx, tc.denomCreationFee)
-			denomCreationFee := suite.App.TokenFactoryKeeper.GetParams(ctx).DenomCreationFee
-			suite.Require().Equal(tc.denomCreationFee.DenomCreationFee, denomCreationFee)
-
-			// note balance, create a tokenfactory denom, then note balance again
-			preCreateBalance := suite.App.BankKeeper.GetAllBalances(ctx, suite.TestAccs[0])
-			res, err := suite.msgServer.CreateDenom(sdk.WrapSDKContext(ctx), types.NewMsgCreateDenom(suite.TestAccs[0].String(), tc.subdenom))
-			postCreateBalance := suite.App.BankKeeper.GetAllBalances(ctx, suite.TestAccs[0])
-			if tc.valid {
-				suite.Require().NoError(err)
-				suite.Require().True(preCreateBalance.Sub(postCreateBalance).IsEqual(denomCreationFee))
-
-				// Make sure that the admin is set correctly
-				queryRes, err := suite.queryClient.DenomAuthorityMetadata(ctx.Context(), &types.QueryDenomAuthorityMetadataRequest{
-					Denom: res.GetNewTokenDenom(),
-				})
-
-				suite.Require().NoError(err)
-				suite.Require().Equal(suite.TestAccs[0].String(), queryRes.AuthorityMetadata.Admin)
-
-			} else {
-				suite.Require().Error(err)
-				// Ensure we don't charge if we expect an error
-				suite.Require().True(preCreateBalance.IsEqual(postCreateBalance))
-			}
+			// Test create denom message
+			suite.msgServer.CreateDenom(sdk.WrapSDKContext(ctx), types.NewMsgCreateDenom(suite.TestAccs[0].String(), tc.subdenom))
+			// Ensure current number and type of event is emitted
 			suite.AssertEventEmitted(ctx, types.TypeMsgCreateDenom, tc.expectedMessageEvents)
 		})
 	}
 }
 
+// TestChangeAdminDenomMsg tests TypeMsgChangeAdmin message is emitted on a successful admin change
 func (suite *KeeperTestSuite) TestChangeAdminDenomMsg() {
 	for _, tc := range []struct {
 		desc                    string
@@ -209,19 +141,6 @@ func (suite *KeeperTestSuite) TestChangeAdminDenomMsg() {
 		expectedMintPass        bool
 		expectedMessageEvents   int
 	}{
-		{
-			desc: "creator admin can't mint after setting to '' ",
-			msgChangeAdmin: func(denom string) *types.MsgChangeAdmin {
-				return types.NewMsgChangeAdmin(suite.TestAccs[0].String(), denom, "")
-			},
-			expectedChangeAdminPass: true,
-			expectedMessageEvents:   1,
-			expectedAdminIndex:      -1,
-			msgMint: func(denom string) *types.MsgMint {
-				return types.NewMsgMint(suite.TestAccs[0].String(), sdk.NewInt64Coin(denom, 5))
-			},
-			expectedMintPass: false,
-		},
 		{
 			desc: "non-admins can't change the existing admin",
 			msgChangeAdmin: func(denom string) *types.MsgChangeAdmin {
@@ -249,54 +168,21 @@ func (suite *KeeperTestSuite) TestChangeAdminDenomMsg() {
 			suite.SetupTest()
 			ctx := suite.Ctx.WithEventManager(sdk.NewEventManager())
 			suite.Require().Equal(0, len(ctx.EventManager().Events()))
-
 			// Create a denom and mint
 			res, err := suite.msgServer.CreateDenom(sdk.WrapSDKContext(ctx), types.NewMsgCreateDenom(suite.TestAccs[0].String(), "bitcoin"))
 			suite.Require().NoError(err)
-
 			testDenom := res.GetNewTokenDenom()
-
-			_, err = suite.msgServer.Mint(sdk.WrapSDKContext(ctx), types.NewMsgMint(suite.TestAccs[0].String(), sdk.NewInt64Coin(testDenom, 10)))
-			suite.Require().NoError(err)
-
-			response, err := suite.msgServer.ChangeAdmin(sdk.WrapSDKContext(ctx), tc.msgChangeAdmin(testDenom))
-			if tc.expectedChangeAdminPass {
-				suite.Require().NoError(err)
-				suite.Require().NotNil(response)
-			} else {
-				suite.Require().Error(err)
-			}
-
+			suite.msgServer.Mint(sdk.WrapSDKContext(ctx), types.NewMsgMint(suite.TestAccs[0].String(), sdk.NewInt64Coin(testDenom, 10)))
+			// Test change admin message
+			suite.msgServer.ChangeAdmin(sdk.WrapSDKContext(ctx), tc.msgChangeAdmin(testDenom))
+			// Ensure current number and type of event is emitted
 			suite.AssertEventEmitted(ctx, types.TypeMsgChangeAdmin, tc.expectedMessageEvents)
-
-			queryRes, err := suite.queryClient.DenomAuthorityMetadata(ctx.Context(), &types.QueryDenomAuthorityMetadataRequest{
-				Denom: testDenom,
-			})
-			suite.Require().NoError(err)
-
-			// expectedAdminIndex with negative value is assumed as admin with value of ""
-			const emptyStringAdminIndexFlag = -1
-			if tc.expectedAdminIndex == emptyStringAdminIndexFlag {
-				suite.Require().Equal("", queryRes.AuthorityMetadata.Admin)
-			} else {
-				suite.Require().Equal(suite.TestAccs[tc.expectedAdminIndex].String(), queryRes.AuthorityMetadata.Admin)
-			}
-
-			// we test mint to test if admin authority is performed properly after admin change.
-			if tc.msgMint != nil {
-				_, err := suite.msgServer.Mint(sdk.WrapSDKContext(ctx), tc.msgMint(testDenom))
-				if tc.expectedMintPass {
-					suite.Require().NoError(err)
-				} else {
-					suite.Require().Error(err)
-				}
-			}
 		})
 	}
 }
 
+// TestSetDenomMetaDataMsg tests TypeMsgSetDenomMetadata message is emitted on a successful denom metadata change
 func (suite *KeeperTestSuite) TestSetDenomMetaDataMsg() {
-
 	// setup test
 	suite.SetupTest()
 	suite.CreateDefaultDenom()
@@ -350,80 +236,13 @@ func (suite *KeeperTestSuite) TestSetDenomMetaDataMsg() {
 			}),
 			expectedPass: false,
 		},
-		{
-			desc: "non-factory denom",
-			msgSetDenomMetadata: *types.NewMsgSetDenomMetadata(suite.TestAccs[0].String(), banktypes.Metadata{
-				Description: "yeehaw",
-				DenomUnits: []*banktypes.DenomUnit{
-					{
-						Denom:    "uosmo",
-						Exponent: 0,
-					},
-					{
-						Denom:    "uosmoo",
-						Exponent: 6,
-					},
-				},
-				Base:    "uosmo",
-				Display: "uosmoo",
-				Name:    "OSMO",
-				Symbol:  "OSMO",
-			}),
-			expectedPass: false,
-		},
-		{
-			desc: "wrong admin",
-			msgSetDenomMetadata: *types.NewMsgSetDenomMetadata(suite.TestAccs[1].String(), banktypes.Metadata{
-				Description: "yeehaw",
-				DenomUnits: []*banktypes.DenomUnit{
-					{
-						Denom:    suite.defaultDenom,
-						Exponent: 0,
-					},
-					{
-						Denom:    "uosmo",
-						Exponent: 6,
-					},
-				},
-				Base:    suite.defaultDenom,
-				Display: "uosmo",
-				Name:    "OSMO",
-				Symbol:  "OSMO",
-			}),
-			expectedPass: false,
-		},
-		{
-			desc: "invalid metadata (missing display denom unit)",
-			msgSetDenomMetadata: *types.NewMsgSetDenomMetadata(suite.TestAccs[0].String(), banktypes.Metadata{
-				Description: "yeehaw",
-				DenomUnits: []*banktypes.DenomUnit{
-					{
-						Denom:    suite.defaultDenom,
-						Exponent: 0,
-					},
-				},
-				Base:    suite.defaultDenom,
-				Display: "uosmo",
-				Name:    "OSMO",
-				Symbol:  "OSMO",
-			}),
-			expectedPass: false,
-		},
 	} {
 		suite.Run(fmt.Sprintf("Case %s", tc.desc), func() {
 			ctx := suite.Ctx.WithEventManager(sdk.NewEventManager())
 			suite.Require().Equal(0, len(ctx.EventManager().Events()))
-			response, err := suite.msgServer.SetDenomMetadata(sdk.WrapSDKContext(ctx), &tc.msgSetDenomMetadata)
-			if tc.expectedPass {
-				suite.Require().NoError(err)
-				suite.Require().NotNil(response)
-
-				md, found := suite.App.BankKeeper.GetDenomMetaData(ctx, suite.defaultDenom)
-				suite.Require().True(found)
-				suite.Require().Equal(tc.msgSetDenomMetadata.Metadata.Name, md.Name)
-			} else {
-				suite.Require().Error(err)
-			}
+			// Test set denom metadata message
+			suite.msgServer.SetDenomMetadata(sdk.WrapSDKContext(ctx), &tc.msgSetDenomMetadata)
+			// Ensure current number and type of event is emitted
 			suite.AssertEventEmitted(ctx, types.TypeMsgSetDenomMetadata, tc.expectedMessageEvents)
 		})
 	}
