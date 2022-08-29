ac87e7d846b47c6977b6a4a369d46206757ab372
mattverse <mattpark1028@gmail.com>
2022-08-30 00:36:18 +0900

    Add IBC transfer types msg transfer

--- app/upgrades/v12/upgrades.go
+++ app/upgrades/v12/upgrades.go
@@ -8,7 +8,10 @@ import (
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	ibctransfertypes "github.com/cosmos/ibc-go/v3/modules/apps/transfer/types"
+
 	icahosttypes "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/host/types"
+
 	gammtypes "github.com/osmosis-labs/osmosis/v11/x/gamm/types"
 	superfluidtypes "github.com/osmosis-labs/osmosis/v11/x/superfluid/types"
 
@@ -54,6 +57,9 @@ func CreateUpgradeHandler(
 		hostParams := icahosttypes.Params{
 			HostEnabled: true,
 			AllowMessages: []string{
+				// Change: Added MsgTrasnsfer
+				sdk.MsgTypeURL(&ibctransfertypes.MsgTransfer{}),
+
 				sdk.MsgTypeURL(&banktypes.MsgSend{}),
 				sdk.MsgTypeURL(&stakingtypes.MsgDelegate{}),
 				sdk.MsgTypeURL(&stakingtypes.MsgBeginRedelegate{}),
