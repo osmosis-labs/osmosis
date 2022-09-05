487c5209fa63f9204ff3ccecc9fbd9eb60670ffd
Robert Zaremba <robert@zaremba.ch>
2022-09-06 00:51:37 +0200

    update keys

--- x/streamswap/keys.go
+++ x/streamswap/keys.go
@@ -4,9 +4,6 @@ const (
 	// ModuleName is the module name constant used in many places
 	ModuleName = "streamswap"
 
-	// RouterKey is the message route for authz
+	// RouterKey is the message route key
 	RouterKey = ModuleName
-
-	// QuerierRoute is the querier route for authz
-	QuerierRoute = ModuleName
 )
