c4766c7302de294de902d69c26e192bb4ecfa747
Dev Ojha <ValarDragon@users.noreply.github.com>
2022-09-09 00:29:17 +0200

    Fix lexicographic ordering of get all most recent keys for pool (#2665)
    * Add failing test to demonstrate error
    
    * Commit bug fix

--- x/twap/types/keys.go
+++ x/twap/types/keys.go
@@ -42,7 +42,8 @@ var (
 // TODO: make utility command to automatically interlace separators
 
 func FormatMostRecentTWAPKey(poolId uint64, denom1, denom2 string) []byte {
-	return []byte(fmt.Sprintf("%s%d%s%s%s%s", mostRecentTWAPsPrefix, poolId, KeySeparator, denom1, KeySeparator, denom2))
+	poolIdS := osmoutils.FormatFixedLengthU64(poolId)
+	return []byte(fmt.Sprintf("%s%s%s%s%s%s", mostRecentTWAPsPrefix, poolIdS, KeySeparator, denom1, KeySeparator, denom2))
 }
 
 // TODO: Replace historical management with ORM, we currently accept 2x write amplification right now.
@@ -97,8 +98,10 @@ func ParseTimeFromHistoricalPoolIndexKey(key []byte) (time.Time, error) {
 // GetAllMostRecentTwapsForPool returns all of the most recent twap records for a pool id.
 // if the pool id doesn't exist, then this returns a blank list.
 func GetAllMostRecentTwapsForPool(store sdk.KVStore, poolId uint64) ([]TwapRecord, error) {
-	startPrefix := fmt.Sprintf("%s%d%s", mostRecentTWAPsPrefix, poolId, KeySeparator)
-	endPrefix := fmt.Sprintf("%s%d%s", mostRecentTWAPsPrefix, poolId+1, KeySeparator)
+	poolIdS := osmoutils.FormatFixedLengthU64(poolId)
+	poolIdPlusOneS := osmoutils.FormatFixedLengthU64(poolId + 1)
+	startPrefix := fmt.Sprintf("%s%s%s", mostRecentTWAPsPrefix, poolIdS, KeySeparator)
+	endPrefix := fmt.Sprintf("%s%s%s", mostRecentTWAPsPrefix, poolIdPlusOneS, KeySeparator)
 	return osmoutils.GatherValuesFromStore(store, []byte(startPrefix), []byte(endPrefix), ParseTwapFromBz)
 }
 
