dba0541f08f969552ce17e8a1286ed8b09544487
alpo <yukseloglua@berkeley.edu>
2022-09-10 14:04:22 -0700

    add checks for invalid cfmm inputs

--- x/gamm/pool-models/stableswap/amm_test.go
+++ x/gamm/pool-models/stableswap/amm_test.go
@@ -19,42 +19,70 @@ func TestCFMMInvariantTwoAssets(t *testing.T) {
 	kErrTolerance := sdk.OneDec()
 
 	tests := []struct {
-		xReserve sdk.Dec
-		yReserve sdk.Dec
-		yIn      sdk.Dec
+		xReserve    sdk.Dec
+		yReserve    sdk.Dec
+		yIn         sdk.Dec
+		expectPanic bool
 	}{
 		{
 			sdk.NewDec(100),
 			sdk.NewDec(100),
 			sdk.NewDec(1),
+			false,
 		},
 		{
 			sdk.NewDec(100),
 			sdk.NewDec(100),
 			sdk.NewDec(1000),
+			false,
 		},
 		// {
 		// 	sdk.NewDec(100000),
 		// 	sdk.NewDec(100000),
 		// 	sdk.NewDec(10000),
 		// },
+
+		// panic catching
+		{ // xReserve negative
+			sdk.NewDec(-100),
+			sdk.NewDec(100),
+			sdk.NewDec(1),
+			true,
+		},
+		{ // yReserve negative
+			sdk.NewDec(100),
+			sdk.NewDec(-100),
+			sdk.NewDec(1),
+			true,
+		},
+		{ // yIn negative
+			sdk.NewDec(100),
+			sdk.NewDec(100),
+			sdk.NewDec(-1),
+			true,
+		},
 	}
 
 	for _, test := range tests {
-		// using two-asset cfmm
-		k0 := cfmmConstant(test.xReserve, test.yReserve)
-		xOut := solveCfmm(test.xReserve, test.yReserve, test.yIn)
+		// system under test
+		sut := func() {
+			// using two-asset cfmm
+			k0 := cfmmConstant(test.xReserve, test.yReserve)
+			xOut := solveCfmm(test.xReserve, test.yReserve, test.yIn)
 
-		k1 := cfmmConstant(test.xReserve.Sub(xOut), test.yReserve.Add(test.yIn))
-		osmoassert.DecApproxEq(t, k0, k1, kErrTolerance)
+			k1 := cfmmConstant(test.xReserve.Sub(xOut), test.yReserve.Add(test.yIn))
+			osmoassert.DecApproxEq(t, k0, k1, kErrTolerance)
 
-		// using multi-asset cfmm (should be equivalent with u = 1, w = 0)
-		k2 := cfmmConstantMulti(test.xReserve, test.yReserve, sdk.OneDec(), sdk.ZeroDec())
-		osmoassert.DecApproxEq(t, k2, k0, kErrTolerance)
-		xOut2 := solveCfmmMulti(test.xReserve, test.yReserve, sdk.ZeroDec(), test.yIn)
-		fmt.Println(xOut2)
-		k3 := cfmmConstantMulti(test.xReserve.Sub(xOut2), test.yReserve.Add(test.yIn), sdk.OneDec(), sdk.ZeroDec())
-		osmoassert.DecApproxEq(t, k2, k3, kErrTolerance)
+			// using multi-asset cfmm (should be equivalent with u = 1, w = 0)
+			k2 := cfmmConstantMulti(test.xReserve, test.yReserve, sdk.OneDec(), sdk.ZeroDec())
+			osmoassert.DecApproxEq(t, k2, k0, kErrTolerance)
+			xOut2 := solveCfmmMulti(test.xReserve, test.yReserve, sdk.ZeroDec(), test.yIn)
+			fmt.Println(xOut2)
+			k3 := cfmmConstantMulti(test.xReserve.Sub(xOut2), test.yReserve.Add(test.yIn), sdk.OneDec(), sdk.ZeroDec())
+			osmoassert.DecApproxEq(t, k2, k3, kErrTolerance)
+		}
+
+		osmoassert.ConditionalPanic(t, test.expectPanic, sut)
 	}
 }
 
@@ -67,6 +95,7 @@ func TestCFMMInvariantMultiAssets(t *testing.T) {
 		uReserve    sdk.Dec
 		wSumSquares sdk.Dec
 		yIn         sdk.Dec
+		expectPanic bool
 	}{
 		{
 			sdk.NewDec(100),
@@ -75,6 +104,7 @@ func TestCFMMInvariantMultiAssets(t *testing.T) {
 			sdk.NewDec(200),
 			sdk.NewDec(20000),
 			sdk.NewDec(1),
+			false,
 		},
 		{
 			sdk.NewDec(100),
@@ -82,21 +112,73 @@ func TestCFMMInvariantMultiAssets(t *testing.T) {
 			sdk.NewDec(200),
 			sdk.NewDec(20000),
 			sdk.NewDec(1000),
+			false,
 		},
 		// {
 		// 	sdk.NewDec(100000),
 		// 	sdk.NewDec(100000),
 		// 	sdk.NewDec(10000),
 		// },
+
+		// panic catching
+		{ // negative xReserve
+			sdk.NewDec(-100),
+			sdk.NewDec(100),
+			// represents a 4-asset pool with 100 in each reserve
+			sdk.NewDec(200),
+			sdk.NewDec(20000),
+			sdk.NewDec(1),
+			true,
+		},
+		{ // negative yReserve
+			sdk.NewDec(100),
+			sdk.NewDec(-100),
+			// represents a 4-asset pool with 100 in each reserve
+			sdk.NewDec(200),
+			sdk.NewDec(20000),
+			sdk.NewDec(1),
+			true,
+		},
+		{ // negative uReserve
+			sdk.NewDec(100),
+			sdk.NewDec(100),
+			// represents a 4-asset pool with 100 in each reserve
+			sdk.NewDec(-200),
+			sdk.NewDec(20000),
+			sdk.NewDec(1),
+			true,
+		},
+		{ // negative sumSquares
+			sdk.NewDec(100),
+			sdk.NewDec(100),
+			// represents a 4-asset pool with 100 in each reserve
+			sdk.NewDec(200),
+			sdk.NewDec(-20000),
+			sdk.NewDec(1),
+			true,
+		},
+		{ // negative yIn
+			sdk.NewDec(100),
+			sdk.NewDec(100),
+			// represents a 4-asset pool with 100 in each reserve
+			sdk.NewDec(200),
+			sdk.NewDec(-20000),
+			sdk.NewDec(1),
+			true,
+		},
 	}
 
 	for _, test := range tests {
-		// using multi-asset cfmm
-		k2 := cfmmConstantMulti(test.xReserve, test.yReserve, test.uReserve, test.wSumSquares)
-		xOut2 := solveCfmmMulti(test.xReserve, test.yReserve, test.wSumSquares, test.yIn)
-		fmt.Println(xOut2)
-		k3 := cfmmConstantMulti(test.xReserve.Sub(xOut2), test.yReserve.Add(test.yIn), test.uReserve, test.wSumSquares)
-		osmoassert.DecApproxEq(t, k2, k3, kErrTolerance)
+		// system under test
+		sut := func() {
+			// using multi-asset cfmm
+			k2 := cfmmConstantMulti(test.xReserve, test.yReserve, test.uReserve, test.wSumSquares)
+			xOut2 := solveCfmmMulti(test.xReserve, test.yReserve, test.wSumSquares, test.yIn)
+			k3 := cfmmConstantMulti(test.xReserve.Sub(xOut2), test.yReserve.Add(test.yIn), test.uReserve, test.wSumSquares)
+			osmoassert.DecApproxEq(t, k2, k3, kErrTolerance)
+		}
+
+		osmoassert.ConditionalPanic(t, test.expectPanic, sut)
 	}
 }
 
