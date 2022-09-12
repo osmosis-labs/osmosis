3aae38d4583e45f88fb078a6c41046b52df42571
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2022-09-13 00:10:29 +0700

    rename & format code

--- osmomath/sigfig_round_test.go
+++ osmomath/sigfig_round_test.go
@@ -13,64 +13,64 @@ import (
 )
 
 func TestSigFigRound(t *testing.T) {
-	// sigfig = 100000000
-	sigfig := gammtypes.SpotPriceSigFigs
+	// sigfig = 8
+	tenToSigFig := gammtypes.SpotPriceSigFigs
 
 	testCases := []struct {
 		name           string
 		decimal        sdk.Dec
-		sigfig         sdk.Int
+		tenToSigFig    sdk.Int
 		expectedResult sdk.Dec
 	}{
 		{
 			name:           "Zero decimal",
 			decimal:        sdk.ZeroDec(),
-			sigfig:         sigfig,
+			tenToSigFig:    tenToSigFig,
 			expectedResult: sdk.ZeroDec(),
 		},
 		{
-			name:           "Zero sigfig",
+			name:           "Zero tenToSigFig",
 			decimal:        sdk.MustNewDecFromStr("2.123"),
-			sigfig:         sdk.ZeroInt(),
+			tenToSigFig:    sdk.ZeroInt(),
 			expectedResult: sdk.ZeroDec(),
 		},
 		// With input, decimal >= 0.1. We have:
 		// 	- dTimesK = 63.045
 		// 	- k = 0
 		// Applying the formula, we have:
-		//  - numerator = (dTimesK  * sigfig).RoundInt() = 6304500000
-		//  - denominator = sigfig * 10^k = 100000000
+		//  - numerator = (dTimesK  * tenToSigFig).RoundInt() = 6304500000
+		//  - denominator = tenToSigFig * 10^k = 100000000
 		//  - result = numerator / denominator = 63
 		{
-			name:           "Big decimal, default sigfig",
+			name:           "Big decimal, default tenToSigFig",
 			decimal:        sdk.MustNewDecFromStr("63.045"),
-			sigfig:         sigfig,
+			tenToSigFig:    tenToSigFig,
 			expectedResult: sdk.MustNewDecFromStr("63.045"),
 		},
 		// With input, decimal < 0.1. We have:
 		// 	- dTimesK = 0.86724
 		// 	- k = 1
 		// Applying the formula, we have:
-		//  - numerator = (dTimesK  * sigfig).RoundInt() = 86724596
-		//  - denominator = sigfig * 10^k = 1000000000
+		//  - numerator = (dTimesK  * tenToSigFig).RoundInt() = 86724596
+		//  - denominator = tenToSigFig * 10^k = 1000000000
 		//  - result = numerator / denominator = 0.086724
 		{
-			name:           "Small decimal, default sigfig",
+			name:           "Small decimal, default tenToSigFig",
 			decimal:        sdk.MustNewDecFromStr("0.0867245957"),
-			sigfig:         sigfig,
+			tenToSigFig:    tenToSigFig,
 			expectedResult: sdk.MustNewDecFromStr("0.086724596"),
 		},
 		// With input, decimal < 0.1. We have:
 		// 	- dTimesK = 0.86724
 		// 	- k = 1
 		// Applying the formula, we have:
-		//  - numerator = (dTimesK  * sigfig).RoundInt() = 87
-		//  - denominator = sigfig * 10^k = 1000
+		//  - numerator = (dTimesK  * tenToSigFig).RoundInt() = 87
+		//  - denominator = tenToSigFig * 10^k = 1000
 		//  - result = numerator / denominator = 0.087
 		{
-			name:           "Small decimal, random sigfig",
+			name:           "Small decimal, random tenToSigFig",
 			decimal:        sdk.MustNewDecFromStr("0.086724"),
-			sigfig:         sdk.NewInt(100),
+			tenToSigFig:    sdk.NewInt(100),
 			expectedResult: sdk.MustNewDecFromStr("0.087"),
 		},
 	}
@@ -79,9 +79,8 @@ func TestSigFigRound(t *testing.T) {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			var actualResult sdk.Dec
-			osmoassert.ConditionalPanic(t, tc.sigfig.Equal(sdk.ZeroInt()), func() {
-				actualResult = SigFigRound(tc.decimal, tc.sigfig)
-				fmt.Println(sdk.NewDec(10).Power(8).TruncateInt())
+			osmoassert.ConditionalPanic(t, tc.tenToSigFig.Equal(sdk.ZeroInt()), func() {
+				actualResult = SigFigRound(tc.decimal, tc.tenToSigFig)
 				require.Equal(
 					t,
 					tc.expectedResult,
