419e9ba1bf8aa036fab2a511118a00a9270cdbbe
Matt, Park <45252226+mattverse@users.noreply.github.com>
2022-09-23 16:01:44 +0900

    Update x/twap/logic_test.go

--- x/twap/logic_test.go
+++ x/twap/logic_test.go
@@ -1139,7 +1139,6 @@ func (s *TestSuite) TestUpdateRecords() {
 			preSetRecords: []types.TwapRecord{threeAssetRecordAB, threeAssetRecordAC, threeAssetRecordBC, withLastErrTime(tPlus10sp5ThreeAssetRecordAB, tPlus10sp5ThreeAssetRecordAB.Time), tPlus10sp5ThreeAssetRecordAC, tPlus10sp5ThreeAssetRecordBC},
 			poolId:        threeAssetRecordAB.PoolId,
 			blockTime: threeAssetRecordAB.Time.Add(time.Second * 11),
-
 			spOverrides: []spOverride{
 				{
 					baseDenom:  threeAssetRecordAB.Asset0Denom,
