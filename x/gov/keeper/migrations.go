d62d715e1ba123f15e3763d9751ef586a2ffeae8
Dev Ojha <dojha@berkeley.edu>
2022-09-25 12:48:16 +0200

    Just move proto & gov folder

---
+++ x/gov/keeper/migrations.go
@@ -0,0 +1,33 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	v043 "github.com/cosmos/cosmos-sdk/x/gov/legacy/v043"
+	v3 "github.com/cosmos/cosmos-sdk/x/gov/legacy/v3"
+	v4 "github.com/cosmos/cosmos-sdk/x/gov/legacy/v4"
+)
+
+// Migrator is a struct for handling in-place store migrations.
+type Migrator struct {
+	keeper Keeper
+}
+
+// NewMigrator returns a new Migrator.
+func NewMigrator(keeper Keeper) Migrator {
+	return Migrator{keeper: keeper}
+}
+
+// Migrate1to2 migrates from version 1 to 2.
+func (m Migrator) Migrate1to2(ctx sdk.Context) error {
+	return v043.MigrateStore(ctx, m.keeper.storeKey, m.keeper.cdc)
+}
+
+// Migrate2to3 migrates from version 2 to 3.
+func (m Migrator) Migrate2to3(ctx sdk.Context) error {
+	return v3.MigrateStore(ctx, m.keeper.paramSpace)
+}
+
+// Migrate3to4 migrates from version 3 to 4.
+func (m Migrator) Migrate3to4(ctx sdk.Context) error {
+	return v4.MigrateStore(ctx, m.keeper.paramSpace)
+}
