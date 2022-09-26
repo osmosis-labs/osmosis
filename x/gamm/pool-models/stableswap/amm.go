26d0ad43924e68e14b4aed11e87628d849b84681
alpo <62043214+AlpinYukseloglu@users.noreply.github.com>
2022-09-26 03:51:16 -0700

    [stableswap]: Combine `solveCfmm` and `solveCfmmMulti` (#2847)
    Closes: #2845
    
    ## What is the purpose of the change
    
    This PR merges `solveCfmmMulti` into `solveCfmm` and updates tests and pool logic accordingly.
    
    Note: majority of diff is from #2843 and test refactors (one side benefit of this change is that it allows our tests to be _drastically_ cleaner and more readable (no more manual `uReserve` and `wSumSquares` calculations!))
    
    Context from issue:
    
    > All pool-level CFMM calls currently only use solveCfmm, which is only connected to our two-asset CFMM solver. Many of these functions also hard code the assumption that there are only two assets (e.g. by always specifically taking the [first two](https://github.com/osmosis-labs/osmosis/blob/39b32018d736f93fbb4ecdc85d53dae457eec5a1/x/gamm/pool-models/stableswap/amm.go#L379) assets in pool reserves).
    > 
    > We should combine these two solver functions and properly direct inputs towards the correct solver depending on the number of assets they have.
    
    ## Brief Changelog
    
    - Merge `solveCfmmMulti` into `solveCfmm`
    - Change `solveCfmm` to take a single `sdk.Coins` or `PoolAssets` input (latter might be safer if possible)
    - Add logic in `solveCfmm` that routes to two-asset solver if only two assets and to multi-asset solver otherwise. This logic should calculate `uReserve` and `wSumSquares` internally from the input tokens to prevent unintuitive calculations elsewhere in the codebase and to prevent user error around these calcs in the future
    - Update pool function logic to accommodate multi-asset pools (previously would just take first two tokens in token reserves)
    
    ## Testing and Verifying
    
    All tests previously relating to `solveCfmm` and `solveCfmmMulti` should now be properly refactored to use the new `solveCfmm`
    
    ## Documentation and Release Note
    
      - Does this pull request introduce a new feature or user-facing behavior changes? (no)
      - Is a relevant changelog entry added to the `Unreleased` section in `CHANGELOG.md`? (no)
      - How is the feature or change documented? (not documented)

--- x/gamm/pool-models/stableswap/amm.go
+++ x/gamm/pool-models/stableswap/amm.go
@@ -43,238 +43,25 @@ func cfmmConstantMulti(xReserve, yReserve, uReserve, vSumSquares osmomath.BigDec
 	return xyu.Mul(x2.Add(y2).Add(vSumSquares))
 }
 
-// solidly CFMM is xy(x^2 + y^2) = k
+// solidly CFMM is xy(x^2 + y^2) = k, and our multi-asset CFMM is xyz(x^2 + y^2 + w) = k
 // So we want to solve for a given addition of `b` units of y into the pool,
 // how many units `a` of x do we get out.
-// So we solve the following expression for `a`
+// So we solve the following expression for `a` in two-asset pools:
 // xy(x^2 + y^2) = (x - a)(y + b)((x - a)^2 + (y + b)^2)
-func solveCfmm(xReserve, yReserve, yIn osmomath.BigDec) osmomath.BigDec {
-	if !xReserve.IsPositive() || !yReserve.IsPositive() || !yIn.IsPositive() {
-		panic("invalid input: reserves and input must be positive")
-	}
-
-	// use the following wolfram alpha link to solve the equation
-	// https://www.wolframalpha.com/input?i=solve+for+a%2C+xy%28x%5E2+%2B+y%5E2%29+%3D+%28x+-+a%29%28y+%2B+b%29%28%28x+-+a%29%5E2+%2B+%28y+%2Bb%29%5E2%29+
-	// This returns (copied from wolfram):
-	// assuming (correctly) that b + y!=0
-	// a = (-27 b^2 x^3 y - 27 b^2 x y^3 + sqrt((-27 b^2 x^3 y - 27 b^2 x y^3 - 54 b x^3 y^2 - 54 b x y^4 - 27 x^3 y^3 - 27 x y^5)^2 + 4 (3 b^4 + 12 b^3 y + 18 b^2 y^2 + 12 b y^3 + 3 y^4)^3) - 54 b x^3 y^2 - 54 b x y^4 - 27 x^3 y^3 - 27 x y^5)^(1/3)/(3 2^(1/3) (b + y)) - (2^(1/3) (3 b^4 + 12 b^3 y + 18 b^2 y^2 + 12 b y^3 + 3 y^4))/(3 (b + y) (-27 b^2 x^3 y - 27 b^2 x y^3 + sqrt((-27 b^2 x^3 y - 27 b^2 x y^3 - 54 b x^3 y^2 - 54 b x y^4 - 27 x^3 y^3 - 27 x y^5)^2 + 4 (3 b^4 + 12 b^3 y + 18 b^2 y^2 + 12 b y^3 + 3 y^4)^3) - 54 b x^3 y^2 - 54 b x y^4 - 27 x^3 y^3 - 27 x y^5)^(1/3)) + (b x + x y)/(b + y) and b + y!=0
-	// We simplify and separate out terms to get that its the following:
-	// The key substitutions are that 3(b+y)^4 = 3 b^4 + 12 b^3 y + 18 b^2 y^2 + 12 b y^3 + 3 y^4
-	// and -27 x y (b + y)^2 (x^2 + y^2) = -27 b^2 x^3 y - 27 b^2 x y^3 - 54 b x^3 y^2 - 54 b x y^4 - 27 x^3 y^3 - 27 x y^5
-	// I added {} myself, making better distinctions between entirely distinct terms.
-	// a = {(-27 x y (b + y)^2 (x^2 + y^2)
-	//			+ sqrt(
-	//				(-27 x y (b + y)^2 (x^2 + y^2))^2
-	//				+ 108 ((b+y)^4)^3
-	// 			)^(1/3)
-	// 		  / (3 2^(1/3) (b + y))}
-	//		- {(2^(1/3) (3 (b + y)^4))
-	//		  /(3 (b + y)
-	// 			(-27 x y (b + y)^2 (x^2 + y^2)
-	// 				+ sqrt(
-	//					(-27 x y (b + y)^2 (x^2 + y^2))^2
-	//					+ 108 ((b+y)^4)^3)
-	// 			)^(1/3))}
-	//      + {(b x + x y)/(b + y)}
-	// we further simplify, and call:
-	// foo = (-27 x y (b + y)^2 (x^2 + y^2)
-	// 			+ sqrt(
-	//				(-27 x y (b + y)^2 (x^2 + y^2))^2
-	//				+ 108 ((b+y)^4)^3)
-	//		 )^(1/3)
-	// Thus, a is then:
-	// a = {foo / (3 2^(1/3) (b + y))}
-	//		- {(3 * 2^(1/3) (b+y)^4)
-	//		  /(3 (b + y) foo)}
-	//      + {(b x + x y)/(b + y)}
-	// Let:
-	// term1 = {foo / (3 2^(1/3) (b + y))}
-	// term2 = {(3 * 2^(1/3) (b+y)^4) /(3 (b + y) foo)} =  2^(1/3) (b+y)^3 / foo
-	// term3 = {(b x + x y)/(b + y)}
-
-	// prelude, compute all the xy cross terms. Consider keeping these precomputed in the struct,
-	// and maybe in state.
-	x := xReserve
-	y := yReserve
-	x2py2 := x.Mul(x).Add(y.Mul(y))
-
-	xy := x.Mul(y)
-
-	b := yIn
-
-	bpy := b.Add(y)
-	bpy2 := bpy.Mul(bpy)
-	bpy3 := bpy2.Mul(bpy)
-	bpy4 := bpy2.Mul(bpy2)
-
-	// TODO: Come back and optimize alot of the calculations
-
-	// Now we compute foo
-	// foo = (-27 x y (b + y)^2 (x^2 + y^2)
-	// 			+ sqrt(
-	//				(-27 x y (b + y)^2 (x^2 + y^2))^2
-	//				+ 108 ((b+y)^4)^3)
-	//		 )^(1/3)
-	// This has a y^12 term in it, which is unappealing, so we spend some energy reducing this max bitlen.
-	// foo = (-27 x y (b + y)^2 (x^2 + y^2)
-	// 			+ (b + y)^2 sqrt(
-	//				729 (x y (x^2 + y^2))^2
-	//				+ 108 (b+y)^8)
-	//		 )^(1/3)
-	// let e = x y (x^2 + y^2))
-	// foo = (-27 (b + y)^2 e
-	// 			+ (b + y)^2 sqrt(
-	//				729 e^2 + 108 (b+y)^8)
-	//		 )^(1/3)
-
-	e := xy.Mul(x2py2) // xy(x^2 + y^2)
-
-	// t1 = -27 (b + y)^2 e
-	t1 := e.Mul(bpy2).MulInt64(-27)
-
-	// compute d = (b + y)^2 sqrt(729 e^2 + 108 (b+y)^8)
-	bpy8 := bpy4.Mul(bpy4)
-	sqrt_inner := e.Mul(e).MulInt64(729).Add(bpy8.MulInt64(108)) // 729 e^2 + 108 (b+y)^8
-	sqrt, err := sqrt_inner.ApproxSqrt()
-	if err != nil {
-		panic(err)
-	}
-	d := sqrt.Mul(bpy2)
-
-	// foo = (t1 + d)^(1/3)
-	foo3 := t1.Add(d)
-	foo, _ := foo3.ApproxRoot(3)
-
-	// a = {foo / (3 2^(1/3) (b + y))}
-	//		- {(2^(1/3) banana) / (3 (b + y) foo}
-	//      + {(b x + x y)/(b + y)}
-
-	// term1 := {foo / (3 2^(1/3) (b + y))}
-	term1Denominator := threeCubeRootTwo.Mul(bpy)
-	term1 := foo.Quo(term1Denominator)
-	// term2 := {(2^(1/3) (b+y)^3) / (foo}
-	term2 := cubeRootTwo.Mul(bpy3)
-	term2 = term2.Quo(foo)
-	// term3 := {(b x + x y)/(b + y)}
-	term3Numerator := b.Mul(x).Add(xy)
-	term3 := term3Numerator.Quo(bpy)
-
-	a := term1.Sub(term2).Add(term3)
-
-	if a.GTE(xReserve) {
-		panic("invalid output: greater than full pool reserves")
-	}
-
-	return a
-}
-
-// Our multi-asset CFMM is xyz(x^2 + y^2 + w) = k
-// So we want to solve for a given addition of `b` units of y into the pool,
-// how many units `a` of x do we get out.
-// So we solve the following expression for `a`
+// and the following expression for `a` in multi-asset pools:
 // xyz(x^2 + y^2 + w) = (x - a)(y + b)z((x - a)^2 + (y + b)^2 + w)
-func solveCfmmMulti(xReserve, yReserve, wSumSquares, yIn osmomath.BigDec) osmomath.BigDec {
-	if !xReserve.IsPositive() || !yReserve.IsPositive() || !yIn.IsPositive() {
-		panic("invalid input: reserves and input must be positive")
+func solveCfmm(xReserve, yReserve osmomath.BigDec, remReserves []osmomath.BigDec, yIn osmomath.BigDec) osmomath.BigDec {
+	if len(remReserves) == 0 {
+		return solveCFMMBinarySearch(cfmmConstant)(xReserve, yReserve, yIn)
 	}
 
-	// Use the following wolfram alpha link to solve the equation
-	// https://www.wolframalpha.com/input?i=solve+for+a%2C+xyz%28x%5E2+%2B+y%5E2+%2B+w%29+%3D+%28x+-+a%29%28y+%2B+b%29z%28%28x+-+a%29%5E2+%2B+%28y+%2Bb%29%5E2+%2B+w%29
-	// This returns (copied from wolfram):
-	// assuming (correctly) that b + y!=0
-	// a = (-27 b^2 w x y - 27 b^2 x^3 y - 27 b^2 x y^3 + sqrt((-27 b^2 w x y - 27 b^2 x^3 y - 27 b^2 x y^3 - 54 b w x y^2 - 54 b x^3 y^2 - 54 b x y^4 - 27 w x y^3 - 27 x^3 y^3 - 27 x y^5)^2 + 4 (3 b^4 + 12 b^3 y + 3 b^2 w + 18 b^2 y^2 + 6 b w y + 12 b y^3 + 3 w y^2 + 3 y^4)^3) - 54 b w x y^2 - 54 b x^3 y^2 - 54 b x y^4 - 27 w x y^3 - 27 x^3 y^3 - 27 x y^5)^(1/3)/(3 2^(1/3) (b + y)) - (2^(1/3) (3 b^4 + 12 b^3 y + 3 b^2 w + 18 b^2 y^2 + 6 b w y + 12 b y^3 + 3 w y^2 + 3 y^4))/(3 (b + y) (-27 b^2 w x y - 27 b^2 x^3 y - 27 b^2 x y^3 + sqrt((-27 b^2 w x y - 27 b^2 x^3 y - 27 b^2 x y^3 - 54 b w x y^2 - 54 b x^3 y^2 - 54 b x y^4 - 27 w x y^3 - 27 x^3 y^3 - 27 x y^5)^2 + 4 (3 b^4 + 12 b^3 y + 3 b^2 w + 18 b^2 y^2 + 6 b w y + 12 b y^3 + 3 w y^2 + 3 y^4)^3) - 54 b w x y^2 - 54 b x^3 y^2 - 54 b x y^4 - 27 w x y^3 - 27 x^3 y^3 - 27 x y^5)^(1/3)) + (b x + x y)/(b + y) and b + y!=0
-	//
-	// The key substitutions are (where w represents the sum of the squares as represented in the multi-asset CFMM function):
-	// 1. S1: 3 (b + y)^2 (b^2 + 2 b y + y^2 + w) = 3 b^4 + 12 b^3 y + 3b^2w + 18 b^2 y^2 + 6bwy + 12 b y^3 + 3wy^2 + 3 y^4
-	// 2. S2: -27 x y (b + y)^2 (x^2 + y^2 + w) = -27b^2xyw - 27b^2x^3y - 27b^2xy^3 - 54bxy^2w - 54bx^3y^2 - 54bxy^4 - 27xy^3w - 27x^3y^3 - 27xy^5
-	//
-	// This is the simplified version using the substitutions above, to be expanded afterwards below:
-	// a = (1 / [3 * 2^(1/3) * (b + y)])
-	// 			* [S2 + sqrt(S2^2 + 4*(S1^3))]^(1/3)
-	//			- {2^(1/3) * S1 /
-	// 				[3 * (b + y) * (S2 + sqrt(S2^2 + 4*(S1^3)))^(1/3)]}
-	// 			+ [(bx + xy) / (b + y)]
-	//
-	// To further simplify, let:
-	// foo = (S2 + sqrt(S2^2 + 4*(S1^3)))^(1/3)
-	// bpy = (b + y)
-	//
-	// Thus, a further simplifies to:
-	// a = (foo / (3 * 2^(1/3) * bpy))
-	//			- (2^(1/3) * S1 / (3 * bpy * foo))
-	// 			+ ((bx + xy) / bpy)
-	//
-	// Finally, let:
-	// term1 = (foo / (3 * 2^(1/3) * bpy))
-	// term2 = (2^(1/3) * S1 / (3 * bpy * foo))
-	// term3 = ((bx + xy) / bpy)
-	//
-	// The final result should be:
-	// a = term1 - term2 + term3
-
-	// Prelude, compute all the xy cross terms. Consider keeping these precomputed in the struct,
-	// and maybe in state.
-
-	x := xReserve
-	x2 := x.Mul(x)
-	y := yReserve
-	y2 := y.Mul(y)
-	w := wSumSquares
-	b := yIn
-
-	xy := x.Mul(y)
-
-	bpy := b.Add(y)
-	bpy2 := bpy.Mul(bpy)
-	// bpy3 := bpy2.Mul(bpy)
-	// bpy4 := bpy2.Mul(bpy2)
-
-	// S1 = 3 (b + y)^2 (b^2 + 2 b y + y^2 + w)
-	s1_inner := bpy2.Add(w) // (b^2 + 2 b y + y^2 + w) = (b+y)^2 + w
-	s1 := bpy2.MulInt64(3).Mul(s1_inner)
-	// S2 = -27 x y (b + y)^2 (x^2 + y^2 + w)
-	s2_inner := x2.Add(y2).Add(w) // (x^2 + y^2 + w)
-	s2 := bpy2.MulInt64(-27).Mul(xy).Mul(s2_inner)
-
-	// Calculating foo directly causes an integer overflow due to having a y^12 term, so we expand and factor to reduce its max bitlen:
-	// Original foo = (S2 + sqrt(S2^2 + 4*(S1^3)))^(1/3)
-	// Expand:
-	// foo = (S2 + sqrt((-27 x y (b + y)^2 (x^2 + y^2 + w))^2 +
-	// 					4*(3 (b + y)^2 (b^2 + 2 b y + y^2 + w))^3)
-	// 		 )^(1/3)
-	// Factor (b+y)^2 out from within the square root:
-	// foo = (S2 + (b + y)^2
-	// 			* sqrt((-27 x y (x^2 + y^2 + w))^2 +
-	// 					4 * ((b + y)^2) * (3 (b^2 + 2 b y + y^2 + w))^3)
-	// 		 )^(1/3)
-	sqrt_inner_term1 := xy.MulInt64(-27).Mul(s2_inner)                // (-27 x y (x^2 + y^2 + w))
-	sqrt_inner_term1_2 := sqrt_inner_term1.Mul(sqrt_inner_term1)      // sqrt_inner_term1^2
-	s1_inner_3 := (s1_inner).Mul(s1_inner).Mul(s1_inner).MulInt64(27) // (3 (b^2 + 2 b y + y^2 + w))^3 = 27 s1_inner^3
-	sqrt_inner_term2 := s1_inner_3.MulInt64(4).Mul(bpy2)              // 4 * ((b + y)^2) * s1_inner_3
-	sqrt_inner := sqrt_inner_term1_2.Add(sqrt_inner_term2)            // sqrt_inner_term1 + sqrt_inner_term2
-	sqrt, err := sqrt_inner.ApproxSqrt()
-	if err != nil {
-		panic(err)
+	uReserve := osmomath.OneDec()
+	wSumSquares := osmomath.ZeroDec()
+	for _, assetReserve := range remReserves {
+		uReserve = uReserve.Mul(assetReserve)
+		wSumSquares = wSumSquares.Add(assetReserve.Mul(assetReserve))
 	}
-	foo3 := s2.Add(bpy2.Mul(sqrt))
-	foo, _ := foo3.ApproxRoot(3)
-
-	// term1 = (foo / (3 * 2^(1/3) * bar))
-	term1Denominator := threeCubeRootTwo.Mul(bpy) // 3 * 2^(1/3) * (b + y)
-	term1 := foo.Quo(term1Denominator)
-	// term2 = (2^(1/3) * S1 / (3 * bar * foo))
-	term2 := (cubeRootTwo.Mul(s1)).Quo(foo.Mul(bpy).MulInt64(3))
-	// term3 = ((bx + xy) / bpy)
-	term3Numerator := b.Mul(x).Add(xy)
-	term3 := term3Numerator.Quo(bpy)
-
-	a := term1.Sub(term2).Add(term3)
-
-	if a.GTE(xReserve) {
-		panic("invalid output: greater than full pool reserves")
-	}
-
-	return a
+	return solveCFMMBinarySearchMulti(cfmmConstantMulti)(xReserve, yReserve, uReserve, wSumSquares, yIn)
 }
 
 func approxDecEqual(a, b, tol osmomath.BigDec) bool {
@@ -324,25 +111,29 @@ func solveCFMMBinarySearch(constantFunction func(osmomath.BigDec, osmomath.BigDe
 // added for future extension
 func solveCFMMBinarySearchMulti(constantFunction func(osmomath.BigDec, osmomath.BigDec, osmomath.BigDec, osmomath.BigDec) osmomath.BigDec) func(osmomath.BigDec, osmomath.BigDec, osmomath.BigDec, osmomath.BigDec, osmomath.BigDec) osmomath.BigDec {
 	return func(xReserve, yReserve, uReserve, wSumSquares, yIn osmomath.BigDec) osmomath.BigDec {
-		if !xReserve.IsPositive() || !yReserve.IsPositive() || wSumSquares.IsNegative() || !yIn.IsPositive() {
+		if !xReserve.IsPositive() || !yReserve.IsPositive() || !uReserve.IsPositive() || wSumSquares.IsNegative() || !yIn.IsPositive() {
 			panic("invalid input: reserves and input must be positive")
+		} else if yIn.GTE(yReserve) {
+			panic("cannot input more than pool reserves")
 		}
 		k := constantFunction(xReserve, yReserve, uReserve, wSumSquares)
-		yf := yReserve.Add(yIn)
-		x_low_est := osmomath.ZeroDec()
-		x_high_est := xReserve
-		x_est := (x_high_est.Add(x_low_est)).Quo(twodec)
-		cur_k := constantFunction(x_est, yf, uReserve, wSumSquares)
-		for i := 0; !approxDecEqual(cur_k, k, k_threshold) && i < 256; i++ {
-			if cur_k.GT(k) {
-				x_high_est = x_est
-			} else if cur_k.LT(k) {
-				x_low_est = x_est
-			}
-			x_est = (x_high_est.Add(x_low_est)).Quo(twodec)
-			cur_k = constantFunction(x_est, yf, uReserve, wSumSquares)
+		yFinal := yReserve.Add(yIn)
+		xLowEst := osmomath.ZeroDec()
+		xHighEst := xReserve
+		maxIterations := 256
+		errTolerance := osmoutils.ErrTolerance{AdditiveTolerance: sdk.OneInt(), MultiplicativeTolerance: sdk.Dec{}}
+
+		// create single-input CFMM to pass into binary search
+		calcXEst := func(xEst osmomath.BigDec) (osmomath.BigDec, error) {
+			return constantFunction(xEst, yFinal, uReserve, wSumSquares), nil
 		}
-		xOut := xReserve.Sub(x_est)
+
+		xEst, err := osmoutils.BinarySearchBigDec(calcXEst, xLowEst, xHighEst, k, errTolerance, maxIterations)
+		if err != nil {
+			panic(err)
+		}
+
+		xOut := xReserve.Sub(xEst)
 		if xOut.GTE(xReserve) {
 			panic("invalid output: greater than full pool reserves")
 		}
@@ -367,7 +158,7 @@ func spotPrice(baseReserve, quoteReserve osmomath.BigDec) osmomath.BigDec {
 	// xReserve & yReserve.
 	a := osmomath.OneDec()
 	// no need to divide by a, since a = 1.
-	return solveCfmm(baseReserve, quoteReserve, a)
+	return solveCfmm(baseReserve, quoteReserve, []osmomath.BigDec{}, a)
 }
 
 // returns outAmt as a decimal
@@ -377,8 +168,9 @@ func (p *Pool) calcOutAmtGivenIn(tokenIn sdk.Coin, tokenOutDenom string, swapFee
 		return sdk.Dec{}, err
 	}
 	tokenInSupply, tokenOutSupply := reserves[0], reserves[1]
+	remReserves := osmomath.BigDecFromSDKDecSlice(reserves[2:])
 	// We are solving for the amount of token out, hence x = tokenOutSupply, y = tokenInSupply
-	cfmmOut := solveCfmm(osmomath.BigDecFromSDKDec(tokenOutSupply), osmomath.BigDecFromSDKDec(tokenInSupply), osmomath.BigDecFromSDKDec(tokenIn.Amount.ToDec()))
+	cfmmOut := solveCfmm(osmomath.BigDecFromSDKDec(tokenOutSupply), osmomath.BigDecFromSDKDec(tokenInSupply), remReserves, osmomath.BigDecFromSDKDec(tokenIn.Amount.ToDec()))
 	outAmt := p.getDescaledPoolAmt(tokenOutDenom, cfmmOut)
 	return outAmt.SDKDec(), nil
 }
@@ -390,9 +182,10 @@ func (p *Pool) calcInAmtGivenOut(tokenOut sdk.Coin, tokenInDenom string, swapFee
 		return sdk.Dec{}, err
 	}
 	tokenInSupply, tokenOutSupply := reserves[0], reserves[1]
+	remReserves := osmomath.BigDecFromSDKDecSlice(reserves[2:])
 	// We are solving for the amount of token in, cfmm(x,y) = cfmm(x + x_in, y - y_out)
 	// x = tokenInSupply, y = tokenOutSupply, yIn = -tokenOutAmount
-	cfmmIn := solveCfmm(osmomath.BigDecFromSDKDec(tokenInSupply), osmomath.BigDecFromSDKDec(tokenOutSupply), osmomath.BigDecFromSDKDec(tokenOut.Amount.ToDec().Neg()))
+	cfmmIn := solveCfmm(osmomath.BigDecFromSDKDec(tokenInSupply), osmomath.BigDecFromSDKDec(tokenOutSupply), remReserves, osmomath.BigDecFromSDKDec(tokenOut.Amount.ToDec().Neg()))
 	inAmt := p.getDescaledPoolAmt(tokenInDenom, cfmmIn.Neg())
 	return inAmt.SDKDec(), nil
 }
