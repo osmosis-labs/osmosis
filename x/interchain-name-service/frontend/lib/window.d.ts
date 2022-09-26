b2db9788bd443737aa284bc9def1d0c416884790
Richard Liu <richard.y.liu@berkeley.edu>
2022-09-26 05:00:19 -0700

    remove unnecessary files

--- x/interchain-name-service/frontend/lib/window.d.ts
+++
@@ -1,6 +0,0 @@
-import { Window as KeplrWindow } from "@keplr-wallet/types";
-
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface Window extends KeplrWindow {}
-}
--- x/interchain-name-service/ts/sdk/README.md
+++
@@ -1,1 +0,0 @@
-# interchain-name-service-sdk
