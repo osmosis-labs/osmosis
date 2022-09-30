3e6c81442bb560ad5271b59a3aff5885bf2089dd
Dev Ojha <ValarDragon@users.noreply.github.com>
2022-09-30 09:14:59 +0200

    Add a simulator executor types package (#2857)
    * Add a simulator executor types package
    
    * Delete unsupported genesis configurations
    
    * fix typecast
    
    * Delete / simplify more of the genesis code
    
    * More docs
    
    * Move internal package to be within executor, move stats to internal package
    
    * lint
    
    * lint
    
    * Update simulation/executor/internal/executortypes/simmanager.go
    
    * Update tests/simulator/state.go
    
    * Update simulation/executor/params.go
    
    Co-authored-by: Adam Tucker <adam@osmosis.team>

--- simulation/executor/types.go
+++ simulation/executor/types.go
@@ -6,14 +6,18 @@ import (
 	"time"
 
 	legacysim "github.com/cosmos/cosmos-sdk/types/simulation"
+	abci "github.com/tendermint/tendermint/abci/types"
 
 	"github.com/osmosis-labs/osmosis/v12/simulation/simtypes"
 )
 
 // AppStateFn returns the app state json bytes and the genesis accounts
-type AppStateFn func(simManager *simtypes.Manager, r *rand.Rand, accs []legacysim.Account, config InitializationConfig) (
+type AppStateFn func(simManager simtypes.ModuleGenesisGenerator, r *rand.Rand, accs []legacysim.Account, config InitializationConfig) (
 	appState json.RawMessage, accounts []legacysim.Account, chainId string, genesisTimestamp time.Time,
 )
+
+type InitChainFn func(simManager simtypes.ModuleGenesisGenerator, r *rand.Rand, accs []legacysim.Account, config InitializationConfig) (
+	accounts []legacysim.Account, req abci.RequestInitChain)
 
 // RandomAccountFn returns a slice of n random simulation accounts
 type RandomAccountFn func(r *rand.Rand, n int) []legacysim.Account
