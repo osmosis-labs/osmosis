8e91461d1327914d17829a186bc299835e90786b
alpo <yukseloglua@berkeley.edu>
2022-09-30 22:40:49 -0400

    fix inverse rel test suite and a number of stableswap bugs

--- x/gamm/pool-models/stableswap/util_test.go
+++ x/gamm/pool-models/stableswap/util_test.go
@@ -9,11 +9,11 @@ import (
 	"github.com/osmosis-labs/osmosis/v12/x/gamm/types"
 )
 
-func createTestPool(t *testing.T, poolLiquidity sdk.Coins, swapFee, exitFee sdk.Dec) types.PoolI {
+func createTestPool(t *testing.T, poolLiquidity sdk.Coins, swapFee, exitFee sdk.Dec, scalingFactors []uint64) types.PoolI {
 	pool, err := NewStableswapPool(1, PoolParams{
 		SwapFee: swapFee,
 		ExitFee: exitFee,
-	}, poolLiquidity, []uint64{1, 1}, "", "")
+	}, poolLiquidity, scalingFactors, "", "")
 
 	require.NoError(t, err)
 
