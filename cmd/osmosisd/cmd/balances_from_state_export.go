1f0c71d5244da6521672ed177646064e7b4a9864
Adam Tucker <adam@osmosis.team>
2022-10-02 11:47:57 -0500

    feat: cli command to create csv for merkledrop (#2928)
    * add cli logic to create csv for merkledrop
    
    * Update cmd/osmosisd/cmd/balances_from_state_export.go
    
    Co-authored-by: Roman <roman@osmosis.team>
    
    Co-authored-by: Roman <roman@osmosis.team>

--- cmd/osmosisd/cmd/balances_from_state_export.go
+++ cmd/osmosisd/cmd/balances_from_state_export.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -24,6 +25,7 @@ import (
 )
 
 const FlagSelectPoolIds = "breakdown-by-pool-ids"
+const FlagMinimumStakeAmount = "minimum-stake-amount"
 
 type DeriveSnapshot struct {
 	NumberAccounts uint64                    `json:"num_accounts"`
@@ -300,6 +302,78 @@ Example:
 
 	cmd.Flags().String(FlagSelectPoolIds, "",
 		"Output a special breakdown for amount LP'd to the provided pools. Usage --breakdown-by-pool-ids=1,2,605")
+
+	return cmd
+}
+
+// StakedToCSVCmd generates a airdrop.csv from a provided exported balances.json.
+func StakedToCSVCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "staked-to-csv [input-balances-file] [output-airdrop-csv]",
+		Short: "Export a airdrop csv from a provided balances export",
+		Long: `Export a airdrop csv from a provided balances export (from export-derive-balances)
+Example:
+	osmosisd staked-to-csv ../balances.json ../airdrop.csv
+`,
+		Args: cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+			serverCtx := server.GetServerContextFromCmd(cmd)
+			config := serverCtx.Config
+			config.SetRoot(clientCtx.HomeDir)
+
+			balancesFile := args[0]
+
+			snapshotOutput := args[1]
+
+			minStakeAmount, _ := cmd.Flags().GetInt64(FlagMinimumStakeAmount)
+
+			var deriveSnapshot DeriveSnapshot
+
+			sourceFile, err := os.Open(balancesFile)
+			if err != nil {
+				return err
+			}
+			// remember to close the file at the end of the function
+			defer sourceFile.Close()
+
+			// decode the balances json file into the struct array
+			if err := json.NewDecoder(sourceFile).Decode(&deriveSnapshot); err != nil {
+				return err
+			}
+
+			// create a new file to store CSV data
+			outputFile, err := os.Create(snapshotOutput)
+			if err != nil {
+				return err
+			}
+			defer outputFile.Close()
+
+			// write the header of the CSV file
+			writer := csv.NewWriter(outputFile)
+			defer writer.Flush()
+
+			header := []string{"address", "staked"}
+			if err := writer.Write(header); err != nil {
+				return err
+			}
+
+			// iterate through all accounts, leave out accounts that do not meet the user provided min stake amount
+			for _, r := range deriveSnapshot.Accounts {
+				var csvRow []string
+				if r.Staked.GT(sdk.NewInt(minStakeAmount)) {
+					csvRow = append(csvRow, r.Address, r.Staked.String())
+					if err := writer.Write(csvRow); err != nil {
+						return err
+					}
+				}
+			}
+
+			return nil
+		},
+	}
+
+	cmd.Flags().Int64(FlagMinimumStakeAmount, 0, "Specify minimum amount (non inclusive) accounts must stake to be included in airdrop (default: 0)")
 
 	return cmd
 }
