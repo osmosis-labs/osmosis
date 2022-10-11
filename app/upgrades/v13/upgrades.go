4fed7db03b4d41aef68af3669f99e2cab0e32ace
Adam Tucker <adam@osmosis.team>
2022-10-11 14:19:23 -0500

    chore: v13 e2e and upgrade boilerplate (#2967)
    * v13 handler and e2e updates
    
    * v13 e2e changes w boilerplate
    
    * add errors
    
    * lint
    
    * ran gofumpt
    
    Co-authored-by: Nicolas Lara <nicolaslara@gmail.com>

--- app/upgrades/v13/upgrades.go
+++ app/upgrades/v13/upgrades.go
@@ -5,10 +5,9 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
-	lockuptypes "github.com/osmosis-labs/osmosis/v12/x/lockup/types"
-
 	"github.com/osmosis-labs/osmosis/v12/app/keepers"
 	"github.com/osmosis-labs/osmosis/v12/app/upgrades"
+	lockuptypes "github.com/osmosis-labs/osmosis/v12/x/lockup/types"
 )
 
 func CreateUpgradeHandler(
@@ -19,7 +18,6 @@ func CreateUpgradeHandler(
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		keepers.LockupKeeper.SetParams(ctx, lockuptypes.DefaultParams())
-
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
 }
