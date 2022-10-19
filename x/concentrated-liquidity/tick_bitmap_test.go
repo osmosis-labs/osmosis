d90137825890ea1d85ae55d554b9b8293b532a46
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-10-19 16:49:23 -0700

    bez: updates

--- x/concentrated-liquidity/tick_bitmap_test.go
+++ x/concentrated-liquidity/tick_bitmap_test.go
@@ -1,24 +1,13 @@
 package concentrated_liquidity_test
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestFoo(t *testing.T) {
-	// 	function position(int24 tick) private pure returns (int16 wordPos, uint8 bitPos) {
-	//     wordPos = int16(tick >> 8);
-	//     bitPos = uint8(uint24(tick % 256));
-	// }
+func TestTickBitmap_FlipTick(t *testing.T) {
 
-	// tick = 85176
-	// word_pos = tick >> 8 # or tick // 2**8
-	// bit_pos = tick % 256
-	// print(f"Word {word_pos}, bit {bit_pos}")
-	// # Word 332, bit 184
+}
 
-	tick := 85176
-	wordPos := tick >> 8
+func TestTickBitmap_NextInitializedTickWithinOneWord(t *testing.T) {
 
-	fmt.Println(wordPos)
 }
