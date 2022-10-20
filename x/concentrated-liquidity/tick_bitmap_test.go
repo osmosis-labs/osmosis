565979b2491393519e6384dd562970ace7573796
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-10-20 10:47:40 -0700

    bez: updates

--- x/concentrated-liquidity/tick_bitmap_test.go
+++ x/concentrated-liquidity/tick_bitmap_test.go
@@ -2,10 +2,17 @@ package concentrated_liquidity_test
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	cl "github.com/osmosis-labs/osmosis/v12/x/concentrated-liquidity"
 )
 
 func TestTickBitmap_FlipTick(t *testing.T) {
+	tb := cl.NewTickBitmap()
 
+	require.NoError(t, tb.FlipTick(85176, 1))
+	require.Error(t, tb.FlipTick(85176, 1000))
 }
 
 func TestTickBitmap_NextInitializedTickWithinOneWord(t *testing.T) {
