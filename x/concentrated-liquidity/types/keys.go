f4b7fc484a18944edfd6b6a023f20fc7388f9076
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-10-20 14:29:24 -0700

    bez: updates

--- x/concentrated-liquidity/types/keys.go
+++ x/concentrated-liquidity/types/keys.go
@@ -1,33 +1,37 @@
 package types
 
 import (
-	"fmt"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
 )
 
 const (
 	ModuleName = "concentrated-liquidity"
 
-	StoreKey  = ModuleName
-	RouterKey = ModuleName
-
-	QuerierRoute = ModuleName
-	// Contract: Coin denoms cannot contain this character
-	KeySeparator = "|"
+	StoreKey = ModuleName
 )
 
+// Key prefixes
 var (
-	TickPrefix     = "tick_prefix" + KeySeparator
-	PositionPrefix = "position_prefix" + KeySeparator
+	TickPrefix     = []byte{0x01}
+	PositionPrefix = []byte{0x02}
 )
 
 // KeyTick uses pool Id and tick index
-func KeyTick(poolId uint64, tickIndex sdk.Int) []byte {
-	return []byte(fmt.Sprintf("%s%d%s", TickPrefix, poolId, tickIndex.String()))
+func KeyTick(poolId uint64, tickIndex int64) []byte {
+	var key []byte
+	key = append(key, TickPrefix...)
+	key = append(key, sdk.Uint64ToBigEndian(poolId)...)
+	key = append(key, sdk.Uint64ToBigEndian(uint64(tickIndex))...)
+	return key
 }
 
 // KeyPosition uses pool Id, owner, lower tick and upper tick for keys
-func KeyPosition(poolId uint64, address string, lowerTick, upperTick sdk.Int) []byte {
-	return []byte(fmt.Sprintf("%s%d%s%s%s", PositionPrefix, poolId, address, lowerTick.String(), upperTick.String()))
+func KeyPosition(poolId uint64, addr sdk.AccAddress, lowerTick, upperTick int64) []byte {
+	var key []byte
+	key = append(key, PositionPrefix...)
+	key = append(key, address.MustLengthPrefix(addr)...)
+	key = append(key, sdk.Uint64ToBigEndian(uint64(lowerTick))...)
+	key = append(key, sdk.Uint64ToBigEndian(uint64(upperTick))...)
+	return key
 }
