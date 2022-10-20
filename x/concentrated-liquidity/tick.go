210fb273ddac5dba60e744556cbdf9fa7356257f
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-10-20 15:44:48 -0700

    bez: updates

--- x/concentrated-liquidity/tick.go
+++ x/concentrated-liquidity/tick.go
@@ -3,6 +3,7 @@ package concentrated_liquidity
 import (
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	db "github.com/tendermint/tm-db"
 
 	"github.com/osmosis-labs/osmosis/v12/osmoutils"
 	types "github.com/osmosis-labs/osmosis/v12/x/concentrated-liquidity/types"
@@ -21,7 +22,7 @@ func (k Keeper) UpdateTickWithNewLiquidity(ctx sdk.Context, poolId uint64, tickI
 // NextInitializedTick returns the next initialized tick index based on the
 // current or provided tick index. If no initialized tick exists, <0, false>
 // will be returned.
-func (k Keeper) NextInitializedTick(ctx sdk.Context, poolId uint64, tickIndex int64) (next int64, initialized bool) {
+func (k Keeper) NextInitializedTick(ctx sdk.Context, poolId uint64, tickIndex int64, lte bool) (next int64, initialized bool) {
 	store := ctx.KVStore(k.storeKey)
 
 	// Construct a prefix store with a prefix of <TickPrefix | poolID>, allowing
@@ -29,7 +30,13 @@ func (k Keeper) NextInitializedTick(ctx sdk.Context, poolId uint64, tickIndex in
 	prefixBz := types.KeyTickPrefix(poolId)
 	prefixStore := prefix.NewStore(store, prefixBz)
 
-	iter := prefixStore.Iterator(sdk.Uint64ToBigEndian(uint64(tickIndex)), nil)
+	var iter db.Iterator
+	if lte {
+		iter = prefixStore.ReverseIterator(sdk.Uint64ToBigEndian(uint64(tickIndex)), nil)
+	} else {
+		iter = prefixStore.Iterator(sdk.Uint64ToBigEndian(uint64(tickIndex)), nil)
+	}
+
 	defer iter.Close()
 
 	i := 0
