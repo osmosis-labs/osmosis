42f637e9c88dc864a9d40eca537d8fa56ebe0232
Adam Tucker <adam@osmosis.team>
2022-10-20 17:05:25 -0500

    feat: further CL swap generalization (#3078)
    * further swap generalizing
    
    * code review changes

--- pool_test.go
+++ pool_test.go
@@ -11,39 +11,46 @@ import (
 
 func TestCalcOutAmtGivenIn(t *testing.T) {
 	ctx := sdk.Context{}
-	pool := cl.NewConcentratedLiquidityPool(1)
+	poolDenoms := []string{"eth", "usdc"}
+	pool, err := cl.NewConcentratedLiquidityPool(1, poolDenoms)
+	require.NoError(t, err)
 
 	// test asset a to b logic
-	tokensIn := sdk.NewCoins(sdk.NewCoin("eth", sdk.NewInt(133700)))
+	tokenIn := sdk.NewCoin("eth", sdk.NewInt(133700))
 	tokenOutDenom := "usdc"
 	swapFee := sdk.NewDec(0)
 
-	amountOut, _ := pool.CalcOutAmtGivenIn(ctx, tokensIn, tokenOutDenom, swapFee)
+	amountOut, err := pool.CalcOutAmtGivenIn(ctx, tokenIn, tokenOutDenom, swapFee)
+	require.NoError(t, err)
 	require.Equal(t, sdk.NewDec(663944647).String(), amountOut.Amount.ToDec().String())
 
 	// test asset b to a logic
-	tokensIn = sdk.NewCoins(sdk.NewCoin("usdc", sdk.NewInt(4199999999)))
+	tokenIn = sdk.NewCoin("usdc", sdk.NewInt(4199999999))
 	tokenOutDenom = "eth"
 	swapFee = sdk.NewDec(0)
 
-	amountOut, _ = pool.CalcOutAmtGivenIn(ctx, tokensIn, tokenOutDenom, swapFee)
+	amountOut, err = pool.CalcOutAmtGivenIn(ctx, tokenIn, tokenOutDenom, swapFee)
+	require.NoError(t, err)
 	require.Equal(t, sdk.NewDec(805287), amountOut.Amount.ToDec())
 
 	// test with swap fee
-	tokensIn = sdk.NewCoins(sdk.NewCoin("usdc", sdk.NewInt(4199999999)))
+	tokenIn = sdk.NewCoin("usdc", sdk.NewInt(4199999999))
 	tokenOutDenom = "eth"
 	swapFee = sdk.NewDecWithPrec(2, 2)
 
-	amountOut, _ = pool.CalcOutAmtGivenIn(ctx, tokensIn, tokenOutDenom, swapFee)
+	amountOut, err = pool.CalcOutAmtGivenIn(ctx, tokenIn, tokenOutDenom, swapFee)
+	require.NoError(t, err)
 	require.Equal(t, sdk.NewDec(789834), amountOut.Amount.ToDec())
 }
 
 func TestCalcInAmtGivenOut(t *testing.T) {
 	ctx := sdk.Context{}
-	pool := cl.NewConcentratedLiquidityPool(1)
+	poolDenoms := []string{"eth", "usdc"}
+	pool, err := cl.NewConcentratedLiquidityPool(1, poolDenoms)
+	require.NoError(t, err)
 
 	// test asset a to b logic
-	tokensOut := sdk.NewCoins(sdk.NewCoin("usdc", sdk.NewInt(4199999999)))
+	tokensOut := sdk.NewCoin("usdc", sdk.NewInt(4199999999))
 	tokenInDenom := "eth"
 	swapFee := sdk.NewDec(0)
 
@@ -52,7 +59,7 @@ func TestCalcInAmtGivenOut(t *testing.T) {
 	require.Equal(t, sdk.NewDec(805287), amountIn.Amount.ToDec())
 
 	// test asset b to a logic
-	tokensOut = sdk.NewCoins(sdk.NewCoin("eth", sdk.NewInt(133700)))
+	tokensOut = sdk.NewCoin("eth", sdk.NewInt(133700))
 	tokenInDenom = "usdc"
 	swapFee = sdk.NewDec(0)
 
@@ -61,10 +68,44 @@ func TestCalcInAmtGivenOut(t *testing.T) {
 	require.Equal(t, sdk.NewDec(663944647), amountIn.Amount.ToDec())
 
 	// test asset a to b logic
-	tokensOut = sdk.NewCoins(sdk.NewCoin("usdc", sdk.NewInt(4199999999)))
+	tokensOut = sdk.NewCoin("usdc", sdk.NewInt(4199999999))
 	tokenInDenom = "eth"
 	swapFee = sdk.NewDecWithPrec(2, 2)
 
-	amountIn, _ = pool.CalcInAmtGivenOut(ctx, tokensOut, tokenInDenom, swapFee)
+	amountIn, err = pool.CalcInAmtGivenOut(ctx, tokensOut, tokenInDenom, swapFee)
+	require.NoError(t, err)
 	require.Equal(t, sdk.NewDec(821721), amountIn.Amount.ToDec())
+}
+
+func TestSetInitialPoolDenoms(t *testing.T) {
+	poolDenoms := []string{"eth", "usdc"}
+	pool, err := cl.NewConcentratedLiquidityPool(1, poolDenoms)
+	require.NoError(t, err)
+	require.Equal(t, pool.Token0, poolDenoms[0])
+	require.Equal(t, pool.Token1, poolDenoms[1])
+
+	newPoolDenoms := []string{"axel", "osmo"}
+	err = pool.SetInitialPoolDenoms(newPoolDenoms)
+	require.NoError(t, err)
+	require.Equal(t, pool.Token0, newPoolDenoms[0])
+	require.Equal(t, pool.Token1, newPoolDenoms[1])
+
+	unorderedPoolDenoms := []string{"usdc", "eth"}
+	err = pool.SetInitialPoolDenoms(unorderedPoolDenoms)
+	require.NoError(t, err)
+	require.Equal(t, pool.Token0, unorderedPoolDenoms[1])
+	require.Equal(t, pool.Token1, unorderedPoolDenoms[0])
+
+	tooManyPoolDenoms := []string{"usdc", "eth", "osmo"}
+	err = pool.SetInitialPoolDenoms(tooManyPoolDenoms)
+	require.Error(t, err)
+
+	tooFewPoolDenoms := []string{"usdc"}
+	err = pool.SetInitialPoolDenoms(tooFewPoolDenoms)
+	require.Error(t, err)
+
+	sameDenoms := []string{"usdc", "usdc"}
+	err = pool.SetInitialPoolDenoms(sameDenoms)
+	require.Error(t, err)
+
 }
