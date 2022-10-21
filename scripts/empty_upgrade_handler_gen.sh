447a35c05e7abaaf466bcbd46278ac488e6fd565
Ruslan Akhtariev <ruslanakhtariev@Ruslans-MacBook-Air.local>
2022-10-21 13:54:22 +0700

    also changes app.go

--- scripts/empty_upgrade_handler_gen.sh
+++ scripts/empty_upgrade_handler_gen.sh
@@ -2,58 +2,58 @@
 
 # this script creates an empty directory in app/upgrades called "vX" where X is a previous version + 1 and
 # copies upgrade handler from the previous release with increased version where needed. Also insures that all the imports 
-# make use of a current MODULE version from go mod (see: 
+# make use of a current module version from go mod (see: 
 
-# MODULE=$(go mod edit -json | jq ".MODULE.Path") in this script
+# module=$(go mod edit -json | jq ".Module.Path") in this script
 
 # )
 # Github workflow which calls this script can be found here: osmosis/.github/workflows/auto-update-upgrade.yml
 
- LATEST_VERSION=0
+ latest_version=0
  for f in app/upgrades/*; do 
      s_f=(${f//// })
      version=${s_f[2]}
      num_version=${version//[!0-9]/}
-     if [[ $num_version -gt $LATEST_VERSION ]]; then
+     if [[ $num_version -gt $latest_version ]]; then
         LATEST_FILE=$f
-        LATEST_VERSION=$num_version
+        latest_version=$num_version
      fi
  done
- VERSION_CREATE=$((LATEST_VERSION+1))
- NEW_FILE=./app/upgrades/v${VERSION_CREATE}
+ version_create=$((latest_version+1))
+ new_file=./app/upgrades/v${version_create}
 
- mkdir $NEW_FILE
+ mkdir $new_file
 
- touch $NEW_FILE/constants.go
- touch $NEW_FILE/upgrades.go
+ touch $new_file/constants.go
+ touch $new_file/upgrades.go
 
- MODULE=$(go mod edit -json | jq ".MODULE.Path")
- MODULE=${MODULE%?}
- path=${MODULE%???}
+ module=$(go mod edit -json | jq ".Module.Path")
+ module=${module%?}
+ path=${module%???}
 
- cp ./app/upgrades/v${LATEST_VERSION}/constants.go $NEW_FILE/constants.go
- cp ./app/upgrades/v${LATEST_VERSION}/upgrades.go $NEW_FILE/upgrades.go
+ cp ./app/upgrades/v${latest_version}/constants.go $new_file/constants.go
+ cp ./app/upgrades/v${latest_version}/upgrades.go $new_file/upgrades.go
 
- sed -i "s/v$LATEST_VERSION/v$VERSION_CREATE/g" $NEW_FILE/constants.go
- sed -i "s/v$LATEST_VERSION/v$VERSION_CREATE/g" $NEW_FILE/upgrades.go
+ sed -i  "s/v$latest_version/v$version_create/g" $new_file/constants.go
+ sed -i  "s/v$latest_version/v$version_create/g" $new_file/upgrades.go
 
- BRACKS='"'
+ bracks='"'
 
  # change imports in case go mod changed
- sed -i "s|.*/app/upgrades.*|\t$MODULE/app/upgrades$BRACKS|" $NEW_FILE/constants.go
- sed -i "s|.*/app/upgrades.*|\t$MODULE/app/upgrades$BRACKS|" $NEW_FILE/upgrades.go
- sed -i "s|.*/app/keepers.*|\t$MODULE/app/keepers$BRACKS|" $NEW_FILE/upgrades.go
- sed -i "s|.*/x/lockup/types.*|\tlockuptypes $MODULE/x/lockup/types$BRACKS|" $NEW_FILE/upgrades.go
+ sed -i  "s|.*/app/upgrades.*|\t$module/app/upgrades$bracks|" $new_file/constants.go
+ sed -i  "s|.*/app/upgrades.*|\t$module/app/upgrades$bracks|" $new_file/upgrades.go
+ sed -i  "s|.*/app/keepers.*|\t$module/app/keepers$bracks|" $new_file/upgrades.go
+ sed -i  "s|.*/x/lockup/types.*|\tlockuptypes $module/x/lockup/types$bracks|" $new_file/upgrades.go
 
  # change app/app.go file
- APP_FILE=./app/app.go
- UPGRADES_LINE=$(grep -F upgrades.Upgrade{ $APP_FILE)
- UPGRADES_LINE="${UPGRADES_LINE%?}, v${VERSION_CREATE}.Upgrade}"
- sed -i "s|.*upgrades.Upgrade{.*|$UPGRADES_LINE|" $APP_FILE 
+ app_file=./app/app.go
+ UPGRADES_LINE=$(grep -F upgrades.Upgrade{ $app_file)
+ UPGRADES_LINE="${UPGRADES_LINE%?}, v${version_create}.Upgrade}"
+ sed -i  "s|.*upgrades.Upgrade{.*|$UPGRADES_LINE|" $app_file 
 
- PREV_IMPORT="v$LATEST_VERSION $MODULE/app/upgrades/v$LATEST_VERSION$BRACKS"
- NEW_IMPORT="v$VERSION_CREATE $MODULE/app/upgrades/v$VERSION_CREATE$BRACKS"
- sed -i "s|.*$PREV_IMPORT.*|\t$PREV_IMPORT\n\t$NEW_IMPORT|" $APP_FILE
+ PREV_IMPORT="v$latest_version $module/app/upgrades/v$latest_version$bracks"
+ NEW_IMPORT="v$version_create $module/app/upgrades/v$version_create$bracks"
+ sed -i "s|.*$PREV_IMPORT.*|\t$PREV_IMPORT\n\t$NEW_IMPORT|" $app_file
  
  # change e2e version in makefile
- sed -i "s/E2E_UPGRADE_VERSION := ${BRACKS}v$LATEST_VERSION$BRACKS/E2E_UPGRADE_VERSION := ${BRACKS}v$VERSION_CREATE$BRACKS/" ./Makefile
+ sed -i  "s/E2E_UPGRADE_VERSION := ${bracks}v$latest_version$bracks/E2E_UPGRADE_VERSION := ${bracks}v$version_create$bracks/" ./Makefile
