b6218b5001352dd9220b239e15a23dc4a4c5d8a4
Ruslan Akhtariev <ruslanakhtariev@Ruslans-MacBook-Air.local>
2022-10-21 13:48:04 +0700

    also changes app.go

--- scripts/empty_upgrade_handler_gen.sh
+++ scripts/empty_upgrade_handler_gen.sh
@@ -2,24 +2,24 @@
 
 # this script creates an empty directory in app/upgrades called "vX" where X is a previous version + 1 and
 # copies upgrade handler from the previous release with increased version where needed. Also insures that all the imports 
-# make use of a current module version from go mod (see: 
+# make use of a current MODULE version from go mod (see: 
 
-# module=$(go mod edit -json | jq ".Module.Path") in this script
+# MODULE=$(go mod edit -json | jq ".MODULE.Path") in this script
 
 # )
 # Github workflow which calls this script can be found here: osmosis/.github/workflows/auto-update-upgrade.yml
 
- latest_version=0
+ LATEST_VERSION=0
  for f in app/upgrades/*; do 
      s_f=(${f//// })
      version=${s_f[2]}
      num_version=${version//[!0-9]/}
-     if [[ $num_version -gt $latest_version ]]; then
+     if [[ $num_version -gt $LATEST_VERSION ]]; then
         LATEST_FILE=$f
-        latest_version=$num_version
+        LATEST_VERSION=$num_version
      fi
  done
- VERSION_CREATE=$((latest_version+1))
+ VERSION_CREATE=$((LATEST_VERSION+1))
  NEW_FILE=./app/upgrades/v${VERSION_CREATE}
 
  mkdir $NEW_FILE
@@ -27,33 +27,33 @@
  touch $NEW_FILE/constants.go
  touch $NEW_FILE/upgrades.go
 
- module=$(go mod edit -json | jq ".Module.Path")
- module=${module%?}
- path=${module%???}
+ MODULE=$(go mod edit -json | jq ".MODULE.Path")
+ MODULE=${MODULE%?}
+ path=${MODULE%???}
 
- cp ./app/upgrades/v${latest_version}/constants.go $NEW_FILE/constants.go
- cp ./app/upgrades/v${latest_version}/upgrades.go $NEW_FILE/upgrades.go
+ cp ./app/upgrades/v${LATEST_VERSION}/constants.go $NEW_FILE/constants.go
+ cp ./app/upgrades/v${LATEST_VERSION}/upgrades.go $NEW_FILE/upgrades.go
 
- sed -i "s/v$latest_version/v$VERSION_CREATE/g" $NEW_FILE/constants.go
- sed -i "s/v$latest_version/v$VERSION_CREATE/g" $NEW_FILE/upgrades.go
+ sed -i "s/v$LATEST_VERSION/v$VERSION_CREATE/g" $NEW_FILE/constants.go
+ sed -i "s/v$LATEST_VERSION/v$VERSION_CREATE/g" $NEW_FILE/upgrades.go
 
- bracks='"'
+ BRACKS='"'
 
  # change imports in case go mod changed
- sed -i "s|.*/app/upgrades.*|\t$module/app/upgrades$bracks|" $NEW_FILE/constants.go
- sed -i "s|.*/app/upgrades.*|\t$module/app/upgrades$bracks|" $NEW_FILE/upgrades.go
- sed -i "s|.*/app/keepers.*|\t$module/app/keepers$bracks|" $NEW_FILE/upgrades.go
- sed -i "s|.*/x/lockup/types.*|\tlockuptypes $module/x/lockup/types$bracks|" $NEW_FILE/upgrades.go
+ sed -i "s|.*/app/upgrades.*|\t$MODULE/app/upgrades$BRACKS|" $NEW_FILE/constants.go
+ sed -i "s|.*/app/upgrades.*|\t$MODULE/app/upgrades$BRACKS|" $NEW_FILE/upgrades.go
+ sed -i "s|.*/app/keepers.*|\t$MODULE/app/keepers$BRACKS|" $NEW_FILE/upgrades.go
+ sed -i "s|.*/x/lockup/types.*|\tlockuptypes $MODULE/x/lockup/types$BRACKS|" $NEW_FILE/upgrades.go
 
  # change app/app.go file
- app_file=./app/app.go
- UPGRADES_LINE=$(grep -F upgrades.Upgrade{ $app_file)
+ APP_FILE=./app/app.go
+ UPGRADES_LINE=$(grep -F upgrades.Upgrade{ $APP_FILE)
  UPGRADES_LINE="${UPGRADES_LINE%?}, v${VERSION_CREATE}.Upgrade}"
- sed -i "s|.*upgrades.Upgrade{.*|$UPGRADES_LINE|" $app_file 
+ sed -i "s|.*upgrades.Upgrade{.*|$UPGRADES_LINE|" $APP_FILE 
 
- PREV_IMPORT="v$latest_version $module/app/upgrades/v$latest_version$bracks"
- NEW_IMPORT="v$VERSION_CREATE $module/app/upgrades/v$VERSION_CREATE$bracks"
- sed -i"s|.*$PREV_IMPORT.*|\t$PREV_IMPORT\n\t$NEW_IMPORT|" $app_file
+ PREV_IMPORT="v$LATEST_VERSION $MODULE/app/upgrades/v$LATEST_VERSION$BRACKS"
+ NEW_IMPORT="v$VERSION_CREATE $MODULE/app/upgrades/v$VERSION_CREATE$BRACKS"
+ sed -i "s|.*$PREV_IMPORT.*|\t$PREV_IMPORT\n\t$NEW_IMPORT|" $APP_FILE
  
  # change e2e version in makefile
- sed -i "s/E2E_UPGRADE_VERSION := ${bracks}v$latest_version$bracks/E2E_UPGRADE_VERSION := ${bracks}v$VERSION_CREATE$bracks/" ./Makefile
+ sed -i "s/E2E_UPGRADE_VERSION := ${BRACKS}v$LATEST_VERSION$BRACKS/E2E_UPGRADE_VERSION := ${BRACKS}v$VERSION_CREATE$BRACKS/" ./Makefile
