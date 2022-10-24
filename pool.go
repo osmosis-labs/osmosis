b50a7b4493ae10cb62683f5b3eb8ce3d3efcb3fa
Roman <roman@osmosis.team>
2022-10-24 18:42:05 -0500

    refactor(CL): move multihop to swap router, wire to concentrated liquidity module (#3128)
    * feat/refactor(CL): move multihop to swaprouter, connect to concentrated liquidity
    
    * comments
    
    * godoc for RouteExactAmountOut
    
    * link issues
    
    * delete multihop_test.go
    
    * Update x/swaprouter/router_test.go
    
    Co-authored-by: Adam Tucker <adam@osmosis.team>
    
    * Update x/swaprouter/router_test.go
    
    Co-authored-by: Adam Tucker <adam@osmosis.team>
    
    * Update x/concentrated-liquidity/pool.go
    
    Co-authored-by: Adam Tucker <adam@osmosis.team>
    
    * Update x/gamm/keeper/swap.go
    
    Co-authored-by: Adam Tucker <adam@osmosis.team>
    
    Co-authored-by: Adam Tucker <adam@osmosis.team>

--- pool.go
+++ pool.go
@@ -1,6 +1,7 @@
 package concentrated_liquidity
 
 import (
+	"errors"
 	fmt "fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -28,6 +29,11 @@ func (k Keeper) CreateNewConcentratedLiquidityPool(ctx sdk.Context, poolId uint6
 	k.setPoolById(ctx, poolId, pool)
 
 	return pool, nil
+}
+
+// GetPool returns a pool with a given id.
+func (k Keeper) GetPool(ctx sdk.Context, poolId uint64) (types.PoolI, error) {
+	return nil, errors.New("not implemented")
 }
 
 func priceToTick(price sdk.Dec) sdk.Int {
