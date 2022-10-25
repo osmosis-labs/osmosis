5f4cdf6c8b0b11aa4c196e4a20b40b56aefae735
Adam Tucker <adamleetucker@outlook.com>
2022-10-25 10:50:29 -0700

    change liquidity to dec and pass test

--- x/concentrated-liquidity/lp.go
+++ x/concentrated-liquidity/lp.go
@@ -17,44 +17,44 @@ import (
 // Returns error if:
 // TODO: list error cases
 // TODO: table-driven tests
-func (k Keeper) createPosition(ctx sdk.Context, poolId uint64, owner sdk.AccAddress, amount0Desired, amount1Desired, amount0Min, amount1Min sdk.Int, lowerTick, upperTick int64) (amtDenom0, amtDenom1, liquidityCreated sdk.Int, err error) {
+func (k Keeper) createPosition(ctx sdk.Context, poolId uint64, owner sdk.AccAddress, amount0Desired, amount1Desired, amount0Min, amount1Min sdk.Int, lowerTick, upperTick int64) (amtDenom0, amtDenom1 sdk.Int, liquidityCreated sdk.Dec, err error) {
 	// TODO: calculate from amounts given
 	liquidityIn := sdk.MustNewDecFromStr("1517.882323")
 
 	// ensure types.MinTick <= lowerTick < types.MaxTick
 	// TODO (bez): Add unit tests.
 	if lowerTick < types.MinTick || lowerTick >= types.MaxTick {
-		return sdk.Int{}, sdk.Int{}, sdk.Int{}, fmt.Errorf("invalid lower tick: %d", lowerTick)
+		return sdk.Int{}, sdk.Int{}, sdk.Dec{}, fmt.Errorf("invalid lower tick: %d", lowerTick)
 	}
 
 	// ensure types.MaxTick < upperTick <= types.MinTick
 	// TODO (bez): Add unit tests.
 	if upperTick > types.MaxTick || upperTick <= types.MinTick {
-		return sdk.Int{}, sdk.Int{}, sdk.Int{}, fmt.Errorf("invalid upper tick: %d", upperTick)
+		return sdk.Int{}, sdk.Int{}, sdk.Dec{}, fmt.Errorf("invalid upper tick: %d", upperTick)
 	}
 
 	if liquidityIn.IsZero() {
-		return sdk.Int{}, sdk.Int{}, sdk.Int{}, fmt.Errorf("token in amount is zero")
+		return sdk.Int{}, sdk.Int{}, sdk.Dec{}, fmt.Errorf("token in amount is zero")
 	}
 
 	// update tickInfo state
 	// TODO: come back to sdk.Int vs sdk.Dec state & truncation
 	err = k.initOrUpdateTick(ctx, poolId, lowerTick, liquidityIn.TruncateInt(), false)
 	if err != nil {
-		return sdk.Int{}, sdk.Int{}, sdk.Int{}, err
+		return sdk.Int{}, sdk.Int{}, sdk.Dec{}, err
 	}
 
 	// TODO: come back to sdk.Int vs sdk.Dec state & truncation
 	err = k.initOrUpdateTick(ctx, poolId, upperTick, liquidityIn.TruncateInt(), true)
 	if err != nil {
-		return sdk.Int{}, sdk.Int{}, sdk.Int{}, err
+		return sdk.Int{}, sdk.Int{}, sdk.Dec{}, err
 	}
 
 	// update position state
 	// TODO: come back to sdk.Int vs sdk.Dec state & truncation
 	err = k.initOrUpdatePosition(ctx, poolId, owner, lowerTick, upperTick, liquidityIn.TruncateInt())
 	if err != nil {
-		return sdk.Int{}, sdk.Int{}, sdk.Int{}, err
+		return sdk.Int{}, sdk.Int{}, sdk.Dec{}, err
 	}
 
 	// now calculate amount for token0 and token1
@@ -67,6 +67,9 @@ func (k Keeper) createPosition(ctx sdk.Context, poolId uint64, owner sdk.AccAddr
 	amtDenom0 = calcAmount0Delta(liquidityIn, currentSqrtPrice, sqrtRatioUpperTick).RoundInt()
 	amtDenom1 = calcAmount1Delta(liquidityIn, currentSqrtPrice, sqrtRatioLowerTick).RoundInt()
 
+	// TODO: calculate from final amounts
+	liquidityCreated = sdk.MustNewDecFromStr("1517.882323")
+
 	return amtDenom0, amtDenom1, liquidityCreated, nil
 }
 
@@ -75,7 +78,7 @@ func (k Keeper) createPosition(ctx sdk.Context, poolId uint64, owner sdk.AccAddr
 // Returns error if
 // - there is no position in the given tick ranges
 // - if tick ranges are invalid
-// - if attemps to withdraw an amount higher than originally provided in createPosition for a given range
+// - if attempts to withdraw an amount higher than originally provided in createPosition for a given range
 // TODO: implement and table-driven tests
 func (k Keeper) withdrawPosition(ctx sdk.Context, poolId uint64, owner sdk.AccAddress, lowerTick, upperTick int64, liquidityAmount sdk.Int) (amtDenom0, amtDenom1 sdk.Int, err error) {
 	panic("not implemented")
