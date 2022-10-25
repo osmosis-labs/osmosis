5cd8a697368a9f5a564aa600f7706aa4ad6f39e4
Roman <roman@osmosis.team>
2022-10-25 16:36:18 -0400

    Update x/concentrated-liquidity/msg_server.go
    Co-authored-by: Adam Tucker <adam@osmosis.team>

--- x/concentrated-liquidity/msg_server.go
+++ x/concentrated-liquidity/msg_server.go
@@ -28,7 +28,7 @@ func (server msgServer) CreatePosition(goCtx context.Context, msg *types.MsgCrea
 		return nil, err
 	}
 
-	actualAmount0, actualAmount1, liquidityCeated, err := server.keeper.createPosition(ctx, msg.PoolId, sender, msg.TokenDesired0.Amount, msg.TokenDesired1.Amount, msg.TokenMinAmount0, msg.TokenMinAmount1, msg.LowerTick, msg.UpperTick)
+	actualAmount0, actualAmount1, liquidityCreated, err := server.keeper.createPosition(ctx, msg.PoolId, sender, msg.TokenDesired0.Amount, msg.TokenDesired1.Amount, msg.TokenMinAmount0, msg.TokenMinAmount1, msg.LowerTick, msg.UpperTick)
 	if err != nil {
 		return nil, err
 	}
