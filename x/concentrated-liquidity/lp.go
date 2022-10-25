c8ec090b30ee06323a8403d3c043e87ab5d121e1
Matt, Park <45252226+mattverse@users.noreply.github.com>
2022-10-24 20:44:26 -0700

    Update x/concentrated-liquidity/lp.go
    Co-authored-by: Adam Tucker <adam@osmosis.team>

--- x/concentrated-liquidity/lp.go
+++ x/concentrated-liquidity/lp.go
@@ -35,7 +35,7 @@ func (k Keeper) Mint(ctx sdk.Context, poolId uint64, owner sdk.AccAddress, liqui
 	// TODO: come back to sdk.Int vs sdk.Dec state & truncation
 	err = k.initOrUpdateTick(ctx, poolId, upperTick, liquidityIn.TruncateInt(), true)
 	if err != nil {
-		return sdk.Int{}, sdk.Int{}, nil
+		return sdk.Int{}, sdk.Int{}, err
 	}
 
 	// update position state
