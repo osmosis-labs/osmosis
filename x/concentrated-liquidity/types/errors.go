dc78e6dbd792074f33cd8d3599315da3d5473e80
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2022-11-01 12:54:02 +0700

    sdkerr => go err

--- x/concentrated-liquidity/types/errors.go
+++ x/concentrated-liquidity/types/errors.go
@@ -2,23 +2,13 @@ package types
 
 import (
 	fmt "fmt"
-
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-type PoolDoesNotExistError struct {
-	PoolId uint64
-}
-
-func (e PoolDoesNotExistError) Error() string {
-	return fmt.Sprintf("pool with ID %d does not exist", e.PoolId)
-}
-
-// x/gamm module sentinel errors.
+// x/concentrated-liquidity module sentinel errors.
 var (
-	ErrInvalidLowerUpperTick = sdkerrors.Register(ModuleName, 1, "lower tick must be lesser than upper")
-	ErrInvalidLowerTick      = sdkerrors.Register(ModuleName, 2, "lower tick must be in valid range")
-	ErrLimitUpperTick        = sdkerrors.Register(ModuleName, 3, "upper tick must be in valid range")
+	ErrInvalidLowerUpperTick = fmt.Errorf("lower tick must be lesser than upper")
+	ErrInvalidLowerTick      = fmt.Errorf("lower tick must be in valid range")
+	ErrLimitUpperTick        = fmt.Errorf("upper tick must be in valid range")
 
-	ErrNotPositiveRequireAmount = sdkerrors.Register(ModuleName, 21, "required amount should be positive")
+	ErrNotPositiveRequireAmount = fmt.Errorf("required amount should be positive")
 )
