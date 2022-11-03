ed9b5cc24ff69122d0bfbb01767f5d36fcbf9c72
Adam Tucker <adamleetucker@outlook.com>
2022-11-03 16:52:42 -0500

    fix test

--- x/concentrated-liquidity/pool_test.go
+++ x/concentrated-liquidity/pool_test.go
@@ -152,7 +152,7 @@ func (s *KeeperTestSuite) TestCalcOutAmtGivenIn() {
 			priceLimit:    sdk.NewDec(6106),
 			// we expect to put 10000 usdc in and in return get 1.820536 eth back
 			// TODO: see why we don't get 9938.148 usdc and 1.80615 eth
-			expectedTokenIn:  sdk.NewCoin("usdc", sdk.NewInt(9999999999)),
+			expectedTokenIn:  sdk.NewCoin("usdc", sdk.NewInt(10000000000)),
 			expectedTokenOut: sdk.NewCoin("eth", sdk.NewInt(1820536)),
 			expectedTick:     sdk.NewInt(87173),
 			newLowerPrice:    sdk.NewDec(5501),
