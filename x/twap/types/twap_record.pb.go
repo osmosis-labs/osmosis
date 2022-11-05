7aba287917f2281fc25ff41bd6a14b770fcc887c
mattverse <mattpark1028@gmail.com>
2022-11-05 17:44:30 +0900

    Proto changes :)

--- x/twap/types/twap_record.pb.go
+++ x/twap/types/twap_record.pb.go
@@ -51,10 +51,10 @@ type TwapRecord struct {
 	Time time.Time `protobuf:"bytes,5,opt,name=time,proto3,stdtime" json:"time" yaml:"record_time"`
 	// We store the last spot prices in the struct, so that we can interpolate
 	// accumulator values for times between when accumulator records are stored.
-	P0LastSpotPrice             github_com_cosmos_cosmos_sdk_types.Dec `protobuf:"bytes,6,opt,name=p0_last_spot_price,json=p0LastSpotPrice,proto3,customtype=github.com/cosmos/cosmos-sdk/types.Dec" json:"p0_last_spot_price"`
-	P1LastSpotPrice             github_com_cosmos_cosmos_sdk_types.Dec `protobuf:"bytes,7,opt,name=p1_last_spot_price,json=p1LastSpotPrice,proto3,customtype=github.com/cosmos/cosmos-sdk/types.Dec" json:"p1_last_spot_price"`
-	P0ArithmeticTwapAccumulator github_com_cosmos_cosmos_sdk_types.Dec `protobuf:"bytes,8,opt,name=p0_arithmetic_twap_accumulator,json=p0ArithmeticTwapAccumulator,proto3,customtype=github.com/cosmos/cosmos-sdk/types.Dec" json:"p0_arithmetic_twap_accumulator"`
-	P1ArithmeticTwapAccumulator github_com_cosmos_cosmos_sdk_types.Dec `protobuf:"bytes,9,opt,name=p1_arithmetic_twap_accumulator,json=p1ArithmeticTwapAccumulator,proto3,customtype=github.com/cosmos/cosmos-sdk/types.Dec" json:"p1_arithmetic_twap_accumulator"`
+	P1LastSpotPrice             github_com_cosmos_cosmos_sdk_types.Dec `protobuf:"bytes,6,opt,name=p1_last_spot_price,json=p1LastSpotPrice,proto3,customtype=github.com/cosmos/cosmos-sdk/types.Dec" json:"p1_last_spot_price"`
+	P0LastSpotPrice             github_com_cosmos_cosmos_sdk_types.Dec `protobuf:"bytes,7,opt,name=p0_last_spot_price,json=p0LastSpotPrice,proto3,customtype=github.com/cosmos/cosmos-sdk/types.Dec" json:"p0_last_spot_price"`
+	P1ArithmeticTwapAccumulator github_com_cosmos_cosmos_sdk_types.Dec `protobuf:"bytes,8,opt,name=p1_arithmetic_twap_accumulator,json=p1ArithmeticTwapAccumulator,proto3,customtype=github.com/cosmos/cosmos-sdk/types.Dec" json:"p1_arithmetic_twap_accumulator"`
+	P0ArithmeticTwapAccumulator github_com_cosmos_cosmos_sdk_types.Dec `protobuf:"bytes,9,opt,name=p0_arithmetic_twap_accumulator,json=p0ArithmeticTwapAccumulator,proto3,customtype=github.com/cosmos/cosmos-sdk/types.Dec" json:"p0_arithmetic_twap_accumulator"`
 	// This field contains the time in which the last spot price error occured.
 	// It is used to alert the caller if they are getting a potentially erroneous
 	// TWAP, due to an unforeseen underlying error.
@@ -165,20 +165,20 @@ var fileDescriptor_dbf5c78678e601aa = []byte{
 	0xa8, 0x02, 0x37, 0x8d, 0xdb, 0x9d, 0xa1, 0xaa, 0x7c, 0xe9, 0xad, 0xb9, 0xbd, 0x58, 0x27, 0x5a,
 	0xef, 0x40, 0x5b, 0xcc, 0x60, 0xdf, 0xef, 0x1b, 0x83, 0xce, 0x71, 0xcf, 0x57, 0x03, 0xfa, 0xcb,
 	0x01, 0xfd, 0xd3, 0xe5, 0x80, 0x23, 0xe7, 0xaa, 0x71, 0x5b, 0x37, 0x8d, 0x6b, 0xad, 0xf1, 0x44,
-	0xb2, 0x77, 0xf9, 0xc3, 0x35, 0x62, 0xc9, 0xb1, 0x3e, 0x01, 0x8b, 0x86, 0xb0, 0x44, 0x8c, 0x43,
+	0xb2, 0x77, 0xf9, 0xc3, 0x35, 0x62, 0xc9, 0xb1, 0x3e, 0x01, 0x8b, 0x46, 0xb0, 0x44, 0x8c, 0x43,
 	0x46, 0x09, 0x87, 0xb4, 0x2e, 0x52, 0x6c, 0x9b, 0xa2, 0xf7, 0x91, 0x2f, 0x08, 0xdf, 0x1b, 0xf7,
 	0x30, 0x2f, 0xf8, 0x78, 0x9a, 0xf8, 0x29, 0xa9, 0xf4, 0xf1, 0xeb, 0xc7, 0x11, 0xcb, 0x3e, 0x07,
-	0x7c, 0x46, 0x31, 0xf3, 0x4f, 0x70, 0x1a, 0x1f, 0xd0, 0xf0, 0x2d, 0x62, 0xfc, 0x03, 0x25, 0xfc,
-	0xbd, 0xc0, 0x48, 0x78, 0xf4, 0x17, 0x7c, 0x77, 0x4b, 0x78, 0xb4, 0x0e, 0x67, 0xc0, 0xa1, 0x21,
+	0x7c, 0x46, 0x31, 0xf3, 0x4f, 0x70, 0x1a, 0x1f, 0xd0, 0xe8, 0x2d, 0x62, 0xfc, 0x03, 0x25, 0xfc,
+	0xbd, 0xc0, 0x48, 0x78, 0xf8, 0x17, 0x7c, 0x77, 0x4b, 0x78, 0xb8, 0x0e, 0x67, 0xc0, 0xa1, 0x11,
 	0x44, 0x75, 0xc1, 0xc7, 0x15, 0xe6, 0x45, 0x0a, 0xe5, 0x05, 0x44, 0x69, 0x3a, 0xad, 0xa6, 0x25,
-	0xe2, 0xa4, 0xb6, 0x1f, 0x6c, 0x55, 0xe8, 0x29, 0x0d, 0x87, 0x2b, 0xa8, 0xb8, 0x1b, 0xc3, 0x3f,
-	0x48, 0x59, 0x34, 0xfa, 0x6f, 0xd1, 0xbd, 0x2d, 0x8b, 0x46, 0xff, 0x2e, 0x7a, 0x06, 0x0e, 0xe4,
+	0xe2, 0xa4, 0xb6, 0x1f, 0x6c, 0x55, 0xe8, 0x29, 0x8d, 0x86, 0x2b, 0xa8, 0xb8, 0x1b, 0xc3, 0x3f,
+	0x48, 0x59, 0x34, 0xfc, 0x6f, 0xd1, 0xbd, 0x2d, 0x8b, 0x86, 0xff, 0x2e, 0x7a, 0x06, 0x0e, 0xe4,
 	0x19, 0xe2, 0xba, 0x26, 0xb5, 0xdc, 0xa0, 0xdd, 0xb9, 0x73, 0xfd, 0x9e, 0x5e, 0xff, 0x23, 0xb5,
 	0xfe, 0x0d, 0x80, 0xba, 0x02, 0xfb, 0xc2, 0xfb, 0x4a, 0x38, 0x45, 0xde, 0xe8, 0xcd, 0xd5, 0xdc,
 	0x31, 0xae, 0xe7, 0x8e, 0xf1, 0x73, 0xee, 0x18, 0x97, 0x0b, 0xa7, 0x75, 0xbd, 0x70, 0x5a, 0xdf,
 	0x16, 0x4e, 0xeb, 0x63, 0x78, 0x6b, 0x0c, 0xfd, 0x51, 0x1f, 0x95, 0x28, 0x61, 0x4b, 0x23, 0x38,
 	0x8f, 0x8e, 0x83, 0x2f, 0xea, 0x7f, 0x20, 0x87, 0x4a, 0x4c, 0xd9, 0xd2, 0x8b, 0xdf, 0x01, 0x00,
-	0x00, 0xff, 0xff, 0xdf, 0xff, 0x6c, 0x30, 0x2c, 0x04, 0x00, 0x00,
+	0x00, 0xff, 0xff, 0x6a, 0x8b, 0xe3, 0xd1, 0x2c, 0x04, 0x00, 0x00,
 }
 
 func (m *TwapRecord) Marshal() (dAtA []byte, err error) {
@@ -210,16 +210,6 @@ func (m *TwapRecord) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	i--
 	dAtA[i] = 0x5a
 	{
-		size := m.P1ArithmeticTwapAccumulator.Size()
-		i -= size
-		if _, err := m.P1ArithmeticTwapAccumulator.MarshalTo(dAtA[i:]); err != nil {
-			return 0, err
-		}
-		i = encodeVarintTwapRecord(dAtA, i, uint64(size))
-	}
-	i--
-	dAtA[i] = 0x4a
-	{
 		size := m.P0ArithmeticTwapAccumulator.Size()
 		i -= size
 		if _, err := m.P0ArithmeticTwapAccumulator.MarshalTo(dAtA[i:]); err != nil {
@@ -228,11 +218,21 @@ func (m *TwapRecord) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i = encodeVarintTwapRecord(dAtA, i, uint64(size))
 	}
 	i--
+	dAtA[i] = 0x4a
+	{
+		size := m.P1ArithmeticTwapAccumulator.Size()
+		i -= size
+		if _, err := m.P1ArithmeticTwapAccumulator.MarshalTo(dAtA[i:]); err != nil {
+			return 0, err
+		}
+		i = encodeVarintTwapRecord(dAtA, i, uint64(size))
+	}
+	i--
 	dAtA[i] = 0x42
 	{
-		size := m.P1LastSpotPrice.Size()
+		size := m.P0LastSpotPrice.Size()
 		i -= size
-		if _, err := m.P1LastSpotPrice.MarshalTo(dAtA[i:]); err != nil {
+		if _, err := m.P0LastSpotPrice.MarshalTo(dAtA[i:]); err != nil {
 			return 0, err
 		}
 		i = encodeVarintTwapRecord(dAtA, i, uint64(size))
@@ -240,9 +240,9 @@ func (m *TwapRecord) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	i--
 	dAtA[i] = 0x3a
 	{
-		size := m.P0LastSpotPrice.Size()
+		size := m.P1LastSpotPrice.Size()
 		i -= size
-		if _, err := m.P0LastSpotPrice.MarshalTo(dAtA[i:]); err != nil {
+		if _, err := m.P1LastSpotPrice.MarshalTo(dAtA[i:]); err != nil {
 			return 0, err
 		}
 		i = encodeVarintTwapRecord(dAtA, i, uint64(size))
@@ -317,13 +317,13 @@ func (m *TwapRecord) Size() (n int) {
 	}
 	l = github_com_gogo_protobuf_types.SizeOfStdTime(m.Time)
 	n += 1 + l + sovTwapRecord(uint64(l))
-	l = m.P0LastSpotPrice.Size()
-	n += 1 + l + sovTwapRecord(uint64(l))
 	l = m.P1LastSpotPrice.Size()
 	n += 1 + l + sovTwapRecord(uint64(l))
-	l = m.P0ArithmeticTwapAccumulator.Size()
+	l = m.P0LastSpotPrice.Size()
 	n += 1 + l + sovTwapRecord(uint64(l))
 	l = m.P1ArithmeticTwapAccumulator.Size()
+	n += 1 + l + sovTwapRecord(uint64(l))
+	l = m.P0ArithmeticTwapAccumulator.Size()
 	n += 1 + l + sovTwapRecord(uint64(l))
 	l = github_com_gogo_protobuf_types.SizeOfStdTime(m.LastErrorTime)
 	n += 1 + l + sovTwapRecord(uint64(l))
@@ -502,40 +502,6 @@ func (m *TwapRecord) Unmarshal(dAtA []byte) error {
 			iNdEx = postIndex
 		case 6:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field P0LastSpotPrice", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowTwapRecord
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthTwapRecord
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthTwapRecord
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			if err := m.P0LastSpotPrice.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
-		case 7:
-			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field P1LastSpotPrice", wireType)
 			}
 			var stringLen uint64
@@ -568,9 +534,9 @@ func (m *TwapRecord) Unmarshal(dAtA []byte) error {
 				return err
 			}
 			iNdEx = postIndex
-		case 8:
+		case 7:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field P0ArithmeticTwapAccumulator", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field P0LastSpotPrice", wireType)
 			}
 			var stringLen uint64
 			for shift := uint(0); ; shift += 7 {
@@ -598,11 +564,11 @@ func (m *TwapRecord) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			if err := m.P0ArithmeticTwapAccumulator.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+			if err := m.P0LastSpotPrice.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
-		case 9:
+		case 8:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field P1ArithmeticTwapAccumulator", wireType)
 			}
@@ -633,6 +599,40 @@ func (m *TwapRecord) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			if err := m.P1ArithmeticTwapAccumulator.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		case 9:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field P0ArithmeticTwapAccumulator", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTwapRecord
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthTwapRecord
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthTwapRecord
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if err := m.P0ArithmeticTwapAccumulator.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
