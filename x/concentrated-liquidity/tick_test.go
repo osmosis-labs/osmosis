bfc5f9365a7aaa194368b8f1367c9a35a877f694
Adam Tucker <adamleetucker@outlook.com>
2022-11-05 22:16:09 -0500

    clean up tick test

--- x/concentrated-liquidity/tick_test.go
+++ x/concentrated-liquidity/tick_test.go
@@ -128,29 +128,26 @@ func TestKeeper_NextInitializedTick(t *testing.T) {
 }
 
 func (suite *KeeperTestSuite) TestTickToSqrtPrice() {
-	testCases := []struct {
-		name              string
+	testCases := map[string]struct {
 		tickIndex         sdk.Int
 		sqrtPriceExpected string
 	}{
-		{
-			"happy path 1",
-			sdk.NewInt(85176),
-			"70.710004849206351867", // 70.710004849206120647
+		"happy path 1": {
+			tickIndex:         sdk.NewInt(85176),
+			sqrtPriceExpected: "70.710004849206351867", // 70.710004849206120647
 			// https://www.wolframalpha.com/input?i2d=true&i=Power%5B1.0001%2CDivide%5B85176%2C2%5D%5D
 		},
-		{
-			"happy path 2",
-			sdk.NewInt(86129),
-			"74.160724590951092256", // 74.160724590950847046
+		"happy path 2": {
+			tickIndex:         sdk.NewInt(86129),
+			sqrtPriceExpected: "74.160724590951092256", // 74.160724590950847046
 			// https://www.wolframalpha.com/input?i2d=true&i=Power%5B1.0001%2CDivide%5B86129%2C2%5D%5D
 		},
 	}
 
-	for _, tc := range testCases {
+	for name, tc := range testCases {
 		tc := tc
 
-		suite.Run(tc.name, func() {
+		suite.Run(name, func() {
 			sqrtPrice, err := cl.TickToSqrtPrice(tc.tickIndex)
 			suite.Require().NoError(err)
 			suite.Require().Equal(tc.sqrtPriceExpected, sqrtPrice.String())
