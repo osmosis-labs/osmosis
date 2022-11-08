9788ade32ca7c1f2a30b50e6aedfad641d724a90
stackman27 <sis1001@berkeley.edu>
2022-11-08 14:36:48 -0800

    Revert "[ValSet-Pref] Module Wired up and created MsgSetValidatorSetPreference  (#2892)"
    This reverts commit 2ee66e40f543481c3a43c40034a916679d16ce31.

--- app/upgrades/v13/constants.go
+++ app/upgrades/v13/constants.go
@@ -1,19 +1,16 @@
 package v13
 
 import (
-	store "github.com/cosmos/cosmos-sdk/store/types"
 	"github.com/osmosis-labs/osmosis/v12/app/upgrades"
-	valsetpreftypes "github.com/osmosis-labs/osmosis/v12/x/valset-pref/types"
+
+	store "github.com/cosmos/cosmos-sdk/store/types"
 )
 
-// UpgradeName defines the on-chain upgrade name for the Osmosis v13 upgrade.
+// UpgradeName defines the on-chain upgrade name for the Osmosis v9 upgrade.
 const UpgradeName = "v13"
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateUpgradeHandler,
-	StoreUpgrades: store.StoreUpgrades{
-		Added:   []string{valsetpreftypes.StoreKey},
-		Deleted: []string{}, // double check bech32ibc
-	},
+	StoreUpgrades:        store.StoreUpgrades{},
 }
