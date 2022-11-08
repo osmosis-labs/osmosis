0b360432d6300e12ef805e061c7a3e1d62b17533
Ruslan Akhtariev <ruslanakhtariev@Ruslans-MacBook-Air.local>
2022-11-08 12:06:46 +0700

    reset

--- x/lockup/module.go
+++ x/lockup/module.go
@@ -16,7 +16,6 @@ import (
 	"encoding/json"
 	"fmt"
 
-	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
 	"github.com/gorilla/mux"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/spf13/cobra"
@@ -80,11 +79,6 @@ func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config client.TxEncod
 		return fmt.Errorf("failed to unmarshal %s genesis state: %w", types.ModuleName, err)
 	}
 	return genState.Validate()
-}
-
-// AutoCLIOptions implements the autocli.HasAutoCLIConfig interface.
-func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
-	return types.AutoCLIOptions
 }
 
 // RegisterRESTRoutes registers the capability module's REST service handlers.
