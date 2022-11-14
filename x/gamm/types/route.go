b46e711c1167e3e71eafc7793633359ee2c05b78
Adam Tucker <adamleetucker@outlook.com>
2022-11-14 10:52:16 -0600

    remove unused code

--- x/gamm/types/route.go
+++ x/gamm/types/route.go
@@ -2,15 +2,9 @@ package types
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
-	appparams "github.com/osmosis-labs/osmosis/v12/app/params"
 )
 
 type SwapAmountInRoutes []SwapAmountInRoute
-
-func (routes SwapAmountInRoutes) IsOsmoRoutedMultihop() bool {
-	return len(routes) == 2 && (routes[0].TokenOutDenom == appparams.BaseCoinUnit) && (routes[0].PoolId != routes[1].PoolId)
-}
 
 func (routes SwapAmountInRoutes) Validate() error {
 	if len(routes) == 0 {
@@ -52,10 +46,6 @@ func (routes SwapAmountInRoutes) Length() int {
 }
 
 type SwapAmountOutRoutes []SwapAmountOutRoute
-
-func (routes SwapAmountOutRoutes) IsOsmoRoutedMultihop() bool {
-	return len(routes) == 2 && (routes[1].TokenInDenom == appparams.BaseCoinUnit)
-}
 
 func (routes SwapAmountOutRoutes) Validate() error {
 	if len(routes) == 0 {
