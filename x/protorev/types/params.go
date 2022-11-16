b3830d8e7c11d8574739460b4cf652fd5426bc6d
davidterpay <david.terpay@gmail.com>
2022-11-16 14:53:55 -0500

    new proto messages

--- x/protorev/types/params.go
+++ x/protorev/types/params.go
@@ -1,15 +1,10 @@
 package types
 
 import (
-	
-
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
-	"gopkg.in/yaml.v2"
 )
 
 var _ paramtypes.ParamSet = (*Params)(nil)
-
-
 
 // ParamKeyTable the param key table for launch module
 func ParamKeyTable() paramtypes.KeyTable {
@@ -17,31 +12,21 @@ func ParamKeyTable() paramtypes.KeyTable {
 }
 
 // NewParams creates a new Params instance
-func NewParams(
-) Params {
-	return Params{
-	}
+func NewParams() Params {
+	return Params{}
 }
 
 // DefaultParams returns a default set of parameters
 func DefaultParams() Params {
-	return NewParams(
-	)
+	return NewParams()
 }
 
 // ParamSetPairs get the params.ParamSet
 func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
-	return paramtypes.ParamSetPairs{
-	}
+	return paramtypes.ParamSetPairs{}
 }
 
 // Validate validates the set of params
 func (p Params) Validate() error {
 	return nil
-}
-
-// String implements the Stringer interface.
-func (p Params) String() string {
-	out, _ := yaml.Marshal(p)
-	return string(out)
 }
