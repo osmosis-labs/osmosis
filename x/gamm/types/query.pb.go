6eae18d11bcff3191c6e3bde134c82cae40e39e8
Dev Ojha <ValarDragon@users.noreply.github.com>
2022-11-17 00:37:33 -0800

    Code duplication approach to fixing EstimateSwap (#3424)
    * Try duplicating estimate swap logic
    
    * Fix lint
    
    * Make proto-all
    
    * Adding some test cases :)
    
    Co-authored-by: mattverse <mattpark1028@gmail.com>

--- x/gamm/types/query.pb.go
+++ x/gamm/types/query.pb.go
@@ -1160,6 +1160,7 @@ func (m *QuerySpotPriceResponse) GetSpotPrice() string {
 
 // =============================== EstimateSwapExactAmountIn
 type QuerySwapExactAmountInRequest struct {
+	// TODO: CHANGE THIS TO RESERVED IN A PATCH RELEASE
 	Sender  string              `protobuf:"bytes,1,opt,name=sender,proto3" json:"sender,omitempty" yaml:"sender"`
 	PoolId  uint64              `protobuf:"varint,2,opt,name=pool_id,json=poolId,proto3" json:"pool_id,omitempty" yaml:"pool_id"`
 	TokenIn string              `protobuf:"bytes,3,opt,name=token_in,json=tokenIn,proto3" json:"token_in,omitempty" yaml:"token_in"`
@@ -1266,6 +1267,7 @@ var xxx_messageInfo_QuerySwapExactAmountInResponse proto.InternalMessageInfo
 
 // =============================== EstimateSwapExactAmountOut
 type QuerySwapExactAmountOutRequest struct {
+	// TODO: CHANGE THIS TO RESERVED IN A PATCH RELEASE
 	Sender   string               `protobuf:"bytes,1,opt,name=sender,proto3" json:"sender,omitempty" yaml:"sender"`
 	PoolId   uint64               `protobuf:"varint,2,opt,name=pool_id,json=poolId,proto3" json:"pool_id,omitempty" yaml:"pool_id"`
 	Routes   []SwapAmountOutRoute `protobuf:"bytes,3,rep,name=routes,proto3" json:"routes" yaml:"routes"`
