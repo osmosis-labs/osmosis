dc3d140f2aeea37035538c803ceb872b22383ed4
Đỗ Việt Hoàng <hoangdv2429@gmail.com>
2022-11-18 14:37:47 +0700

    adding test cases for ValidateScalingFactors

--- x/gamm/pool-models/stableswap/pool_test.go
+++ x/gamm/pool-models/stableswap/pool_test.go
@@ -1253,3 +1253,46 @@ func TestStableswapSpotPrice(t *testing.T) {
 		})
 	}
 }
+
+func TestValidateScalingFactors(t *testing.T) {
+
+	tests := map[string]struct {
+		scalingFactors []uint64
+		numAssets      int
+		expectError    bool
+	}{
+		"number of scaling factors match number of assets": {
+			numAssets:      4,
+			scalingFactors: []uint64{10, 10, 10, 10},
+			expectError:    false,
+		},
+		"number of scaling factors and assets mismatch": {
+			numAssets:      3,
+			scalingFactors: []uint64{10, 10, 10, 10},
+			expectError:    true,
+		},
+		"number of scaling factors is zero": {
+			numAssets:      0,
+			scalingFactors: []uint64{10, 10, 10},
+			expectError:    true,
+		},
+		"number of scaling factors is less than zero": {
+			numAssets:      -5,
+			scalingFactors: []uint64{10, 10, 10},
+			expectError:    true,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			err := validateScalingFactors(tc.scalingFactors, tc.numAssets)
+
+			if tc.expectError != false {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+		})
+	}
+}
