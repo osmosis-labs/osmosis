2bbf28d8e1e465b96bdd063005718b251f6dc8ed
Roman <roman@osmosis.team>
2022-11-18 19:33:25 -0500

    Update x/concentrated-liquidity/lp_test.go
    Co-authored-by: Adam Tucker <adam@osmosis.team>

--- x/concentrated-liquidity/lp_test.go
+++ x/concentrated-liquidity/lp_test.go
@@ -157,7 +157,7 @@ func (s *KeeperTestSuite) TestWithdrawPosition() {
 			},
 		},
 		"error: insufficient liqudity": {
-			// setup parameters for creation a pool and position.
+			// setup parameters for creating a pool and position.
 			setupConfig: baseCase,
 
 			// system under test parameters
