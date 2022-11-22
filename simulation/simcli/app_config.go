045a2d50378eefdbfea74633125ff78784902d78
Jacob Gadikian <jacobgadikian@gmail.com>
2022-11-22 20:12:52 +0700

    rebuild protos (didn't work)

--- simulation/simcli/app_config.go
+++ simulation/simcli/app_config.go
@@ -22,82 +22,80 @@ import (
 	lockuptypes "github.com/osmosis-labs/osmosis/v13/x/lockup/types"
 	superfluidtypes "github.com/osmosis-labs/osmosis/v13/x/superfluid/types"
 	twaptypes "github.com/osmosis-labs/osmosis/v13/x/twap/types"
-	// lockupmodule "../../autocli/lockup"
+	// lockupmodule "github.com/osmosis-labs/osmosis/v13/autocli/lockup"
 )
 
-var (
-	// application configuration (used by depinject)
-	AppConfig = appconfig.Compose(&appv1alpha1.Config{
-		Modules: []*appv1alpha1.ModuleConfig{
-			{
-				Name: "runtime",
-				Config: appconfig.WrapAny(&runtimev1alpha1.Module{
-					AppName: "simulation",
+// application configuration (used by depinject)
+var AppConfig = appconfig.Compose(&appv1alpha1.Config{
+	Modules: []*appv1alpha1.ModuleConfig{
+		{
+			Name: "runtime",
+			Config: appconfig.WrapAny(&runtimev1alpha1.Module{
+				AppName: "simulation",
 
-					BeginBlockers: []string{
-						upgradetypes.ModuleName,
-						epochstypes.ModuleName,
-						capabilitytypes.ModuleName,
-						distrtypes.ModuleName,
-						slashingtypes.ModuleName,
-						evidencetypes.ModuleName,
-						stakingtypes.ModuleName,
-						superfluidtypes.ModuleName,
-						ibchost.ModuleName,
-					},
+				BeginBlockers: []string{
+					upgradetypes.ModuleName,
+					epochstypes.ModuleName,
+					capabilitytypes.ModuleName,
+					distrtypes.ModuleName,
+					slashingtypes.ModuleName,
+					evidencetypes.ModuleName,
+					stakingtypes.ModuleName,
+					superfluidtypes.ModuleName,
+					ibchost.ModuleName,
+				},
 
-					EndBlockers: []string{
-						govtypes.ModuleName,
-						stakingtypes.ModuleName,
-						twaptypes.ModuleName,
-					},
+				EndBlockers: []string{
+					govtypes.ModuleName,
+					stakingtypes.ModuleName,
+					twaptypes.ModuleName,
+				},
 
-					InitGenesis: []string{
-						// capabilitytypes.ModuleName,
-						// authtypes.ModuleName,
-						// banktypes.ModuleName,
-						// distrtypes.ModuleName,
-						// stakingtypes.ModuleName,
-						// slashingtypes.ModuleName,
-						// govtypes.ModuleName,
-						// minttypes.ModuleName,
-						// crisistypes.ModuleName,
-						// ibchost.ModuleName,
-						// icatypes.ModuleName,
-						// gammtypes.ModuleName,
-						// twaptypes.ModuleName,
-						// txfeestypes.ModuleName,
-						// genutiltypes.ModuleName,
-						// evidencetypes.ModuleName,
-						// paramstypes.ModuleName,
-						// upgradetypes.ModuleName,
-						// vestingtypes.ModuleName,
-						// ibctransfertypes.ModuleName,
-						// poolincentivestypes.ModuleName,
-						// superfluidtypes.ModuleName,
-						// tokenfactorytypes.ModuleName,
-						// valsetpreftypes.ModuleName,
-						// incentivestypes.ModuleName,
-						// epochstypes.ModuleName,
-						// lockuptypes.ModuleName,
-						// authz.ModuleName,
-						// // wasm after ibc transfer
-						// wasm.ModuleName,
-						// // ibc_hooks after auth keeper
-						// ibc_hooks.ModuleName,
+				InitGenesis: []string{
+					// capabilitytypes.ModuleName,
+					// authtypes.ModuleName,
+					// banktypes.ModuleName,
+					// distrtypes.ModuleName,
+					// stakingtypes.ModuleName,
+					// slashingtypes.ModuleName,
+					// govtypes.ModuleName,
+					// minttypes.ModuleName,
+					// crisistypes.ModuleName,
+					// ibchost.ModuleName,
+					// icatypes.ModuleName,
+					// gammtypes.ModuleName,
+					// twaptypes.ModuleName,
+					// txfeestypes.ModuleName,
+					// genutiltypes.ModuleName,
+					// evidencetypes.ModuleName,
+					// paramstypes.ModuleName,
+					// upgradetypes.ModuleName,
+					// vestingtypes.ModuleName,
+					// ibctransfertypes.ModuleName,
+					// poolincentivestypes.ModuleName,
+					// superfluidtypes.ModuleName,
+					// tokenfactorytypes.ModuleName,
+					// valsetpreftypes.ModuleName,
+					// incentivestypes.ModuleName,
+					// epochstypes.ModuleName,
+					// lockuptypes.ModuleName,
+					// authz.ModuleName,
+					// // wasm after ibc transfer
+					// wasm.ModuleName,
+					// // ibc_hooks after auth keeper
+					// ibc_hooks.ModuleName,
+				},
+				OverrideStoreKeys: []*runtimev1alpha1.StoreKeyConfig{
+					{
+						ModuleName: authtypes.ModuleName,
+						KvStoreKey: "acc",
 					},
-					OverrideStoreKeys: []*runtimev1alpha1.StoreKeyConfig{
-						{
-							ModuleName: authtypes.ModuleName,
-							KvStoreKey: "acc",
-						},
-					},
-				}),
-			},
-			{
-				Name:   lockuptypes.ModuleName,
-				Config: appconfig.WrapAny(&lockuptypes.Module{}),
-			},
+				},
+			}),
 		},
-	})
-)
+		{
+			Name:   lockuptypes.ModuleName,
+			Config: appconfig.WrapAny(&lockuptypes.Module{}),
+		},
+	},
+})
