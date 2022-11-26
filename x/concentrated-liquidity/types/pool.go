72bb3b76aadccd5c5ed1ae675dc7de342e9dc5d8
Adam Tucker <adamleetucker@outlook.com>
2022-11-25 21:11:53 -0600

    add PoolI to clextension

--- x/concentrated-liquidity/types/pool.go
+++ x/concentrated-liquidity/types/pool.go
@@ -3,29 +3,13 @@ package types
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	proto "github.com/gogo/protobuf/proto"
+
+	swaproutertypes "github.com/osmosis-labs/osmosis/v13/x/swaprouter/types"
 )
 
 type ConcentratedPoolExtension interface {
-	// gammtypes.PoolI
+	swaproutertypes.PoolI
 	proto.Message
-
-	GetAddress() sdk.AccAddress
-	String() string
-	GetId() uint64
-	// GetSwapFee returns the pool's swap fee, based on the current state.
-	// Pools may choose to make their swap fees dependent upon state
-	// (prior TWAPs, network downtime, other pool states, etc.)
-	// hence Context is provided as an argument.
-	GetSwapFee(ctx sdk.Context) sdk.Dec
-	// GetExitFee returns the pool's exit fee, based on the current state.
-	// Pools may choose to make their exit fees dependent upon state.
-	GetExitFee(ctx sdk.Context) sdk.Dec
-	// Returns whether the pool has swaps enabled at the moment
-	IsActive(ctx sdk.Context) bool
-	// GetTotalShares returns the total number of LP shares in the pool
-	GetTotalShares() sdk.Int
-
-	SpotPrice(ctx sdk.Context, baseAssetDenom string, quoteAssetDenom string) (sdk.Dec, error)
 
 	// TODO: move these to separate interfaces
 	GetToken0() string
