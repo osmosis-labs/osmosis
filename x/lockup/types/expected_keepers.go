b4811988585fcc9a60833a909f4ed36322540a7a
Ruslan Akhtariev <ruslanakhtariev@Ruslans-MacBook-Air.local>
2022-11-27 21:51:48 +0700

    bring back bank keeper methods

--- x/lockup/types/expected_keepers.go
+++ x/lockup/types/expected_keepers.go
@@ -9,7 +9,7 @@ type AccountKeeper interface {
 	GetAllAccounts(ctx sdk.Context) []authtypes.AccountI
 	GetModuleAccount(ctx sdk.Context, moduleName string) authtypes.ModuleAccountI
 	GetModuleAddress(moduleName string) sdk.AccAddress
-	GetAccount(ctx sdk.Context, addr sdk.AccAddress) authtypes.AccountI 
+	GetAccount(ctx sdk.Context, addr sdk.AccAddress) authtypes.AccountI
 	IterateAccounts(ctx sdk.Context, process func(authtypes.AccountI) (stop bool))
 	SetModuleAccount(sdk.Context, authtypes.ModuleAccountI)
 }
@@ -17,6 +17,10 @@ type AccountKeeper interface {
 // BankKeeper defines the expected interface needed to retrieve account balances.
 type BankKeeper interface {
 	GetAllBalances(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
+
+	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+
 	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
 	LockedCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
 	SpendableCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
