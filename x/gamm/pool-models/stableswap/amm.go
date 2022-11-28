c071736cb48a94a7104626aa61363f91bda14b2b
Roman <roman@osmosis.team>
2022-11-27 23:28:14 -0500

    clean up stableswap comments (#3442)
    Closes: #XXX
    
    ## What is the purpose of the change
    
    - Remove dead code
    - Elaborate on godoc
    - Add TODO to clarify solver spec

--- x/gamm/pool-models/stableswap/amm.go
+++ x/gamm/pool-models/stableswap/amm.go
@@ -72,10 +72,9 @@ func cfmmConstantMulti(xReserve, yReserve, u, v osmomath.BigDec) osmomath.BigDec
 // Solidly's CFMM is xy(x^2 + y^2) = k, and our multi-asset CFMM is xyz(x^2 + y^2 + w) = k
 // So we want to solve for a given addition of `b` units of y into the pool,
 // how many units `a` of x do we get out.
-// So we solve the following expression for `a` in two-asset pools:
-// xy(x^2 + y^2) = (x - a)(y + b)((x - a)^2 + (y + b)^2)
-// and the following expression for `a` in multi-asset pools:
-// xyz(x^2 + y^2 + w) = (x - a)(y + b)z((x - a)^2 + (y + b)^2 + w)
+// So we solve the following expression for `a`:
+// xy(x^2 + y^2 + w) = (x - a)(y + b)((x - a)^2 + (y + b)^2 + w)
+// with w set to 0 for 2 asset pools
 func solveCfmm(xReserve, yReserve osmomath.BigDec, remReserves []osmomath.BigDec, yIn osmomath.BigDec) osmomath.BigDec {
 	wSumSquares := osmomath.ZeroDec()
 	for _, assetReserve := range remReserves {
