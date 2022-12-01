47cf91b2506262ac5d16dc8f5c1708a1fa9a369c
Adam Tucker <adam@osmosis.team>
2022-12-01 13:03:41 -0600

    test[CL]: initOrUpdatePosition table driven tests (3/6) (#3567)
    * initOrUpdateTick test cases
    
    * godoc add
    
    * add a few more test cases
    
    * address Roman's comments
    
    * address Roman's comments
    
    * getPoolById table driven test
    
    * add godoc
    
    * initOrUpdatePosition table driven test
    
    * use errors and not strings
    
    * fix test
    
    * fix test
    
    * address Roman's comments
    
    * address comments
    
    * Update x/concentrated-liquidity/position_test.go
    
    Co-authored-by: Roman <roman@osmosis.team>
    
    Co-authored-by: Roman <roman@osmosis.team>

---
+++ x/concentrated-liquidity/position_test.go
@@ -0,0 +1,118 @@
+package concentrated_liquidity_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/osmosis-labs/osmosis/v13/x/concentrated-liquidity/types"
+)
+
+func (s *KeeperTestSuite) TestInitOrUpdatePosition() {
+	const validPoolId = 1
+	type param struct {
+		poolId         uint64
+		lowerTick      int64
+		upperTick      int64
+		liquidityDelta sdk.Dec
+		liquidityIn    sdk.Dec
+	}
+
+	tests := []struct {
+		name              string
+		param             param
+		positionExists    bool
+		expectedLiquidity sdk.Dec
+		expectedErr       error
+	}{
+		{
+			name: "Init position from -50 to 50 with 50000000000 liquidity",
+			param: param{
+				poolId:         validPoolId,
+				lowerTick:      -50,
+				upperTick:      50,
+				liquidityDelta: DefaultLiquidityAmt,
+			},
+			positionExists:    false,
+			expectedLiquidity: DefaultLiquidityAmt,
+		},
+		{
+			name: "Update position from -50 to 50 that already contains 50000000000 liquidity with 50000000000 more liquidity",
+			param: param{
+				poolId:         validPoolId,
+				lowerTick:      -50,
+				upperTick:      50,
+				liquidityDelta: DefaultLiquidityAmt,
+			},
+			positionExists:    true,
+			expectedLiquidity: DefaultLiquidityAmt.Add(DefaultLiquidityAmt),
+		},
+		{
+			name: "Init position for non-existing pool",
+			param: param{
+				poolId:         2,
+				lowerTick:      -50,
+				upperTick:      50,
+				liquidityDelta: DefaultLiquidityAmt,
+			},
+			positionExists: false,
+			expectedErr:    types.PoolNotFoundError{PoolId: 2},
+		},
+		{
+			name: "Init position from -50 to 50 with -50000000000 liquidity",
+			param: param{
+				poolId:         validPoolId,
+				lowerTick:      -50,
+				upperTick:      50,
+				liquidityDelta: DefaultLiquidityAmt.Neg(),
+			},
+			positionExists: false,
+			expectedErr:    types.NegativeLiquidityError{Liquidity: DefaultLiquidityAmt.Neg()},
+		},
+	}
+
+	for _, test := range tests {
+		s.Run(test.name, func() {
+			// Init suite for each test.
+			s.Setup()
+
+			// Create a CL pool with poolId 1
+			_, err := s.App.ConcentratedLiquidityKeeper.CreateNewConcentratedLiquidityPool(s.Ctx, validPoolId, ETH, USDC, DefaultCurrSqrtPrice, DefaultCurrTick)
+			s.Require().NoError(err)
+
+			// If positionExists set, initialize the specified position with defaultLiquidityAmt
+			preexistingLiquidity := sdk.ZeroDec()
+			if test.positionExists {
+				err := s.App.ConcentratedLiquidityKeeper.InitOrUpdatePosition(s.Ctx, test.param.poolId, s.TestAccs[0], test.param.lowerTick, test.param.upperTick, test.param.liquidityDelta)
+				s.Require().NoError(err)
+				preexistingLiquidity = DefaultLiquidityAmt
+			}
+
+			// Get the position info for poolId 1
+			positionInfo, err := s.App.ConcentratedLiquidityKeeper.GetPosition(s.Ctx, validPoolId, s.TestAccs[0], test.param.lowerTick, test.param.upperTick)
+			if test.positionExists {
+				// If we had a position before, ensure the position info displays proper liquidity
+				s.Require().NoError(err)
+				s.Require().Equal(preexistingLiquidity, positionInfo.Liquidity)
+			} else {
+				// If we did not have a position before, ensure getting the non-existent position returns an error
+				s.Require().Error(err)
+				s.Require().ErrorContains(err, types.PositionNotFoundError{PoolId: validPoolId, LowerTick: test.param.lowerTick, UpperTick: test.param.upperTick}.Error())
+			}
+
+			// System under test. Initialize or update the position according to the test case
+			err = s.App.ConcentratedLiquidityKeeper.InitOrUpdatePosition(s.Ctx, test.param.poolId, s.TestAccs[0], test.param.lowerTick, test.param.upperTick, test.param.liquidityDelta)
+			if test.expectedErr != nil {
+				s.Require().Error(err)
+				s.Require().ErrorContains(err, test.expectedErr.Error())
+				return
+			}
+			s.Require().NoError(err)
+
+			// Get the tick info for poolId 1
+			positionInfo, err = s.App.ConcentratedLiquidityKeeper.GetPosition(s.Ctx, validPoolId, s.TestAccs[0], test.param.lowerTick, test.param.upperTick)
+			s.Require().NoError(err)
+
+			// Check that the initialized or updated position matches our expectation
+			s.Require().Equal(test.expectedLiquidity, positionInfo.Liquidity)
+		})
+	}
+}
