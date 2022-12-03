4751054cd151204ecdbcb0c95fce82a8fe2e6892
Ruslan Akhtariev <46343690+RusAkh@users.noreply.github.com>
2022-12-03 22:40:54 +0700

    cache encoding config (#3568)
    Co-authored-by: Ruslan Akhtariev <ruslanakhtariev@Ruslans-MacBook-Air.local>

--- app/encoding.go
+++ app/encoding.go
@@ -6,6 +6,14 @@ import (
 	"github.com/cosmos/cosmos-sdk/std"
 )
 
+var (
+	encodingConfig params.EncodingConfig = MakeEncodingConfig()
+)
+
+func GetEncodingConfig() params.EncodingConfig {
+	return encodingConfig
+}
+
 // MakeEncodingConfig creates an EncodingConfig.
 func MakeEncodingConfig() params.EncodingConfig {
 	encodingConfig := params.MakeEncodingConfig()
