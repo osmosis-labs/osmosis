193e276b5f108bb2e286fc4aa1b3963ba8eff9d7
Roman <roman@osmosis.team>
2022-12-05 12:37:52 -0800

    fixes

--- app/upgrades/v14/constants.go
+++ app/upgrades/v14/constants.go
@@ -1,9 +1,10 @@
 package v14
 
 import (
-	"github.com/osmosis-labs/osmosis/v13/app/upgrades"
-
 	store "github.com/cosmos/cosmos-sdk/store/types"
+
+	"github.com/osmosis-labs/osmosis/v13/app/upgrades"
+	valsetpreftypes "github.com/osmosis-labs/osmosis/v13/x/valset-pref/types"
 )
 
 // UpgradeName defines the on-chain upgrade name for the Osmosis v14 upgrade.
@@ -12,8 +13,8 @@ const UpgradeName = "v14"
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateUpgradeHandler,
-	StoreUpgrades:        store.StoreUpgrades{
-	        Added:   []string{},
+	StoreUpgrades: store.StoreUpgrades{
+		Added:   []string{valsetpreftypes.StoreKey},
 		Deleted: []string{},
 	},
 }
