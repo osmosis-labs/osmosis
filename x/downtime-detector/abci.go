e3b4b83c22a9103b0fb78e8d6a27959ff6aacab8
Dev Ojha <dojha@berkeley.edu>
2022-12-13 16:56:44 -0600

    Apply code comment

--- x/downtime-detector/abci.go
+++ x/downtime-detector/abci.go
@@ -15,13 +15,14 @@ func (k *Keeper) BeginBlock(ctx sdk.Context) {
 		ctx.Logger().Error("Downtime-detector, could not get last block time, did initialization happen correctly. " + err.Error())
 	}
 	downtime := curTime.Sub(lastBlockTime)
-	if downtime > 30*time.Second {
-		k.saveDowntimeUpdates(ctx, downtime)
-	}
+	k.saveDowntimeUpdates(ctx, downtime)
 	k.StoreLastBlockTime(ctx, curTime)
 }
 
 func (k *Keeper) saveDowntimeUpdates(ctx sdk.Context, downtime time.Duration) {
+	if downtime < 30*time.Second {
+		return
+	}
 	types.DowntimeToDuration.Ascend(0, func(downType types.Downtime, duration time.Duration) bool {
 		// if downtime < duration of this entry, stop iterating further, don't update this entry.
 		if downtime < duration {
