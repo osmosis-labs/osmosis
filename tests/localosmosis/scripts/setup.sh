40ed53ce0002ed1160e73d27ea4e6fa41be37fba
Adam Tucker <adamleetucker@outlook.com>
2022-12-14 13:41:35 -0600

    add github issue link

--- tests/localosmosis/scripts/setup.sh
+++ tests/localosmosis/scripts/setup.sh
@@ -56,6 +56,7 @@ edit_genesis () {
     dasel put string -f $GENESIS '.app_state.mint.params.epoch_identifier' "day"
 
     # TODO: Update pool creation fee params via swaprouter module instead
+    # See https://github.com/osmosis-labs/osmosis/issues/3725
     # dasel put string -f $GENESIS '.app_state.gamm.params.pool_creation_fee.[0].denom' "uosmo"
 
     # Update txfee basedenom
