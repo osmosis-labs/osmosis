13b7a94079a74b2bb4c6a84fb28a4a9385b5e00c
Roman <roman@osmosis.team>
2022-12-16 11:39:49 -0800

    test(x/swaprouter): TestCreatePool

--- x/swaprouter/create_pool_test.go
+++ x/swaprouter/create_pool_test.go
@@ -2,14 +2,15 @@ package swaprouter_test
 
 import (
 	"fmt"
+	"reflect"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/osmosis-labs/osmosis/v13/app/apptesting"
 	"github.com/osmosis-labs/osmosis/v13/x/gamm/pool-models/balancer"
 	balancertypes "github.com/osmosis-labs/osmosis/v13/x/gamm/pool-models/balancer"
-	"github.com/osmosis-labs/osmosis/v13/x/gamm/types"
-	swaproutertypes "github.com/osmosis-labs/osmosis/v13/x/swaprouter/types"
+	gammtypes "github.com/osmosis-labs/osmosis/v13/x/gamm/types"
+	"github.com/osmosis-labs/osmosis/v13/x/swaprouter/types"
 )
 
 func (suite *KeeperTestSuite) TestPoolCreationFee() {
@@ -62,7 +63,7 @@ func (suite *KeeperTestSuite) TestPoolCreationFee() {
 		swaprouterKeeper := suite.App.SwapRouterKeeper
 
 		// set pool creation fee
-		swaprouterKeeper.SetParams(suite.Ctx, swaproutertypes.Params{
+		swaprouterKeeper.SetParams(suite.Ctx, types.Params{
 			PoolCreationFee: test.poolCreationFee,
 		})
 
@@ -84,8 +85,8 @@ func (suite *KeeperTestSuite) TestPoolCreationFee() {
 			// check to make sure new pool exists and has minted the correct number of pool shares
 			pool, err := gammKeeper.GetPoolAndPoke(suite.Ctx, poolId)
 			suite.Require().NoError(err, "test: %v", test.name)
-			suite.Require().Equal(types.InitPoolSharesSupply.String(), pool.GetTotalShares().String(),
-				fmt.Sprintf("share token should be minted as %s initially", types.InitPoolSharesSupply.String()),
+			suite.Require().Equal(gammtypes.InitPoolSharesSupply.String(), pool.GetTotalShares().String(),
+				fmt.Sprintf("share token should be minted as %s initially", gammtypes.InitPoolSharesSupply.String()),
 			)
 
 			// make sure pool creation fee is correctly sent to community pool
@@ -96,7 +97,7 @@ func (suite *KeeperTestSuite) TestPoolCreationFee() {
 			for _, asset := range test.msg.GetPoolAssets() {
 				expectedPoolTokens = expectedPoolTokens.Add(asset.Token)
 			}
-			expectedPoolShares := sdk.NewCoin(types.GetPoolShareDenom(pool.GetId()), types.InitPoolSharesSupply)
+			expectedPoolShares := sdk.NewCoin(gammtypes.GetPoolShareDenom(pool.GetId()), gammtypes.InitPoolSharesSupply)
 
 			// make sure sender's balance is updated correctly
 			senderBal := bankKeeper.GetAllBalances(suite.Ctx, sender)
@@ -109,5 +110,72 @@ func (suite *KeeperTestSuite) TestPoolCreationFee() {
 		} else {
 			suite.Require().Error(err, "test: %v", test.name)
 		}
+	}
+}
+
+// TestCreatePool tests that all possible pools are created correctly.
+func (suite *KeeperTestSuite) TestCreatePool() {
+
+	validBalancerPoolMsg := balancer.NewMsgCreateBalancerPool(suite.TestAccs[0], balancer.NewPoolParams(sdk.ZeroDec(), sdk.ZeroDec(), nil), []balancer.PoolAsset{
+		{
+			Token:  sdk.NewCoin(foo, defaultInitPoolAmount),
+			Weight: sdk.NewInt(1),
+		},
+		{
+			Token:  sdk.NewCoin(bar, defaultInitPoolAmount),
+			Weight: sdk.NewInt(1),
+		},
+	}, "")
+
+	tests := []struct {
+		name               string
+		creatorFundAmount  sdk.Coins
+		msg                types.CreatePoolMsg
+		expectedModuleType reflect.Type
+		expectError        bool
+	}{
+		{
+			name:               "first balancer pool - success",
+			creatorFundAmount:  sdk.NewCoins(sdk.NewCoin(foo, defaultInitPoolAmount.Mul(sdk.NewInt(2))), sdk.NewCoin(bar, defaultInitPoolAmount.Mul(sdk.NewInt(2)))),
+			msg:                validBalancerPoolMsg,
+			expectedModuleType: gammKeeperType,
+		},
+		{
+			name:               "second balancer pool - success",
+			creatorFundAmount:  sdk.NewCoins(sdk.NewCoin(foo, defaultInitPoolAmount.Mul(sdk.NewInt(2))), sdk.NewCoin(bar, defaultInitPoolAmount.Mul(sdk.NewInt(2)))),
+			msg:                validBalancerPoolMsg,
+			expectedModuleType: gammKeeperType,
+		},
+		// TODO: add stableswap test
+		// TODO: add concentrated-liquidity test
+		// TODO: cover errors and edge cases
+	}
+
+	for i, tc := range tests {
+		suite.Run(tc.name, func() {
+			tc := tc
+
+			swaprouterKeeper := suite.App.SwapRouterKeeper
+			ctx := suite.Ctx
+
+			poolCreationFee := swaprouterKeeper.GetParams(suite.Ctx).PoolCreationFee
+			suite.FundAcc(suite.TestAccs[0], append(tc.creatorFundAmount, poolCreationFee...))
+
+			poolId, err := swaprouterKeeper.CreatePool(ctx, tc.msg)
+
+			if tc.expectError {
+				suite.Require().Error(err)
+				return
+			}
+
+			// Validate pool.
+			suite.Require().NoError(err)
+			suite.Require().Equal(uint64(i+1), poolId)
+
+			// Validate that mapping pool id -> module type has been persisted.
+			swapModule, err := swaprouterKeeper.GetPoolModule(ctx, poolId)
+			suite.Require().NoError(err)
+			suite.Require().Equal(tc.expectedModuleType, reflect.TypeOf(swapModule))
+		})
 	}
 }
