77967b8370b9c4bdaa0db8204077f1f8f1c62a14
Adam Tucker <adamleetucker@outlook.com>
2022-12-18 17:17:02 -0600

    more speedups

--- tests/e2e/configurer/config/constants.go
+++ tests/e2e/configurer/config/constants.go
@@ -6,13 +6,13 @@ const (
 	// if not skipping upgrade, how many blocks we allow for fork to run pre upgrade state creation
 	ForkHeightPreUpgradeOffset int64 = 60
 	// estimated number of blocks it takes to submit for a proposal
-	PropSubmitBlocks float32 = 10
+	PropSubmitBlocks float32 = 3
 	// estimated number of blocks it takes to deposit for a proposal
-	PropDepositBlocks float32 = 10
+	PropDepositBlocks float32 = 3
 	// number of blocks it takes to vote for a single validator to vote for a proposal
-	PropVoteBlocks float32 = 1.2
+	PropVoteBlocks float32 = 1
 	// number of blocks used as a calculation buffer
-	PropBufferBlocks float32 = 6
+	PropBufferBlocks float32 = 3
 	// max retries for json unmarshalling
 	MaxRetries = 60
 )
