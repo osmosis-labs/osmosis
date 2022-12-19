793bb50bdf0083f74fcb4c2c47851eb85bad91a2
Dev Ojha <ValarDragon@users.noreply.github.com>
2022-12-19 09:25:17 -0600

    Make osmoutils store + module account tests not import osmosis (#3775)
    * Satisfy store_helper code
    
    * Make osmoutils tests not integrated

---
+++ osmoutils/noapptest/ctx.go
@@ -0,0 +1,32 @@
+package noapptest
+
+import (
+	"time"
+
+	"github.com/cosmos/cosmos-sdk/store"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/tendermint/tendermint/libs/log"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
+	dbm "github.com/tendermint/tm-db"
+)
+
+func CtxWithStoreKeys(keys []sdk.StoreKey, header tmproto.Header, isCheckTx bool) sdk.Context {
+	db := dbm.NewMemDB()
+	logger := log.NewNopLogger()
+	cms := store.NewCommitMultiStore(db, logger)
+	for _, key := range keys {
+		cms.MountStoreWithDB(key, sdk.StoreTypeIAVL, nil)
+	}
+	err := cms.LoadLatestVersion()
+	if err != nil {
+		panic(err)
+	}
+	return sdk.NewContext(cms, header, isCheckTx, logger)
+}
+
+func DefaultCtxWithStoreKeys(storeKeys []sdk.StoreKey) sdk.Context {
+	header := tmproto.Header{Height: 1, ChainID: "osmoutils-test-1", Time: time.Now().UTC()}
+	deliverTx := false
+	return CtxWithStoreKeys(storeKeys, header, deliverTx)
+}
