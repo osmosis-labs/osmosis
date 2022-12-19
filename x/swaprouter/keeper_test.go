21adfde8536564493a8cc3cba74f86a68d472133
Roman <roman@osmosis.team>
2022-12-19 18:37:57 -0500

    test(x/swaprouter): TestGetPoolModule (#3794)
    * test(x/swaprouter): TestGetPoolModule
    
    * adam comment

--- x/swaprouter/keeper_test.go
+++ x/swaprouter/keeper_test.go
@@ -37,7 +37,7 @@ func (suite *KeeperTestSuite) createPoolFromType(poolType types.PoolType) {
 		suite.PrepareBalancerPool()
 		return
 	case types.Stableswap:
-		// TODO
+		suite.PrepareBasicStableswapPool()
 		return
 	case types.Concentrated:
 		// TODO
