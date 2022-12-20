0e889e2c2c2607b2c2f4127d4cb2991e0d196dc7
Roman <ackhtariev@gmail.com>
2022-12-20 00:17:52 -0500

    query gen

--- x/twap/client/grpc/grpc_query.go
+++ x/twap/client/grpc/grpc_query.go
@@ -30,26 +30,6 @@ func (q Querier) Params(grpcCtx context.Context,
 	return q.Q.Params(ctx, *req)
 }
 
-func (q Querier) ArithmeticTwapToNow(grpcCtx context.Context,
-	req *queryproto.ArithmeticTwapToNowRequest,
-) (*queryproto.ArithmeticTwapToNowResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "empty request")
-	}
-	ctx := sdk.UnwrapSDKContext(grpcCtx)
-	return q.Q.ArithmeticTwapToNow(ctx, *req)
-}
-
-func (q Querier) ArithmeticTwap(grpcCtx context.Context,
-	req *queryproto.ArithmeticTwapRequest,
-) (*queryproto.ArithmeticTwapResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "empty request")
-	}
-	ctx := sdk.UnwrapSDKContext(grpcCtx)
-	return q.Q.ArithmeticTwap(ctx, *req)
-}
-
 func (q Querier) GeometricTwapToNow(grpcCtx context.Context,
 	req *queryproto.GeometricTwapToNowRequest,
 ) (*queryproto.GeometricTwapToNowResponse, error) {
@@ -68,5 +48,25 @@ func (q Querier) GeometricTwap(grpcCtx context.Context,
 	}
 	ctx := sdk.UnwrapSDKContext(grpcCtx)
 	return q.Q.GeometricTwap(ctx, *req)
+}
+
+func (q Querier) ArithmeticTwapToNow(grpcCtx context.Context,
+	req *queryproto.ArithmeticTwapToNowRequest,
+) (*queryproto.ArithmeticTwapToNowResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+	ctx := sdk.UnwrapSDKContext(grpcCtx)
+	return q.Q.ArithmeticTwapToNow(ctx, *req)
+}
+
+func (q Querier) ArithmeticTwap(grpcCtx context.Context,
+	req *queryproto.ArithmeticTwapRequest,
+) (*queryproto.ArithmeticTwapResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+	ctx := sdk.UnwrapSDKContext(grpcCtx)
+	return q.Q.ArithmeticTwap(ctx, *req)
 }
 
