682356bc19c6fdcfe8180161de965f7e920c90ea
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2022-12-22 21:48:25 +0700

    add  more check when create .env

--- scripts/create-environment.sh
+++ scripts/create-environment.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 if [ -d "$HOME/.osmosisd" ]; then
-  echo "OSMOSISD_ENVIRONMENT=mainnet" > $HOME/.osmosisd/.env
+  if [ ! -f $HOME/.osmosisd/.env ]; then
+    echo "OSMOSISD_ENVIRONMENT=mainnet" > $HOME/.osmosisd/.env
+  fi
+else
+  echo "./osmosisd not exist"
 fi
+
+cat $HOME/.osmosisd/.env
