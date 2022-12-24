fda9d67f9aff28033f5753b85675ae1c741f99db
Ruslan Akhtariev <rakhtariev@icloud.com>
2022-12-24 11:42:33 +0100

    test for ibc aliases map

---
+++ cmd/osmosisd/data/ibc_aliases_test.go
@@ -0,0 +1,20 @@
+package data
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetIBCAliasesMap(t *testing.T) {
+	ibcAliases := GetIBCAliasesMap()
+	require.NotEmpty(t, ibcAliases)
+
+	// a few denom checks
+	require.Equal(t, ibcAliases["uatom"], "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2")
+	require.Equal(t, ibcAliases["uosmo"], "uosmo")
+
+	// check that subsequent calls return the same map
+	secondIBCAliases := GetIBCAliasesMap()
+	require.Equal(t, ibcAliases, secondIBCAliases)
+}
