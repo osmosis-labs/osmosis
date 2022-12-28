9b01d66564c1520f140eaa8198a11e4a06ccf742
Roman <ackhtariev@gmail.com>
2022-12-28 13:34:19 -0500

    more skips

--- x/concentrated-liquidity/genesis_test.go
+++ x/concentrated-liquidity/genesis_test.go
@@ -22,6 +22,9 @@ var (
 // TestInitGenesis tests the InitGenesis function of the ConcentratedLiquidityKeeper.
 // It checks that the state is initialized correctly based on the provided genesis.
 func TestInitGenesis(t *testing.T) {
+
+	t.Skip("TODO: re-enable this when CL state-breakage PR is merged.")
+
 	// Set up the app and context
 	app := osmoapp.Setup(false)
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
@@ -39,6 +42,9 @@ func TestInitGenesis(t *testing.T) {
 // TestExportGenesis tests the ExportGenesis function of the ConcentratedLiquidityKeeper.
 // It checks that the correct genesis state is returned.
 func TestExportGenesis(t *testing.T) {
+
+	t.Skip("TODO: re-enable this when CL state-breakage PR is merged.")
+
 	// Set up the app and context
 	app := osmoapp.Setup(false)
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
