64ddb37978ed497ad5da21f4647c856bac1a8b6f
stackman27 <sis1001@berkeley.edu>
2022-12-28 14:59:22 -0800

    fixed simulation

--- x/valset-pref/types/keys.go
+++ x/valset-pref/types/keys.go
@@ -2,7 +2,7 @@ package types
 
 var (
 	// ModuleName defines the module name
-	ModuleName = "valset-pref"
+	ModuleName = "valsetpref"
 
 	// StoreKey defines the primary module store key
 	StoreKey = ModuleName
