028dd7c0c22a7d63e2796fc9a73d09e339c4cf03
Nicolas Lara <nicolaslara@gmail.com>
2023-01-03 10:18:55 +0100

    reverted errors to the ibc standard

--- x/ibc-hooks/types/errors.go
+++ x/ibc-hooks/types/errors.go
@@ -1,8 +1,14 @@
 package types
 
+import sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
 var (
-	ErrBadPacketMetadataMsg = "cannot unmarshal metadata: '%v'. %s"
 	ErrBadMetadataFormatMsg = "wasm metadata not properly formatted for: '%v'. %s"
 	ErrBadExecutionMsg      = "cannot execute contract: %v"
-	ErrBadResponse          = "cannot create response: %v"
+
+	ErrMsgValidation = sdkerrors.Register("wasm-hooks", 1, "error in wasmhook message validation")
+	ErrMarshaling    = sdkerrors.Register("wasm-hooks", 2, "cannot marshal the ICS20 packet")
+	ErrInvalidPacket = sdkerrors.Register("wasm-hooks", 3, "invalid packet data")
+	ErrBadResponse   = sdkerrors.Register("wasm-hooks", 4, "cannot create response")
+	ErrWasmError     = sdkerrors.Register("wasm-hooks", 5, "wasm error")
 )
