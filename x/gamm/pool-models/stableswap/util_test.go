5b24509c8fa0a3298c94c517cbc3c20e521b5c90
Dev Ojha <dojha@berkeley.edu>
2023-01-06 21:38:53 +0800

    Fix conflict

--- x/gamm/pool-models/stableswap/util_test.go
+++ x/gamm/pool-models/stableswap/util_test.go
@@ -9,13 +9,9 @@ import (
 	"github.com/osmosis-labs/osmosis/v13/x/gamm/types"
 )
 
-<<<<<<< HEAD
 func createTestPool(t *testing.T, poolLiquidity sdk.Coins, swapFee, exitFee sdk.Dec, scalingFactors []uint64) types.PoolI {
-=======
-func createTestPool(t *testing.T, poolLiquidity sdk.Coins, swapFee, exitFee sdk.Dec, scalingFactors []uint64) types.CFMMPoolI {
 	scalingFactors, _ = applyScalingFactorMultiplier(scalingFactors)
 
->>>>>>> 2ac5d356 (Gamm stableswap improvements (#3839))
 	pool, err := NewStableswapPool(1, PoolParams{
 		SwapFee: swapFee,
 		ExitFee: exitFee,
