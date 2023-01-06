07448a793aaa488959b10c1a4b9dbc9289240789
Nicolas Lara <nicolaslara@gmail.com>
2023-01-06 10:08:45 +0100

    increment error codes

--- x/ibc-hooks/types/errors.go
+++ x/ibc-hooks/types/errors.go
@@ -6,10 +6,10 @@ var (
 	ErrBadMetadataFormatMsg = "wasm metadata not properly formatted for: '%v'. %s"
 	ErrBadExecutionMsg      = "cannot execute contract: %v"
 
-	ErrMsgValidation = sdkerrors.Register("wasm-hooks", 1, "error in wasmhook message validation")
-	ErrMarshaling    = sdkerrors.Register("wasm-hooks", 2, "cannot marshal the ICS20 packet")
-	ErrInvalidPacket = sdkerrors.Register("wasm-hooks", 3, "invalid packet data")
-	ErrBadResponse   = sdkerrors.Register("wasm-hooks", 4, "cannot create response")
-	ErrWasmError     = sdkerrors.Register("wasm-hooks", 5, "wasm error")
-	ErrBadSender     = sdkerrors.Register("wasm-hooks", 6, "bad sender")
+	ErrMsgValidation = sdkerrors.Register("wasm-hooks", 2, "error in wasmhook message validation")
+	ErrMarshaling    = sdkerrors.Register("wasm-hooks", 3, "cannot marshal the ICS20 packet")
+	ErrInvalidPacket = sdkerrors.Register("wasm-hooks", 4, "invalid packet data")
+	ErrBadResponse   = sdkerrors.Register("wasm-hooks", 5, "cannot create response")
+	ErrWasmError     = sdkerrors.Register("wasm-hooks", 6, "wasm error")
+	ErrBadSender     = sdkerrors.Register("wasm-hooks", 7, "bad sender")
 )
