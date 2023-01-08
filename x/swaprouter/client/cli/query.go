c23f7496c1a79cc10df63ec6915b02ef53d69b58
Ruslan Akhtariev <46343690+RusAkh@users.noreply.github.com>
2023-01-08 09:05:55 +0700

    refactor(x/swaprouter): update swaprouter CLI with osmocli; deprecate comparable gamm CLI (#3875)
    * NewSwapExactAmountInCmd: migrate to osmocli / remove from gamm
    
    * NewSwapExactAmountOutCmd to osmocli
    
    * GetCmdNumPools query to osmocli/swaprouter
    
    * EstimateSwapExactAmountIn to osmocli  and swaprouter
    
    * GetCmdEstimateSwapExactAmountOut to osmocli/swaprouter
    
    * TestGetCmdNumPools was a method for integration test suite??? removed it
    
    * change gamm to swaprouter in scripts/docs
    
    * restore go work sum
    
    * revert gamm deprecation
    
    * go work sum
    
    * back gamm
    
    * one more swaprouter to gamm change
    
    * change go work sum

--- x/swaprouter/client/cli/query.go
+++ x/swaprouter/client/cli/query.go
@@ -1,175 +1,95 @@
 package cli
 
 import (
-	"fmt"
 	"strconv"
-	"strings"
 
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/flags"
-	"github.com/cosmos/cosmos-sdk/version"
+	"github.com/gogo/protobuf/proto"
 	"github.com/spf13/cobra"
+	flag "github.com/spf13/pflag"
 
+	"github.com/osmosis-labs/osmosis/osmoutils/osmocli"
 	"github.com/osmosis-labs/osmosis/v13/x/swaprouter/client/queryproto"
 	"github.com/osmosis-labs/osmosis/v13/x/swaprouter/types"
 )
 
 // GetQueryCmd returns the cli query commands for this module.
 func GetQueryCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:                        types.ModuleName,
-		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
-		DisableFlagParsing:         true,
-		SuggestionsMinimumDistance: 2,
-		RunE:                       client.ValidateCmd,
-	}
+	cmd := osmocli.QueryIndexCmd(types.ModuleName)
 
-	cmd.AddCommand(
-		GetCmdEstimateSwapExactAmountIn(),
-		GetCmdEstimateSwapExactAmountOut(),
-		GetCmdNumPools(),
-	)
+	osmocli.AddQueryCmd(cmd, queryproto.NewQueryClient, GetCmdNumPools)
+	osmocli.AddQueryCmd(cmd, queryproto.NewQueryClient, GetCmdEstimateSwapExactAmountIn)
+	osmocli.AddQueryCmd(cmd, queryproto.NewQueryClient, GetCmdEstimateSwapExactAmountOut)
 
 	return cmd
 }
 
 // GetCmdEstimateSwapExactAmountIn returns estimation of output coin when amount of x token input.
-func GetCmdEstimateSwapExactAmountIn() *cobra.Command {
-	cmd := &cobra.Command{
+func GetCmdEstimateSwapExactAmountIn() (*osmocli.QueryDescriptor, *queryproto.EstimateSwapExactAmountInRequest) {
+	return &osmocli.QueryDescriptor{
 		Use:   "estimate-swap-exact-amount-in <poolID> <sender> <tokenIn>",
 		Short: "Query estimate-swap-exact-amount-in",
-		Long: strings.TrimSpace(
-			fmt.Sprintf(`Query estimate-swap-exact-amount-in.
-Example:
-$ %s query swaprouter estimate-swap-exact-amount-in 1 osm11vmx8jtggpd9u7qr0t8vxclycz85u925sazglr7 stake --swap-route-pool-ids=2 --swap-route-pool-ids=3
-`,
-				version.AppName,
-			),
-		),
-		Args: cobra.ExactArgs(3),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientQueryContext(cmd)
-			if err != nil {
-				return err
-			}
-			queryClient := queryproto.NewQueryClient(clientCtx)
-
-			poolID, err := strconv.Atoi(args[0])
-			if err != nil {
-				return err
-			}
-
-			routes, err := swapAmountInRoutes(cmd.Flags())
-			if err != nil {
-				return err
-			}
-
-			res, err := queryClient.EstimateSwapExactAmountIn(cmd.Context(), &queryproto.EstimateSwapExactAmountInRequest{
-				Sender:  args[1],        // TODO: where sender is used?
-				PoolId:  uint64(poolID), // TODO: is this poolId used?
-				TokenIn: args[2],
-				Routes:  routes,
-			})
-			if err != nil {
-				return err
-			}
-
-			return clientCtx.PrintProto(res)
-		},
-	}
-
-	cmd.Flags().AddFlagSet(FlagSetQuerySwapRoutes())
-	flags.AddQueryFlagsToCmd(cmd)
-	_ = cmd.MarkFlagRequired(FlagSwapRoutePoolIds)
-	_ = cmd.MarkFlagRequired(FlagSwapRouteDenoms)
-
-	return cmd
+		Long: `Query estimate-swap-exact-amount-in.{{.ExampleHeader}}
+{{.CommandPrefix}} estimate-swap-exact-amount-in 1 osm11vmx8jtggpd9u7qr0t8vxclycz85u925sazglr7 stake --swap-route-pool-ids=2 --swap-route-pool-ids=3`,
+		ParseQuery: EstimateSwapExactAmountInParseArgs,
+		Flags:      osmocli.FlagDesc{RequiredFlags: []*flag.FlagSet{FlagSetMultihopSwapRoutes()}},
+	}, &queryproto.EstimateSwapExactAmountInRequest{}
 }
 
 // GetCmdEstimateSwapExactAmountOut returns estimation of input coin to get exact amount of x token output.
-func GetCmdEstimateSwapExactAmountOut() *cobra.Command {
-	cmd := &cobra.Command{
+func GetCmdEstimateSwapExactAmountOut() (*osmocli.QueryDescriptor, *queryproto.EstimateSwapExactAmountOutRequest) {
+	return &osmocli.QueryDescriptor{
 		Use:   "estimate-swap-exact-amount-out <poolID> <sender> <tokenOut>",
 		Short: "Query estimate-swap-exact-amount-out",
-		Long: strings.TrimSpace(
-			fmt.Sprintf(`Query estimate-swap-exact-amount-out.
-Example:
-$ %s query swaprouter estimate-swap-exact-amount-out 1 osm11vmx8jtggpd9u7qr0t8vxclycz85u925sazglr7 stake --swap-route-pool-ids=2 --swap-route-pool-ids=3
-`,
-				version.AppName,
-			),
-		),
-		Args: cobra.ExactArgs(3),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientQueryContext(cmd)
-			if err != nil {
-				return err
-			}
-			queryClient := queryproto.NewQueryClient(clientCtx)
-
-			poolID, err := strconv.Atoi(args[0])
-			if err != nil {
-				return err
-			}
-
-			routes, err := swapAmountOutRoutes(cmd.Flags())
-			if err != nil {
-				return err
-			}
-
-			res, err := queryClient.EstimateSwapExactAmountOut(cmd.Context(), &queryproto.EstimateSwapExactAmountOutRequest{
-				Sender:   args[1],        // TODO: where sender is used?
-				PoolId:   uint64(poolID), // TODO: is this poolId used?
-				Routes:   routes,
-				TokenOut: args[2],
-			})
-			if err != nil {
-				return err
-			}
-
-			return clientCtx.PrintProto(res)
-		},
-	}
-
-	cmd.Flags().AddFlagSet(FlagSetQuerySwapRoutes())
-	flags.AddQueryFlagsToCmd(cmd)
-	_ = cmd.MarkFlagRequired(FlagSwapRoutePoolIds)
-	_ = cmd.MarkFlagRequired(FlagSwapRouteDenoms)
-
-	return cmd
+		Long: `Query estimate-swap-exact-amount-out.{{.ExampleHeader}}
+{{.CommandPrefix}} estimate-swap-exact-amount-out 1 osm11vmx8jtggpd9u7qr0t8vxclycz85u925sazglr7 stake --swap-route-pool-ids=2 --swap-route-pool-ids=3`,
+		ParseQuery: EstimateSwapExactAmountOutParseArgs,
+		Flags:      osmocli.FlagDesc{RequiredFlags: []*flag.FlagSet{FlagSetMultihopSwapRoutes()}},
+	}, &queryproto.EstimateSwapExactAmountOutRequest{}
 }
 
 // GetCmdNumPools return number of pools available.
-func GetCmdNumPools() *cobra.Command {
-	cmd := &cobra.Command{
+func GetCmdNumPools() (*osmocli.QueryDescriptor, *queryproto.NumPoolsRequest) {
+	return &osmocli.QueryDescriptor{
 		Use:   "num-pools",
 		Short: "Query number of pools",
-		Long: strings.TrimSpace(
-			fmt.Sprintf(`Query number of pools.
-Example:
-$ %s query swaprouter num-pools
-`,
-				version.AppName,
-			),
-		),
-		Args: cobra.ExactArgs(0),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientQueryContext(cmd)
-			if err != nil {
-				return err
-			}
-			queryClient := queryproto.NewQueryClient(clientCtx)
+		Long:  "{{.Short}}",
+	}, &queryproto.NumPoolsRequest{}
+}
 
-			res, err := queryClient.NumPools(cmd.Context(), &queryproto.NumPoolsRequest{})
-			if err != nil {
-				return err
-			}
-
-			return clientCtx.PrintProto(res)
-		},
+func EstimateSwapExactAmountInParseArgs(args []string, fs *flag.FlagSet) (proto.Message, error) {
+	poolID, err := strconv.Atoi(args[0])
+	if err != nil {
+		return nil, err
 	}
 
-	flags.AddQueryFlagsToCmd(cmd)
+	routes, err := swapAmountInRoutes(fs)
+	if err != nil {
+		return nil, err
+	}
 
-	return cmd
+	return &queryproto.EstimateSwapExactAmountInRequest{
+		Sender:  args[1],        // TODO: where sender is used?
+		PoolId:  uint64(poolID), // TODO: is this poolId used?
+		TokenIn: args[2],
+		Routes:  routes,
+	}, nil
+}
+
+func EstimateSwapExactAmountOutParseArgs(args []string, fs *flag.FlagSet) (proto.Message, error) {
+	poolID, err := strconv.Atoi(args[0])
+	if err != nil {
+		return nil, err
+	}
+
+	routes, err := swapAmountOutRoutes(fs)
+	if err != nil {
+		return nil, err
+	}
+
+	return &queryproto.EstimateSwapExactAmountOutRequest{
+		Sender:   args[1],        // TODO: where sender is used?
+		PoolId:   uint64(poolID), // TODO: is this poolId used?
+		Routes:   routes,
+		TokenOut: args[2],
+	}, nil
 }
