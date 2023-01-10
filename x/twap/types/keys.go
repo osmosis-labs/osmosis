2d1b464142ecae5660f145c8ae7014cb526f38ee
Roman <roman@osmosis.team>
2023-01-10 11:04:42 -0500

    fix(twap/e2e): set geometric accum to zero; reproduce testnet bug (#3972)
    * fix(twap/e2e): set geometric accum to zero; reproduce testnet bug
    
    * test TestParseTwapFromBz
    
    * clean up
    
    (cherry picked from commit a693a3b0390dedc9ac26800f99d43adec203d068)

--- x/twap/types/keys.go
+++ x/twap/types/keys.go
@@ -87,5 +87,8 @@ func ParseTwapFromBz(bz []byte) (twap TwapRecord, err error) {
 		return TwapRecord{}, errors.New("twap not found")
 	}
 	err = proto.Unmarshal(bz, &twap)
+	if twap.GeometricTwapAccumulator.IsNil() {
+		twap.GeometricTwapAccumulator = sdk.ZeroDec()
+	}
 	return twap, err
 }
