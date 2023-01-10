2448a993d6a075e10f7e8fda310f88e52cab9075
Adam Tucker <adamleetucker@outlook.com>
2023-01-10 12:11:40 -0600

    fix import path

--- x/concentrated-liquidity/internal/math/tick.go
+++ x/concentrated-liquidity/internal/math/tick.go
@@ -5,7 +5,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	"github.com/osmosis-labs/osmosis/v13/x/concentrated-liquidity/types"
+	"github.com/osmosis-labs/osmosis/v14/x/concentrated-liquidity/types"
 )
 
 var sdkNineDec = sdk.NewDec(9)
