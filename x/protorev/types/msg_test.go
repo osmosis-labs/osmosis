ec2be89818620a58e48c7cf5ff092545fc14bfdd
davidterpay <david.terpay@gmail.com>
2023-01-11 18:11:13 -0600

    stateless validation update for generalizable routes

--- x/protorev/types/msg_test.go
+++ x/protorev/types/msg_test.go
@@ -21,11 +21,6 @@ func TestMsgsTestSuite(t *testing.T) {
 func (suite *MsgsTestSuite) TestMsgSetHotRoutes() {
 	validArbRoutes := types.CreateSeacherRoutes(3, types.OsmosisDenomination, "ethereum", types.AtomDenomination, types.AtomDenomination)
 
-	notThreePoolArbRoutes := types.CreateSeacherRoutes(3, types.OsmosisDenomination, "ethereum", types.AtomDenomination, types.AtomDenomination)
-	extraTrade := types.NewTrade(100000, "a", "b")
-	notThreePoolArbRoutes.ArbRoutes = append(notThreePoolArbRoutes.ArbRoutes, &types.Route{[]*types.Trade{&extraTrade}})
-
-	invalidArbDenoms := types.CreateSeacherRoutes(3, types.OsmosisDenomination, "ethereum", "juno", "juno")
 	mismatchedDenoms := types.CreateSeacherRoutes(3, types.OsmosisDenomination, "ethereum", types.AtomDenomination, types.OsmosisDenomination)
 	cases := []struct {
 		description string
@@ -55,18 +50,6 @@ func (suite *MsgsTestSuite) TestMsgSetHotRoutes() {
 			"Invalid message (with duplicate arb routes)",
 			createAccount().String(),
 			[]*types.TokenPairArbRoutes{&validArbRoutes, &validArbRoutes},
-			false,
-		},
-		{
-			"Invalid message (with invalid arb routes)",
-			createAccount().String(),
-			[]*types.TokenPairArbRoutes{&notThreePoolArbRoutes},
-			false,
-		},
-		{
-			"Invalid message (with invalid arb denoms)",
-			createAccount().String(),
-			[]*types.TokenPairArbRoutes{&invalidArbDenoms},
 			false,
 		},
 		{
