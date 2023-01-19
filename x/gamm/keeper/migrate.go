5b27f755a559bdf1c5d4b8127c9a02a11b32a537
Adam Tucker <adamleetucker@outlook.com>
2023-01-19 14:31:42 -0600

    add gov handler

---
+++ x/gamm/keeper/migrate.go
@@ -0,0 +1,121 @@
+package keeper
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/osmosis-labs/osmosis/osmoutils"
+	"github.com/osmosis-labs/osmosis/v14/x/gamm/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (k Keeper) GetMigrationInfo(ctx sdk.Context) types.MigrationRecords {
+	store := ctx.KVStore(k.storeKey)
+	migrationInfo := types.MigrationRecords{}
+	osmoutils.MustGet(store, types.KeyMigrationInfo, &migrationInfo)
+	return migrationInfo
+}
+
+func (k Keeper) SetMigrationInfo(ctx sdk.Context, migrationInfo types.MigrationRecords) {
+	store := ctx.KVStore(k.storeKey)
+	osmoutils.MustSet(store, types.KeyMigrationInfo, &migrationInfo)
+}
+
+// validateRecords validates a list of records to ensure that:
+// 1) there are no duplicates,
+// 2) the records are in sorted order.
+// 3) the pool IDs are valid
+func (k Keeper) validateRecords(ctx sdk.Context, records ...types.BalancerToConcentratedPoolLink) error {
+	lastBalancerPoolID := uint64(0)
+	balancerIdFlags := make(map[uint64]bool)
+
+	for _, record := range records {
+		if balancerIdFlags[record.BalancerPoolId] {
+			return fmt.Errorf(
+				"Balancer pool ID #%d has duplications.",
+				record.BalancerPoolId,
+			)
+		}
+
+		// Ensure records are sorted
+		if record.BalancerPoolId < lastBalancerPoolID {
+			return fmt.Errorf(
+				"Balancer pool ID #%d came after Gauge ID #%d.",
+				record.BalancerPoolId, lastBalancerPoolID,
+			)
+		}
+
+		// Ensure the first pool exists and that it is of type balancer
+		poolType, err := k.GetPoolType(ctx, record.BalancerPoolId)
+		if err != nil {
+			return err
+		}
+		if poolType.String() != "Balancer" {
+			return fmt.Errorf("Balancer pool ID #%d is not of type balancer", record.BalancerPoolId)
+		}
+
+		// Ensure the concentrated pool exists
+		// TODO: Get GetPoolType to work for cl pools from gamm
+		_, err = k.poolManager.GetPoolModule(ctx, record.ClPoolId)
+		if err != nil {
+			return err
+		}
+
+		lastBalancerPoolID = record.BalancerPoolId
+
+		balancerIdFlags[record.BalancerPoolId] = true
+	}
+	return nil
+}
+
+// This is checked for no err when a proposal is made, and executed when a proposal passes.
+func (k Keeper) ReplaceMigrationRecords(ctx sdk.Context, records ...types.BalancerToConcentratedPoolLink) error {
+	migrationInfo := k.GetMigrationInfo(ctx)
+
+	err := k.validateRecords(ctx, records...)
+	if err != nil {
+		return err
+	}
+
+	migrationInfo.BalancerToConcentratedPoolLinks = records
+
+	k.SetMigrationInfo(ctx, migrationInfo)
+	return nil
+}
+
+// UpdateDistrRecords is checked for no err when a proposal is made, and executed when a proposal passes.
+func (k Keeper) UpdateMigrationRecords(ctx sdk.Context, records ...types.BalancerToConcentratedPoolLink) error {
+	recordsMap := make(map[uint64]types.BalancerToConcentratedPoolLink)
+
+	for _, existingRecord := range k.GetMigrationInfo(ctx).BalancerToConcentratedPoolLinks {
+		recordsMap[existingRecord.BalancerPoolId] = existingRecord
+	}
+
+	err := k.validateRecords(ctx, records...)
+	if err != nil {
+		return err
+	}
+
+	for _, record := range records {
+		recordsMap[record.BalancerPoolId] = record
+	}
+
+	newRecords := []types.BalancerToConcentratedPoolLink{}
+
+	// if the clPoolId is 0, we remove the entire record
+	for _, val := range recordsMap {
+		if val.ClPoolId != 0 {
+			newRecords = append(newRecords, val)
+		}
+	}
+
+	sort.SliceStable(newRecords, func(i, j int) bool {
+		return newRecords[i].BalancerPoolId < newRecords[j].BalancerPoolId
+	})
+
+	k.SetMigrationInfo(ctx, types.MigrationRecords{
+		BalancerToConcentratedPoolLinks: newRecords,
+	})
+	return nil
+}
---
+++ x/gamm/keeper/migrate_test.go
@@ -0,0 +1,304 @@
+package keeper_test
+
+import (
+	"github.com/osmosis-labs/osmosis/v14/x/gamm/types"
+)
+
+func (suite *KeeperTestSuite) TestReplaceDistrRecords() {
+	tests := []struct {
+		name                    string
+		testingMigrationRecords []types.BalancerToConcentratedPoolLink
+		isPoolPrepared          bool
+		expectErr               bool
+	}{
+		{
+			name: "Non existent pool.",
+			testingMigrationRecords: []types.BalancerToConcentratedPoolLink{{
+				BalancerPoolId: 1,
+				ClPoolId:       3,
+			}},
+			isPoolPrepared: false,
+			expectErr:      true,
+		},
+		{
+			name: "Adding two of the same balancer pool id at once should error",
+			testingMigrationRecords: []types.BalancerToConcentratedPoolLink{
+				{
+					BalancerPoolId: 1,
+					ClPoolId:       3,
+				},
+				{
+					BalancerPoolId: 1,
+					ClPoolId:       3,
+				},
+			},
+			isPoolPrepared: true,
+			expectErr:      true,
+		},
+		{
+			name: "Adding unsorted balancer pools should error",
+			testingMigrationRecords: []types.BalancerToConcentratedPoolLink{
+				{
+					BalancerPoolId: 2,
+					ClPoolId:       4,
+				},
+				{
+					BalancerPoolId: 1,
+					ClPoolId:       3,
+				},
+			},
+			isPoolPrepared: true,
+			expectErr:      true,
+		},
+		{
+			name: "Normal case with two records",
+			testingMigrationRecords: []types.BalancerToConcentratedPoolLink{
+				{
+					BalancerPoolId: 1,
+					ClPoolId:       3,
+				},
+				{
+					BalancerPoolId: 2,
+					ClPoolId:       4,
+				},
+			},
+			isPoolPrepared: true,
+			expectErr:      false,
+		},
+		{
+			name: "Try to set one of the BalancerPoolIds to a cl pool Id",
+			testingMigrationRecords: []types.BalancerToConcentratedPoolLink{
+				{
+					BalancerPoolId: 2,
+					ClPoolId:       4,
+				},
+				{
+					BalancerPoolId: 3,
+					ClPoolId:       1,
+				},
+			},
+			isPoolPrepared: true,
+			expectErr:      true,
+		},
+	}
+
+	for _, test := range tests {
+		suite.Run(test.name, func() {
+			suite.SetupTest()
+			keeper := suite.App.GAMMKeeper
+
+			if test.isPoolPrepared {
+				// Our testing environment is as follows:
+				// Balancer pool IDs: 1, 2
+				// Concentrated pool IDs: 3, 4
+				suite.PrepareBalancerPool()
+				suite.PrepareBalancerPool()
+				suite.PrepareConcentratedPool()
+				suite.PrepareConcentratedPool()
+			}
+
+			err := keeper.ReplaceMigrationRecords(suite.Ctx, test.testingMigrationRecords...)
+			if test.expectErr {
+				suite.Require().Error(err)
+			} else {
+				suite.Require().NoError(err)
+
+				migrationInfo := keeper.GetMigrationInfo(suite.Ctx)
+				suite.Require().Equal(len(test.testingMigrationRecords), len(migrationInfo.BalancerToConcentratedPoolLinks))
+				for i, record := range test.testingMigrationRecords {
+					suite.Require().Equal(record.BalancerPoolId, migrationInfo.BalancerToConcentratedPoolLinks[i].BalancerPoolId)
+					suite.Require().Equal(record.ClPoolId, migrationInfo.BalancerToConcentratedPoolLinks[i].ClPoolId)
+				}
+			}
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) TestUpdateDistrRecords() {
+	tests := []struct {
+		name                     string
+		testingMigrationRecords  []types.BalancerToConcentratedPoolLink
+		expectedResultingRecords []types.BalancerToConcentratedPoolLink
+		isPoolPrepared           bool
+		isPrexistingRecordsSet   bool
+		expectErr                bool
+	}{
+		{
+			name: "Non existent pool.",
+			testingMigrationRecords: []types.BalancerToConcentratedPoolLink{{
+				BalancerPoolId: 1,
+				ClPoolId:       6,
+			}},
+			isPoolPrepared:         false,
+			isPrexistingRecordsSet: false,
+			expectErr:              true,
+		},
+		{
+			name: "Adding two of the same balancer pool ids at once should error",
+			testingMigrationRecords: []types.BalancerToConcentratedPoolLink{
+				{
+					BalancerPoolId: 1,
+					ClPoolId:       6,
+				},
+				{
+					BalancerPoolId: 1,
+					ClPoolId:       6,
+				},
+			},
+			isPoolPrepared:         true,
+			isPrexistingRecordsSet: true,
+			expectErr:              true,
+		},
+		{
+			name: "Adding unsorted balancer pools should error",
+			testingMigrationRecords: []types.BalancerToConcentratedPoolLink{
+				{
+					BalancerPoolId: 2,
+					ClPoolId:       7,
+				},
+				{
+					BalancerPoolId: 1,
+					ClPoolId:       6,
+				},
+			},
+			isPoolPrepared:         true,
+			isPrexistingRecordsSet: true,
+			expectErr:              true,
+		},
+		{
+			name: "Normal case with two records",
+			testingMigrationRecords: []types.BalancerToConcentratedPoolLink{
+				{
+					BalancerPoolId: 1,
+					ClPoolId:       6,
+				},
+				{
+					BalancerPoolId: 2,
+					ClPoolId:       8,
+				},
+			},
+			expectedResultingRecords: []types.BalancerToConcentratedPoolLink{
+				{
+					BalancerPoolId: 1,
+					ClPoolId:       6,
+				},
+				{
+					BalancerPoolId: 2,
+					ClPoolId:       8,
+				},
+				{
+					BalancerPoolId: 3,
+					ClPoolId:       7,
+				},
+			},
+			isPoolPrepared:         true,
+			isPrexistingRecordsSet: true,
+			expectErr:              false,
+		},
+		{
+			name: "Modify existing record, delete existing record, leave a record alone, add new record",
+			testingMigrationRecords: []types.BalancerToConcentratedPoolLink{
+				{
+					BalancerPoolId: 1,
+					ClPoolId:       6,
+				},
+				{
+					BalancerPoolId: 2,
+					ClPoolId:       0,
+				},
+				{
+					BalancerPoolId: 4,
+					ClPoolId:       8,
+				},
+			},
+			expectedResultingRecords: []types.BalancerToConcentratedPoolLink{
+				{
+					BalancerPoolId: 1,
+					ClPoolId:       6,
+				},
+				{
+					BalancerPoolId: 3,
+					ClPoolId:       7,
+				},
+				{
+					BalancerPoolId: 4,
+					ClPoolId:       8,
+				},
+			},
+			isPoolPrepared:         true,
+			isPrexistingRecordsSet: true,
+			expectErr:              false,
+		},
+		{
+			name: "Try to set one of the BalancerPoolIds to a cl pool Id",
+			testingMigrationRecords: []types.BalancerToConcentratedPoolLink{
+				{
+					BalancerPoolId: 2,
+					ClPoolId:       4,
+				},
+				{
+					BalancerPoolId: 5,
+					ClPoolId:       6,
+				},
+			},
+			isPoolPrepared:         true,
+			isPrexistingRecordsSet: true,
+			expectErr:              true,
+		},
+	}
+
+	for _, test := range tests {
+		suite.Run(test.name, func() {
+			suite.SetupTest()
+			keeper := suite.App.GAMMKeeper
+
+			if test.isPoolPrepared {
+				// Our testing environment is as follows:
+				// Balancer pool IDs: 1, 2, 3, 4
+				// Concentrated pool IDs: 5, 6, 7, 8
+				suite.PrepareBalancerPool()
+				suite.PrepareBalancerPool()
+				suite.PrepareBalancerPool()
+				suite.PrepareBalancerPool()
+				suite.PrepareConcentratedPool()
+				suite.PrepareConcentratedPool()
+				suite.PrepareConcentratedPool()
+				suite.PrepareConcentratedPool()
+			}
+
+			if test.isPrexistingRecordsSet {
+				// Set up existing records so we can update them
+				existingRecords := []types.BalancerToConcentratedPoolLink{
+					{
+						BalancerPoolId: 1,
+						ClPoolId:       5,
+					},
+					{
+						BalancerPoolId: 2,
+						ClPoolId:       6,
+					},
+					{
+						BalancerPoolId: 3,
+						ClPoolId:       7,
+					},
+				}
+				err := keeper.ReplaceMigrationRecords(suite.Ctx, existingRecords...)
+				suite.Require().NoError(err)
+			}
+
+			err := keeper.UpdateMigrationRecords(suite.Ctx, test.testingMigrationRecords...)
+			if test.expectErr {
+				suite.Require().Error(err)
+			} else {
+				suite.Require().NoError(err)
+
+				migrationInfo := keeper.GetMigrationInfo(suite.Ctx)
+				suite.Require().Equal(len(test.expectedResultingRecords), len(migrationInfo.BalancerToConcentratedPoolLinks))
+				for i, record := range test.expectedResultingRecords {
+					suite.Require().Equal(record.BalancerPoolId, migrationInfo.BalancerToConcentratedPoolLinks[i].BalancerPoolId)
+					suite.Require().Equal(record.ClPoolId, migrationInfo.BalancerToConcentratedPoolLinks[i].ClPoolId)
+				}
+			}
+		})
+	}
+}
