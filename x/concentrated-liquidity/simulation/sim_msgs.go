19e3e4beaec63d05cde5f56604eb1e60e45aa41e
stackman27 <sis1001@berkeley.edu>
2023-01-22 00:40:57 -0800

    fixed linter

--- x/concentrated-liquidity/simulation/sim_msgs.go
+++ x/concentrated-liquidity/simulation/sim_msgs.go
@@ -1,26 +1,17 @@
 package simulation
 
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
+// func RandomMsgCreateConcentratedPool(k clkeeper.Keeper, sim *osmosimtypes.SimCtx, ctx sdk.Context) (*clmodeltypes.MsgCreateConcentratedPool, error) {
+// 	return nil, nil
+// }
 
-	osmosimtypes "github.com/osmosis-labs/osmosis/v14/simulation/simtypes"
-	clkeeper "github.com/osmosis-labs/osmosis/v14/x/concentrated-liquidity"
-	clmodeltypes "github.com/osmosis-labs/osmosis/v14/x/concentrated-liquidity/model"
-	cltypes "github.com/osmosis-labs/osmosis/v14/x/concentrated-liquidity/types"
-)
+// func RandMsgCreatePosition(k clkeeper.Keeper, sim *osmosimtypes.SimCtx, ctx sdk.Context) (*cltypes.MsgCreatePosition, error) {
+// 	return nil, nil
+// }
 
-func RandomMsgCreateConcentratedPool(k clkeeper.Keeper, sim *osmosimtypes.SimCtx, ctx sdk.Context) (*clmodeltypes.MsgCreateConcentratedPool, error) {
-	return nil, nil
-}
+// func RandMsgWithdrawPosition(k clkeeper.Keeper, sim *osmosimtypes.SimCtx, ctx sdk.Context) (*cltypes.MsgWithdrawPosition, error) {
+// 	return nil, nil
+// }
 
-func RandMsgCreatePosition(k clkeeper.Keeper, sim *osmosimtypes.SimCtx, ctx sdk.Context) (*cltypes.MsgCreatePosition, error) {
-	return nil, nil
-}
-
-func RandMsgWithdrawPosition(k clkeeper.Keeper, sim *osmosimtypes.SimCtx, ctx sdk.Context) (*cltypes.MsgWithdrawPosition, error) {
-	return nil, nil
-}
-
-func RandMsgCollectFees(k clkeeper.Keeper, sim *osmosimtypes.SimCtx, ctx sdk.Context) (*cltypes.MsgCollectFees, error) {
-	return nil, nil
-}
+// func RandMsgCollectFees(k clkeeper.Keeper, sim *osmosimtypes.SimCtx, ctx sdk.Context) (*cltypes.MsgCollectFees, error) {
+// 	return nil, nil
+// }
