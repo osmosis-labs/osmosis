d03955a2c0b5019d1f92a60b07d2f76b12557d83
vuong <nguyenvuong1122000@gmail.com>
2023-01-30 00:38:15 +0700

    override QueryFnName

--- x/gamm/client/cli/query.go
+++ x/gamm/client/cli/query.go
@@ -203,8 +203,9 @@ func GetCmdEstimateSwapExactAmountIn() (*osmocli.QueryDescriptor, *types.QuerySw
 		Short: "Query estimate-swap-exact-amount-in",
 		Long: `Query estimate-swap-exact-amount-in.{{.ExampleHeader}}
 {{.CommandPrefix}} estimate-swap-exact-amount-in 1 osm11vmx8jtggpd9u7qr0t8vxclycz85u925sazglr7 stake --swap-route-pool-ids=2 --swap-route-pool-ids=3`,
-		ParseQuery: EstimateSwapExactAmountInParseArgs,
-		Flags:      osmocli.FlagDesc{RequiredFlags: []*flag.FlagSet{FlagSetMultihopSwapRoutes()}},
+		ParseQuery:  EstimateSwapExactAmountInParseArgs,
+		Flags:       osmocli.FlagDesc{RequiredFlags: []*flag.FlagSet{FlagSetMultihopSwapRoutes()}},
+		QueryFnName: "EstimateSwapExactAmountIn",
 	}, &types.QuerySwapExactAmountInRequest{}
 }
 
@@ -216,8 +217,9 @@ func GetCmdEstimateSwapExactAmountOut() (*osmocli.QueryDescriptor, *types.QueryS
 		Short: "Query estimate-swap-exact-amount-out",
 		Long: `Query estimate-swap-exact-amount-out.{{.ExampleHeader}}
 {{.CommandPrefix}} estimate-swap-exact-amount-out 1 osm11vmx8jtggpd9u7qr0t8vxclycz85u925sazglr7 stake --swap-route-pool-ids=2 --swap-route-pool-ids=3`,
-		ParseQuery: EstimateSwapExactAmountOutParseArgs,
-		Flags:      osmocli.FlagDesc{RequiredFlags: []*flag.FlagSet{FlagSetMultihopSwapRoutes()}},
+		ParseQuery:  EstimateSwapExactAmountOutParseArgs,
+		Flags:       osmocli.FlagDesc{RequiredFlags: []*flag.FlagSet{FlagSetMultihopSwapRoutes()}},
+		QueryFnName: "EstimateSwapExactAmountIn",
 	}, &types.QuerySwapExactAmountOutRequest{}
 }
 
