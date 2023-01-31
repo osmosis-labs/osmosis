20d2e35a4b93236da37f766cffe5c919328352d0
Nicolas Lara <nicolaslara@gmail.com>
2023-01-31 13:42:18 +0100

    simple config for timeout callbacks

--- x/ibc-hooks/types/keys.go
+++ x/ibc-hooks/types/keys.go
@@ -5,4 +5,6 @@ const (
 	StoreKey       = "hooks-for-ibc" // not using the module name because of collisions with key "ibc"
 	IBCCallbackKey = "ibc_callback"
 	SenderPrefix   = "ibc-wasm-hook-intermediary"
+
+	AllowContractsToFailTimeout = false
 )
