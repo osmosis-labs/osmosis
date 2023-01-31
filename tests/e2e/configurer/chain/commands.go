a51508e49f4cc1d8d7c78da63df81f210de98419
Pham Anh Minh <93236465+phamminh0811@users.noreply.github.com>
2023-01-31 20:30:14 +0700

    Update tests/e2e/configurer/chain/commands.go
    delete redundant
    
    Co-authored-by: Roman <roman@osmosis.team>

--- tests/e2e/configurer/chain/commands.go
+++ tests/e2e/configurer/chain/commands.go
@@ -34,7 +34,6 @@ func (n *NodeConfig) CreateStableSwapPool(poolFile, from string) uint64 {
 	n.LogActionF("creating stableswap pool from file %s", poolFile)
 	cmd := []string{"osmosisd", "tx", "gamm", "create-pool", fmt.Sprintf("--pool-file=/osmosis/%s", poolFile), "--pool-type=stableswap", fmt.Sprintf("--from=%s", from)}
 	_, _, err := n.containerManager.ExecTxCmd(n.t, n.chainId, n.Name, cmd)
-	println(err)
 	require.NoError(n.t, err)
 
 	poolID := n.QueryNumPools()
