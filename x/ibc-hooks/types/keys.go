45d33101aaeb378dc81dde975eb83c7e30b96ec5
Nicolas Lara <nicolaslara@gmail.com>
2023-02-01 10:42:21 +0100

    completely remove the ability to fail on timeouts

--- x/ibc-hooks/types/keys.go
+++ x/ibc-hooks/types/keys.go
@@ -5,6 +5,4 @@ const (
 	StoreKey       = "hooks-for-ibc" // not using the module name because of collisions with key "ibc"
 	IBCCallbackKey = "ibc_callback"
 	SenderPrefix   = "ibc-wasm-hook-intermediary"
-
-	AllowContractsToFailTimeout = false
 )
