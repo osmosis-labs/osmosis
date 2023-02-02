338eb6377c52e24295522952311e3cdda89d98fe
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-02-02 12:44:59 +0700

    wip on e2e

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -23,10 +23,9 @@ import (
 	appparams "github.com/osmosis-labs/osmosis/v14/app/params"
 	"github.com/osmosis-labs/osmosis/v14/tests/e2e/configurer/config"
 	"github.com/osmosis-labs/osmosis/v14/tests/e2e/initialization"
-	cl "github.com/osmosis-labs/osmosis/v14/x/concentrated-liquidity"
 )
 
-func (s *IntegrationTestSuite) TestConcentratedLiquidity() {
+func (s *IntegrationTestSuite) TestAAAConcentratedLiquidity() {
 	chainA := s.configurer.GetChainConfig(0)
 	node, err := chainA.GetDefaultNode()
 	s.Require().NoError(err)
@@ -36,6 +35,7 @@ func (s *IntegrationTestSuite) TestConcentratedLiquidity() {
 		denom1                    string = "uosmo"
 		tickSpacing               uint64 = 1
 		precisionFactorAtPriceOne int64  = -1
+		frozenUntil               int64  = time.Unix(86400, 0).Unix()
 		swapFee                          = "0.01"
 	)
 	poolID := node.CreateConcentratedPool(initialization.ValidatorWalletName, denom0, denom1, tickSpacing, precisionFactorAtPriceOne, swapFee)
@@ -51,23 +51,69 @@ func (s *IntegrationTestSuite) TestConcentratedLiquidity() {
 	s.Require().Equal(concentratedPool.GetPrecisionFactorAtPriceOne(), sdk.NewInt(precisionFactorAtPriceOne))
 	s.Require().Equal(concentratedPool.GetSwapFee(sdk.Context{}), sdk.MustNewDecFromStr(swapFee))
 
-	minTick, maxTick := cl.GetMinAndMaxTicksFromExponentAtPriceOne(sdk.NewInt(precisionFactorAtPriceOne))
+	// minTick, maxTick := cl.GetMinAndMaxTicksFromExponentAtPriceOne(sdk.NewInt(precisionFactorAtPriceOne))
+	node.CreateConcentratedPosition(initialization.ValidatorWalletName, "200", "400", fmt.Sprintf("1000%s", denom0), fmt.Sprintf("1000%s", denom1), 0, 0, frozenUntil, poolID)
+	positions := node.QueryConcentratedPositions(node.Node.PublicAddress)
+	fmt.Println(positions)
+	// // get 3 addresses to create positions
+	// address1 := node.CreateWalletWithAssets("addr1")
+	// address2 := node.CreateWalletWithAssets("addr2")
+	// address3 := node.CreateWalletWithAssets("addr3")
 
-	// get 3 addresses to create positions 
-	address1 := chainA.NodeConfigs[0].PublicAddress
-	address2 := chainA.NodeConfigs[1].PublicAddress
-	address3 := chainA.NodeConfigs[2].PublicAddress
+	// // Create 2 positions for node1: overlap together, overlap with 2 node3 positions)
+	// node.CreateConcentratedPosition(address1, "[-1200]", "400", fmt.Sprintf("1000%s", denom0), fmt.Sprintf("1000%s", denom1), 0, 0, frozenUntil, poolID)
+	// node.CreateConcentratedPosition(address1, "[-400]", "400", fmt.Sprintf("1000%s", denom0), fmt.Sprintf("1000%s", denom1), 0, 0, frozenUntil, poolID)
 
-	// Create 2 positions for node1: overlap together, overlap with 2 node3 positions)
-	node.CreateConcentratedPosition(address1, "[-1200]", "400", fmt.Sprintf("1000%s", denom0), fmt.Sprintf("1000%s", denom1), 0, 0, poolID)
-	node.CreateConcentratedPosition(address1, "[-400]", "400", fmt.Sprintf("1000%s", denom0), fmt.Sprintf("1000%s", denom1), 0, 0, poolID)
+	// // Create 1 position for node2: does not overlap with anything, ends at maximum
+	// node.CreateConcentratedPosition(address2, "2200", fmt.Sprintf("%d", maxTick), fmt.Sprintf("1000%s", denom0), fmt.Sprintf("1000%s", denom1), 0, 0, frozenUntil, poolID)
 
-	// Create 1 position for node2: does not overlap with anything, ends at maximum
-	node.CreateConcentratedPosition(address2, "2200", fmt.Sprintf("%d", maxTick), fmt.Sprintf("1000%s", denom0), fmt.Sprintf("1000%s", denom1), 0, 0, poolID)
+	// // Create 2 positions for node3: overlap together, overlap with 2 node1 positions, one position starts from minimum
+	// node.CreateConcentratedPosition(address3, "[-1600]", "[-200]", fmt.Sprintf("1000%s", denom0), fmt.Sprintf("1000%s", denom1), 0, 0, frozenUntil, poolID)
+	// node.CreateConcentratedPosition(address3, fmt.Sprintf("[%d]", minTick), "1400", fmt.Sprintf("1000%s", denom0), fmt.Sprintf("1000%s", denom1), 0, 0, frozenUntil, poolID)
 
-	// Create 2 positions for node3: overlap together, overlap with 2 node1 positions, one position starts from minimum
-	node.CreateConcentratedPosition(address3, "[-1600]", "[-200]", fmt.Sprintf("1000%s", denom0), fmt.Sprintf("1000%s", denom1), 0, 0, poolID)
-	node.CreateConcentratedPosition(address3, fmt.Sprintf("[%d]", minTick), "1400", fmt.Sprintf("1000%s", denom0), fmt.Sprintf("1000%s", denom1), 0, 0, poolID)
+	// // get newly created positions
+	// positionsAddress1 := node.QueryConcentratedPositions(address1)
+	// positionsAddress2 := node.QueryConcentratedPositions(address2)
+	// positionsAddress3 := node.QueryConcentratedPositions(address3)
+
+	// // assert number of positions per address
+	// s.Require().Equal(len(positionsAddress1), 2)
+	// s.Require().Equal(len(positionsAddress2), 1)
+	// s.Require().Equal(len(positionsAddress3), 2)
+
+	// // Assert returned positions:
+
+	// // assert positions for address1
+	// a1p1 := positionsAddress1[0]
+	// a1p2 := positionsAddress1[1]
+	// // first position first address
+	// s.Require().Equal(a1p1.PoolId, poolID)
+	// s.Require().Equal(a1p1.LowerTick, -1200)
+	// s.Require().Equal(a1p1.UpperTick, 400)
+	// // second position second address
+	// s.Require().Equal(a1p2.PoolId, poolID)
+	// s.Require().Equal(a1p2.LowerTick, -400)
+	// s.Require().Equal(a1p2.UpperTick, 400)
+
+	// // assert positions for address2
+	// a2p1 := positionsAddress2[0]
+	// // first position second address
+	// s.Require().Equal(a2p1.PoolId, poolID)
+	// s.Require().Equal(a2p1.LowerTick, 2200)
+	// s.Require().Equal(a2p1.UpperTick, maxTick)
+
+	// // assert positions for address3
+	// a3p1 := positionsAddress3[0]
+	// a3p2 := positionsAddress3[1]
+	// // first position third address
+	// s.Require().Equal(a3p1.PoolId, poolID)
+	// s.Require().Equal(a3p1.LowerTick, -1600)
+	// s.Require().Equal(a3p1.UpperTick, -200)
+	// // second position third address
+	// s.Require().Equal(a3p2.PoolId, poolID)
+	// s.Require().Equal(a3p2.LowerTick, minTick)
+	// s.Require().Equal(a3p2.UpperTick, 1400)
+
 }
 
 // TestGeometricTwapMigration tests that the geometric twap record
