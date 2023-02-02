826832aaf2e760872176622c3e49cd22942c0b82
alpo <62043214+AlpinYukseloglu@users.noreply.github.com>
2023-02-02 10:27:50 -0800

    [accum][CL Incentives]: Track total shares for accumulators (#4204)
    * implement total share tracking for accumulators
    
    * use seed in testing and rename test helper

--- osmoutils/accum/export_test.go
+++ osmoutils/accum/export_test.go
@@ -31,11 +31,15 @@ func (accum AccumulatorObject) GetAllPositions() ([]Record, error) {
 }
 
 // Creates an accumulator object for testing purposes
-func CreateRawAccumObject(store store.KVStore, name string, value sdk.DecCoins) AccumulatorObject {
+func MakeTestAccumulator(store store.KVStore, name string, value sdk.DecCoins, totalShares sdk.Dec) AccumulatorObject {
+	// We store an accumulator object in state even if unused in tests
+	// because position operations still require GetAccumulator to work
+	_ = MakeAccumulator(store, name)
 	return AccumulatorObject{
 		store: store,
 		name:  name,
 		value: value,
+		totalShares: totalShares,
 	}
 }
 
