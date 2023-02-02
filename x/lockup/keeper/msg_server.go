8f42d6075b6f494b2bedcf89d413fe33e3db880e
Roman <roman@osmosis.team>
2023-02-02 13:33:52 +0700

    remove force unlock

--- x/lockup/keeper/msg_server.go
+++ x/lockup/keeper/msg_server.go
@@ -192,19 +192,19 @@ func (server msgServer) ForceUnlock(goCtx context.Context, msg *types.MsgForceUn
 		return &types.MsgForceUnlockResponse{Success: false}, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "Sender (%s) does not match lock owner (%s)", msg.Owner, lock.Owner)
 	}
 
-	// check for chain parameter that the address is allowed to force unlock
-	forceUnlockAllowedAddresses := server.keeper.GetParams(ctx).ForceUnlockAllowedAddresses
-	found := false
-	for _, addr := range forceUnlockAllowedAddresses {
-		// defense in depth, double checking the message owner and lock owner are both the same and is one of the allowed force unlock addresses
-		if addr == lock.Owner && addr == msg.Owner {
-			found = true
-			break
-		}
-	}
-	if !found {
-		return &types.MsgForceUnlockResponse{Success: false}, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "Sender (%s) not allowed to force unlock", lock.Owner)
-	}
+	// // check for chain parameter that the address is allowed to force unlock
+	// forceUnlockAllowedAddresses := server.keeper.GetParams(ctx).ForceUnlockAllowedAddresses
+	// found := false
+	// for _, addr := range forceUnlockAllowedAddresses {
+	// 	// defense in depth, double checking the message owner and lock owner are both the same and is one of the allowed force unlock addresses
+	// 	if addr == lock.Owner && addr == msg.Owner {
+	// 		found = true
+	// 		break
+	// 	}
+	// }
+	// if !found {
+	// 	return &types.MsgForceUnlockResponse{Success: false}, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "Sender (%s) not allowed to force unlock", lock.Owner)
+	// }
 
 	// check that given lock is not superfluid staked
 	synthLocks := server.keeper.GetAllSyntheticLockupsByLockup(ctx, lock.ID)
