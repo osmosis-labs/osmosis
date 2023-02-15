2876bdb30bf7d26ae1fef2f59a380ab0f8155b25
ducnt131 <ntd1312000@gmail.com>
2023-02-15 22:50:42 +0700

    Update hermes version for e2e

--- tests/e2e/containers/config.go
+++ tests/e2e/containers/config.go
@@ -29,8 +29,8 @@ const (
 	previousVersionInitRepository = "osmolabs/osmosis-e2e-init-chain"
 	previousVersionInitTag        = "v14.x-f6813bcb-1674140667-manual"
 	// Hermes repo/version for relayer
-	relayerRepository = "osmolabs/hermes"
-	relayerTag        = "0.13.0"
+	relayerRepository = "informalsystems/hermes"
+	relayerTag        = "1.2.0"
 )
 
 // Returns ImageConfig needed for running e2e test.
