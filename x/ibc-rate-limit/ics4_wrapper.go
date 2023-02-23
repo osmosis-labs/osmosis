d3781d3c18ee94719127f7b555235f4d048a8c6b
Nicolas Lara <nicolaslara@gmail.com>
2023-02-23 12:22:16 +0100

    params experiment for test with state-export

--- x/ibc-rate-limit/ics4_wrapper.go
+++ x/ibc-rate-limit/ics4_wrapper.go
@@ -86,7 +86,16 @@ func (i *ICS4Wrapper) GetContractAddress(ctx sdk.Context) (contract string) {
 }
 
 func (i *ICS4Wrapper) GetParams(ctx sdk.Context) (params types.Params) {
-	i.paramSpace.GetParamSet(ctx, &params)
+	// This was previously done via i.paramSpace.GetParamSet(ctx, &params). That will
+	// panic if the params don't exist. This is a workaround to avoid that panic.
+	// Params should be refactored to just use a raw kvstore.
+	empty := types.Params{}
+	for _, pair := range params.ParamSetPairs() {
+		i.paramSpace.GetIfExists(ctx, pair.Key, pair.Value)
+	}
+	if params == empty {
+		return types.DefaultParams()
+	}
 	return params
 }
 
