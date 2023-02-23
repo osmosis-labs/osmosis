2585873c6047f513aa5265f8956895a8e20df731
Nicolas Lara <nicolaslara@gmail.com>
2023-02-23 17:44:25 +0100

    backport genesis

--- x/ibc-rate-limit/types/keys.go
+++ x/ibc-rate-limit/types/keys.go
@@ -1,5 +1,14 @@
 package types
 
+import "strings"
+
 const (
 	ModuleName = "rate-limited-ibc" // IBC at the end to avoid conflicts with the ibc prefix
+
+)
+
+var (
+	// RouterKey is the message route. Can only contain
+	// alphanumeric characters.
+	RouterKey = strings.ReplaceAll(ModuleName, "-", "")
 )
