006f54a2d6fc3078ffa55c641827486e3ea239b5
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-03-09 12:58:12 +0700

    docs change

--- scripts/update-go-mod.sh
+++ scripts/update-go-mod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script for checking diff between two commits and updating osmoutils, osmomath or ibc-hooks if any were between two commits
+# Script for checking `git diff` between two commits and updating osmoutils, osmomath or ibc-hooks if any were between two commits
 # Used by Go Mod Auto Version Update workflow
 # First argument: sha of a first commit
 # Second argument: sha of a second commit
