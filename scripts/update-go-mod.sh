96f315920e692599244718173b4b34cd6822b95b
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-03-09 12:54:14 +0700

    commits before/after push

--- scripts/update-go-mod.sh
+++ scripts/update-go-mod.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 is_updated() {
@@ -9,12 +8,12 @@ is_updated() {
     return 0
 }
 
-main_commit=$(git rev-parse origin/$GITHUB_BASE_REF)
-head_commit=$(git rev-parse origin/$GITHUB_HEAD_REF)
+commit_before=$1
+commit_after=$2
 
-changed_osmoutils=$(git diff --name-only $main_commit $head_commit | grep osmoutils)
-changed_osmomath=$(git diff --name-only $main_commit $head_commit | grep osmomath)
-changed_ibc_hooks=$(git diff --name-only $main_commit $head_commit | grep x/ibc-hooks)
+changed_osmoutils=$(git diff --name-only $commit_before $commit_after | grep osmoutils)
+changed_osmomath=$(git diff --name-only $commit_before $commit_after | grep osmomath)
+changed_ibc_hooks=$(git diff --name-only $commit_before $commit_after | grep x/ibc-hooks)
 
 is_updated $changed_osmoutils
 update_osmoutils=$?
@@ -25,27 +24,19 @@ update_osmomath=$?
 is_updated $changed_ibc_hooks
 update_ibc_hooks=$?
 
-any_updated=0 # we do not want to run `go mod tidy`` in case none of these files have changed
-
 if [ $update_osmoutils -eq 1 ]
 then 
-    go get github.com/osmosis-labs/osmosis/osmoutils@$head_commit
-    any_updated=1
+    go get github.com/osmosis-labs/osmosis/osmoutils@$commit_after
 fi
 
 if [ $update_osmomath -eq 1 ]
 then 
-    go get github.com/osmosis-labs/osmosis/osmomath@$head_commit
-    any_updated=1
+    go get github.com/osmosis-labs/osmosis/osmomath@$commit_after
 fi
 
 if [ $update_ibc_hooks -eq 1 ]
 then 
-    go get github.com/osmosis-labs/osmosis/x/ibc-hooks@$head_commit
-    any_updated=1
+    go get github.com/osmosis-labs/osmosis/x/ibc-hooks@$commit_after
 fi
 
-if [ $any_updated -eq 1 ]
-then
-    go mod tidy
-fi
+go mod tidy
