c68695da2e740ca043239010f1d0298fb6f4d86b
Niccolo Raspa <nraspa@live.it>
2023-03-10 15:00:45 +0100

    Re-add add_keys.sh utils script

---
+++ tests/localosmosis/utils/add_keys.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function add_key {
+    echo "$2" | osmosisd keys add "$1" --recover --keyring-backend test
+}
+
+add_key "localosmosis-validator" "bottom loan skill merry east cradle onion journey palm apology verb edit desert impose absurd oil bubble sweet glove shallow size build burst effort" 
+add_key "localosmosis-pools" "traffic cool olive pottery elegant innocent aisle dial genuine install shy uncle ride federal soon shift flight program cave famous provide cute pole struggle" 
+add_key "localosmosis-faucet" "only item always south dry begin barely seed wire praise chapter bomb remind abandon erase safe point vehicle tuition release half denial receive water" 
+add_key "localosmosis-validator-mainent" "only item always south dry begin barely seed wire praise chapter bomb remind abandon erase safe point vehicle tuition release half denial receive water" 
