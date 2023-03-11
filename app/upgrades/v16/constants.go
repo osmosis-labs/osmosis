599b2141e432bcc8f00347ae91d89621ddbbf180
Roman <roman@osmosis.team>
2023-03-11 11:58:45 -0500

    add cl to v16

--- app/upgrades/v16/constants.go
+++ app/upgrades/v16/constants.go
@@ -4,6 +4,8 @@ import (
 	"github.com/osmosis-labs/osmosis/v15/app/upgrades"
 
 	store "github.com/cosmos/cosmos-sdk/store/types"
+
+	cltypes "github.com/osmosis-labs/osmosis/v15/x/concentrated-liquidity/types"
 )
 
 // UpgradeName defines the on-chain upgrade name for the Osmosis v16 upgrade.
@@ -12,8 +14,8 @@ const UpgradeName = "v16"
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateUpgradeHandler,
-	StoreUpgrades:        store.StoreUpgrades{
-		Added:   []string{},
+	StoreUpgrades: store.StoreUpgrades{
+		Added:   []string{cltypes.StoreKey},
 		Deleted: []string{},
-    },
+	},
 }
