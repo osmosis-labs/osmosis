9bb3fa751647a0ee33fbb7d92b020eeed6592c8d
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-03-15 11:02:22 +0700

    add epochs

--- scripts/update-go-mod.sh
+++ scripts/update-go-mod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script for checking `git diff` between two commits and updating osmoutils, osmomath or ibc-hooks if any were changed between two commits
+# Script for checking `git diff` between two commits and updating osmoutils, osmomath, epochs or ibc-hooks if any were changed between two commits
 # Used by Go Mod Auto Version Update workflow
 # First argument: sha of a first commit
 # Second argument: sha of a second commit
@@ -19,6 +19,7 @@ commit_after=$2
 changed_osmoutils=$(git diff --name-only $commit_before $commit_after | grep osmoutils)
 changed_osmomath=$(git diff --name-only $commit_before $commit_after | grep osmomath)
 changed_ibc_hooks=$(git diff --name-only $commit_before $commit_after | grep x/ibc-hooks)
+changed_epochs=$(git diff --name-only $commit_before $commit_after | grep x/epochs)
 
 is_updated $changed_osmoutils
 update_osmoutils=$?
@@ -28,6 +29,9 @@ update_osmomath=$?
 
 is_updated $changed_ibc_hooks
 update_ibc_hooks=$?
+
+is_updated $changed_epochs
+update_epochs=$?
 
 if [ $update_osmoutils -eq 1 ]
 then 
@@ -42,6 +46,11 @@ fi
 if [ $update_ibc_hooks -eq 1 ]
 then 
     go get github.com/osmosis-labs/osmosis/x/ibc-hooks@$commit_after
+fi
+
+if [ $update_epochs -eq 1 ]
+then 
+    go get github.com/osmosis-labs/osmosis/x/epochs@$commit_after
 fi
 
 go mod tidy
