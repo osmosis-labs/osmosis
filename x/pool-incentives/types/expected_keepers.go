72205bb3a2499675a1ddeb152e19ef3e70cb3713
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2023-03-24 20:12:41 +0700

    lint

--- x/pool-incentives/types/expected_keepers.go
+++ x/pool-incentives/types/expected_keepers.go
@@ -32,7 +32,7 @@ type IncentivesKeeper interface {
 	CreateGauge(ctx sdk.Context, isPerpetual bool, owner sdk.AccAddress, coins sdk.Coins, distrTo lockuptypes.QueryCondition, startTime time.Time, numEpochsPaidOver uint64) (uint64, error)
 	GetGaugeByID(ctx sdk.Context, gaugeID uint64) (*incentivestypes.Gauge, error)
 	GetGauges(ctx sdk.Context) []incentivestypes.Gauge
-	GetActiveGauges(ctx sdk.Context) []incentivestypes.Gauge 
+	GetActiveGauges(ctx sdk.Context) []incentivestypes.Gauge
 
 	AddToGaugeRewards(ctx sdk.Context, owner sdk.AccAddress, coins sdk.Coins, gaugeID uint64) error
 }
