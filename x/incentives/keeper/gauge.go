46fda09023843d927b276899dfb2e5822654d808
stackman27 <sis1001@berkeley.edu>
2023-04-03 21:53:42 -0700

    added gas cost for add reward to gauge

--- x/incentives/keeper/gauge.go
+++ x/incentives/keeper/gauge.go
@@ -152,6 +152,10 @@ func (k Keeper) AddToGaugeRewards(ctx sdk.Context, owner sdk.AccAddress, coins s
 	if gauge.IsFinishedGauge(ctx.BlockTime()) {
 		return errors.New("gauge is already completed")
 	}
+
+	// Fixed gas consumption adding reward to gauges based on the number of coins to add
+	ctx.GasMeter().ConsumeGas(uint64(types.BaseGasFeeForAddRewardToGauge*len(coins)), "add to gauge reward cost")
+
 	if err := k.bk.SendCoinsFromAccountToModule(ctx, owner, types.ModuleName, coins); err != nil {
 		return err
 	}
---
+++ x/incentives/types/constants.go
@@ -0,0 +1,5 @@
+package types
+
+var (
+	BaseGasFeeForAddRewardToGauge = 10_000
+)
