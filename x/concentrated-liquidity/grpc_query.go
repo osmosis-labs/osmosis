c0ad180bbb7ebc0500ac375da9cd87de535d05a8
Roman <roman@osmosis.team>
2023-04-07 14:21:04 -0400

    fix(CL): invalid sqrt price initialization breaking liquidity net in direction query (#4861)
    * fix(CL): invalid sqrt price initialization breaking liquidity net in direction query
    
    * add prints to go-client script
    
    * README
    
    * fix test
    
    * warning
    
    * Update x/concentrated-liquidity/lp.go
    
    Co-authored-by: Adam Tucker <adam@osmosis.team>
    
    ---------
    
    Co-authored-by: Adam Tucker <adam@osmosis.team>

--- x/concentrated-liquidity/grpc_query.go
+++ x/concentrated-liquidity/grpc_query.go
@@ -212,7 +212,7 @@ func (q Querier) LiquidityNetInDirection(goCtx context.Context, req *clquery.Que
 		return nil, err
 	}
 
-	return &clquery.QueryLiquidityNetInDirectionResponse{LiquidityDepths: liquidityDepths, CurrentLiquidity: pool.GetLiquidity()}, nil
+	return &clquery.QueryLiquidityNetInDirectionResponse{LiquidityDepths: liquidityDepths, CurrentLiquidity: pool.GetLiquidity(), CurrentTick: pool.GetCurrentTick().Int64()}, nil
 }
 
 func (q Querier) ClaimableFees(ctx context.Context, req *clquery.QueryClaimableFeesRequest) (*clquery.QueryClaimableFeesResponse, error) {
