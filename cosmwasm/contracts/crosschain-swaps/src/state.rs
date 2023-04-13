512654fb35845f807cdc9179984db9e2afc2e564
Nicolas Lara <nicolaslara@gmail.com>
2023-04-13 15:28:52 +0200

    Unwrap badly wrapped tokens before a XCS (#4809)
    * added changelog
    
    * unwrap before swap
    
    * added table tests to add more cases
    
    * added new test
    
    * test updates and preparation for fixing the firste hop bug
    
    * reorg
    
    * cleanup
    
    * added comment to define path
    
    * comment
    
    * fix tests and added x86 bytecode
    
    * clippy
    
    * x86 contracts
    
    * fix test
    
    * added extra test case
    
    * added last test
    
    * using propper tokens
    
    * simplified ctx
    
    * test cleanup
    
    * test cleanup
    
    * bytecode and lint

--- cosmwasm/contracts/crosschain-swaps/src/state.rs
+++ cosmwasm/contracts/crosschain-swaps/src/state.rs
@@ -1,9 +1,10 @@
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, Timestamp};
 use cw_storage_plus::{Item, Map};
+use registry::msg::SerializableJson;
 use swaprouter::msg::ExecuteMsg as SwapRouterExecute;
 
-use crate::msg::{FailedDeliveryAction, SerializableJson};
+use crate::msg::FailedDeliveryAction;
 
 #[cw_serde]
 pub struct Config {
