e8fee09b4d7f3ffbe5e6761c09ac2865bbc7a465
Roman <roman@osmosis.team>
2023-04-16 10:21:23 -0700

    fix twap tests

--- x/twap/keeper_test.go
+++ x/twap/keeper_test.go
@@ -42,6 +42,12 @@ func (s *TestSuite) SetupTest() {
 	s.Setup()
 	s.twapkeeper = s.App.TwapKeeper
 	s.Ctx = s.Ctx.WithBlockTime(baseTime)
+
+	s.SetupDefaultConcentratedLiquidityAuthorizedQuoteDenoms()
+	// add x/twap test specific denoms
+	concentratedLiquidityParams := s.App.ConcentratedLiquidityKeeper.GetParams(s.Ctx)
+	concentratedLiquidityParams.AuthorizedQuoteDenoms = append(concentratedLiquidityParams.AuthorizedQuoteDenoms, denom0, denom1, denom2)
+	s.App.ConcentratedLiquidityKeeper.SetParams(s.Ctx, concentratedLiquidityParams)
 }
 
 var (
