49c7eaf28e1df76397db9d86bb0f317e8cfbbc87
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2023-04-24 17:49:25 -0400

    add another todo

--- x/protorev/keeper/developer_fees.go
+++ x/protorev/keeper/developer_fees.go
@@ -33,38 +33,6 @@ func (k Keeper) SendDeveloperFeesToDeveloperAccount(ctx sdk.Context) error {
 	return nil
 }
 
-// Deprecated: Can be removed in v16
-// UpdateDeveloperFees updates the fees that developers can withdraw from the module account
-func (k Keeper) UpdateDeveloperFees(ctx sdk.Context, denom string, profit sdk.Int) error {
-	daysSinceGenesis, err := k.GetDaysSinceModuleGenesis(ctx)
-	if err != nil {
-		return err
-	}
-
-	// Calculate the developer fee
-	if daysSinceGenesis < types.Phase1Length {
-		profit = profit.MulRaw(types.ProfitSplitPhase1).QuoRaw(100)
-	} else if daysSinceGenesis < types.Phase2Length {
-		profit = profit.MulRaw(types.ProfitSplitPhase2).QuoRaw(100)
-	} else {
-		profit = profit.MulRaw(types.ProfitSplitPhase3).QuoRaw(100)
-	}
-
-	// Get the developer fees for the denom, if not there then set it to 0 and initialize it
-	currentDeveloperFee, err := k.GetDeveloperFees(ctx, denom)
-	if err != nil {
-		currentDeveloperFee = sdk.NewCoin(denom, sdk.ZeroInt())
-	}
-	currentDeveloperFee.Amount = currentDeveloperFee.Amount.Add(profit)
-
-	// Set the developer fees for the denom
-	if err = k.SetDeveloperFees(ctx, currentDeveloperFee); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // SendDeveloperFee sends the developer fee from the module account to the developer account
 func (k Keeper) SendDeveloperFee(ctx sdk.Context, arbProfit sdk.Coin) error {
 	// Developer account must be set in order to be able to withdraw developer fees
