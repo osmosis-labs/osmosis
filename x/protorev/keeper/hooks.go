3212d18d48fcc52e68086194f3139c9e092ebb75
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2023-04-27 19:00:09 -0400

    add hook logic for cl pools and refactor

--- x/protorev/keeper/hooks.go
+++ x/protorev/keeper/hooks.go
@@ -20,85 +20,41 @@ var (
 // Create new ProtoRev hooks.
 func (k Keeper) Hooks() Hooks { return Hooks{k} }
 
+// ----------------------------------------------------------------------------
+// GAMM HOOKS
+// ----------------------------------------------------------------------------
+
 // AfterCFMMPoolCreated hook checks and potentially stores the pool via the highest liquidity method.
 func (h Hooks) AfterCFMMPoolCreated(ctx sdk.Context, sender sdk.AccAddress, poolId uint64) {
-	baseDenoms, err := h.k.GetAllBaseDenoms(ctx)
-	if err != nil {
-		ctx.Logger().Error("Protorev error getting base denoms in AfterCFMMPoolCreated hook", err)
-		return
-	}
-
-	baseDenomMap := make(map[string]bool)
-	for _, baseDenom := range baseDenoms {
-		baseDenomMap[baseDenom.Denom] = true
-	}
-
-	pool, err := h.k.gammKeeper.GetPoolAndPoke(ctx, poolId)
-	if err != nil {
-		ctx.Logger().Error("Protorev error getting pool in AfterCFMMPoolCreated hook", err)
-		return
-	}
-
-	coins := pool.GetTotalPoolLiquidity(ctx)
-
-	// Pool must be active and the number of coins must be 2
-	if pool.IsActive(ctx) && len(coins) == 2 {
-		tokenA := coins[0]
-		tokenB := coins[1]
-
-		liquidity := tokenA.Amount.Mul(tokenB.Amount)
-
-		if baseDenomMap[tokenA.Denom] {
-			h.k.compareAndStorePool(ctx, poolId, liquidity, tokenA.Denom, tokenB.Denom)
-		}
-		if baseDenomMap[tokenB.Denom] {
-			h.k.compareAndStorePool(ctx, poolId, liquidity, tokenB.Denom, tokenA.Denom)
-		}
-	}
+	h.k.afterPoolCreated(ctx, poolId)
 }
 
 // AfterJoinPool stores swaps to be checked by protorev given the coins entered into the pool.
 func (h Hooks) AfterJoinPool(ctx sdk.Context, sender sdk.AccAddress, poolId uint64, enterCoins sdk.Coins, shareOutAmount sdk.Int) {
 	// TODO: Probably generalize to allow for more join denoms
-	// Right now, the main message only allows for a single join denom
-	// But I imagine we will want to allow for multiple join denoms in the future
-	// Since the function allows for multile join denoms, just not the main interface
-	if len(enterCoins) != 1 {
-		return
-	}
-
 	h.k.storeJoinExitPoolMsgs(ctx, sender, poolId, enterCoins[0].Denom, true)
 }
 
 // AfterExitPool stores swaps to be checked by protorev given the coins exited from the pool.
 func (h Hooks) AfterExitPool(ctx sdk.Context, sender sdk.AccAddress, poolId uint64, shareInAmount sdk.Int, exitCoins sdk.Coins) {
 	// TODO: Probably generalize to allow for more exit denoms
-	// Right now, the main message only allows for a single exit denom
-	// But I imagine we will want to allow for multiple exit denoms in the future
-	// Since the function allows for multile exit denoms, just not the main interface
-	if len(exitCoins) != 1 {
-		return
-	}
-
 	h.k.storeJoinExitPoolMsgs(ctx, sender, poolId, exitCoins[0].Denom, false)
 }
 
 // AfterCFMMSwap stores swaps to be checked by protorev given the coins swapped in the pool.
 func (h Hooks) AfterCFMMSwap(ctx sdk.Context, sender sdk.AccAddress, poolId uint64, input sdk.Coins, output sdk.Coins) {
-	swapToBackrun := types.Trade{
-		Pool:     poolId,
-		TokenIn:  input[0].Denom,  // As of v15, it is safe to assume only one input token
-		TokenOut: output[0].Denom, // As of v15, it is safe to assume only one output token
-	}
-
-	if err := h.k.AddSwapsToSwapsToBackrun(ctx, []types.Trade{swapToBackrun}); err != nil {
-		ctx.Logger().Error("Protorev error adding swap to backrun from AfterCFMMSwap hook", err)
-	}
+	// As of v15, it is safe to assume only one input token and one output token
+	// TODO: Probably generalize to allow for more swap denoms
+	h.k.storeSwap(ctx, poolId, input[0].Denom, output[0].Denom)
 }
 
-// AfterConcentratedPoolCreated creates a single gauge for the concentrated liquidity pool.
+// ----------------------------------------------------------------------------
+// CONCENTRATED LIQUIDITY HOOKS
+// ----------------------------------------------------------------------------
+
+// AfterConcentratedPoolCreated checks and potentially stores the pool via the highest liquidity method.
 func (h Hooks) AfterConcentratedPoolCreated(ctx sdk.Context, sender sdk.AccAddress, poolId uint64) {
-	fmt.Println("AfterConcentratedPoolCreated hook is a noop in Protorev.")
+	h.k.afterPoolCreated(ctx, poolId)
 }
 
 // AfterInitialPoolPositionCreated is a noop.
@@ -111,9 +67,69 @@ func (h Hooks) AfterLastPoolPositionRemoved(ctx sdk.Context, sender sdk.AccAddre
 	fmt.Println("AfterLastPoolPositionRemoved hook is a noop in Protorev.")
 }
 
-// AfterConcentratedPoolSwap is a noop.
+// AfterConcentratedPoolSwap stores swaps to be checked by protorev given the coins swapped in the pool.
 func (h Hooks) AfterConcentratedPoolSwap(ctx sdk.Context, sender sdk.AccAddress, poolId uint64, input sdk.Coins, output sdk.Coins) {
-	fmt.Println("AfterConcentratedPoolSwap hook is a noop in Protorev.")
+	// As of v15, it is safe to assume only one input token and one output token
+	// TODO: Probably generalize to allow for more swap denoms
+	h.k.storeSwap(ctx, poolId, input[0].Denom, output[0].Denom)
+}
+
+// ----------------------------------------------------------------------------
+// HELPER METHODS
+// ----------------------------------------------------------------------------
+
+// afterPoolCreated checks if the new pool should be stored as the highest liquidity pool
+func (k Keeper) afterPoolCreated(ctx sdk.Context, poolId uint64) {
+	baseDenoms, err := k.GetAllBaseDenoms(ctx)
+	if err != nil {
+		ctx.Logger().Error("Protorev error getting base denoms in AfterCFMMPoolCreated hook", err)
+		return
+	}
+
+	baseDenomMap := make(map[string]bool)
+	for _, baseDenom := range baseDenoms {
+		baseDenomMap[baseDenom.Denom] = true
+	}
+
+	pool, err := k.poolmanagerKeeper.GetPool(ctx, poolId)
+	if err != nil {
+		ctx.Logger().Error("Protorev error getting pool in AfterCFMMPoolCreated hook", err)
+		return
+	}
+
+	coins, err := k.poolmanagerKeeper.GetTotalPoolLiquidity(ctx, poolId)
+	if err != nil {
+		ctx.Logger().Error("Protorev error getting total pool liquidity in after swap hook", err)
+		return
+	}
+
+	// Pool must be active and the number of coins must be 2
+	if pool.IsActive(ctx) && len(coins) == 2 {
+		tokenA := coins[0]
+		tokenB := coins[1]
+
+		liquidity := tokenA.Amount.Mul(tokenB.Amount)
+
+		if baseDenomMap[tokenA.Denom] {
+			k.compareAndStorePool(ctx, poolId, liquidity, tokenA.Denom, tokenB.Denom)
+		}
+		if baseDenomMap[tokenB.Denom] {
+			k.compareAndStorePool(ctx, poolId, liquidity, tokenB.Denom, tokenA.Denom)
+		}
+	}
+}
+
+// storeSwap stores a swap to be checked by protorev when attempting backruns.
+func (k Keeper) storeSwap(ctx sdk.Context, poolId uint64, tokenIn, tokenOut string) {
+	swapToBackrun := types.Trade{
+		Pool:     poolId,
+		TokenIn:  tokenIn,
+		TokenOut: tokenOut,
+	}
+
+	if err := k.AddSwapsToSwapsToBackrun(ctx, []types.Trade{swapToBackrun}); err != nil {
+		ctx.Logger().Error("Protorev error adding swap to backrun from storeSwap", err)
+	}
 }
 
 // compareAndStorePool compares the liquidity of the new pool with the liquidity of the stored pool, and stores the new pool if it has more liquidity.
