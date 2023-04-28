9a629aa0e950b9c12c0cae943a845fc30bfb877b
Roman <roman@osmosis.team>
2023-04-27 20:12:09 -0400

    comment

--- x/concentrated-liquidity/swapstrategy/one_for_zero_test.go
+++ x/concentrated-liquidity/swapstrategy/one_for_zero_test.go
@@ -130,12 +130,12 @@ func (suite *StrategyTestSuite) TestComputeSwapStepOutGivenIn_OneForZero() {
 			// Difference between given amount remaining in and amount in actually consumed which qpproximately equals to fee.
 			expectedFeeChargeTotal: defaultAmountOne.Sub(sdk.NewDec(100)).Quo(sdk.OneDec().Sub(defaultFee)).Sub(defaultAmountOne.Sub(sdk.NewDec(100)).Ceil()),
 		},
-		"5: no fee - reach target": {
+		"5: custom amounts at high price levels - reach tarrget": {
 			sqrtPriceCurrent: sqrt(100_000_000),
 			sqrtPriceTarget:  sqrt(100_000_100),
 			liquidity:        math.GetLiquidityFromAmounts(sqrt(1), sqrt(100_000_000), sqrt(100_000_100), defaultAmountZero.TruncateInt(), defaultAmountOne.TruncateInt()),
 
-			// TODO: manually estimate this
+			// this value is exactly enough to reach the target
 			amountOneInRemaining: sdk.NewDec(1336900668450),
 			swapFee:              sdk.ZeroDec(),
 
