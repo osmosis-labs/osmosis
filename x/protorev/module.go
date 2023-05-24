314d1c4a9b8e3b60c21e713bf87f368905a9e14d
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2023-05-24 14:14:26 -0400

    Revert "comment out everything upgrade related to isolate problem"
    This reverts commit da4efcab3c690ade326e204e1702a41bffb60138.

--- x/protorev/module.go
+++ x/protorev/module.go
@@ -167,7 +167,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion is a sequence number for state-breaking change of the module. It should be incremented on each consensus-breaking change introduced by the module. To avoid wrong/empty versions, the initial version should be set to 1
-func (AppModule) ConsensusVersion() uint64 { return 1 }
+func (AppModule) ConsensusVersion() uint64 { return 2 }
 
 // BeginBlock contains the logic that is automatically triggered at the beginning of each block
 func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}
