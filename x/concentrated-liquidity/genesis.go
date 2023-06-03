613fd46bf97b2e7990f00d0ffcb5634af1dd73d3
Sishir Giri <sis1001@berkeley.edu>
2023-06-02 19:57:16 -0700

    [CL][Internal Review][CollectIncentives] fix uptime Accumulator init genesis [part2] (#5411)
    * updated
    
    * removed
    
    * cleanup
    
    * fixing
    
    * fixed test

--- x/concentrated-liquidity/genesis.go
+++ x/concentrated-liquidity/genesis.go
@@ -79,6 +79,16 @@ func (k Keeper) InitGenesis(ctx sdk.Context, genState genesis.GenesisState) {
 		spreadRewardPositionKey := types.KeySpreadRewardPositionAccumulator(positionWrapper.Position.PositionId)
 
 		k.initOrUpdateAccumPosition(ctx, spreadRewardAccumObject, positionWrapper.SpreadRewardAccumRecord.AccumValuePerShare, spreadRewardPositionKey, positionWrapper.SpreadRewardAccumRecord.NumShares, positionWrapper.SpreadRewardAccumRecord.UnclaimedRewardsTotal, positionWrapper.SpreadRewardAccumRecord.Options)
+
+		positionName := string(types.KeyPositionId(positionWrapper.Position.PositionId))
+		uptimeAccumulators, err := k.GetUptimeAccumulators(ctx, positionWrapper.Position.PoolId)
+		if err != nil {
+			panic(err)
+		}
+
+		for uptimeIndex, uptimeRecord := range positionWrapper.UptimeAccumRecords {
+			k.initOrUpdateAccumPosition(ctx, uptimeAccumulators[uptimeIndex], uptimeRecord.AccumValuePerShare, positionName, uptimeRecord.NumShares, uptimeRecord.UnclaimedRewardsTotal, uptimeRecord.Options)
+		}
 	}
 }
 
@@ -189,10 +199,28 @@ func (k Keeper) ExportGenesis(ctx sdk.Context) *genesis.GenesisState {
 			panic(err)
 		}
 
+		// Retrieve uptime incentive accumulator state for position
+		positionName := string(types.KeyPositionId(position.PositionId))
+		uptimeAccumulators, err := k.GetUptimeAccumulators(ctx, position.PoolId)
+		if err != nil {
+			panic(err)
+		}
+
+		uptimeAccumObject := make([]accum.Record, len(uptimeAccumulators))
+		for uptimeIndex := range types.SupportedUptimes {
+			accumRecord, err := uptimeAccumulators[uptimeIndex].GetPosition(positionName)
+			if err != nil {
+				panic(err)
+			}
+
+			uptimeAccumObject[uptimeIndex] = accumRecord
+		}
+
 		positionData = append(positionData, genesis.PositionData{
 			LockId:                  lockId,
 			Position:                &position,
 			SpreadRewardAccumRecord: spreadRewardAccumPositionRecord,
+			UptimeAccumRecords:      uptimeAccumObject,
 		})
 	}
 
