8ef5c5c79205f0163f575d633faf6e82906eb6ef
Roman Akhtariev <roman@osmosis.team>
2023-06-14 03:08:42 +0000

    revert test changes

--- cmd/osmosisd/cmd/root.go
+++ cmd/osmosisd/cmd/root.go
@@ -149,7 +149,7 @@ func initAppConfig() (string, interface{}) {
 [osmosis-mempool]
 # This is the max allowed gas any tx. 
 # This is only for local mempool purposes, and thus	is only ran on check tx.
-max-gas-wanted-per-tx = "120000000"
+max-gas-wanted-per-tx = "25000000"
 
 # This is the minimum gas fee any arbitrage tx should have, denominated in uosmo per gas
 # Default value of ".005" then means that a tx with 1 million gas costs (.005 uosmo/gas) * 1_000_000 gas = .005 osmo
