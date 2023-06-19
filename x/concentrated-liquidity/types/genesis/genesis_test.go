80b4be7a21942242b7b00b245b912dc77f3d2783
Adam Tucker <adamleetucker@outlook.com>
2023-06-19 12:02:26 -0500

    fix test

--- x/concentrated-liquidity/types/genesis/genesis_test.go
+++ x/concentrated-liquidity/types/genesis/genesis_test.go
@@ -23,20 +23,20 @@ func TestValidateGenesis(t *testing.T) {
 		{
 			name: "invalid params",
 			genesis: *&genesis.GenesisState{
-				Params:         types.Params{},
-				PoolData:       genesis.DefaultGenesis().PoolData,
-				PositionData:   genesis.DefaultGenesis().GetPositionData(),
-				NextPositionId: genesis.DefaultGenesis().GetNextPositionId(),
+				Params:                types.Params{},
+				PoolData:              genesis.DefaultGenesis().PoolData,
+				NextPositionId:        genesis.DefaultGenesis().GetNextPositionId(),
+				NextIncentiveRecordId: genesis.DefaultGenesis().GetNextIncentiveRecordId(),
 			},
 			exepectedError: true,
 		},
 		{
 			name: "next position id is zero",
 			genesis: *&genesis.GenesisState{
-				Params:         genesis.DefaultGenesis().GetParams(),
-				PoolData:       genesis.DefaultGenesis().PoolData,
-				PositionData:   genesis.DefaultGenesis().GetPositionData(),
-				NextPositionId: 0,
+				Params:                genesis.DefaultGenesis().GetParams(),
+				PoolData:              genesis.DefaultGenesis().PoolData,
+				NextPositionId:        0,
+				NextIncentiveRecordId: genesis.DefaultGenesis().GetNextIncentiveRecordId(),
 			},
 			exepectedError: true,
 		},
@@ -45,7 +45,7 @@ func TestValidateGenesis(t *testing.T) {
 			genesis: *&genesis.GenesisState{
 				Params:                genesis.DefaultGenesis().GetParams(),
 				PoolData:              genesis.DefaultGenesis().PoolData,
-				PositionData:          genesis.DefaultGenesis().GetPositionData(),
+				NextPositionId:        genesis.DefaultGenesis().GetNextPositionId(),
 				NextIncentiveRecordId: 0,
 			},
 			exepectedError: true,
