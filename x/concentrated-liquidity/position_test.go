1cc4f312e4c6bf825cce6ad86ff68ef2e26d0b9c
Roman <roman@osmosis.team>
2023-06-19 15:01:01 -0400

    chore: fix conflict

--- x/concentrated-liquidity/position_test.go
+++ x/concentrated-liquidity/position_test.go
@@ -2525,7 +2525,7 @@ func (s *KeeperTestSuite) TestTickRoundingEdgeCase() {
 	swapAddr := testAccs[2]
 	desiredTokenOut := sdk.NewCoin(USDC, sdk.NewInt(10000))
 	s.FundAcc(swapAddr, sdk.NewCoins(sdk.NewCoin(ETH, sdk.NewInt(1000000000000000000))))
-	_, _, _, _, _, err := s.clk.SwapInAmtGivenOut(s.Ctx, swapAddr, pool, desiredTokenOut, ETH, sdk.ZeroDec(), sdk.ZeroDec())
+	_, _, _, err := s.clk.SwapInAmtGivenOut(s.Ctx, swapAddr, pool, desiredTokenOut, ETH, sdk.ZeroDec(), sdk.ZeroDec())
 	s.Require().NoError(err)
 
 	// Both positions should be able to withdraw successfully
