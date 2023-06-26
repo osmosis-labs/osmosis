f3336dca7de36a64d7eba00484b95f94f21cc328
Adam Tucker <adamleetucker@outlook.com>
2023-06-26 16:09:08 -0500

    update comment

--- app/upgrades/v16/upgrades.go
+++ app/upgrades/v16/upgrades.go
@@ -139,9 +139,9 @@ func CreateUpgradeHandler(
 			return nil, err
 		}
 
-		// Swap one DAI for OSMO from the community pool.
+		// Swap in a maximum of 10 OSMO for one DAI via the community pool.
 		oneDai := sdk.NewCoin(DAIIBCDenom, sdk.NewInt(1000000000000000000))
-		tokenInAmt, err := keepers.GAMMKeeper.SwapExactAmountOut(ctx, communityPoolAddress, daiOsmoPool, DesiredDenom0, sdk.NewInt(1000000), oneDai, sdk.ZeroDec())
+		tokenInAmt, err := keepers.GAMMKeeper.SwapExactAmountOut(ctx, communityPoolAddress, daiOsmoPool, DesiredDenom0, sdk.NewInt(10000000), oneDai, sdk.ZeroDec())
 		if err != nil {
 			return nil, err
 		}
