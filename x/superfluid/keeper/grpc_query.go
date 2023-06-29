0b7700a4558c0418499395055166526c2f916874
Adam Tucker <adam@osmosis.team>
2023-06-28 20:29:41 -0500

    fix: cl sf positions per user per pool query (#5668)
    * check if lock is mature in sf query
    
    * check if lock has matured in query
    
    * Update position.go
    
    * simpler change

--- x/superfluid/keeper/grpc_query.go
+++ x/superfluid/keeper/grpc_query.go
@@ -304,7 +304,12 @@ func (q Querier) UserSuperfluidPositionsPerConcentratedPoolBreakdown(goCtx conte
 		case cltypes.PositionIdToLockNotFoundError:
 			continue
 		case nil:
+			// If we have hit this logic branch, it means that, at one point, the lockId provided existed. If we fetch it again
+			// and it doesn't exist, that means that the lock has matured.
 			lock, err := q.Keeper.lk.GetLockByID(ctx, lockId)
+			if err == errorsmod.Wrap(lockuptypes.ErrLockupNotFound, fmt.Sprintf("lock with ID %d does not exist", lock.GetID())) {
+				continue
+			}
 			if err != nil {
 				return nil, err
 			}
