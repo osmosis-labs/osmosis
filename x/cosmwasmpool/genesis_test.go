a7590c0e1a8523d2d582528b1e31982d00362f12
Adam Tucker <adamleetucker@outlook.com>
2023-07-01 21:02:15 -0500

    remove prints and dead code

--- x/cosmwasmpool/genesis_test.go
+++ x/cosmwasmpool/genesis_test.go
@@ -1,10 +1,7 @@
 package cosmwasmpool_test
 
 import (
-	"fmt"
-
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
-	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/osmosis-labs/osmosis/v16/x/cosmwasmpool/types"
@@ -21,24 +18,14 @@ func (s *PoolModuleSuite) TestInitGenesis() {
 		pool := s.PrepareCustomTransmuterPool(s.TestAccs[0], defaultDenoms)
 		s.JoinTransmuterPool(s.TestAccs[0], pool.GetId(), initalDefaultSupply)
 		expectedTotalLiquidity = expectedTotalLiquidity.Add(initalDefaultSupply...)
-		fmt.Println("poolsss", pool.GetTotalPoolLiquidity(s.Ctx))
 	}
-
-	pool, _ := s.App.CosmwasmPoolKeeper.GetPoolById(s.Ctx, 1)
-	fmt.Println("pool liq", pool.GetTotalPoolLiquidity(s.Ctx))
 
 	pools, err := s.App.CosmwasmPoolKeeper.GetPools(s.Ctx)
 	if err != nil {
 		panic(err)
 	}
 
-	cosmwasmPool, _ := pools[0].(types.CosmWasmExtension)
-	fmt.Println("cosmwasmPool", cosmwasmPool.GetTotalPoolLiquidity(s.Ctx))
-
 	cosmwasmPoolPreInit := pools[0]
-
-	sf := cosmwasmPoolPreInit.GetSpreadFactor(s.Ctx)
-	fmt.Println("spread factor", sf)
 
 	poolAnys := []*codectypes.Any{}
 	for _, poolI := range pools {
@@ -46,8 +33,6 @@ func (s *PoolModuleSuite) TestInitGenesis() {
 		if !ok {
 			panic("invalid pool type")
 		}
-		sf := cosmwasmPool.GetSpreadFactor(s.Ctx)
-		fmt.Println("spread factor 2", sf)
 		cosmwasmPool.SetWasmKeeper(s.App.WasmKeeper)
 		any, err := codectypes.NewAnyWithValue(cosmwasmPool)
 		if err != nil {
@@ -55,12 +40,6 @@ func (s *PoolModuleSuite) TestInitGenesis() {
 		}
 		poolAnys = append(poolAnys, any)
 	}
-
-	fmt.Println("pools liq", pools[0].(types.CosmWasmExtension).GetTotalPoolLiquidity(s.Ctx))
-
-	liquiditytotal, err := s.App.CosmwasmPoolKeeper.GetTotalLiquidity(s.Ctx)
-	s.Require().NoError(err)
-	fmt.Println("TOTAL PRE liquidity", liquiditytotal.String())
 
 	// We need to export the wasm module and reimport it after we reset the test environment
 	// This is because cosmwasmpools point to a contract address, and if we don't export this as well
@@ -100,27 +79,11 @@ func (s *PoolModuleSuite) TestInitGenesis() {
 
 func (s *PoolModuleSuite) TestExportGenesis() {
 	s.Setup()
-	//validTransmuterCodeId := uint64(1)
 
-	// acc1 := s.TestAccs[0]
-	// err := simapp.FundAccount(s.App.BankKeeper, s.Ctx, acc1, sdk.NewCoins(
-	// 	sdk.NewCoin("uosmo", sdk.NewInt(10000000000)),
-	// 	sdk.NewInt64Coin("foo", 100000),
-	// 	sdk.NewInt64Coin("bar", 100000),
-	// ))
-	// s.Require().NoError(err)
-
-	// msg := model.NewMsgCreateCosmWasmPool(validTransmuterCodeId, s.TestAccs[0], s.GetDefaultTransmuterInstantiateMsgBytes())
-	// _, err = s.App.PoolManagerKeeper.CreatePool(s.Ctx, msg)
-	// s.Require().NoError(err)
-	s.FundAcc(s.TestAccs[0], initalDefaultSupply)
-	s.PrepareCustomTransmuterPool(s.TestAccs[0], defaultDenoms)
-
-	// msg = model.NewMsgCreateCosmWasmPool(validTransmuterCodeId, s.TestAccs[0], s.GetDefaultTransmuterInstantiateMsgBytes())
-	// _, err = s.App.PoolManagerKeeper.CreatePool(s.Ctx, msg)
-	// s.Require().NoError(err)
-	s.FundAcc(s.TestAccs[0], initalDefaultSupply)
-	s.PrepareCustomTransmuterPool(s.TestAccs[0], defaultDenoms)
+	for i := 0; i < 2; i++ {
+		s.FundAcc(s.TestAccs[0], initalDefaultSupply)
+		s.PrepareCustomTransmuterPool(s.TestAccs[0], defaultDenoms)
+	}
 
 	genesis := s.App.CosmwasmPoolKeeper.ExportGenesis(s.Ctx)
 	s.Require().Len(genesis.Pools, 2)
@@ -128,19 +91,7 @@ func (s *PoolModuleSuite) TestExportGenesis() {
 
 func (s *PoolModuleSuite) TestMarshalUnmarshalGenesis() {
 	s.Setup()
-	//validTransmuterCodeId := uint64(1)
 
-	acc1 := s.TestAccs[0]
-	err := simapp.FundAccount(s.App.BankKeeper, s.Ctx, acc1, sdk.NewCoins(
-		sdk.NewCoin("uosmo", sdk.NewInt(10000000000)),
-		sdk.NewInt64Coin("foo", 100000),
-		sdk.NewInt64Coin("bar", 100000),
-	))
-	s.Require().NoError(err)
-
-	// msg := model.NewMsgCreateCosmWasmPool(validTransmuterCodeId, s.TestAccs[0], s.GetDefaultTransmuterInstantiateMsgBytes())
-	// _, err = s.App.PoolManagerKeeper.CreatePool(s.Ctx, msg)
-	// s.Require().NoError(err)
 	s.FundAcc(s.TestAccs[0], initalDefaultSupply)
 	s.PrepareCustomTransmuterPool(s.TestAccs[0], defaultDenoms)
 
