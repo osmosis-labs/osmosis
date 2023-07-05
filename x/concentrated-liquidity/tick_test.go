62d3fe214ccb88b465b26bb2b05fe1649752a875
Roman <roman@osmosis.team>
2023-07-05 00:19:53 -0400

    test: parallel fuzz swap test suite (#5755)
    * updates
    
    * updates
    
    * swap to next initialized tick
    
    * parallelizm
    
    * updates
    
    * restore tests
    
    * clean up
    
    * UX updates
    
    * fix test
    
    * fix tests
    
    * add fatalError concept
    
    ---------
    
    Co-authored-by: Adam Tucker <adamleetucker@outlook.com>
    Co-authored-by: Dev Ojha <ValarDragon@users.noreply.github.com>
    Co-authored-by: Dev Ojha <dojha@berkeley.edu>
    (cherry picked from commit c99d7c2396ac3f6dab171b949821a91f634320f4)

--- x/concentrated-liquidity/tick_test.go
+++ x/concentrated-liquidity/tick_test.go
@@ -1199,7 +1199,7 @@ func (s *KeeperTestSuite) TestGetTickLiquidityNetInDirection() {
 
 			poolId:        defaultPoolId,
 			tokenIn:       ETH,
-			boundTick:     sdk.NewInt(DefaultMinTick - 1),
+			boundTick:     sdk.NewInt(DefaultMinCurrentTick - 1),
 			expectedError: true,
 		},
 		{
