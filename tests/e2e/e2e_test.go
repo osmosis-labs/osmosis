92fe433512612a3e5e02306bd7acc024cb112577
Jacob Gadikian <jacobgadikian@gmail.com>
2023-07-09 01:38:03 +0800

    use t.Parallel correctly

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -37,6 +37,8 @@ import (
 
 // TODO: Find more scalable way to do this
 func (s *IntegrationTestSuite) TestAllE2E() {
+	t := s.T()
+	t.Parallel()
 	// There appears to be an E2E quirk that requires a sleep here
 	time.Sleep(3 * time.Second)
 
