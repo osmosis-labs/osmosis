4aeaaa5ca1b6459729e6133e51690cf83f03cc45
Osmosis <105710778+osmo-bot@users.noreply.github.com>
2023-07-25 01:12:55 +0200

    auto: code-gen upgrade handler v17 (#5881)
    * [create-pull-request] automated change
    
    * remove no longer needed v16 specific e2e test
    
    ---------
    
    Co-authored-by: czarcas7ic <czarcas7ic@users.noreply.github.com>
    Co-authored-by: Adam Tucker <adamleetucker@outlook.com>

--- app/app.go
+++ app/app.go
@@ -57,6 +57,7 @@ import (
 	v14 "github.com/osmosis-labs/osmosis/v16/app/upgrades/v14"
 	v15 "github.com/osmosis-labs/osmosis/v16/app/upgrades/v15"
 	v16 "github.com/osmosis-labs/osmosis/v16/app/upgrades/v16"
+	v17 "github.com/osmosis-labs/osmosis/v16/app/upgrades/v17"
 	v3 "github.com/osmosis-labs/osmosis/v16/app/upgrades/v3"
 	v4 "github.com/osmosis-labs/osmosis/v16/app/upgrades/v4"
 	v5 "github.com/osmosis-labs/osmosis/v16/app/upgrades/v5"
@@ -102,7 +103,7 @@ var (
 
 	// _ sdksimapp.App = (*OsmosisApp)(nil)
 
-	Upgrades = []upgrades.Upgrade{v4.Upgrade, v5.Upgrade, v7.Upgrade, v9.Upgrade, v11.Upgrade, v12.Upgrade, v13.Upgrade, v14.Upgrade, v15.Upgrade, v16.Upgrade}
+	Upgrades = []upgrades.Upgrade{v4.Upgrade, v5.Upgrade, v7.Upgrade, v9.Upgrade, v11.Upgrade, v12.Upgrade, v13.Upgrade, v14.Upgrade, v15.Upgrade, v16.Upgrade, v17.Upgrade}
 	Forks    = []upgrades.Fork{v3.Fork, v6.Fork, v8.Fork, v10.Fork}
 )
 
---
+++ app/upgrades/v17/upgrades.go
@@ -0,0 +1,28 @@
+package v17
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	"github.com/osmosis-labs/osmosis/v16/app/keepers"
+	"github.com/osmosis-labs/osmosis/v16/app/upgrades"
+)
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	bpm upgrades.BaseAppParamManager,
+	keepers *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		// Run migrations before applying any other state changes.
+		// NOTE: DO NOT PUT ANY STATE CHANGES BEFORE RunMigrations().
+		migrations, err := mm.RunMigrations(ctx, configurator, fromVM)
+		if err != nil {
+			return nil, err
+		}
+
+		return migrations, nil
+	}
+}
