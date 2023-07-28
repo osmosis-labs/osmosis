c335b4e00e5942c4db17cafd5cbe91c68a23b499
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2023-07-28 18:13:13 +0200

    Fix pointer to store upgrades inside loop (#5899)
    ## What is the purpose of the change
    
    Due to how `for` loop works, if many upgrade plans are specified, the store upgrades are taken from the last one on the list, not the one matched by the upgrade name. To fix this, the copy of the object is made, before taking the pointer.
    
    ## Testing and Verifying
    
    This change is a trivial rework / code cleanup without any test coverage.
    
    ## Documentation and Release Note
    
      - [ ] Does this pull request introduce a new feature or user-facing behavior changes? - not applicable
      - [ ] Changelog entry added to `Unreleased` section of `CHANGELOG.md`? - not applicable
    
    Where is the change documented? 
      - [ ] Specification (`x/{module}/README.md`)
      - [ ] Osmosis documentation site
      - [ ] Code comments?
      - [x] N/A

--- app/app.go
+++ app/app.go
@@ -452,7 +452,8 @@ func (app *OsmosisApp) setupUpgradeStoreLoaders() {
 
 	for _, upgrade := range Upgrades {
 		if upgradeInfo.Name == upgrade.UpgradeName {
-			app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &upgrade.StoreUpgrades))
+			storeUpgrades := upgrade.StoreUpgrades
+			app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
 		}
 	}
 }
