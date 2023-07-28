e78d4a8f9de56666b1e99b5b95c5973777d7c85d
Adam Tucker <adamleetucker@outlook.com>
2023-07-28 14:27:38 -0500

    change to tokenoutdenom

--- x/concentrated-liquidity/swaps.go
+++ x/concentrated-liquidity/swaps.go
@@ -848,7 +848,7 @@ func (k Keeper) ComputeMaxInAmtGivenMaxTicksCrossed(
 	// Utilize the total amount of tokenOutDenom in the pool as the specified amountOut, since we want
 	// the limitation to be the tick crossing, not the amountOut.
 	balances := k.bankKeeper.GetAllBalances(ctx, p.GetAddress())
-	swapState := newSwapState(balances.AmountOf(tokenInDenom), p, swapStrategy)
+	swapState := newSwapState(balances.AmountOf(tokenOutDenom), p, swapStrategy)
 
 	nextInitTickIter := swapStrategy.InitializeNextTickIterator(cacheCtx, poolId, swapState.tick)
 	defer nextInitTickIter.Close()
