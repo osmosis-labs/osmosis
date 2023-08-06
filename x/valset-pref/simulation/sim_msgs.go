af47f40b1221bb0960c3624e0415a4f5445b8f68
Sishir Giri <sis1001@berkeley.edu>
2023-08-06 12:40:40 -0700

    enabled valset sim (#5966)

--- x/valset-pref/simulation/sim_msgs.go
+++ x/valset-pref/simulation/sim_msgs.go
@@ -73,7 +73,7 @@ func RandomMsgUnDelegateFromValSet(k valsetkeeper.Keeper, sim *osmosimtypes.SimC
 		return nil, fmt.Errorf("Validator not found")
 	}
 
-	// check if the user has delegated tokens to the valset
+	// check if the user has delegated tokens to the valset or existing delegation
 	del, found := sim.StakingKeeper().GetDelegation(ctx, delAddr, val)
 	if !found {
 		return nil, fmt.Errorf("user hasn't delegated tokens to the validator, %s", val.String())
