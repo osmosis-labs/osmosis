aae894f60f2e70bf180f54b06c4de80ff4f0f240
Sishir Giri <sis1001@berkeley.edu>
2023-08-08 18:55:42 -0700

    Fix: Flip existing twapRecords base/quote price denoms (#5939)
    * added logic
    
    * added upgrade handler test
    
    * added changelog
    
    * flip spot price too
    
    * adams suggestions
    
    * rebased
    
    * adams final suggestion
    
    * roman comments
    
    * roman suggestion
    
    * cleanup
    
    * romans suggestion
    
    * lint
    
    * misc cleanup
    
    * added test
    
    * more tests
    
    * added verbose tests
    
    * nit
    
    * romans comment for different twap request
    
    * nit
    
    * roman suggestion
    
    * adams suggestion

--- x/twap/keeper.go
+++ x/twap/keeper.go
@@ -73,7 +73,7 @@ func (k Keeper) InitGenesis(ctx sdk.Context, genState *types.GenesisState) {
 func (k Keeper) ExportGenesis(ctx sdk.Context) *types.GenesisState {
 	// These are ordered in increasing order, guaranteed by the iterator
 	// that is prefixed by time.
-	twapRecords, err := k.getAllHistoricalTimeIndexedTWAPs(ctx)
+	twapRecords, err := k.GetAllHistoricalTimeIndexedTWAPs(ctx)
 	if err != nil {
 		panic(err)
 	}
