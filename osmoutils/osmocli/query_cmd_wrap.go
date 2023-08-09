18bbd60b68168db7b309f9838c6175c18ef10d5b
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-08-09 16:48:14 +0200

    dynamically attach fields' values to use of cobra cmd

--- osmoutils/osmocli/query_cmd_wrap.go
+++ osmoutils/osmocli/query_cmd_wrap.go
@@ -12,6 +12,8 @@ import (
 	"github.com/gogo/protobuf/proto"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
+
+	"github.com/osmosis-labs/osmosis/osmoutils"
 )
 
 // global variable set on index command.
@@ -38,6 +40,8 @@ type QueryDescriptor struct {
 	ModuleName string
 	numArgs    int
 }
+
+var nonAttachableFields []string = []string{"sender", "pagination"}
 
 func QueryIndexCmd(moduleName string) *cobra.Command {
 	cmd := IndexCmd(moduleName)
@@ -89,6 +93,7 @@ func BuildQueryCli[reqP proto.Message, querier any](desc *QueryDescriptor, newQu
 		}
 	}
 
+	attachFieldsToUse[reqP](desc)
 	cmd := &cobra.Command{
 		Use:   desc.Use,
 		Short: desc.Short,
@@ -177,5 +182,19 @@ func queryLogic[querier any](desc *QueryDescriptor,
 		}
 
 		return clientCtx.PrintProto(res)
+	}
+}
+
+func attachFieldsToUse[reqP proto.Message](desc *QueryDescriptor) {
+	req := osmoutils.MakeNew[reqP]()
+	v := reflect.ValueOf(req).Type().Elem() // get underlying non-pointer struct
+	for i := 0; i < v.NumField(); i++ {
+		fn := strings.ToLower(v.Field(i).Name)
+
+		if desc.CustomFlagOverrides[fn] != "" || osmoutils.Contains(nonAttachableFields, fn) {
+			continue
+		}
+
+		desc.Use += fmt.Sprintf(" [%s]", fn)
 	}
 }
