15390e0876654400491340fda1db6a09e14ebf42
PaddyMc <paddymchale@hotmail.com>
2023-08-16 13:06:30 +0100

    chore: remove outdated tests

--- x/authenticator/genesis.go
+++ x/authenticator/genesis.go
@@ -8,7 +8,6 @@ import (
 
 // InitGenesis initializes the module's state from a provided genesis state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
-	// this line is used by starport scaffolding # genesis/module/init
 	k.SetParams(ctx, genState.Params)
 }
 
@@ -16,8 +15,6 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	genesis := types.DefaultGenesis()
 	genesis.Params = k.GetParams(ctx)
-
-	// this line is used by starport scaffolding # genesis/module/export
 
 	return genesis
 }
