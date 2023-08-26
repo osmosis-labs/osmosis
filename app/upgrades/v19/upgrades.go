4cb0230bdaad540ee07a461fa523c8dcf89f795f
Adam Tucker <adam@osmosis.team>
2023-08-25 22:52:54 -0500

    Update upgrades.go

--- app/upgrades/v19/upgrades.go
+++ app/upgrades/v19/upgrades.go
@@ -28,7 +28,7 @@ func CreateUpgradeHandler(
 		// This needs to be moved because the pool manager requires access to these denoms to determine if the taker fee should
 		// be swapped into OSMO or not. The concentrated liquidity module already requires access to the pool manager keeper,
 		// so the right move in this case is to move this parameter upwards in order to prevent circular dependencies.
-		// TODO: In v19 upgrade handler, delete this param from the concentrated liquidity params.
+		// TODO: In v20 upgrade handler, delete this param from the concentrated liquidity params.
 		currentConcentratedLiquidityParams := keepers.ConcentratedLiquidityKeeper.GetParams(ctx)
 		defaultPoolManagerParams := poolmanagertypes.DefaultParams()
 		defaultPoolManagerParams.AuthorizedQuoteDenoms = currentConcentratedLiquidityParams.AuthorizedQuoteDenoms
