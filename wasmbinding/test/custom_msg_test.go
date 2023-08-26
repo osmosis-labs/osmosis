eb0b00b6105380f8b3bb8c7458877cc8e778acb0
Jacob Gadikian <jacobgadikian@gmail.com>
2023-08-26 14:16:00 +0800

    fix lint

--- wasmbinding/test/custom_msg_test.go
+++ wasmbinding/test/custom_msg_test.go
@@ -8,11 +8,10 @@ import (
 	"github.com/CosmWasm/wasmd/x/wasm/keeper"
 	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/stretchr/testify/require"
-
 	"github.com/osmosis-labs/osmosis/v17/app"
 	"github.com/osmosis-labs/osmosis/v17/app/apptesting"
 	"github.com/osmosis-labs/osmosis/v17/wasmbinding/bindings"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCreateDenomMsg(t *testing.T) {
