a903415ac2b182eeed103fa0339470e903c6d38d
mattverse <mattpark1028@gmail.com>
2023-08-28 23:25:38 +0900

    Remove cosmwasm pool export

--- x/cosmwasmpool/genesis.go
+++ x/cosmwasmpool/genesis.go
@@ -11,40 +11,40 @@ import (
 func (k *Keeper) InitGenesis(ctx sdk.Context, gen *types.GenesisState, unpacker codectypes.AnyUnpacker) {
 	k.SetParams(ctx, gen.Params)
 
-	// Add each genesis state pool to the x/cosmwasmpool module's state
-	for _, any := range gen.Pools {
-		var pool types.CosmWasmExtension
-		err := unpacker.UnpackAny(any, &pool)
-		if err != nil {
-			panic(err)
-		}
-		k.SetPool(ctx, pool)
-	}
+	// // Add each genesis state pool to the x/cosmwasmpool module's state
+	// for _, any := range gen.Pools {
+	// 	var pool types.CosmWasmExtension
+	// 	err := unpacker.UnpackAny(any, &pool)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	k.SetPool(ctx, pool)
+	// }
 }
 
 // ExportGenesis returns the cosmwasm pool's exported genesis.
 func (k Keeper) ExportGenesis(ctx sdk.Context) *types.GenesisState {
 	params := k.GetParams(ctx)
 
-	pools, err := k.GetPools(ctx)
-	if err != nil {
-		panic(err)
-	}
-	poolAnys := []*codectypes.Any{}
-	for _, poolI := range pools {
-		cosmwasmPool, ok := poolI.(types.CosmWasmExtension)
-		if !ok {
-			panic("invalid pool type")
-		}
-		any, err := codectypes.NewAnyWithValue(cosmwasmPool)
-		if err != nil {
-			panic(err)
-		}
-		poolAnys = append(poolAnys, any)
-	}
+	// pools, err := k.GetPools(ctx)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// poolAnys := []*codectypes.Any{}
+	// for _, poolI := range pools {
+	// 	cosmwasmPool, ok := poolI.(types.CosmWasmExtension)
+	// 	if !ok {
+	// 		panic("invalid pool type")
+	// 	}
+	// 	any, err := codectypes.NewAnyWithValue(cosmwasmPool)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	poolAnys = append(poolAnys, any)
+	// }
 
 	return &types.GenesisState{
 		Params: params,
-		Pools:  poolAnys,
+		// Pools:  poolAnys,
 	}
 }
