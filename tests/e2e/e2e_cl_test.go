6920c4e404d717e22068fac819756fe0040a0d3d
Nicolas Lara <nicolaslara@gmail.com>
2023-09-06 16:08:48 +0200

    better name

--- tests/e2e/e2e_cl_test.go
+++ tests/e2e/e2e_cl_test.go
@@ -27,7 +27,7 @@ import (
 // Note: do not use chain B in this test as it has taker fee set.
 // This TWAP test depends on specific values that might be affected
 // by the taker fee.
-func (s *IntegrationTestSuite) CreateConcentratedLiquidityPoolVoting_And_TWAPTest() {
+func (s *IntegrationTestSuite) CreateConcentratedLiquidityPoolVoting_And_TWAP_Test() {
 	chainA, chainANode := s.getChainACfgs()
 
 	poolId, err := chainA.SubmitCreateConcentratedPoolProposal(chainANode)
