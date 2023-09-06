8319ecc93fb52382e592677cc6c0437496c5e41e
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-09-06 22:47:55 +0300

    use new truncators in CalculatePriceToTick

--- x/concentrated-liquidity/math/tick.go
+++ x/concentrated-liquidity/math/tick.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/osmosis-labs/osmosis/osmomath"
 	"github.com/osmosis-labs/osmosis/v19/x/concentrated-liquidity/types"
 )
@@ -199,10 +201,9 @@ func CalculatePriceToTick(price osmomath.BigDec) (tickIndex int64, err error) {
 	// N.B. this exists to maintain backwards compatibility with
 	// the old version of the function that operated on decimal with precision of 18.
 	if price.GTE(types.MinSpotPriceBigDec) {
-		// TODO: implement efficient big decimal truncation.
 		// It is acceptable to truncate price as the minimum we support is
 		// 10**-12 which is above the smallest value of sdk.Dec.
-		price = osmomath.BigDecFromDec(price.Dec())
+		price.ChopPrecisionMut(sdk.Precision)
 	}
 
 	// The approach here is to try determine which "geometric spacing" are we in.
