f88d122ab5cee36ce34ebb278bd0b788a081e2bc
Nicolas Lara <nicolaslara@gmail.com>
2023-09-07 12:16:49 +0200

    added post habdler

--- app/posthandler.go
+++ app/posthandler.go
@@ -2,11 +2,18 @@ package app
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authenticators "github.com/osmosis-labs/osmosis/v19/x/authenticator/keeper"
+	authpost "github.com/osmosis-labs/osmosis/v19/x/authenticator/post"
 
 	protorevkeeper "github.com/osmosis-labs/osmosis/v19/x/protorev/keeper"
 )
 
-func NewPostHandler(protoRevKeeper *protorevkeeper.Keeper) sdk.AnteHandler {
-	protoRevDecorator := protorevkeeper.NewProtoRevDecorator(*protoRevKeeper)
-	return sdk.ChainAnteDecorators(protoRevDecorator)
+func NewPostHandler(
+	protoRevKeeper *protorevkeeper.Keeper,
+	authenticatorKeeper *authenticators.Keeper,
+) sdk.AnteHandler {
+	return sdk.ChainAnteDecorators(
+		authpost.NewAuthenticatorDecorator(authenticatorKeeper),
+		protorevkeeper.NewProtoRevDecorator(*protoRevKeeper),
+	)
 }
