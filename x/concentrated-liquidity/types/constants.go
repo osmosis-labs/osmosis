ae09d3ae9bc8ebf0e7df1443258b1a8b39ffc63f
Roman <roman@osmosis.team>
2023-09-11 12:23:58 -0400

    remove unused constant

--- x/concentrated-liquidity/types/constants.go
+++ x/concentrated-liquidity/types/constants.go
@@ -41,7 +41,6 @@ var (
 	MinSqrtPrice       = osmomath.MustMonotonicSqrt(MinSpotPrice)
 	MaxSqrtPriceBigDec = osmomath.BigDecFromDec(MaxSqrtPrice)
 	MinSqrtPriceBigDec = osmomath.BigDecFromDec(MinSqrtPrice)
-	MinSqrtPriceV2     = osmomath.MustMonotonicSqrtBigDec(MinSpotPriceV2)
 
 	// Supported uptimes preset to 1 ns, 1 min, 1 hr, 1D, 1W, 2W
 	SupportedUptimes        = []time.Duration{time.Nanosecond, time.Minute, time.Hour, time.Hour * 24, time.Hour * 24 * 7, time.Hour * 24 * 7 * 2}
