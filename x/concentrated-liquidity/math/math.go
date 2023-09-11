f149a903967ffd2094744d86f2b7eb96cf7f84ae
Roman <roman@osmosis.team>
2023-09-10 20:02:21 -0400

    fix rounding direction

--- x/concentrated-liquidity/math/math.go
+++ x/concentrated-liquidity/math/math.go
@@ -66,13 +66,14 @@ func CalcAmount0Delta(liq, sqrtPriceA, sqrtPriceB osmomath.BigDec, roundUp bool)
 	// additionally, without rounding, there exists cases where the swapState.amountSpecifiedRemaining.IsPositive() for loop within
 	// the CalcOut/In functions never actually reach zero due to dust that would have never gotten counted towards the amount (numbers after the 10^6 place)
 	if roundUp {
-		// Note that we do MulTruncate so that the denominator is smaller as this is
+		// Note that we do MulRoundUp so that the numerator is larger as this is
 		// the case where we want to round up to favor the pool.
+		// For the same reasons, QuoRoundUp to round up at precision end after division.
 		// Examples include:
 		// - calculating amountIn during swap
 		// - adding liquidity (request user to provide more tokens in in favor of the pool)
 		// The denominator is truncated to get a higher final amount.
-		return liq.MulRoundUp(diff).QuoTruncate(sqrtPriceA).QuoTruncate(sqrtPriceB).Ceil()
+		return liq.MulRoundUp(diff).QuoRoundUp(sqrtPriceA).QuoRoundUp(sqrtPriceB).Ceil()
 	}
 	// These are truncated at precision end to round in favor of the pool when:
 	// - calculating amount out during swap
