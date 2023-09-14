46c8d2f5da70f8af3ca10d12e82f731a40b8431c
PaddyMc <paddymchale@hotmail.com>
2023-09-14 21:11:19 +0100

    chore: remove rest endpoints

--- x/lockup/client/rest/rest.go
+++ x/lockup/client/rest/rest.go
@@ -4,9 +4,6 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	clientrest "github.com/cosmos/cosmos-sdk/client/rest"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/rest"
 )
 
 // REST Variable names
@@ -21,15 +18,6 @@ const (
 
 // RegisterRoutes register query and tx rest routes.
 func RegisterRoutes(clientCtx client.Context, rtr *mux.Router) {
-	r := clientrest.WithHTTPDeprecationHeaders(rtr)
-	registerQueryRoutes(clientCtx, r)
-	registerTxHandlers(clientCtx, r)
-}
-
-// LockTokensReq defines the properties of a MsgLockTokens request.
-type LockTokensReq struct {
-	BaseReq  rest.BaseReq   `json:"base_req" yaml:"base_req"`
-	Owner    sdk.AccAddress `json:"owner,omitempty" yaml:"owner"`
-	Duration string         `json:"duration,omitempty" yaml:"duration"`
-	Coins    sdk.Coins      `json:"coins" yaml:"coins"`
+	registerQueryRoutes(clientCtx, rtr)
+	registerTxHandlers(clientCtx, rtr)
 }
