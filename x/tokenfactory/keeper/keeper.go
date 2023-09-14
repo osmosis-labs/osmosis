e998f42fc81c239b9847d1131a31aa740e1b076f
PaddyMc <paddymchale@hotmail.com>
2023-09-14 23:16:40 +0100

    chore: skd.StoreKey changed storetypes.StoreKey in tokenfactory

--- x/tokenfactory/keeper/keeper.go
+++ x/tokenfactory/keeper/keeper.go
@@ -6,6 +6,7 @@ import (
 	"github.com/cometbft/cometbft/libs/log"
 
 	"github.com/cosmos/cosmos-sdk/store/prefix"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/osmosis-labs/osmosis/v19/x/tokenfactory/types"
@@ -15,7 +16,7 @@ import (
 
 type (
 	Keeper struct {
-		storeKey sdk.StoreKey
+		storeKey storetypes.StoreKey
 
 		paramSpace paramtypes.Subspace
 
@@ -29,7 +30,7 @@ type (
 
 // NewKeeper returns a new instance of the x/tokenfactory keeper
 func NewKeeper(
-	storeKey sdk.StoreKey,
+	storeKey storetypes.StoreKey,
 	paramSpace paramtypes.Subspace,
 	accountKeeper types.AccountKeeper,
 	bankKeeper types.BankKeeper,
