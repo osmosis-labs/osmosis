730ee0092d1bb3d4e957ed5797c8aa9e01a113b1
Nicolas Lara <nicolaslara@gmail.com>
2023-09-23 10:52:28 +0200

    [AccountAbstraction] selecting the authenticator with tx info (#6498)
    * initial implementation of selecting the authenticator with tx info
    
    * better tests

--- x/authenticator/ante/authenticator_test.go
+++ x/authenticator/ante/authenticator_test.go
@@ -3,6 +3,9 @@ package ante_test
 import (
 	"encoding/hex"
 	"fmt"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	authtx "github.com/cosmos/cosmos-sdk/x/auth/tx"
+	authenticatortypes "github.com/osmosis-labs/osmosis/v19/x/authenticator/types"
 	"math/rand"
 	"testing"
 	"time"
@@ -95,26 +98,16 @@ func (s *AutherticatorAnteSuite) TestSignatureVerificationNoAuthenticatorInStore
 	}
 	feeCoins := sdk.Coins{sdk.NewInt64Coin(osmoToken, 2500)}
 
-	tx, _ := GenTx(
-		s.EncodingConfig.TxConfig,
-		[]sdk.Msg{
-			testMsg1,
-			testMsg2,
-		},
-		feeCoins,
-		300000,
-		"",
-		[]uint64{0, 0},
-		[]uint64{0, 0},
-		[]cryptotypes.PrivKey{
-			s.TestPrivKeys[0],
-			s.TestPrivKeys[1],
-		},
-		[]cryptotypes.PrivKey{
-			s.TestPrivKeys[0],
-			s.TestPrivKeys[1],
-		},
-	)
+	tx, _ := GenTx(s.EncodingConfig.TxConfig, []sdk.Msg{
+		testMsg1,
+		testMsg2,
+	}, feeCoins, 300000, "", []uint64{0, 0}, []uint64{0, 0}, []cryptotypes.PrivKey{
+		s.TestPrivKeys[0],
+		s.TestPrivKeys[1],
+	}, []cryptotypes.PrivKey{
+		s.TestPrivKeys[0],
+		s.TestPrivKeys[1],
+	}, []int32{})
 
 	anteHandler := sdk.ChainAnteDecorators(s.AuthenticatorDecorator)
 	_, err := anteHandler(s.Ctx, tx, false)
@@ -149,26 +142,16 @@ func (s *AutherticatorAnteSuite) TestSignatureVerificationWithAuthenticatorInSto
 	)
 	s.Require().NoError(err)
 
-	tx, _ := GenTx(
-		s.EncodingConfig.TxConfig,
-		[]sdk.Msg{
-			testMsg1,
-			testMsg2,
-		},
-		feeCoins,
-		300000,
-		"",
-		[]uint64{0, 0},
-		[]uint64{0, 0},
-		[]cryptotypes.PrivKey{
-			s.TestPrivKeys[0],
-			s.TestPrivKeys[1],
-		},
-		[]cryptotypes.PrivKey{
-			s.TestPrivKeys[0],
-			s.TestPrivKeys[1],
-		},
-	)
+	tx, _ := GenTx(s.EncodingConfig.TxConfig, []sdk.Msg{
+		testMsg1,
+		testMsg2,
+	}, feeCoins, 300000, "", []uint64{0, 0}, []uint64{0, 0}, []cryptotypes.PrivKey{
+		s.TestPrivKeys[0],
+		s.TestPrivKeys[1],
+	}, []cryptotypes.PrivKey{
+		s.TestPrivKeys[0],
+		s.TestPrivKeys[1],
+	}, []int32{})
 
 	anteHandler := sdk.ChainAnteDecorators(s.AuthenticatorDecorator)
 	_, err = anteHandler(s.Ctx, tx, false)
@@ -220,26 +203,16 @@ func (s *AutherticatorAnteSuite) TestSignatureVerificationOutOfGas() {
 	)
 	s.Require().NoError(err)
 
-	tx, _ := GenTx(
-		s.EncodingConfig.TxConfig,
-		[]sdk.Msg{
-			testMsg1,
-			testMsg2,
-		},
-		feeCoins,
-		300000,
-		"",
-		[]uint64{0, 0},
-		[]uint64{0, 0},
-		[]cryptotypes.PrivKey{
-			s.TestPrivKeys[0],
-			s.TestPrivKeys[1],
-		},
-		[]cryptotypes.PrivKey{
-			s.TestPrivKeys[1],
-			s.TestPrivKeys[1],
-		},
-	)
+	tx, _ := GenTx(s.EncodingConfig.TxConfig, []sdk.Msg{
+		testMsg1,
+		testMsg2,
+	}, feeCoins, 300000, "", []uint64{0, 0}, []uint64{0, 0}, []cryptotypes.PrivKey{
+		s.TestPrivKeys[0],
+		s.TestPrivKeys[1],
+	}, []cryptotypes.PrivKey{
+		s.TestPrivKeys[1],
+		s.TestPrivKeys[1],
+	}, []int32{})
 
 	anteHandler := sdk.ChainAnteDecorators(s.AuthenticatorDecorator)
 	_, err = anteHandler(s.Ctx, tx, false)
@@ -259,10 +232,9 @@ func GenTx(
 	feeAmt sdk.Coins,
 	gas uint64,
 	chainID string,
-	accNums,
-	accSeqs []uint64,
-	signers []cryptotypes.PrivKey,
-	signatures []cryptotypes.PrivKey,
+	accNums, accSeqs []uint64,
+	signers, signatures []cryptotypes.PrivKey,
+	selectedAuthenticators []int32,
 ) (sdk.Tx, error) {
 	sigs := make([]signing.SignatureV2, len(signers))
 
@@ -283,7 +255,21 @@ func GenTx(
 		}
 	}
 
-	txBuilder := gen.NewTxBuilder()
+	baseTxBuilder := gen.NewTxBuilder()
+
+	txBuilder, ok := baseTxBuilder.(authtx.ExtensionOptionsTxBuilder)
+	if !ok {
+		return nil, fmt.Errorf("expected authtx.ExtensionOptionsTxBuilder, got %T", baseTxBuilder)
+	}
+	if len(selectedAuthenticators) > 0 {
+		value, err := types.NewAnyWithValue(&authenticatortypes.TxExtension{
+			SelectedAuthenticators: selectedAuthenticators,
+		})
+		if err != nil {
+			return nil, err
+		}
+		txBuilder.SetNonCriticalExtensionOptions(value)
+	}
 
 	err := txBuilder.SetMsgs(msgs...)
 	if err != nil {
@@ -321,4 +307,83 @@ func GenTx(
 	}
 
 	return txBuilder.GetTx(), nil
+}
+
+func (s *AutherticatorAnteSuite) TestSpecificAuthenticator() {
+	osmoToken := "osmo"
+	coins := sdk.Coins{sdk.NewInt64Coin(osmoToken, 2500)}
+
+	// Create a test messages for signing
+	testMsg1 := &banktypes.MsgSend{
+		FromAddress: sdk.MustBech32ifyAddressBytes(osmoToken, s.TestAccAddress[1]),
+		ToAddress:   sdk.MustBech32ifyAddressBytes(osmoToken, s.TestAccAddress[1]),
+		Amount:      coins,
+	}
+	feeCoins := sdk.Coins{sdk.NewInt64Coin(osmoToken, 2500)}
+
+	err := s.OsmosisApp.AuthenticatorKeeper.AddAuthenticator(
+		s.Ctx,
+		s.TestAccAddress[1],
+		"SignatureVerificationAuthenticator",
+		s.TestPrivKeys[0].PubKey().Bytes(),
+	)
+	s.Require().NoError(err)
+
+	err = s.OsmosisApp.AuthenticatorKeeper.AddAuthenticator(
+		s.Ctx,
+		s.TestAccAddress[1],
+		"SignatureVerificationAuthenticator",
+		s.TestPrivKeys[1].PubKey().Bytes(),
+	)
+	s.Require().NoError(err)
+
+	testCases := []struct {
+		name                  string
+		signKey               cryptotypes.PrivKey
+		selectedAuthenticator []int32
+		shouldPass            bool
+		checks                int
+	}{
+		{"Correct authenticator 0", s.TestPrivKeys[0], []int32{0}, true, 1},
+		{"Correct authenticator 1", s.TestPrivKeys[1], []int32{1}, true, 1},
+		{"Incorrect authenticator", s.TestPrivKeys[0], []int32{1}, false, 1},
+		{"Incorrect authenticator", s.TestPrivKeys[1], []int32{0}, false, 1},
+		{"Not Specified for 0", s.TestPrivKeys[0], []int32{}, true, 1},
+		{"Not Specified for 1", s.TestPrivKeys[1], []int32{}, true, 2},
+		{"Bad selection", s.TestPrivKeys[0], []int32{3}, false, 0},
+	}
+
+	approachingGasPerSig := 8000 // Each signature consumes at least this amount (but not much more)
+
+	for _, tc := range testCases {
+		s.Run(tc.name, func() {
+			tx, _ := GenTx(s.EncodingConfig.TxConfig, []sdk.Msg{
+				testMsg1,
+			}, feeCoins, 300000, "", []uint64{0}, []uint64{0}, []cryptotypes.PrivKey{
+				s.TestPrivKeys[1],
+			}, []cryptotypes.PrivKey{
+				tc.signKey,
+			},
+				tc.selectedAuthenticator,
+			)
+
+			anteHandler := sdk.ChainAnteDecorators(s.AuthenticatorDecorator)
+			res, err := anteHandler(s.Ctx.WithGasMeter(sdk.NewGasMeter(300000)), tx, false)
+
+			if tc.shouldPass {
+				s.Require().NoError(err, "Expected to pass but got error")
+			} else {
+				s.Require().Error(err, "Expected to fail but got no error")
+			}
+
+			// ensure only the right amount of sigs have been checked
+			if tc.checks > 0 {
+				s.Require().Greater(res.GasMeter().GasConsumed(), uint64(tc.checks*approachingGasPerSig))
+				s.Require().Less(res.GasMeter().GasConsumed(), uint64((tc.checks+1)*approachingGasPerSig))
+			} else {
+				s.Require().Less(res.GasMeter().GasConsumed(), uint64(2_000))
+			}
+
+		})
+	}
 }
--- x/authenticator/types/codec.go
+++ x/authenticator/types/codec.go
@@ -6,10 +6,18 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
+type AuthenticatorTxOptions interface {
+	GetSelectedAuthenticators() []int32
+}
+
 func RegisterCodec(cdc *codec.LegacyAmino) {
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
+	registry.RegisterImplementations(
+		(*AuthenticatorTxOptions)(nil),
+		&TxExtension{},
+	)
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
 
