39d843771b634f632d07909e20aac3e8d470fd2a
PaddyMc <paddymchale@hotmail.com>
2023-09-25 16:26:58 +0100

    fix: incorrect signature and signer parsing check

--- x/authenticator/authenticator/util.go
+++ x/authenticator/authenticator/util.go
@@ -66,11 +66,6 @@ func GetSignersAndSignatures(
 				}
 				signerToSignature[signerStr] = suppliedSignatures[sigIndex]
 				sigIndex++
-			} else {
-				// ensure that number of signers and signatures are the same
-				// this also ensures that each message has a signer
-				// NOTE: This is the same behavior that currently exists in the node
-				return nil, nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "invalid number of signers")
 			}
 
 			// If dealing with a specific message, capture its signers.
