8cecd4b99afb3914aaff986d5bc96b8e9d3903eb
Nicolas Lara <nicolaslara@gmail.com>
2023-09-26 15:43:45 +0200

    lint

--- x/authenticator/authenticator/spend_limits.go
+++ x/authenticator/authenticator/spend_limits.go
@@ -3,9 +3,10 @@ package authenticator
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/cosmos/cosmos-sdk/store/prefix"
 	"math/big"
 	"time"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 
 	"github.com/osmosis-labs/osmosis/v19/x/authenticator/iface"
 
