81c2b89621f4f757ba7e540b3b48db78cc4a9ba7
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2023-10-02 12:01:45 +0300

    msgServer

--- x/concentrated-liquidity/client/grpc/grpc_query.go
+++ x/concentrated-liquidity/client/grpc/grpc_query.go
@@ -171,3 +171,13 @@ func (q Querier) CFMMPoolIdLinkFromConcentratedPoolId(grpcCtx context.Context,
 	return q.Q.CFMMPoolIdLinkFromConcentratedPoolId(ctx, *req)
 }
 
+func (q Querier) TickRangeUnderlyingAssets(grpcCtx context.Context,
+	req *queryproto.TickRangeUnderlyingAssetsRequest,
+) (*queryproto.TickRangeUnderlyingAssetsResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+	ctx := sdk.UnwrapSDKContext(grpcCtx)
+	return q.Q.TickRangeUnderlyingAssets(ctx, *req)
+}
+
