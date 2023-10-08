36ae0bf41ed6a822026f859d31bca47d65bdeb3c
Adam Tucker <adamleetucker@outlook.com>
2023-10-07 21:24:51 -0600

    fix backport

--- x/pool-incentives/keeper/distr_test.go
+++ x/pool-incentives/keeper/distr_test.go
@@ -6,6 +6,8 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+var defaultCoins = sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(10000)))
+
 func (s *KeeperTestSuite) TestAllocateAsset() {
 	tests := []struct {
 		name                   string
