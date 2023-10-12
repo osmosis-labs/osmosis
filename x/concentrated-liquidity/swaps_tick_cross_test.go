89f28660bb7d7fa7dfa6afb79b98cb490bf0bcb4
Adam Tucker <adamleetucker@outlook.com>
2023-10-12 15:28:54 -0600

    Revert "test(CL): swap to new min tick and back (#6327)"
    This reverts commit e2ef44fb0bbbdcd0c52216cfe01550af9afc0515.

--- x/concentrated-liquidity/swaps_tick_cross_test.go
+++ x/concentrated-liquidity/swaps_tick_cross_test.go
@@ -381,7 +381,7 @@ func (s *KeeperTestSuite) computeSwapAmountsInGivenOut(poolId uint64, curSqrtPri
 		var isWithinDesiredBucketAfterSwap bool
 		if isZeroForOne {
 			// Round up so that we cross the tick by default.
-			curAmountOut := math.CalcAmount1Delta(currentLiquidity, curSqrtPrice, nextInitTickSqrtPrice, true)
+			curAmountOut := math.CalcAmount1Delta(currentLiquidity, curSqrtPrice, nextInitTickSqrtPrice, false)
 
 			amountOut = amountOut.Add(curAmountOut.Dec())
 
