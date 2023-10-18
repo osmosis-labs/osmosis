031a07ffa643bbc2019a98616bf7a0c337045d5e
mattverse <mattpark1028@gmail.com>
2023-10-18 16:41:14 +0900

    Edit it to look for echo

--- scripts/validate-makefile.sh
+++ scripts/validate-makefile.sh
@@ -5,12 +5,11 @@ extract_commands() {
     grep -oP '^[a-zA-Z_-]+:' $1 | sed 's/://'
 }
 
-# Function to check if command is documented in help
+# Function to check if command is mentioned in any @echo line in the Makefile
 is_documented() {
     local cmd=$1
-    local help_cmd="$2"
-    local help_output=$(make -s $help_cmd) # This captures the output of the help command
-    echo "$help_output" | grep -q "$cmd"
+    # This looks for any line starting with @echo and checks if it contains the command.
+    grep '^	@echo' Makefile | grep -q "$cmd"
 }
 
 # Get the Makefile from the main branch
@@ -24,11 +23,11 @@ main_cmds=$(extract_commands Makefile.main)
 # Check for commands that are in the current branch but not in main
 new_cmds=$(comm -23 <(echo "$current_cmds" | sort) <(echo "$main_cmds" | sort))
 
-# Check each new command to see if it's documented
+# Check each new command to see if it's mentioned in any @echo line
 error=0
 for cmd in $new_cmds; do
-    if ! is_documented $cmd "help"; then # replace "help" with your actual help command if it's different
-        echo "Error: New command '$cmd' added without documentation under the help command."
+    if ! is_documented $cmd; then
+        echo "Error: New command '$cmd' added without being mentioned in any @echo line."
         error=1
     fi
 done
