fd6c76034d700620ad81226da1a9f29713d79fdb
mattverse <mattpark1028@gmail.com>
2023-10-18 16:17:32 +0900

    Compare with only that of diff

--- scripts/validate-makefile.sh
+++ scripts/validate-makefile.sh
@@ -3,7 +3,6 @@
 # Function to extract commands from Makefile
 extract_commands() {
     grep -oP '^[a-zA-Z_-]+:' $1 | sed 's/://'
-    # grep -oP '^[a-zA-Z_-]+:' $1 | sed 's/://'
 }
 
 # Function to check if command has help
@@ -13,16 +12,16 @@ has_help() {
     grep -q "^[[:space:]]*$help_cmd" $2
 }
 
-# Compare Makefile and Makefile.prev
-current_cmds=$(extract_commands Makefile)
-previous_cmds=$(extract_commands Makefile.prev)
+# Extract commands from the current branch's Makefile and the main branch's Makefile
+current_cmds=$(extract_commands "$1")  # First argument is expected to be the current branch's Makefile
+main_cmds=$(extract_commands "$2")     # Second argument is expected to be the main branch's Makefile
 
 # Check for new commands and if they have corresponding help
 error=0
 for cmd in $current_cmds; do
-    if ! grep -q "^$cmd$" <<< "$previous_cmds"; then
+    if ! grep -q "^$cmd$" <<< "$main_cmds"; then
         # This is a new command, check for help
-        if ! has_help $cmd Makefile; then
+        if ! has_help $cmd "$1"; then  # Check the current branch's Makefile for the help command
             echo "Error: New command '$cmd' added without corresponding help command."
             error=1
         fi
