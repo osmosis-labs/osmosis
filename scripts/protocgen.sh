44d768c4d7b65beccb787d4d4bfd8decad2c4ea8
Michael Tsitrin <michael@dymension.xyz>
2023-10-22 20:34:28 +0300

    generated proto

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -3,12 +3,13 @@
 set -eo pipefail
 
 # get protoc executions
-go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
+# go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
 
 # get cosmos sdk from github
-go get github.com/cosmos/cosmos-sdk 2>/dev/null
+# go get github.com/cosmos/cosmos-sdk 2>/dev/null
 
 echo "Generating gogo proto code"
+echo "---------------------------"
 cd proto
 proto_dirs=$(find ./osmosis -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
