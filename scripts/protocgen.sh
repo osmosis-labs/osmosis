986e53efb64c880829382dc3505582de41e66f6c
Adam Tucker <adamleetucker@outlook.com>
2023-10-26 14:23:37 -0600

    update script

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,14 +2,14 @@
 
 set -eo pipefail
 
-# go get github.com/cosmos/gogoproto 2>/dev/null
-# go get github.com/cosmos/cosmos-sdk 2>/dev/null
+go get github.com/cosmos/gogoproto 2>/dev/null
+go get github.com/cosmos/cosmos-sdk 2>/dev/null
 
 echo "Generating gogo proto code"
 ls -a
 cd proto
 ls -a
-echo "buf version $(buf --version)"
+
 proto_dirs=$(find ./osmosis -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
