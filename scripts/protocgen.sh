106b43d5d659f5f34d75af1e28bd567fec176aba
Adam Tucker <adamleetucker@outlook.com>
2023-10-26 14:35:25 -0600

    specify output

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -15,7 +15,7 @@ for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep go_package $file &>/dev/null; then
       echo "Generating gogo proto code for $file"
-      buf generate --template buf.gen.gogo.yaml $file
+      buf generate $file --template buf.gen.gogo.yaml -o ./
     fi
   done
 done
@@ -26,9 +26,9 @@ ls -a
 # move proto files to the right places
 #
 # Note: Proto files are suffixed with the current binary version.
-cp -r github.com/osmosis-labs/osmosis/v20/* ./
-cp -r github.com/osmosis-labs/osmosis/osmoutils ./
-cp -r github.com/osmosis-labs/osmosis/x/epochs ./x/
+# cp -r github.com/osmosis-labs/osmosis/v20/* ./
+# cp -r github.com/osmosis-labs/osmosis/osmoutils ./
+# cp -r github.com/osmosis-labs/osmosis/x/epochs ./x/
 #rm -rf github.com
 
 #go mod tidy
