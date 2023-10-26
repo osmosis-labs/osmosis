d7fbf9bf1e9b454e9b94744c7e0fec0a29bbafd9
Adam Tucker <adamleetucker@outlook.com>
2023-10-26 15:43:35 -0600

    FINALLY

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -3,21 +3,17 @@
 set -eo pipefail
 
 echo "Generating gogo proto code"
-ls -a
 cd proto
-ls -a
 
 proto_dirs=$(find ./osmosis -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep go_package $file &>/dev/null; then
       echo "Generating gogo proto code for $file"
-      buf generate $file --template buf.gen.gogo.yaml -v --debug
+      buf generate $file --template buf.gen.gogo.yaml
     fi
   done
 done
-ls -a
-cd ..
 ls -a
 
 # move proto files to the right places
@@ -28,7 +24,7 @@ cp -r github.com/osmosis-labs/osmosis/osmoutils ./
 cp -r github.com/osmosis-labs/osmosis/x/epochs ./x/
 rm -rf github.com
 
-#go mod tidy
+go mod tidy
 
 # TODO: Uncomment once ORM/Pulsar support is needed.
 #
