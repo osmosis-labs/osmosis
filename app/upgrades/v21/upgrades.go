4d829f0b48e6f97ecf823fa49106f4e3d48a186c
Adam Tucker <adamleetucker@outlook.com>
2023-10-27 15:19:50 -0600

    finish upgrade handler

--- app/upgrades/v21/upgrades.go
+++ app/upgrades/v21/upgrades.go
@@ -1,8 +1,6 @@
 package v21
 
 import (
-	"fmt"
-
 	"cosmossdk.io/math"
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	buildertypes "github.com/skip-mev/pob/x/builder/types"
@@ -17,8 +15,17 @@ import (
 
 	"github.com/osmosis-labs/osmosis/v20/app/keepers"
 	"github.com/osmosis-labs/osmosis/v20/app/upgrades"
+	concentratedliquiditytypes "github.com/osmosis-labs/osmosis/v20/x/concentrated-liquidity/types"
+	cosmwasmpooltypes "github.com/osmosis-labs/osmosis/v20/x/cosmwasmpool/types"
+	gammtypes "github.com/osmosis-labs/osmosis/v20/x/gamm/types"
+	incentivestypes "github.com/osmosis-labs/osmosis/v20/x/incentives/types"
+	lockuptypes "github.com/osmosis-labs/osmosis/v20/x/lockup/types"
+	poolincentivestypes "github.com/osmosis-labs/osmosis/v20/x/pool-incentives/types"
+	poolmanagertypes "github.com/osmosis-labs/osmosis/v20/x/poolmanager/types"
 	protorevtypes "github.com/osmosis-labs/osmosis/v20/x/protorev/types"
 	superfluidtypes "github.com/osmosis-labs/osmosis/v20/x/superfluid/types"
+	tokenfactorytypes "github.com/osmosis-labs/osmosis/v20/x/tokenfactory/types"
+	twaptypes "github.com/osmosis-labs/osmosis/v20/x/twap/types"
 
 	// SDK v47 modules
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
@@ -41,14 +48,14 @@ func CreateUpgradeHandler(
 	keepers *keepers.AppKeepers,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		// UNFORKINGNOTE: Add the remaining modules to this
+		// UNFORKINGNOTE: If we don't manually set this to 1, the gov modules doesn't go through its necessary migrations to version 4
 		fromVM[govtypes.ModuleName] = 1
 		baseAppLegacySS := keepers.ParamsKeeper.Subspace(baseapp.Paramspace).WithKeyTable(paramstypes.ConsensusParamsKeyTable())
+
 		// https://github.com/cosmos/cosmos-sdk/pull/12363/files
 		// Set param key table for params module migration
 		for _, subspace := range keepers.ParamsKeeper.GetSubspaces() {
 			subspace := subspace
-			fmt.Printf("subspace: %+v\n", subspace.Name())
 
 			var keyTable paramstypes.KeyTable
 			switch subspace.Name() {
@@ -92,43 +99,32 @@ func CreateUpgradeHandler(
 				keyTable = protorevtypes.ParamKeyTable() //nolint:staticcheck
 			case superfluidtypes.ModuleName:
 				keyTable = superfluidtypes.ParamKeyTable() //nolint:staticcheck
+			// downtime doesn't have params
+			case gammtypes.ModuleName:
+				keyTable = gammtypes.ParamKeyTable() //nolint:staticcheck
+			case twaptypes.ModuleName:
+				keyTable = twaptypes.ParamKeyTable() //nolint:staticcheck
+			case lockuptypes.ModuleName:
+				keyTable = lockuptypes.ParamKeyTable() //nolint:staticcheck
+			// epochs doesn't have params (it really should imo)
+			case incentivestypes.ModuleName:
+				keyTable = incentivestypes.ParamKeyTable() //nolint:staticcheck
+			case poolincentivestypes.ModuleName:
+				keyTable = poolincentivestypes.ParamKeyTable() //nolint:staticcheck
+			// txfees doesn't have params
+			case tokenfactorytypes.ModuleName:
+				keyTable = tokenfactorytypes.ParamKeyTable() //nolint:staticcheck
+			case poolmanagertypes.ModuleName:
+				keyTable = poolmanagertypes.ParamKeyTable() //nolint:staticcheck
+			// valsetpref doesn't have params
+			case concentratedliquiditytypes.ModuleName:
+				keyTable = concentratedliquiditytypes.ParamKeyTable() //nolint:staticcheck
+			case cosmwasmpooltypes.ModuleName:
+				keyTable = cosmwasmpooltypes.ParamKeyTable() //nolint:staticcheck
 
 			default:
 				continue
 			}
-
-			// 			// "Normal" keepers
-			// AccountKeeper                *authkeeper.AccountKeeper
-			// BankKeeper                   bankkeeper.BaseKeeper
-			// AuthzKeeper                  *authzkeeper.Keeper
-			// StakingKeeper                *stakingkeeper.Keeper
-			// DistrKeeper                  *distrkeeper.Keeper
-			// DowntimeKeeper               *downtimedetector.Keeper
-			// SlashingKeeper               *slashingkeeper.Keeper
-			// IBCKeeper                    *ibckeeper.Keeper
-			// IBCHooksKeeper               *ibchookskeeper.Keeper
-			// ICAHostKeeper                *icahostkeeper.Keeper
-			// ICQKeeper                    *icqkeeper.Keeper
-			// TransferKeeper               *ibctransferkeeper.Keeper
-			// EvidenceKeeper               *evidencekeeper.Keeper
-			// GAMMKeeper                   *gammkeeper.Keeper
-			// TwapKeeper                   *twap.Keeper
-			// LockupKeeper                 *lockupkeeper.Keeper
-			// EpochsKeeper                 *epochskeeper.Keeper
-			// IncentivesKeeper             *incentiveskeeper.Keeper
-			// ProtoRevKeeper               *protorevkeeper.Keeper
-			// MintKeeper                   *mintkeeper.Keeper
-			// PoolIncentivesKeeper         *poolincentiveskeeper.Keeper
-			// TxFeesKeeper                 *txfeeskeeper.Keeper
-			// SuperfluidKeeper             *superfluidkeeper.Keeper
-			// GovKeeper                    *govkeeper.Keeper
-			// WasmKeeper                   *wasm.Keeper
-			// ContractKeeper               *wasmkeeper.PermissionedKeeper
-			// TokenFactoryKeeper           *tokenfactorykeeper.Keeper
-			// PoolManagerKeeper            *poolmanager.Keeper
-			// ValidatorSetPreferenceKeeper *valsetpref.Keeper
-			// ConcentratedLiquidityKeeper  *concentratedliquidity.Keeper
-			// CosmwasmPoolKeeper           *cosmwasmpool.Keeper
 
 			if !subspace.HasKeyTable() {
 				subspace.WithKeyTable(keyTable)
@@ -139,14 +135,12 @@ func CreateUpgradeHandler(
 		// The old params module is required to still be imported in your app.go in order to handle this migration.
 		baseapp.MigrateParams(ctx, baseAppLegacySS, &keepers.ConsensusParamsKeeper)
 
-		// Run migrations before applying any other state changes.
-		// NOTE: DO NOT PUT ANY STATE CHANGES BEFORE RunMigrations().
 		migrations, err := mm.RunMigrations(ctx, configurator, fromVM)
 		if err != nil {
 			return nil, err
 		}
 
-		// x/POB
+		// set POB params
 		pobAddr := keepers.AccountKeeper.GetModuleAddress(buildertypes.ModuleName)
 
 		builderParams := buildertypes.DefaultGenesisState().GetParams()
