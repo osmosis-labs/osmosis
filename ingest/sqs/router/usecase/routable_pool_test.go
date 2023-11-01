0319571087e19471e11825f5a8d34f7ce166e452
roman <roman@osmosis.team>
2023-11-01 10:15:11 -0400

    add support for stableswap pools

--- ingest/sqs/router/usecase/routable_pool_test.go
+++ ingest/sqs/router/usecase/routable_pool_test.go
@@ -4,33 +4,40 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	routerusecase "github.com/osmosis-labs/osmosis/v20/ingest/sqs/router/usecase"
+	poolmanagertypes "github.com/osmosis-labs/osmosis/v20/x/poolmanager/types"
 )
 
+// Test quote logic over a specific pool that is of CFMM type.
+// CFMM pools are balancert and stableswap.
 func (s *RouterTestSuite) TestCalculateTokenOutByTokenIn_CFMM() {
-
 	tests := map[string]struct {
 		tokenIn          sdk.Coin
 		tokenOutDenom    string
+		poolType         poolmanagertypes.PoolType
 		expectedTokenOut sdk.Coin
 		expectError      error
 	}{
 		"balancer pool - valid calculation": {
 			tokenIn:       sdk.NewCoin("foo", sdk.NewInt(100)),
 			tokenOutDenom: "bar",
+			poolType:      poolmanagertypes.Balancer,
 		},
-
-		// TODO: add tests for other pools once supported.
+		"stableswap pool - valid calculation": {
+			tokenIn:       sdk.NewCoin("foo", sdk.NewInt(100)),
+			tokenOutDenom: "bar",
+			poolType:      poolmanagertypes.Stableswap,
+		},
 	}
 
 	for name, tc := range tests {
 		s.Run(name, func() {
 			s.Setup()
 
-			balancerPoolID := s.PrepareBalancerPool()
-			balancerPool, err := s.App.PoolManagerKeeper.GetPool(s.Ctx, balancerPoolID)
+			poolID := s.CreatePoolFromType(tc.poolType)
+			pool, err := s.App.PoolManagerKeeper.GetPool(s.Ctx, poolID)
 			s.Require().NoError(err)
 
-			mock := &mockPool{ChainPoolModel: balancerPool}
+			mock := &mockPool{ChainPoolModel: pool}
 			routablePool := routerusecase.NewRoutablePool(mock, tc.tokenOutDenom)
 
 			tokenOut, err := routablePool.CalculateTokenOutByTokenIn(tc.tokenIn)
