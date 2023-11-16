b1251519f708af1ab17b8629ac594c60951b5195
Adam Tucker <adamleetucker@outlook.com>
2023-11-16 13:44:42 -0500

    incomplete test

--- x/poolmanager/routes_test.go
+++ x/poolmanager/routes_test.go
@@ -1,69 +1,63 @@
 package poolmanager_test
 
 import (
-	"fmt"
-	"reflect"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
-	"github.com/osmosis-labs/osmosis/v20/x/poolmanager/types"
 )
 
 // TestGetPoolModule tests that the correct pool module is returned for a given pool id.
 // Additionally, validates that the expected errors are produced when expected.
 func (s *KeeperTestSuite) TestDenomPairRoute() {
 	tests := map[string]struct {
-		poolId            uint64
-		preCreatePoolType types.PoolType
-		routesOverwrite   map[types.PoolType]types.PoolModuleI
-
-		expectedModule reflect.Type
-		expectError    error
+		expectedRoute []uint64
+		expectError   error
 	}{
-		"valid balancer pool": {
-			preCreatePoolType: types.Balancer,
-			poolId:            1,
-			expectedModule:    gammKeeperType,
+		"direct route is best": {
+			expectedRoute: []uint64{5},
+		},
+		"indirect route is best, route via eth": {
+			expectedRoute: []uint64{4, 1},
+		},
+		"indirect route is best, route via bar": {
+			expectedRoute: []uint64{3, 2},
 		},
 	}
 
-	for name, _ := range tests {
+	for name, tc := range tests {
 		s.Run(name, func() {
 			s.SetupTest()
 			poolmanagerKeeper := s.App.PoolManagerKeeper
 
-			s.PrepareConcentratedPool()
-			pool := s.PrepareConcentratedPoolWithCoins("eth", "stake")
-			pool1 := s.PrepareConcentratedPoolWithCoinsAndFullRangePosition("adam", "eth")
-			pool2 := s.PrepareConcentratedPoolWithCoinsAndFullRangePosition("adam", "eth")
-			s.PrepareBalancerPoolWithCoins(sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(2000000000000000000)), sdk.NewCoin("adam", sdk.NewInt(2000000000000000000)))...)
 			s.PrepareBalancerPool()
 
-			s.CreateFullRangePosition(pool, sdk.NewCoins(sdk.NewCoin("eth", sdk.NewInt(1000000000000000000)), sdk.NewCoin("stake", sdk.NewInt(1000000000000000000))))
-			s.CreateFullRangePosition(pool1, sdk.NewCoins(sdk.NewCoin("eth", sdk.NewInt(3000000000000000000)), sdk.NewCoin("adam", sdk.NewInt(3000000000000000000))))
-			s.CreateFullRangePosition(pool2, sdk.NewCoins(sdk.NewCoin("eth", sdk.NewInt(2000000000000000000)), sdk.NewCoin("adam", sdk.NewInt(2000000000000000000))))
+			// Create pools for indirect route
+			ethStake := s.PrepareConcentratedPoolWithCoins("eth", "stake") // pool 1
+			barStake := s.PrepareConcentratedPoolWithCoins("bar", "stake") // pool 2
+			btcBar := s.PrepareConcentratedPoolWithCoins("btc", "bar")     // pool 3
+			btcEth := s.PrepareConcentratedPoolWithCoins("btc", "eth")     // pool 4
 
-			test, err := poolmanagerKeeper.GetPoolLiquidityOfDenom(s.Ctx, 3, "eth")
+			// Create pool for direct route
+			s.PrepareBalancerPoolWithCoins(sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(2000000000000000000)), sdk.NewCoin("adam", sdk.NewInt(2000000000000000000)))...) // pool 5
+
+			// Create uosmo pairings to determine value in a base asset
+			s.PrepareBalancerPoolWithCoins(sdk.NewCoins(sdk.NewCoin("uosmo", sdk.NewInt(100000000)), sdk.NewCoin("adam", sdk.NewInt(100000000)))...)
+			s.PrepareBalancerPoolWithCoins(sdk.NewCoins(sdk.NewCoin("uosmo", sdk.NewInt(100000000000)), sdk.NewCoin("eth", sdk.NewInt(100000000)))...)
+			s.PrepareBalancerPoolWithCoins(sdk.NewCoins(sdk.NewCoin("uosmo", sdk.NewInt(100000000)), sdk.NewCoin("stake", sdk.NewInt(100000000)))...)
+			s.PrepareBalancerPoolWithCoins(sdk.NewCoins(sdk.NewCoin("uosmo", sdk.NewInt(100000000)), sdk.NewCoin("bar", sdk.NewInt(100000000)))...)
+
+			// Create positions as per the test case, which determines what the best route is
+			s.CreateFullRangePosition(ethStake, sdk.NewCoins(sdk.NewCoin("eth", sdk.NewInt(1000000000000000000)), sdk.NewCoin("stake", sdk.NewInt(1000000000000000000))))
+			s.CreateFullRangePosition(barStake, sdk.NewCoins(sdk.NewCoin("bar", sdk.NewInt(1000000000000000000)), sdk.NewCoin("stake", sdk.NewInt(1000000000000000000))))
+			s.CreateFullRangePosition(btcBar, sdk.NewCoins(sdk.NewCoin("bar", sdk.NewInt(1000000000000000000)), sdk.NewCoin("btc", sdk.NewInt(1000000000000000000))))
+			s.CreateFullRangePosition(btcEth, sdk.NewCoins(sdk.NewCoin("eth", sdk.NewInt(1000000000000000000)), sdk.NewCoin("btc", sdk.NewInt(1000000000000000000))))
+
+			// Set the routes, this normally happens at the end of an epoch or at time of upgrade to v21
+			err := poolmanagerKeeper.SetDenomPairRoutes(s.Ctx)
 			s.Require().NoError(err)
 
-			fmt.Println("eth spply 4", test)
-
-			test, err = poolmanagerKeeper.GetPoolLiquidityOfDenom(s.Ctx, 4, "adam")
+			// Get the route
+			route, err := poolmanagerKeeper.GetDenomPairRoute(s.Ctx, "btc", "stake")
 			s.Require().NoError(err)
-
-			fmt.Println("adam spply 5", test)
-
-			test, err = poolmanagerKeeper.GetPoolLiquidityOfDenom(s.Ctx, 2, "stake")
-			s.Require().NoError(err)
-
-			fmt.Println("adam stake 2", test)
-
-			//poolmanagerKeeper.SetDenomPairRoutes(s.Ctx)
-
-			routes, err := poolmanagerKeeper.GetDenomPairRoute(s.Ctx, "adam", "stake")
-			s.Require().NoError(err)
-
-			fmt.Println(routes)
+			s.Require().Equal(tc.expectedRoute, route)
 		})
 	}
 }
