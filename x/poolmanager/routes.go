e5409bd6a3f74c26034534b2abd4db67fd39f44f
Adam Tucker <adamleetucker@outlook.com>
2023-11-16 21:27:27 -0500

    test

--- x/poolmanager/routes.go
+++ x/poolmanager/routes.go
@@ -110,7 +110,10 @@ func (k *Keeper) SetDenomPairRoutes(ctx sdk.Context) error {
 // GetDenomPairRoute returns the route with the highest liquidity between two tokens
 func (k Keeper) GetDenomPairRoute(ctx sdk.Context, inputDenom, outputDenom string) ([]uint64, error) {
 	if k.routeMap == nil {
-		return nil, fmt.Errorf("route map not set")
+		err := k.SetDenomPairRoutes(ctx)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	// Get all direct routes
