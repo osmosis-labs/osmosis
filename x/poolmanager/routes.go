8547fb21852e2d07baa02a65035504a56a28fa73
Adam Tucker <adamleetucker@outlook.com>
2023-11-16 19:46:49 -0500

    Revert "temp for testing"
    This reverts commit 53ede86051b2f603c13b6986d287a14d2639616a.

--- x/poolmanager/routes.go
+++ x/poolmanager/routes.go
@@ -82,13 +82,8 @@ func (k *Keeper) SetDenomPairRoutes(ctx sdk.Context) error {
 
 // GetDenomPairRoute returns the route with the highest liquidity between two tokens
 func (k Keeper) GetDenomPairRoute(ctx sdk.Context, inputDenom, outputDenom string) ([]uint64, error) {
-	// if k.routeMap == nil {
-	// 	return nil, fmt.Errorf("route map not set")
-	// }
-	// TODO, undo
-	err := k.SetDenomPairRoutes(ctx)
-	if err != nil {
-		return nil, err
+	if k.routeMap == nil {
+		return nil, fmt.Errorf("route map not set")
 	}
 
 	// Get all direct routes
