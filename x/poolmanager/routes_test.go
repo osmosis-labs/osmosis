f0704d90357aa2dc3f8dc014815bb82591d5bea0
Adam Tucker <adamleetucker@outlook.com>
2023-11-16 20:43:20 -0500

    add basic test

--- x/poolmanager/routes_test.go
+++ x/poolmanager/routes_test.go
@@ -11,6 +11,8 @@ import (
 func (s *KeeperTestSuite) TestDenomPairRoute() {
 	tests := map[string]struct {
 		setup         func(ethStake, barStake, btcBar, btcEth, btcStake cltypes.ConcentratedPoolExtension)
+		inDenom       string
+		outDenom      string
 		expectedRoute []uint64
 		expectError   error
 	}{
@@ -23,6 +25,8 @@ func (s *KeeperTestSuite) TestDenomPairRoute() {
 				s.CreateFullRangePosition(btcEth, sdk.NewCoins(sdk.NewCoin("eth", sdk.NewInt(1000000000000000000)), sdk.NewCoin("btc", sdk.NewInt(1000000000000000000))))
 				s.CreateFullRangePosition(btcStake, sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(1100000000000000000)), sdk.NewCoin("btc", sdk.NewInt(1100000000000000000))))
 			},
+			inDenom:       "btc",
+			outDenom:      "stake",
 			expectedRoute: []uint64{6},
 		},
 		"indirect route is best, route via eth": {
@@ -34,6 +38,8 @@ func (s *KeeperTestSuite) TestDenomPairRoute() {
 				s.CreateFullRangePosition(btcEth, sdk.NewCoins(sdk.NewCoin("eth", sdk.NewInt(1100000000000000000)), sdk.NewCoin("btc", sdk.NewInt(1100000000000000000))))
 				s.CreateFullRangePosition(btcStake, sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(1000000000000000000)), sdk.NewCoin("btc", sdk.NewInt(1000000000000000000))))
 			},
+			inDenom:       "btc",
+			outDenom:      "stake",
 			expectedRoute: []uint64{5, 2},
 		},
 		"indirect route is best, route via bar": {
@@ -45,7 +51,35 @@ func (s *KeeperTestSuite) TestDenomPairRoute() {
 				s.CreateFullRangePosition(btcEth, sdk.NewCoins(sdk.NewCoin("eth", sdk.NewInt(1000000000000000000)), sdk.NewCoin("btc", sdk.NewInt(1000000000000000000))))
 				s.CreateFullRangePosition(btcStake, sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(1000000000000000000)), sdk.NewCoin("btc", sdk.NewInt(1000000000000000000))))
 			},
+			inDenom:       "btc",
+			outDenom:      "stake",
 			expectedRoute: []uint64{4, 3},
+		},
+		"flip denoms should flip route": {
+			setup: func(ethStake, barStake, btcBar, btcEth, btcStake cltypes.ConcentratedPoolExtension) {
+				// Create positions as per the test case, which determines what the best route is
+				s.CreateFullRangePosition(ethStake, sdk.NewCoins(sdk.NewCoin("eth", sdk.NewInt(1000000000000000000)), sdk.NewCoin("stake", sdk.NewInt(1000000000000000000))))
+				s.CreateFullRangePosition(barStake, sdk.NewCoins(sdk.NewCoin("bar", sdk.NewInt(1000000000000000000)), sdk.NewCoin("stake", sdk.NewInt(1000000000000000000))))
+				s.CreateFullRangePosition(btcBar, sdk.NewCoins(sdk.NewCoin("bar", sdk.NewInt(1100000000000000000)), sdk.NewCoin("btc", sdk.NewInt(1100000000000000000))))
+				s.CreateFullRangePosition(btcEth, sdk.NewCoins(sdk.NewCoin("eth", sdk.NewInt(1000000000000000000)), sdk.NewCoin("btc", sdk.NewInt(1000000000000000000))))
+				s.CreateFullRangePosition(btcStake, sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(1000000000000000000)), sdk.NewCoin("btc", sdk.NewInt(1000000000000000000))))
+			},
+			inDenom:       "stake",
+			outDenom:      "btc",
+			expectedRoute: []uint64{3, 4},
+		},
+		"three route happy path": {
+			setup: func(ethStake, barStake, btcBar, btcEth, btcStake cltypes.ConcentratedPoolExtension) {
+				// Create positions as per the test case, which determines what the best route is
+				s.CreateFullRangePosition(ethStake, sdk.NewCoins(sdk.NewCoin("eth", sdk.NewInt(1000000000000000000)), sdk.NewCoin("stake", sdk.NewInt(1000000000000000000))))
+				s.CreateFullRangePosition(barStake, sdk.NewCoins(sdk.NewCoin("bar", sdk.NewInt(1000000000000000000)), sdk.NewCoin("stake", sdk.NewInt(1000000000000000000))))
+				s.CreateFullRangePosition(btcBar, sdk.NewCoins(sdk.NewCoin("bar", sdk.NewInt(1100000000000000000)), sdk.NewCoin("btc", sdk.NewInt(1100000000000000000))))
+				s.CreateFullRangePosition(btcEth, sdk.NewCoins(sdk.NewCoin("eth", sdk.NewInt(1000000000000000000)), sdk.NewCoin("btc", sdk.NewInt(1000000000000000000))))
+				s.CreateFullRangePosition(btcStake, sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(1000000000000000000)), sdk.NewCoin("btc", sdk.NewInt(1000000000000000000))))
+			},
+			inDenom:       "eth",
+			outDenom:      "stbtc",
+			expectedRoute: []uint64{8, 9, 10},
 		},
 	}
 
@@ -68,6 +102,11 @@ func (s *KeeperTestSuite) TestDenomPairRoute() {
 			// Create cw pools for direct route
 			s.PrepareCustomTransmuterPool(s.TestAccs[0], []string{"btc", "stake"}) // pool 7
 
+			// Create three route (eth -> bar -> test -> stbtc)
+			s.PrepareConcentratedPoolWithCoins("eth", "bar")                        // pool 8
+			s.PrepareConcentratedPoolWithCoins("test", "bar")                       // pool 9
+			s.PrepareCustomTransmuterPool(s.TestAccs[0], []string{"test", "stbtc"}) // pool 10
+
 			tc.setup(ethStake, barStake, btcBar, btcEth, btcStake)
 
 			// Create uosmo pairings to determine value in a base asset
@@ -75,13 +114,15 @@ func (s *KeeperTestSuite) TestDenomPairRoute() {
 			s.PrepareBalancerPoolWithCoins(sdk.NewCoins(sdk.NewCoin("uosmo", sdk.NewInt(10000000)), sdk.NewCoin("eth", sdk.NewInt(10000000)))...)
 			s.PrepareBalancerPoolWithCoins(sdk.NewCoins(sdk.NewCoin("uosmo", sdk.NewInt(10000000)), sdk.NewCoin("stake", sdk.NewInt(10000000)))...)
 			s.PrepareBalancerPoolWithCoins(sdk.NewCoins(sdk.NewCoin("uosmo", sdk.NewInt(10000000)), sdk.NewCoin("bar", sdk.NewInt(10000000)))...)
+			s.PrepareBalancerPoolWithCoins(sdk.NewCoins(sdk.NewCoin("uosmo", sdk.NewInt(10000000)), sdk.NewCoin("test", sdk.NewInt(10000000)))...)
+			s.PrepareBalancerPoolWithCoins(sdk.NewCoins(sdk.NewCoin("uosmo", sdk.NewInt(10000000)), sdk.NewCoin("stbtc", sdk.NewInt(10000000)))...)
 
 			// Set the routes, this normally happens at the end of an epoch or at time of upgrade to v21
 			err := poolmanagerKeeper.SetDenomPairRoutes(s.Ctx)
 			s.Require().NoError(err)
 
 			// Get the route
-			route, err := poolmanagerKeeper.GetDenomPairRoute(s.Ctx, "btc", "stake")
+			route, err := poolmanagerKeeper.GetDenomPairRoute(s.Ctx, tc.inDenom, tc.outDenom)
 			s.Require().NoError(err)
 			s.Require().Equal(tc.expectedRoute, route)
 		})
