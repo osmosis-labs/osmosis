ebce499d64ae281dca157f27e3f267f7b5051408
Adam Tucker <adamleetucker@outlook.com>
2023-11-22 22:39:13 -0700

    add further test cases

--- x/poolmanager/types/routes_test.go
+++ x/poolmanager/types/routes_test.go
@@ -358,51 +358,63 @@ func TestAddEdge(t *testing.T) {
 		end    string
 		token  string
 		poolID uint64
+		setup  func(g *RoutingGraph)
 	}{
 		{
-			name:   "Test 1",
+			name:   "happy path 1",
 			start:  "start1",
 			end:    "end1",
 			token:  "token1",
 			poolID: uint64(1),
 		},
 		{
-			name:   "Test 2",
+			name:   "happy path 2",
 			start:  "start2",
 			end:    "end2",
 			token:  "token2",
 			poolID: uint64(2),
+		},
+		{
+			name:   "start entry already exists, end entry does not",
+			start:  "start1",
+			end:    "end3",
+			token:  "token3",
+			poolID: uint64(3),
+			setup: func(g *RoutingGraph) {
+				g.AddEdge("start1", "end1", "token1", uint64(1))
+			},
+		},
+		{
+			name:   "start entry and end entry already exists",
+			start:  "start1",
+			end:    "end1",
+			token:  "token4",
+			poolID: uint64(4),
+			setup: func(g *RoutingGraph) {
+				g.AddEdge("start1", "end1", "token1", uint64(1))
+			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			g := &RoutingGraph{}
+			if tt.setup != nil {
+				tt.setup(g)
+			}
 			g.AddEdge(tt.start, tt.end, tt.token, tt.poolID)
 
-			if len(g.Entries) != 1 {
-				t.Errorf("Expected 1 entry, got %d", len(g.Entries))
-			}
-
-			startEntry := g.Entries[0]
+			startEntry := g.Entries[len(g.Entries)-1]
 			if startEntry.Key != tt.start {
 				t.Errorf("Expected key to be %s, got %s", tt.start, startEntry.Key)
 			}
 
-			if len(startEntry.Value.Entries) != 1 {
-				t.Errorf("Expected 1 inner entry, got %d", len(startEntry.Value.Entries))
-			}
-
-			endEntry := startEntry.Value.Entries[0]
+			endEntry := startEntry.Value.Entries[len(startEntry.Value.Entries)-1]
 			if endEntry.Key != tt.end {
 				t.Errorf("Expected key to be %s, got %s", tt.end, endEntry.Key)
 			}
 
-			if len(endEntry.Value.Routes) != 1 {
-				t.Errorf("Expected 1 route, got %d", len(endEntry.Value.Routes))
-			}
-
-			route := endEntry.Value.Routes[0]
+			route := endEntry.Value.Routes[len(endEntry.Value.Routes)-1]
 			if route.PoolId != tt.poolID {
 				t.Errorf("Expected poolID to be %d, got %d", tt.poolID, route.PoolId)
 			}
