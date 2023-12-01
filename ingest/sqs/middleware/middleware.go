7e08ecc99d4515a1464febd9d3be23acc5c3175f
Roman <roman@osmosis.team>
2023-12-01 08:46:27 -0800

    feat(SQS): prometheus metrics for latency and rps (#6971)
    * feat(SQS): prometheus metrics for latency and rps
    
    * lint

--- ingest/sqs/middleware/middleware.go
+++ ingest/sqs/middleware/middleware.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"net/url"
+	"time"
+
 	"github.com/labstack/echo"
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -11,17 +14,29 @@ type GoMiddleware struct {
 }
 
 var (
+	// total number of requests counter
 	requestsTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "sqs_requests_total",
 			Help: "Total number of requests.",
 		},
-		[]string{"method"},
+		[]string{"method", "endpoint"},
+	)
+
+	// request latency histogram
+	requestLatency = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "sqs_request_duration_seconds",
+			Help:    "Histogram of request latencies.",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "endpoint"},
 	)
 )
 
 func init() {
 	prometheus.MustRegister(requestsTotal)
+	prometheus.MustRegister(requestLatency)
 }
 
 // CORS will handle the CORS middleware
@@ -37,10 +52,29 @@ func InitMiddleware() *GoMiddleware {
 	return &GoMiddleware{}
 }
 
+// InstrumentMiddleware will handle the instrumentation middleware
 func (m *GoMiddleware) InstrumentMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		start := time.Now()
+
+		parsedURL, err := url.Parse(c.Request().RequestURI)
+		if err != nil {
+			return err
+		}
+
+		requestMethod := c.Request().Method
+		requestPath := parsedURL.Path
+
 		// Increment the request counter
-		requestsTotal.WithLabelValues(c.Request().Method).Inc()
-		return next(c)
+		requestsTotal.WithLabelValues(requestMethod, requestPath).Inc()
+
+		err = next(c)
+
+		duration := time.Since(start).Seconds()
+
+		// Observe the duration with the histogram
+		requestLatency.WithLabelValues(requestMethod, requestPath).Observe(duration)
+
+		return err
 	}
 }
