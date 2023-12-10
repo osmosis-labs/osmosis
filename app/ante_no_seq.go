e934cee0ad6a551438d507b5d55704307d8f9d20
Adam Tucker <adam@osmosis.team>
2023-12-09 21:53:12 -0600

    fix(e2e): remove sequence number checks (#7048)
    * no sequence number checks
    
    * memos
    
    * clean up
    
    * add clarifying comment
    
    * add a comment explaining the memo
    
    * Update app/ante_no_seq.go
    
    Co-authored-by: Roman <roman@osmosis.team>
    
    * add mutex to memo counter
    
    ---------
    
    Co-authored-by: Roman <roman@osmosis.team>

--- app/ante_no_seq.go
+++ app/ante_no_seq.go
@@ -1,5 +1,5 @@
-//go:build !excludeIncrement
-// +build !excludeIncrement
+//go:build excludeIncrement
+// +build excludeIncrement
 
 package app
 
@@ -25,9 +25,9 @@ import (
 
 // Link to default ante handler used by cosmos sdk:
 // https://github.com/cosmos/cosmos-sdk/blob/v0.43.0/x/auth/ante/ante.go#L41
-// N.B. There is a sister file called `ante_no_seq.go` that is used for e2e testing.
-// It leaves out the `IncrementSequenceDecorator` which is not needed for e2e testing.
-// If you make a change here, make sure to make the same change in `ante_no_seq.go`.
+// N.B. There is a sister file called `ante_eq.go` that is used for production.
+// Opposite to this, it also chains `IncrementSequenceDecorator` which is not needed for e2e testing but is critical for production.
+// If you make a change here, make sure to make the same change in `ante.go`.
 func NewAnteHandler(
 	appOpts servertypes.AppOptions,
 	wasmConfig wasmtypes.WasmConfig,
@@ -64,7 +64,7 @@ func NewAnteHandler(
 		ante.NewValidateSigCountDecorator(ak),
 		ante.NewSigGasConsumeDecorator(ak, sigGasConsumer),
 		ante.NewSigVerificationDecorator(ak, signModeHandler),
-		ante.NewIncrementSequenceDecorator(ak),
+		// ante.NewIncrementSequenceDecorator(ak),
 		ibcante.NewRedundantRelayDecorator(channelKeeper),
 	)
 }
