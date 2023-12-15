24271d7be39e30aa523996c151b933b51027e71e
levisyin <150114626+levisyin@users.noreply.github.com>
2023-12-15 14:49:25 +0800

    fix: remove duplicate `query gamm pool` subcommand (#7120)

--- x/gamm/client/cli/query.go
+++ x/gamm/client/cli/query.go
@@ -23,7 +23,6 @@ import (
 // GetQueryCmd returns the cli query commands for this module.
 func GetQueryCmd() *cobra.Command {
 	cmd := osmocli.QueryIndexCmd(types.ModuleName)
-	osmocli.AddQueryCmd(cmd, types.NewQueryClient, GetCmdPool)
 	osmocli.AddQueryCmd(cmd, types.NewQueryClient, GetCmdSpotPrice)
 	osmocli.AddQueryCmd(cmd, types.NewQueryClient, GetCmdPool)
 	osmocli.AddQueryCmd(cmd, types.NewQueryClient, GetCmdPools)
