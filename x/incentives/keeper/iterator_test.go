8d2621d2c57894ceae5f34f7b946e64ef64a2e9e
Roman <roman@osmosis.team>
2023-12-22 16:09:31 -0700

    updates (#7192)

---
+++ x/incentives/keeper/iterator_test.go
@@ -0,0 +1,37 @@
+package keeper_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/osmosis-labs/osmosis/v21/x/incentives/keeper"
+	lockuptypes "github.com/osmosis-labs/osmosis/v21/x/lockup/types"
+)
+
+// This test validates that the FilterLocksByMinDuration function
+// copies the correct locks when filtering.
+// It helps to ensure that we do not use a wrong pointer by referencing
+// a loop variable.
+func TestFilterLocksByMinDuration(t *testing.T) {
+	const (
+		numLocks    = 3
+		minDuration = 2
+	)
+
+	locks := make([]lockuptypes.PeriodLock, numLocks)
+	for i := 0; i < numLocks; i++ {
+		locks[i] = lockuptypes.PeriodLock{
+			ID:       uint64(i + 1),
+			Duration: minDuration,
+		}
+	}
+
+	filteredLocks := keeper.FilterLocksByMinDuration(locks, minDuration)
+
+	require.Equal(t, len(locks), len(filteredLocks))
+
+	for i := 0; i < len(locks); i++ {
+		require.Equal(t, locks[i].ID, filteredLocks[i].ID)
+	}
+}
