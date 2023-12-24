6b8b81248efea0bbef9a83c3cd469630f9079d8b
Dev Ojha <dojha@berkeley.edu>
2023-12-24 10:07:57 -0600

    Add maximum pool ID

--- x/poolmanager/create_pool.go
+++ x/poolmanager/create_pool.go
@@ -99,6 +99,9 @@ func (k Keeper) createPoolZeroLiquidityNoCreationFee(ctx sdk.Context, msg types.
 
 	// Get the next pool ID and increment the pool ID counter.
 	poolId := k.getNextPoolIdAndIncrement(ctx)
+	if poolId >= types.MaxPoolId {
+		return nil, fmt.Errorf("next pool ID %d is greater than max pool ID %d", poolId, types.MaxPoolId)
+	}
 
 	poolType := msg.GetPoolType()
 
