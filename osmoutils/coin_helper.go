ccd1865797f32f9b3714d8658b5fa241f8e4b23c
levisyin <150114626+levisyin@users.noreply.github.com>
2024-01-02 03:48:51 +0800

    refactor: replace MinCoins with sdk coins.Min() (#7127)
    * refactor: replace MinCoins with sdk coins.Min()
    
    * fix: remove unused import

--- osmoutils/coin_helper.go
+++ osmoutils/coin_helper.go
@@ -7,21 +7,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// MinCoins returns the minimum of each denom between both coins.
-// For now it assumes they have the same denoms.
-// TODO: Replace with method in SDK once we update our version
-func MinCoins(coinsA sdk.Coins, coinsB sdk.Coins) sdk.Coins {
-	resCoins := sdk.Coins{}
-	for i, coin := range coinsA {
-		if coinsB[i].Amount.GT(coin.Amount) {
-			resCoins = append(resCoins, coin)
-		} else {
-			resCoins = append(resCoins, coinsB[i])
-		}
-	}
-	return resCoins
-}
-
 // SubDecCoinArrays subtracts the contents of the second param from the first (decCoinsArrayA - decCoinsArrayB)
 // Note that this takes in two _arrays_ of DecCoins, meaning that each term itself is of type DecCoins (i.e. an array of DecCoin).
 func SubDecCoinArrays(decCoinsArrayA []sdk.DecCoins, decCoinsArrayB []sdk.DecCoins) ([]sdk.DecCoins, error) {
