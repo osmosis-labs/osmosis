ff62dbdf05b047e680c3eb597d290d5723d14fc5
Roman <roman@osmosis.team>
2024-01-08 18:13:10 -0700

    refactor: sqs out-of-process (#7242)
    * refactor: sqs out-of-process
    
    * spell
    
    * godocs
    
    * go 1.20
    
    * go work
    
    * updaets
    
    * updates
    
    * sqs deps
    
    * updates
    
    * latest
    
    * updates
    
    * updates
    
    * updates

--- ingest/sqs/sqs_config.go
+++ ingest/sqs/sqs_config.go
@@ -1,18 +1,23 @@
 package sqs
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
+	"github.com/redis/go-redis/v9"
+
+	redisrepo "github.com/osmosis-labs/sqs/sqsdomain/repository/redis"
+	chaininforedisrepo "github.com/osmosis-labs/sqs/sqsdomain/repository/redis/chaininfo"
+	poolsredisrepo "github.com/osmosis-labs/sqs/sqsdomain/repository/redis/pools"
+	routerredisrepo "github.com/osmosis-labs/sqs/sqsdomain/repository/redis/router"
 
 	"github.com/osmosis-labs/osmosis/osmoutils"
 	"github.com/osmosis-labs/osmosis/v21/ingest"
-	redischaininfoingester "github.com/osmosis-labs/osmosis/v21/ingest/sqs/chain_info/ingester/redis"
+	chaininfoingester "github.com/osmosis-labs/osmosis/v21/ingest/sqs/chaininfo/ingester"
 	"github.com/osmosis-labs/osmosis/v21/ingest/sqs/domain"
-	sqslog "github.com/osmosis-labs/osmosis/v21/ingest/sqs/log"
-	"github.com/osmosis-labs/osmosis/v21/ingest/sqs/pools/common"
-	redispoolsingester "github.com/osmosis-labs/osmosis/v21/ingest/sqs/pools/ingester/redis"
+	poolsingester "github.com/osmosis-labs/osmosis/v21/ingest/sqs/pools/ingester"
 )
 
 // Config defines the config for the sidecar query server.
@@ -23,23 +28,13 @@ type Config struct {
 	// Storage defines the storage host and port.
 	StorageHost string `mapstructure:"db-host"`
 	StoragePort string `mapstructure:"db-port"`
-
-	// Defines the web server configuration.
-	ServerAddress             string `mapstructure:"server-address"`
-	ServerTimeoutDurationSecs int    `mapstructure:"timeout-duration-secs"`
-
-	// Defines the logger configuration.
-	LoggerFilename     string `mapstructure:"logger-filename"`
-	LoggerIsProduction bool   `mapstructure:"logger-is-production"`
-	LoggerLevel        string `mapstructure:"logger-level"`
-
-	ChainGRPCGatewayEndpoint string `mapstructure:"grpc-gateway-endpoint"`
-
-	// Router encapsulates the router config.
-	Router *domain.RouterConfig `mapstructure:"router"`
 }
 
-const groupOptName = "osmosis-sqs"
+const (
+	groupOptName = "osmosis-sqs"
+
+	noRoutesCacheExpiry = 0
+)
 
 // DefaultConfig defines the default config for the sidecar query server.
 var DefaultConfig = Config{
@@ -48,27 +43,6 @@ var DefaultConfig = Config{
 
 	StorageHost: "localhost",
 	StoragePort: "6379",
-
-	ServerAddress:             ":9092",
-	ServerTimeoutDurationSecs: 2,
-
-	LoggerFilename:     "sqs.log",
-	LoggerIsProduction: true,
-	LoggerLevel:        "info",
-
-	ChainGRPCGatewayEndpoint: "http://localhost:26657",
-
-	Router: &domain.RouterConfig{
-		PreferredPoolIDs:          []uint64{},
-		MaxPoolsPerRoute:          4,
-		MaxRoutes:                 5,
-		MaxSplitRoutes:            3,
-		MaxSplitIterations:        10,
-		MinOSMOLiquidity:          10000, // 10_000 OSMO
-		RouteUpdateHeightInterval: 0,
-		RouteCacheEnabled:         false,
-		RouteCacheExpirySeconds:   600, // 10 minutes
-	},
 }
 
 // NewConfigFromOptions returns a new sidecar query server config from the given options.
@@ -86,69 +60,36 @@ func NewConfigFromOptions(opts servertypes.AppOptions) Config {
 
 		StorageHost: osmoutils.ParseString(opts, groupOptName, "db-host"),
 		StoragePort: osmoutils.ParseString(opts, groupOptName, "db-port"),
-
-		ServerAddress:             osmoutils.ParseString(opts, groupOptName, "server-address"),
-		ServerTimeoutDurationSecs: osmoutils.ParseInt(opts, groupOptName, "timeout-duration-secs"),
-
-		LoggerFilename:     osmoutils.ParseString(opts, groupOptName, "logger-filename"),
-		LoggerIsProduction: osmoutils.ParseBool(opts, groupOptName, "logger-is-production", false),
-		LoggerLevel:        osmoutils.ParseString(opts, groupOptName, "logger-level"),
-
-		ChainGRPCGatewayEndpoint: osmoutils.ParseString(opts, groupOptName, "grpc-gateway-endpoint"),
-
-		Router: &domain.RouterConfig{
-			PreferredPoolIDs: osmoutils.ParseUint64Slice(opts, groupOptName, "preferred-pool-ids"),
-
-			MaxPoolsPerRoute: osmoutils.ParseInt(opts, groupOptName, "max-pools-per-route"),
-
-			MaxRoutes: osmoutils.ParseInt(opts, groupOptName, "max-routes"),
-
-			MaxSplitRoutes: osmoutils.ParseInt(opts, groupOptName, "max-split-routes"),
-
-			MaxSplitIterations: osmoutils.ParseInt(opts, groupOptName, "max-split-iterations"),
-
-			MinOSMOLiquidity: osmoutils.ParseInt(opts, groupOptName, "min-osmo-liquidity"),
-
-			RouteUpdateHeightInterval: osmoutils.ParseInt(opts, groupOptName, "route-update-height-interval"),
-
-			RouteCacheEnabled: osmoutils.ParseBool(opts, groupOptName, "route-cache-enabled", false),
-
-			RouteCacheExpirySeconds: uint64(osmoutils.ParseInt(opts, groupOptName, "route-cache-expiry-seconds")),
-		},
 	}
 }
 
 // Initialize initializes the sidecar query server and returns the ingester.
-func (c Config) Initialize(appCodec codec.Codec, keepers common.SQSIngestKeepers) (ingest.Ingester, error) {
-	// logger
-	logger, err := sqslog.NewLogger(c.LoggerIsProduction, c.LoggerFilename, c.LoggerLevel)
+func (c Config) Initialize(appCodec codec.Codec, keepers domain.SQSIngestKeepers) (ingest.Ingester, error) {
+	// Create redis client and ensure that it is up.
+	redisAddress := fmt.Sprintf("%s:%s", c.StorageHost, c.StoragePort)
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     redisAddress,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	redisStatus := redisClient.Ping(context.Background())
+	_, err := redisStatus.Result()
 	if err != nil {
-		return nil, fmt.Errorf("error while creating logger: %s", err)
-	}
-	logger.Info("Starting sidecar query server")
-
-	// Create sidecar query server
-	sidecarQueryServer, err := NewSideCarQueryServer(
-		appCodec,
-		*c.Router,
-		c.StorageHost,
-		c.StoragePort,
-		c.ServerAddress,
-		c.ChainGRPCGatewayEndpoint,
-		c.ServerTimeoutDurationSecs,
-		logger)
-	if err != nil {
-		return nil, fmt.Errorf("error while creating sidecar query server: %s", err)
+		return nil, err
 	}
 
-	txManager := sidecarQueryServer.GetTxManager()
+	txManager := redisrepo.NewTxManager(redisClient)
+
+	redisPoolsRepository := poolsredisrepo.New(appCodec, txManager)
+
+	redisRouterRepository := routerredisrepo.New(txManager, noRoutesCacheExpiry)
 
 	// Create pools ingester
-	poolsIngester := redispoolsingester.NewPoolIngester(sidecarQueryServer.GetPoolsRepository(), sidecarQueryServer.GetRouterRepository(), sidecarQueryServer.GetTokensUseCase(), txManager, *c.Router, keepers)
-	poolsIngester.SetLogger(sidecarQueryServer.GetLogger())
+	poolsIngester := poolsingester.NewPoolIngester(redisPoolsRepository, redisRouterRepository, txManager, domain.NewAssetListGetter(), keepers)
 
-	chainInfoingester := redischaininfoingester.NewChainInfoIngester(sidecarQueryServer.GetChainInfoRepository(), txManager)
-	chainInfoingester.SetLogger(sidecarQueryServer.GetLogger())
+	// Create chain info ingester
+	chainInfoRepository := chaininforedisrepo.New(txManager)
+	chainInfoingester := chaininfoingester.New(chainInfoRepository, txManager)
 
 	// Create sqs ingester that encapsulates all ingesters.
 	sqsIngester := NewSidecarQueryServerIngester(poolsIngester, chainInfoingester, txManager)
--- osmomath/go.mod
+++ osmomath/go.mod
@@ -13,11 +13,13 @@ require (
 
 require (
 	cosmossdk.io/errors v1.0.0 // indirect
+	github.com/OneOfOne/xxhash v1.2.8 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.2 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/cometbft/cometbft v0.37.2 // indirect
+	github.com/cometbft/cometbft v0.38.0 // indirect
 	github.com/cometbft/cometbft-db v0.8.0 // indirect
 	github.com/confio/ics23/go v0.9.1 // indirect
 	github.com/cosmos/btcutil v1.0.5 // indirect
@@ -29,7 +31,8 @@ require (
 	github.com/dgraph-io/ristretto v0.1.1 // indirect
 	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/dvsekhvalnov/jose2go v1.6.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-kit/kit v0.12.0 // indirect
 	github.com/go-kit/log v0.2.1 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
@@ -38,8 +41,9 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/btree v1.1.2 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/gtank/merlin v0.1.1 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.5-0.20210104140557-80c98217689d // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -48,18 +52,18 @@ require (
 	github.com/libp2p/go-buffer-pool v0.1.0 // indirect
 	github.com/linxGnu/grocksdb v1.7.16 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
-	github.com/mimoo/StrobeGo v0.0.0-20210601165009-122bf33a46e0 // indirect
+	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/oasisprotocol/curve25519-voi v0.0.0-20220708102147-0a8a51822cae // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/petermattis/goid v0.0.0-20230317030725-371a4b8eda08 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/prometheus/client_golang v1.17.0 // indirect
-	github.com/prometheus/client_model v0.4.1-0.20230718164431-9a2bf3000d16 // indirect
-	github.com/prometheus/common v0.44.0 // indirect
-	github.com/prometheus/procfs v0.11.1 // indirect
-	github.com/sagikazarmark/locafero v0.3.0 // indirect
+	github.com/prometheus/client_golang v1.18.0 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.45.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sasha-s/go-deadlock v0.3.1 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -67,7 +71,7 @@ require (
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.17.0 // indirect
+	github.com/spf13/viper v1.18.2 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/syndtr/goleveldb v1.0.1-0.20220721030215-126854af5e6d // indirect
 	github.com/tendermint/go-amino v0.16.0 // indirect
