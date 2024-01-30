7a3e139dc89fc9ee9a96031cc2db545f6a19a3f3
PaddyMc <PaddyMc@users.noreply.github.com>
2024-01-30 20:19:06 +0000

    [create-pull-request] automated change

--- tests/e2e/containers/config.go
+++ tests/e2e/containers/config.go
@@ -24,10 +24,10 @@ const (
 	// It should be uploaded to Docker Hub. OSMOSIS_E2E_SKIP_UPGRADE should be unset
 	// for this functionality to be used.
 	previousVersionOsmoRepository = "osmolabs/osmosis"
-	previousVersionOsmoTag        = "v21.2.1-e2e-only"
+	previousVersionOsmoTag        = "22.0.0-alpine"
 	// Pre-upgrade repo/tag for osmosis initialization (this should be one version below upgradeVersion)
 	previousVersionInitRepository = "osmolabs/osmosis-e2e-init-chain"
-	previousVersionInitTag        = "v21.1.5"
+	previousVersionInitTag        = "22.0.0"
 	// Hermes repo/version for relayer
 	relayerRepository = "informalsystems/hermes"
 	relayerTag        = "1.5.1"
