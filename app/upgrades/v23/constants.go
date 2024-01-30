fed73678cdccc3cda55625b89b26ebdc1a867882
Osmosis <105710778+osmo-bot@users.noreply.github.com>
2024-01-30 21:53:21 +0100

    [create-pull-request] automated change (#7387)
    Co-authored-by: PaddyMc <PaddyMc@users.noreply.github.com>

---
+++ app/upgrades/v23/constants.go
@@ -0,0 +1,19 @@
+package v23
+
+import (
+	"github.com/osmosis-labs/osmosis/v22/app/upgrades"
+
+	store "github.com/cosmos/cosmos-sdk/store/types"
+)
+
+// UpgradeName defines the on-chain upgrade name for the Osmosis v23 upgrade.
+const UpgradeName = "v23"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: store.StoreUpgrades{
+		Added:   []string{},
+		Deleted: []string{},
+	},
+}
