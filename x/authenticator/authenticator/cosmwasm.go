54c2b1db7be1a2e0695236e20adeabc006b05538
Nicolas Lara <nicolaslara@gmail.com>
2024-02-05 14:12:38 +0100

    allow empty json msg

--- x/authenticator/authenticator/cosmwasm.go
+++ x/authenticator/authenticator/cosmwasm.go
@@ -148,7 +148,10 @@ func GenerateAuthenticationData(ctx sdk.Context, ak *authkeeper.AccountKeeper, s
 		}
 		jsonMsg, err := json.Marshal(msg)
 		if err != nil {
-			return iface.AuthenticationRequest{}, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "failed to marshal msg")
+			// Messages with Anys cannot be marshalled to JSON. It's ok for these to be empty. The consumer should be able to parse the byte value
+			// Example error:
+			//JSON marshal marshaling error for &Any{TypeUrl:/cosmos.bank.v1beta1.SendAuthorization,Value:[10 16 10 5 115 116 97 107 101 18 7 49 48 48 48 48 48 48],XXX_unrecognized:[]}, this is likely because amino is being used directly (instead of codec.LegacyAmino which is preferred) or UnpackInterfacesMessage is not defined for some type which contains a protobuf Any either directly or via one of its members. To see a stacktrace of where the error is coming from, set the var Debug = true in codec/types/compat.go
+			ctx.Logger().Error("failed to marshal msg", "msg", msg)
 		}
 		msgs[i] = iface.LocalAny{
 			TypeURL: encodedMsg.TypeUrl,
