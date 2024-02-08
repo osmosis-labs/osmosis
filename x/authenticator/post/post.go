2974a5a2f88ba661c560e1c0e50ad4056ecca6af
Supanat <supanat.ptk@gmail.com>
2024-02-08 16:52:10 +0700

    Skip ConfirmExecution if message is MsgAddAuthenticator (#7422)
    * delay registering contract keeper
    
    * skip confirm if msg add authenticator
    
    * Revert "skip confirm if msg add authenticator"
    
    This reverts commit a7f420a9d8a9f95b5d041e88f5e137a74780e4ae.
    
    * add is ready flag
    
    * track non ready authenticators and skip in post
    
    * make sure mark ready loop goes through unique entries
    
    * add test for mark authenticator as ready
    
    * usd errorsmod instead of sdkerrors for deprected Wrap

--- x/authenticator/post/post.go
+++ x/authenticator/post/post.go
@@ -6,6 +6,7 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	authenticatorkeeper "github.com/osmosis-labs/osmosis/v21/x/authenticator/keeper"
+	"github.com/osmosis-labs/osmosis/v21/x/authenticator/types"
 	"github.com/osmosis-labs/osmosis/v21/x/authenticator/utils"
 )
 
@@ -34,17 +35,35 @@ func (ad AuthenticatorDecorator) PostHandle(
 	// all state changes are reverted anyway
 	ad.authenticatorKeeper.TransientStore.WriteInto(ctx)
 
+	// collect all the keys for authenticators that are not ready
+	nonReadyAccountAuthenticatorKeys := make(map[string]struct{})
+
 	for msgIndex, msg := range tx.GetMsgs() {
 		account, err := utils.GetAccount(msg)
 		if err != nil {
 			return sdk.Context{}, errorsmod.Wrap(sdkerrors.ErrUnauthorized, "unable to get account")
 		}
-		authenticators, err := ad.authenticatorKeeper.GetAuthenticatorsForAccountOrDefault(ctx, account)
+
+		accountAuthenticators, err := ad.authenticatorKeeper.GetAuthenticatorDataForAccount(ctx, account)
+
 		if err != nil {
 			return sdk.Context{}, err
 		}
 
-		for _, authenticator := range authenticators { // This should execute on *all* authenticators so they can update their state
+		for _, accountAuthenticator := range accountAuthenticators { // This should execute on *all* readied authenticators so they can update their state
+
+			// We want to skip `ConfirmExecution` if the authenticator is newly added
+			// since Authenticate & Track are called on antehandler but newly added authenticator
+			// so Authenticate & Track on newly added authenticator has not been called yet
+			// which means the authenticator is not ready to confirm execution
+			if !accountAuthenticator.IsReady {
+				key := string(types.KeyAccountId(account, accountAuthenticator.Id))
+				nonReadyAccountAuthenticatorKeys[key] = struct{}{}
+				continue
+			}
+
+			authenticator := accountAuthenticator.AsAuthenticator(ad.authenticatorKeeper.AuthenticatorManager)
+
 			// Get the authentication data for the transaction
 			authData, err := authenticator.GetAuthenticationData(ctx, tx, msgIndex, simulate)
 			if err != nil {
@@ -60,6 +79,11 @@ func (ad AuthenticatorDecorator) PostHandle(
 
 			success = successfulExecution.IsConfirm()
 		}
+	}
+
+	// All non-ready authenticators should be ready now
+	for key := range nonReadyAccountAuthenticatorKeys {
+		ad.authenticatorKeeper.MarkAuthenticatorAsReady(ctx, []byte(key))
 	}
 
 	return next(ctx, tx, simulate, success)
