16c84624fc7dfbb610a2ec6d6f139b6369216238
Roman <roman@osmosis.team>
2024-02-15 18:01:47 -0800

    feat(observability): telemetry counters for incentives and sqs

---
+++ x/concentrated-liquidity/types/telemetry.go
@@ -0,0 +1,10 @@
+package types
+
+const (
+	IncentiveEmissionTelemetryName   = "concentrated_liquidity_incentive_emission"
+	IncentiveTruncationTelemetryName = "concentrated_liquidity_incentive_truncation"
+	IncentiveOverflowTelemetryName   = "concentrated_liquidity_incentive_overflow"
+
+	SpreadRewardEmissionTelemetryName   = "concentrated_liquidity_spread_reward_emission"
+	SpreadFactorTruncationTelemetryName = "concentrated_liquidity_sptread_factor_truncation"
+)
---
+++ x/pool-incentives/types/telemetry.go
@@ -0,0 +1,5 @@
+package types
+
+const (
+	NoPoolIdForGaugeTelemetryName = "pool_incentives_no_pool_id_for_gauge"
+)
