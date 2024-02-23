bd27642b687f8dffbb04b0405c7566157ed4bcde
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2024-02-23 11:46:52 +0700

    add msg index to auth request

--- x/authenticator/iface/iface.go
+++ x/authenticator/iface/iface.go
@@ -29,10 +29,17 @@ type SimplifiedSignatureData struct {
 }
 
 type AuthenticationRequest struct {
-	AuthenticatorId     string                  `json:"authenticator_id"`
-	Account             sdk.AccAddress          `json:"account"`
-	Msg                 LocalAny                `json:"msg"`
-	Signature           []byte                  `json:"signature"` // Only allowing messages with a single signer
+	AuthenticatorId string         `json:"authenticator_id"`
+	Account         sdk.AccAddress `json:"account"`
+	Msg             LocalAny       `json:"msg"`
+
+	// Since array size is int, and size depends on the system architecture,
+	// we use uint64 to cover all available architectures.
+	// It is unsigned, so at this point, it can't be negative.
+	MsgIndex uint64 `json:"msg_index"`
+
+	// Only allowing messages with a single signer
+	Signature           []byte                  `json:"signature"`
 	SignModeTxData      SignModeData            `json:"sign_mode_tx_data"`
 	TxData              ExplicitTxData          `json:"tx_data"`
 	SignatureData       SimplifiedSignatureData `json:"signature_data"`
