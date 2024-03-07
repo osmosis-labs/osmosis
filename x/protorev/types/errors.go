172e8e4113e60d1d2aea496ae1e223a2ff15bced
Dev Ojha <dojha@berkeley.edu>
2024-03-07 19:48:33 +0100

    Make a protorev error a constant

--- x/protorev/types/errors.go
+++ x/protorev/types/errors.go
@@ -10,3 +10,5 @@ type NoPoolForDenomPairError struct {
 func (e NoPoolForDenomPairError) Error() string {
 	return fmt.Sprintf("highest liquidity pool between base %s and match denom %s not found", e.BaseDenom, e.MatchDenom)
 }
+
+var ErrRouteDoubleContainsPool = fmt.Errorf("cannot be trading on the same pool twice")
