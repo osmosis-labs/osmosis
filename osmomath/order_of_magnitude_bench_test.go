78e2a8899714594018727a9fda95d75425d6435d
Roman <roman@osmosis.team>
2024-03-13 23:37:53 -0600

    clean up

--- osmomath/order_of_magnitude_bench_test.go
+++ osmomath/order_of_magnitude_bench_test.go
@@ -8,7 +8,6 @@ var (
 
 // go test -benchmem -run=^$ -bench ^BenchmarkGetPrecomputeOrderOfMagnitude$ github.com/osmosis-labs/osmosis/osmomath -count=6
 func BenchmarkGetPrecomputeOrderOfMagnitude(b *testing.B) {
-
 	for i := 0; i < b.N; i++ {
 		_ = GetPrecomputeOrderOfMagnitude(testAmount)
 	}
@@ -16,9 +15,6 @@ func BenchmarkGetPrecomputeOrderOfMagnitude(b *testing.B) {
 
 // go test -benchmem -run=^$ -benchmem -bench ^BenchmarkOrderOfMagnitude$ github.com/osmosis-labs/osmosis/osmomath -count=6 > old
 func BenchmarkOrderOfMagnitude(b *testing.B) {
-	// Create a test amount. Change this based on what you expect to be typical inputs for your application.
-	testAmount := NewInt(1234567890323344555) // You can adjust this value for different benchmark scenarios.
-
 	for i := 0; i < b.N; i++ {
 		_ = OrderOfMagnitude(testAmount.ToLegacyDec())
 	}
