5483bf2476959d1496c9800ffe70f8123c44a0eb
Renat Mukhametzanov <renatmuk@nitka.com>
2024-03-21 19:42:00 +0400

    chore: Remove unused errors

--- x/bridge/observer/bitcoin/errors.go
+++ x/bridge/observer/bitcoin/errors.go
@@ -5,9 +5,7 @@ import (
 )
 
 var (
-	ErrInvalidCfg           = errorsmod.Register(ModuleNameObserver, 1, "invalid configuration")
-	ErrRpcClient            = errorsmod.Register(ModuleNameObserver, 2, "rpc client error")
-	ErrTxInvalidDestination = errorsmod.Register(ModuleNameObserver, 3, "invalid destination in tx")
-	ErrBlockFetch           = errorsmod.Register(ModuleNameObserver, 4, "failed to fetch block")
-	ErrBlockUnavailable     = errorsmod.Register(ModuleNameObserver, 5, "block not available")
+	ErrInvalidCfg       = errorsmod.Register(ModuleNameObserver, 1, "invalid configuration")
+	ErrRpcClient        = errorsmod.Register(ModuleNameObserver, 2, "rpc client error")
+	ErrBlockUnavailable = errorsmod.Register(ModuleNameObserver, 3, "block not available")
 )
