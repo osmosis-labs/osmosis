b49ac048c500763f662de0db17d0b384332f161c
Adam Tucker <adamleetucker@outlook.com>
2024-03-24 15:54:14 -0600

    add usdc to accounts for in place testnet

--- app/app.go
+++ app/app.go
@@ -594,6 +594,7 @@ func InitOsmosisAppForTestnet(app *OsmosisApp, newValAddr bytes.HexBytes, newVal
 
 	defaultCoins := sdk.NewCoins(
 		sdk.NewInt64Coin("ibc/0CD3A0285E1341859B5E86B6AB7682F023D03E97607CCC1DC95706411D866DF7", 1000000000000), // DAI
+		sdk.NewInt64Coin("ibc/498A0751C798A0D9A389AA3691123DADA57DAA4FE165D5C75894505B876BA6E4", 1000000000000), // USDC, for pool creation fee
 		sdk.NewInt64Coin("uosmo", 1000000000000),
 		sdk.NewInt64Coin("uion", 1000000000))
 
