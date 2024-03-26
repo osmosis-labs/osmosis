2e7a3675b758add551fef9aefa5932684a44c611
Adam Tucker <adam@osmosis.team>
2024-03-26 10:27:19 -0600

    fix(e2e): start nodes at same time, error if done condition not met (#7848)
    * error if done condition not met
    
    * better approach
    
    * speed up and fix
    
    (cherry picked from commit ae33741e0289563ffef4154ff3dada692450c904)
    
    # Conflicts:
    #	tests/e2e/configurer/base.go

--- tests/e2e/configurer/base.go
+++ tests/e2e/configurer/base.go
@@ -76,27 +76,30 @@ func (bc *baseConfigurer) RunValidators() error {
 func (bc *baseConfigurer) runValidators(chainConfig *chain.Config) error {
 	bc.t.Logf("starting %s validator containers...", chainConfig.Id)
 
+	errCh := make(chan error, len(chainConfig.NodeConfigs))
 	var wg sync.WaitGroup
-	errCh := make(chan error, len(chainConfig.NodeConfigs)) // Buffer the channel to avoid blocking
 
-	// Increment the WaitGroup counter for each node
-	wg.Add(len(chainConfig.NodeConfigs))
-
-	// Iterate over each node
 	for _, node := range chainConfig.NodeConfigs {
+<<<<<<< HEAD
 		go func(n *chain.NodeConfig) {
 			defer wg.Done()       // Decrement the WaitGroup counter when the goroutine is done
 			errCh <- n.Run(false) // Run the node and send any error to the channel
+=======
+		wg.Add(1)
+		go func(node *chain.NodeConfig) {
+			defer wg.Done()
+			if err := node.Run(false); err != nil {
+				errCh <- err
+			}
+>>>>>>> ae33741e (fix(e2e): start nodes at same time, error if done condition not met (#7848))
 		}(node)
 	}
 
-	// Wait for all goroutines to finish
+	// Wait for all goroutines to complete
 	wg.Wait()
-
-	// Close the error channel since all goroutines are done sending errors
 	close(errCh)
 
-	// Collect errors from the channel
+	// Check if any of the goroutines returned an error
 	for err := range errCh {
 		if err != nil {
 			return err
