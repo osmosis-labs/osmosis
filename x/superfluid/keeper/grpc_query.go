9974050fe11de5cf3be795edaf4e999261d4e1a8
Adam Tucker <adam@osmosis.team>
2024-03-27 10:03:40 -0600

    fix: SuperfluidDelegationsByValidatorDenom does not return equivalent staked amount (#7857)
    * fix SuperfluidDelegationsByValidatorDenom query
    
    * add changelog
    
    (cherry picked from commit f3ff198b223c9645bfc82ce3f4082fa5f71f43d3)
    
    # Conflicts:
    #	CHANGELOG.md

--- x/superfluid/keeper/grpc_query.go
+++ x/superfluid/keeper/grpc_query.go
@@ -263,11 +263,9 @@ func (q Querier) SuperfluidDelegationsByDelegator(goCtx context.Context, req *ty
 		if err != nil {
 			return nil, err
 		}
+
 		coin := sdk.NewCoin(appparams.BaseCoinUnit, equivalentAmount)
 
-		if err != nil {
-			return nil, err
-		}
 		res.SuperfluidDelegationRecords = append(res.SuperfluidDelegationRecords,
 			types.SuperfluidDelegationRecord{
 				DelegatorAddress:       req.DelegatorAddress,
@@ -428,11 +426,21 @@ func (q Querier) SuperfluidDelegationsByValidatorDenom(goCtx context.Context, re
 
 	for _, lock := range periodLocks {
 		lockedCoins := sdk.NewCoin(req.Denom, lock.GetCoins().AmountOf(req.Denom))
+		baseDenom := lock.Coins.GetDenomByIndex(0)
+
+		equivalentAmount, err := q.Keeper.GetSuperfluidOSMOTokens(ctx, baseDenom, lockedCoins.Amount)
+		if err != nil {
+			return nil, err
+		}
+
+		coin := sdk.NewCoin(appparams.BaseCoinUnit, equivalentAmount)
+
 		res.SuperfluidDelegationRecords = append(res.SuperfluidDelegationRecords,
 			types.SuperfluidDelegationRecord{
-				DelegatorAddress: lock.GetOwner(),
-				ValidatorAddress: req.ValidatorAddress,
-				DelegationAmount: lockedCoins,
+				DelegatorAddress:       lock.GetOwner(),
+				ValidatorAddress:       req.ValidatorAddress,
+				DelegationAmount:       lockedCoins,
+				EquivalentStakedAmount: &coin,
 			},
 		)
 	}
