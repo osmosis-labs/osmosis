6ba894fcc965b875193846b8037fc22350708f4f
Roman <roman@osmosis.team>
2024-03-29 15:44:11 -0600

    feat: SQS state streamer (backport #7879)

---
+++ ingest/sqs/domain/service.go
@@ -0,0 +1,52 @@
+package domain
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	poolmanagertypes "github.com/osmosis-labs/osmosis/v23/x/poolmanager/types"
+)
+
+// BlockPoolUpdateTracker is an interface for tracking
+// the pools that were updated in a block.
+// It persists the pools using "Track" methods in its internal state.
+// It tracks the latest pool update, discarding the previous updates.
+// Only on Reset, the internal state is cleared.
+type BlockPoolUpdateTracker interface {
+	// TrackConcentrated tracks the concentrated pool.
+	TrackConcentrated(pool poolmanagertypes.PoolI)
+
+	// TrackConcentratedPoolIDTickChange tracks the concentrated pool ID tick change.
+	// Due to internal implementation, it is non-trivial to apply tick changes.
+	// As a result, we track the pool ID tick change and read the pool with all of its ticks
+	// if at least one tick change was applied within the block.
+	TrackConcentratedPoolIDTickChange(poolID uint64)
+
+	// TrackCFMM tracks the CFMM pool.
+	TrackCFMM(pool poolmanagertypes.PoolI)
+
+	// TrackCosmWasm tracks the CosmWasm pool.
+	TrackCosmWasm(pool poolmanagertypes.PoolI)
+
+	// GetConcentratedPools returns the tracked concentrated pools.
+	GetConcentratedPools() []poolmanagertypes.PoolI
+
+	// GetConcentratedPoolIDTickChange returns the tracked concentrated pool ID tick change.
+	GetConcentratedPoolIDTickChange() map[uint64]struct{}
+
+	// GetCFMMPools returns the tracked CFMM pools.
+	GetCFMMPools() []poolmanagertypes.PoolI
+
+	// GetCosmWasmPools returns the tracked CosmWasm pools.
+	GetCosmWasmPools() []poolmanagertypes.PoolI
+
+	// Reset clears the internal state.
+	Reset()
+}
+
+// NodeStatusChecker is an interface for checking the node status.
+type NodeStatusChecker interface {
+	// IsNodeSyncing checks if the node is syncing.
+	// Returns true if the node is syncing, false otherwise.
+	// Returns error if the node syncing status cannot be determined.
+	IsNodeSyncing(ctx sdk.Context) (bool, error)
+}
