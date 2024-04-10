32b21d2ce1f999cd4415d5249a106814ca591c04
Adam Tucker <adamleetucker@outlook.com>
2024-04-10 15:24:05 -0600

    add most truncated pools

--- x/concentrated-liquidity/types/migration.go
+++ x/concentrated-liquidity/types/migration.go
@@ -1,13 +1,30 @@
 package types
 
-// TODO: Replace with pools we actually want to migrate
 var MigratedSpreadFactorAccumulatorPoolIDs = map[uint64]struct{}{
-	1423: {},
-	1213: {},
-	1298: {},
-	1297: {},
-	1292: {},
-	1431: {},
+	// token0 usdc
+	// token1 dai
+	// liquidity 55.8k
+	1260: {},
+	// token0 eth
+	// token1 usdc
+	// liquidity 449k
+	1264: {},
+	// token0 osmo
+	// token1 eth
+	// liquidity 395k
+	1477: {},
+	// token0 osmo
+	// token1 eth
+	// liquidity 226k
+	1134: {},
+	// token0 usdt
+	// token1 dai
+	// liquidity 4.85k
+	1261: {},
+	// token0 eth
+	// token1 osmo
+	// liquidity 554k
+	1281: {},
 }
 
 var MigratedIncentiveAccumulatorPoolIDs = map[uint64]struct{}{
