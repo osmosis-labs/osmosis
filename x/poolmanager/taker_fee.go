0bced610a59133f83d65243f761644cda9cc820a
Dev Ojha <dojha@berkeley.edu>
2024-04-26 09:53:41 -0700

    Add local cache inside the takerfee

--- x/poolmanager/taker_fee.go
+++ x/poolmanager/taker_fee.go
@@ -1,6 +1,8 @@
 package poolmanager
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -13,9 +15,13 @@ import (
 )
 
 func (k Keeper) GetDefaultTakerFee(ctx sdk.Context) sdk.Dec {
-	var defaultTakerFee sdk.Dec
-	k.paramSpace.Get(ctx, types.KeyDefaultTakerFee, &defaultTakerFee)
-	return defaultTakerFee
+	defaultTakerFeeBz := k.paramSpace.GetRaw(ctx, types.KeyDefaultTakerFee)
+	if !bytes.Equal(defaultTakerFeeBz, k.defaultTakerFeeBz) {
+		var defaultTakerFeeValue sdk.Dec
+		json.Unmarshal(defaultTakerFeeBz, &defaultTakerFeeValue)
+		k.defaultTakerFeeVal = defaultTakerFeeValue
+	}
+	return k.defaultTakerFeeVal
 }
 
 // SetDenomPairTakerFee sets the taker fee for the given trading pair.
