a2714b7522d136e0a8fbfb568ed10560cc281d87
Nicolas Lara <nicolaslara@gmail.com>
2024-05-29 09:48:27 +0200

    updated to work with latest wasmd

--- app/keepers/keepers.go
+++ app/keepers/keepers.go
@@ -570,7 +570,16 @@ func (appKeepers *AppKeepers) InitNormalKeepers(
 
 	// The last arguments can contain custom message handlers, and custom query handlers,
 	// if we want to allow any custom callbacks
-	supportedFeatures := "iterator,staking,stargate,osmosis,cosmwasm_1_1,cosmwasm_1_2,cosmwasm_1_4,cosmwasm_2_0"
+	supportedFeatures := []string{
+		"iterator",
+		"staking",
+		"stargate",
+		"osmosis",
+		"cosmwasm_1_1",
+		"cosmwasm_1_2",
+		"cosmwasm_1_4",
+		"cosmwasm_2_0",
+	}
 
 	wasmOpts = append(owasm.RegisterCustomPlugins(appKeepers.BankKeeper, appKeepers.TokenFactoryKeeper), wasmOpts...)
 	wasmOpts = append(owasm.RegisterStargateQueries(*bApp.GRPCQueryRouter(), appCodec), wasmOpts...)
