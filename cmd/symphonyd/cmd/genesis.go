a05516a27c545f7c8bc36ea2007f9d7967fc70a6
yuraolex <yurii.oleksyshyn@gmail.com>
2024-06-10 18:21:02 +0300

    Added genesis configuration for mint module on mainnet

--- cmd/symphonyd/cmd/genesis.go
+++ cmd/symphonyd/cmd/genesis.go
@@ -360,78 +360,22 @@ func MainnetGenesisParams() GenesisParams {
 	genParams.StakingParams.MinCommissionRate = osmomath.MustNewDecFromStr("0.05")
 
 	genParams.MintParams = minttypes.DefaultParams()
-	genParams.MintParams.EpochIdentifier = "day"                                                     // 1 day
-	genParams.MintParams.GenesisEpochProvisions = osmomath.NewDec(300_000_000_000_000).QuoInt64(365) // 300M * 10^6 / 365 = ~821917.8082191781 * 10^6
+	genParams.MintParams.EpochIdentifier = "week"
+	genParams.MintParams.GenesisEpochProvisions = osmomath.NewDec(150_250)
 	genParams.MintParams.MintDenom = genParams.NativeCoinMetadatas[0].Base
-	genParams.MintParams.ReductionFactor = osmomath.NewDec(2).QuoInt64(3) // 2/3
-	genParams.MintParams.ReductionPeriodInEpochs = 365                    // 1 year (screw leap years)
+	genParams.MintParams.ReductionFactor = osmomath.NewDecWithPrec(5, 1) // 0.5
+	genParams.MintParams.ReductionPeriodInEpochs = 208                   // 4 years
 	genParams.MintParams.DistributionProportions = minttypes.DistributionProportions{
-		Staking:          osmomath.MustNewDecFromStr("0.25"), // 25%
-		DeveloperRewards: osmomath.MustNewDecFromStr("0.25"), // 25%
-		PoolIncentives:   osmomath.MustNewDecFromStr("0.45"), // 45%
-		CommunityPool:    osmomath.MustNewDecFromStr("0.05"), // 5%
+		Staking:          osmomath.MustNewDecFromStr("0.8"), // 80%
+		DeveloperRewards: osmomath.MustNewDecFromStr("0.2"), // 20%
+		PoolIncentives:   osmomath.MustNewDecFromStr("0.0"), // 0%
+		CommunityPool:    osmomath.MustNewDecFromStr("0.0"), // 0%
 	}
 	genParams.MintParams.MintingRewardsDistributionStartEpoch = 1
 	genParams.MintParams.WeightedDeveloperRewardsReceivers = []minttypes.WeightedAddress{
 		{
-			Address: "symphony1u7ryvx794sy5yqwezfryygsce84q287ts98n66",
-			Weight:  osmomath.MustNewDecFromStr("0.2887"),
-		},
-		{
-			Address: "symphony1zrmuw4xux344w4k9pw93qs8d0d7kc0fnhxw4wd",
-			Weight:  osmomath.MustNewDecFromStr("0.2290"),
-		},
-		{
-			Address: "symphony1t9vjrxn6cwdkuf990sncq7akqsz26feaz5euxt",
-			Weight:  osmomath.MustNewDecFromStr("0.1625"),
-		},
-		{
-			Address: "symphony172qywhy2qxcnkvr6vcal23ntz645h20qe5880r",
-			Weight:  osmomath.MustNewDecFromStr("0.109"),
-		},
-		{
-			Address: "symphony195ds5rrxcqcwflj692e6gmykhl9vu0r0qs7tt5",
-			Weight:  osmomath.MustNewDecFromStr("0.0995"),
-		},
-		{
-			Address: "symphony1f2jp2q4qq0f8nlmp0v3ah96h3kqjj0vheprf7q",
-			Weight:  osmomath.MustNewDecFromStr("0.06"),
-		},
-		{
-			Address: "symphony1k27t46ehr7y80ktrtmn9grmc9wkw27ds9hq005",
-			Weight:  osmomath.MustNewDecFromStr("0.015"),
-		},
-		{
-			Address: "symphony1dhtgp9726rx5zv9079xz2wz43pec484akwktn5",
-			Weight:  osmomath.MustNewDecFromStr("0.01"),
-		},
-		{
-			Address: "symphony1fqqucy9y2adaapyjze5g0hv40vp6rt2kt0cjts",
-			Weight:  osmomath.MustNewDecFromStr("0.0075"),
-		},
-		{
-			Address: "symphony192953mpz44nn76vgmknt75vspsnv2k6d9dyc4w",
-			Weight:  osmomath.MustNewDecFromStr("0.007"),
-		},
-		{
-			Address: "symphony1jcchx5enuex05al39y25gl6hyerwj74unntaqx",
-			Weight:  osmomath.MustNewDecFromStr("0.005"),
-		},
-		{
-			Address: "symphony1pt2knp6s8exw7j28gjgmwr2wvw4suc3w8ncunl",
-			Weight:  osmomath.MustNewDecFromStr("0.0025"),
-		},
-		{
-			Address: "symphony1c4zx9pmtn3j4a2eus2mmpclpllpqzgzezte7yz",
-			Weight:  osmomath.MustNewDecFromStr("0.0025"),
-		},
-		{
-			Address: "symphony1d6fwytjdlwzg7hg26zpzrl4y3f5ykft9xetlmk",
-			Weight:  osmomath.MustNewDecFromStr("0.001"),
-		},
-		{
-			Address: "symphony1gmyrqx37tvpmqpkvga6ex4jtv0920hfa3pndqz",
-			Weight:  osmomath.MustNewDecFromStr("0.0008"),
+			Address: "symphony1g6pxgl8g0rnk7q86j9zh7yxsqdsn7jvdmc8fkx",
+			Weight:  osmomath.MustNewDecFromStr("1.0"),
 		},
 	}
 
