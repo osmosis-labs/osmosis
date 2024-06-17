7dd129ac7a8f4b040d9ecb76123e1868c219c516
Dev Ojha <ValarDragon@users.noreply.github.com>
2024-06-17 11:17:46 -0400

    perf: Speedup one JSON unmarshal (#8398)
    * Speedup one JSON unmarshal
    
    * Update changelog
    
    (cherry picked from commit 837b58c34383ba785b517c0c729aea2b77b2c2a8)
    
    # Conflicts:
    #	x/ibc-rate-limit/ibc_module.go

--- x/ibc-rate-limit/ibc_module.go
+++ x/ibc-rate-limit/ibc_module.go
@@ -10,12 +10,20 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
+<<<<<<< HEAD
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 	transfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
 	clienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
 	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
 	porttypes "github.com/cosmos/ibc-go/v7/modules/core/05-port/types"
 	"github.com/cosmos/ibc-go/v7/modules/core/exported"
+=======
+	capabilitytypes "github.com/cosmos/ibc-go/modules/capability/types"
+	clienttypes "github.com/cosmos/ibc-go/v8/modules/core/02-client/types"
+	channeltypes "github.com/cosmos/ibc-go/v8/modules/core/04-channel/types"
+	porttypes "github.com/cosmos/ibc-go/v8/modules/core/05-port/types"
+	"github.com/cosmos/ibc-go/v8/modules/core/exported"
+>>>>>>> 837b58c3 (perf: Speedup one JSON unmarshal (#8398))
 
 	"github.com/osmosis-labs/osmosis/v25/x/ibc-rate-limit/types"
 )
@@ -110,12 +118,17 @@ func (im *IBCModule) OnChanCloseConfirm(
 	return im.app.OnChanCloseConfirm(ctx, portID, channelID)
 }
 
+type receiverParser struct {
+	Receiver string `protobuf:"bytes,4,opt,name=receiver,proto3" json:"receiver,omitempty"`
+}
+
 func ValidateReceiverAddress(packet exported.PacketI) error {
-	var packetData transfertypes.FungibleTokenPacketData
-	if err := json.Unmarshal(packet.GetData(), &packetData); err != nil {
+	var receiverObj receiverParser
+
+	if err := json.Unmarshal(packet.GetData(), &receiverObj); err != nil {
 		return err
 	}
-	if len(packetData.Receiver) >= 4096 {
+	if len(receiverObj.Receiver) >= 4096 {
 		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "IBC Receiver address too long. Max supported length is %d", 4096)
 	}
 	return nil
