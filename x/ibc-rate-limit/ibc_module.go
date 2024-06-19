9ce43a832306c8b9b5ab9a34914cb3a6d16b3fad
Adam Tucker <adamleetucker@outlook.com>
2024-06-19 13:34:30 -0600

    merge conflict

--- x/ibc-rate-limit/ibc_module.go
+++ x/ibc-rate-limit/ibc_module.go
@@ -11,7 +11,6 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
-	transfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
 	clienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
 	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
 	porttypes "github.com/cosmos/ibc-go/v7/modules/core/05-port/types"
