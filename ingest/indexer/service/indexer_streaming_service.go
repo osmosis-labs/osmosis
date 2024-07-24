7b1c98feb3fd074335a1dd13bdc942bf3fe36105
Calvin <1727450+cryptomatictrader@users.noreply.github.com>
2024-07-24 15:43:23 -0700

    fix: [indexer] adjust token in amount for spread factor for CL pools only (#8541)
    * adjust token in amount for spread factor for CL pools only
    
    * Issues addressed as suggested in PR review
    
    ---------
    
    Co-authored-by: Calvin <calvin@osmosis.team>
    (cherry picked from commit bfe47ba301218ca2f7df10cb8bfa9edc49bec3da)
    
    # Conflicts:
    #	ingest/indexer/service/indexer_streaming_service.go

--- ingest/indexer/service/indexer_streaming_service.go
+++ ingest/indexer/service/indexer_streaming_service.go
@@ -3,10 +3,19 @@ package service
 import (
 	"context"
 	"encoding/hex"
+	"errors"
+	"strconv"
 	"strings"
 	"sync"
 
+<<<<<<< HEAD
 	"github.com/cometbft/cometbft/abci/types"
+=======
+	"cosmossdk.io/log"
+	storetypes "cosmossdk.io/store/types"
+	abci "github.com/cometbft/cometbft/abci/types"
+
+>>>>>>> bfe47ba3 (fix: [indexer] adjust token in amount for spread factor for CL pools only (#8541))
 	"github.com/cometbft/cometbft/crypto/tmhash"
 	"github.com/cometbft/cometbft/libs/log"
 	"github.com/cosmos/cosmos-sdk/baseapp"
@@ -14,13 +23,25 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/gogoproto/proto"
 
+	"github.com/osmosis-labs/osmosis/osmomath"
+	concentratedliquiditytypes "github.com/osmosis-labs/osmosis/v25/x/concentrated-liquidity/types"
+	gammtypes "github.com/osmosis-labs/osmosis/v25/x/gamm/types"
+	poolmanagertypes "github.com/osmosis-labs/osmosis/v25/x/poolmanager/types"
+
 	commondomain "github.com/osmosis-labs/osmosis/v25/ingest/common/domain"
 	"github.com/osmosis-labs/osmosis/v25/ingest/indexer/domain"
 	"github.com/osmosis-labs/osmosis/v25/ingest/indexer/service/blockprocessor"
 	sqsdomain "github.com/osmosis-labs/osmosis/v25/ingest/sqs/domain"
 )
 
+<<<<<<< HEAD
 var _ baseapp.StreamingService = (*indexerStreamingService)(nil)
+=======
+var (
+	_      storetypes.ABCIListener = (*indexerStreamingService)(nil)
+	oneDec                         = osmomath.OneDec()
+)
+>>>>>>> bfe47ba3 (fix: [indexer] adjust token in amount for spread factor for CL pools only (#8541))
 
 // ind is a streaming service that processes block data and ingests it into the indexer
 type indexerStreamingService struct {
@@ -137,9 +158,31 @@ func (s *indexerStreamingService) publishTxn(ctx context.Context, req types.Requ
 	feeTx, _ := tx.(sdk.FeeTx)
 	fee := feeTx.GetFee()
 
+<<<<<<< HEAD
 	// Message type
 	txMessages := tx.GetMsgs()
 	msgType := proto.MessageName(txMessages[0])
+=======
+		// Include these events only:
+		// - token_swapped
+		// - pool_joined
+		// - pool_exited
+		// - create_position
+		// - withdraw_position
+		events := res.GetEvents()
+		var includedEvents []domain.EventWrapper
+		for i, event := range events {
+			err := s.adjustTokenInAmountBySpreadFactor(ctx, &event)
+			if err != nil {
+				s.logger.Error("Error adjusting amount by spread factor", "error", err)
+				continue
+			}
+			eventType := event.Type
+			if eventType == gammtypes.TypeEvtTokenSwapped || eventType == gammtypes.TypeEvtPoolJoined || eventType == gammtypes.TypeEvtPoolExited || eventType == concentratedliquiditytypes.TypeEvtCreatePosition || eventType == concentratedliquiditytypes.TypeEvtWithdrawPosition {
+				includedEvents = append(includedEvents, domain.EventWrapper{Index: i, Event: event})
+			}
+		}
+>>>>>>> bfe47ba3 (fix: [indexer] adjust token in amount for spread factor for CL pools only (#8541))
 
 	// Include these events only:
 	// - token_swapped
@@ -171,12 +214,84 @@ func (s *indexerStreamingService) publishTxn(ctx context.Context, req types.Requ
 	return s.client.PublishTransaction(sdkCtx, txn)
 }
 
+<<<<<<< HEAD
 // ListenEndBlock implements baseapp.StreamingService.
 func (s *indexerStreamingService) ListenEndBlock(ctx context.Context, req types.RequestEndBlock, res types.ResponseEndBlock) error {
 	// Reset the transaction index id on end block
 	defer func() {
 		s.txnIndexId = 0
 	}()
+=======
+// adjustAmountBySpreadFactor adjusts the amount by the spread factor.
+// This is done to adjust the amount of tokens in the token_swapped event by the spread factor,
+// as the amount in the event is the amount AFTER the spread factor is applied.
+// therefore, we need to adjust the amount by the spread factor to get the amount BEFORE the spread factor is applied.
+// NOTE: This applies to CL pools only
+func (s *indexerStreamingService) adjustTokenInAmountBySpreadFactor(ctx context.Context, event *abci.Event) error {
+	if event.Type != gammtypes.TypeEvtTokenSwapped {
+		return nil
+	}
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	var poolIdStr string
+	var afterTokensIn string
+	var afterTokensInIndex int
+	attributes := event.Attributes
+	// Find the pool id and tokens in attributes from the token_swapped
+	for index, attribute := range attributes {
+		if poolIdStr != "" && afterTokensIn != "" {
+			break
+		}
+		if attribute.Key == concentratedliquiditytypes.AttributeKeyPoolId {
+			poolIdStr = attribute.Value
+		}
+		if attribute.Key == concentratedliquiditytypes.AttributeKeyTokensIn {
+			afterTokensIn = attribute.Value
+			afterTokensInIndex = index
+		}
+	}
+	if poolIdStr == "" || afterTokensIn == "" {
+		return errors.New("pool id or tokens in not found")
+	}
+	poolId, err := strconv.ParseInt(poolIdStr, 10, 64)
+	if err != nil {
+		return errors.New("failed to parse pool id")
+	}
+	// Get the pool, pool type and its spread factor
+	pool, err := s.keepers.PoolManagerKeeper.GetPool(sdkCtx, uint64(poolId))
+	if err != nil {
+		return errors.New("failed to get pool")
+	}
+	poolType := pool.GetType()
+	// Adjustment required only for CL pools
+	if poolType != poolmanagertypes.Concentrated {
+		return nil
+	}
+	spreadFactor := pool.GetSpreadFactor(sdkCtx)
+	coins, err := sdk.ParseCoinsNormalized(afterTokensIn)
+	if err != nil {
+		return errors.New("failed to parse tokens in")
+	}
+	tokenInAmount := coins[0].Amount.ToLegacyDec()
+	// Adjust the amount by the spread factor, i.e. before = after/(1 - spreadFactor)
+	adjustedAmt := tokenInAmount.Quo(oneDec.Sub(spreadFactor))
+	attributes[afterTokensInIndex].Value = adjustedAmt.String()
+	return nil
+}
+
+// ListenFinalizeBlock updates the streaming service with the latest FinalizeBlock messages
+func (s *indexerStreamingService) ListenFinalizeBlock(ctx context.Context, req abci.RequestFinalizeBlock, res abci.ResponseFinalizeBlock) error {
+	// Log the status only for the first block
+	// Avoid subsequent blocks to avoid spamming the logs
+	if s.blockProcessStrategyManager.ShouldPushAllData() {
+		sdkCtx := sdk.UnwrapSDKContext(ctx)
+		sdkCtx.Logger().Info("Starting indexer ingest ListenFinalizeBlock", "height", sdkCtx.BlockHeight())
+
+		defer func() {
+			sdkCtx.Logger().Info("Finished indexer ingest ListenFinalizeBlock", "height", sdkCtx.BlockHeight())
+		}()
+	}
+
+>>>>>>> bfe47ba3 (fix: [indexer] adjust token in amount for spread factor for CL pools only (#8541))
 	// Publish the block data
 	err := s.publishBlock(ctx, req)
 	if err != nil {
