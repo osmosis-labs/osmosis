42ca0651bf9ff933afa4951d26c5ff8199f9cb40
Calvin <calvin@osmosis.team>
2024-07-25 03:47:35 +0000

    Cherry-pick PR to include token liquidity in the token_swapped event

--- ingest/indexer/service/indexer_streaming_service.go
+++ ingest/indexer/service/indexer_streaming_service.go
@@ -181,6 +181,41 @@ func (s *indexerStreamingService) adjustTokenInAmountBySpreadFactor(ctx context.
 	return nil
 }
 
+func (s *indexerStreamingService) addTokenLiquidity(ctx context.Context, event *types.Event) error {
+	if event.Type != gammtypes.TypeEvtTokenSwapped {
+		return nil
+	}
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	var poolIdStr string
+	attributes := event.Attributes
+	// Find the pool id from the token_swapped
+	for _, attribute := range attributes {
+		if attribute.Key == concentratedliquiditytypes.AttributeKeyPoolId {
+			poolIdStr = attribute.Value
+			break
+		}
+	}
+	if poolIdStr == "" {
+		return errors.New("pool id not found")
+	}
+	poolId, err := strconv.ParseInt(poolIdStr, 10, 64)
+	if err != nil {
+		return err
+	}
+	coins, err := s.keepers.PoolManagerKeeper.GetTotalPoolLiquidity(sdkCtx, uint64(poolId))
+	if err != nil {
+		return err
+	}
+	// Store the liquidity of the token in the attributes map of the event, keyed by "liquidity_" + coin.Denom
+	for _, coin := range coins {
+		event.Attributes = append(event.Attributes, types.EventAttribute{
+			Key:   "liquidity_" + coin.Denom,
+			Value: coin.Amount.String(),
+		})
+	}
+	return nil
+}
+
 // publishTxn publishes the transaction data to the indexer.
 func (s *indexerStreamingService) publishTxn(ctx context.Context, req types.RequestDeliverTx, res types.ResponseDeliverTx) error {
 	sdkCtx := sdk.UnwrapSDKContext(ctx)
@@ -217,6 +252,11 @@ func (s *indexerStreamingService) publishTxn(ctx context.Context, req types.Requ
 		err := s.adjustTokenInAmountBySpreadFactor(ctx, &event)
 		if err != nil {
 			s.logger.Error("Error adjusting amount by spread factor", "error", err)
+			continue
+		}
+		err = s.addTokenLiquidity(ctx, &event)
+		if err != nil {
+			s.logger.Error("Error adding reserves to event", "error", err)
 			continue
 		}
 		eventType := event.Type
