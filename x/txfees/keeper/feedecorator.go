0860ccc6446c6016b342a955ebb9fcf0c99e0e0b
ghost <paddymchale@hotmail.com>
2024-08-01 17:53:33 +0100

    fix: only get height greater

--- x/txfees/keeper/feedecorator.go
+++ x/txfees/keeper/feedecorator.go
@@ -62,8 +62,7 @@ func (mfd MempoolFeeDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate b
 	}
 
 	// TODO: In v26, move this code to the correct spot, and make it CheckTX only.
-	bh := ctx.BlockHeight()
-	if bh <= 16841115 || bh >= 17004043 {
+	if ctx.BlockHeight() >= 17004043 {
 		msgs := tx.GetMsgs()
 		for _, msg := range msgs {
 			// If one of the msgs is an IBC Transfer msg, limit it's size due to current spam potential.
