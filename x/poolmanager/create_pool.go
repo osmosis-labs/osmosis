27cfc8cdc8ad8e06b0fca2c70c5dd04192e92072
Adam Tucker <adamleetucker@outlook.com>
2024-08-11 13:29:16 -0600

    exempt addresses pool creation fee

--- x/poolmanager/create_pool.go
+++ x/poolmanager/create_pool.go
@@ -45,10 +45,9 @@ func (k Keeper) CreatePool(ctx sdk.Context, msg types.CreatePoolMsg) (uint64, er
 		return 0, err
 	}
 
-	// Send pool creation fee from pool creator to community pool
-	poolCreationFee := k.GetParams(ctx).PoolCreationFee
 	sender := msg.PoolCreator()
-	if err := k.communityPoolKeeper.FundCommunityPool(ctx, poolCreationFee, sender); err != nil {
+	err = k.fundCommunityPoolIfNotWhitelisted(ctx, sender)
+	if err != nil {
 		return 0, err
 	}
 
@@ -248,4 +247,30 @@ func parsePoolRouteWithKey(key []byte, value []byte) (types.ModuleRoute, error) 
 	}
 	parsedValue.PoolId = poolId
 	return parsedValue, nil
+}
+
+// fundCommunityPoolIfNotWhitelisted checks if the sender is in the UnrestrictedPoolCreatorWhitelist
+// and funds the community pool if the sender is not whitelisted.
+func (k Keeper) fundCommunityPoolIfNotWhitelisted(ctx sdk.Context, sender sdk.AccAddress) error {
+	// Get the UnrestrictedPoolCreatorWhitelist from the concentrated pool module
+	whitelist := k.concentratedKeeper.GetWhitelistedAddresses(ctx)
+
+	// Check if sender is in the whitelist
+	isWhitelisted := false
+	for _, addr := range whitelist {
+		if addr == sender.String() {
+			isWhitelisted = true
+			break
+		}
+	}
+
+	// Run FundCommunityPool logic if sender is not in the whitelist
+	if !isWhitelisted {
+		poolCreationFee := k.GetParams(ctx).PoolCreationFee
+		if err := k.communityPoolKeeper.FundCommunityPool(ctx, poolCreationFee, sender); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
