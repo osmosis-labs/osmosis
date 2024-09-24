d11e70af18e95b00ee63ee5d460cce5da3bcc9cd
ghost <paddymchale@hotmail.com>
2024-09-24 15:15:04 +0100

    fix: upgrade handler code gen

--- app/upgrades/v27/upgrades.go
+++ app/upgrades/v27/upgrades.go
@@ -1,8 +1,9 @@
 package v27
 
 import (
+	"context"
+
 	upgradetypes "cosmossdk.io/x/upgrade/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 
 	"github.com/osmosis-labs/osmosis/v26/app/keepers"
@@ -15,7 +16,7 @@ func CreateUpgradeHandler(
 	bpm upgrades.BaseAppParamManager,
 	keepers *keepers.AppKeepers,
 ) upgradetypes.UpgradeHandler {
-	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+	return func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		// Run migrations before applying any other state changes.
 		// NOTE: DO NOT PUT ANY STATE CHANGES BEFORE RunMigrations().
 		migrations, err := mm.RunMigrations(ctx, configurator, fromVM)
