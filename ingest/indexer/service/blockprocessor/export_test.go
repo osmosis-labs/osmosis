01f4c8c7acd8551e019dc95e745341c1f04c35d2
Calvin <calvinchso@gmail.com>
2024-10-03 11:39:20 -0700

    feat: [indexer] add test cases to block_update_indexer_block_process_strategy (#8735)
    * add test cases to block_update_indexer_block_process_strat
    
    * enhanced pair publisher mock
    
    * renamed some vars
    
    * optimization - should publish newly created pool only
    
    * updated comments after test case updated
    
    * feat: [indexer] add test cases to full_indexer_block_process_strategy (#8736)
    
    * add test cases to full_indexer_block_process_strategy
    
    * add vars to test file to avoid code dup
    
    * promote common vars for test purposes

---
+++ ingest/indexer/service/blockprocessor/export_test.go
@@ -0,0 +1,42 @@
+package blockprocessor
+
+import (
+	"github.com/cosmos/cosmos-sdk/types"
+
+	commondomain "github.com/osmosis-labs/osmosis/v26/ingest/common/domain"
+	"github.com/osmosis-labs/osmosis/v26/ingest/indexer/domain"
+)
+
+func NewBlockUpdatesIndexerBlockProcessStrategy(blockUpdateProcessUtils commondomain.BlockUpdateProcessUtilsI, client domain.Publisher, poolExtractor commondomain.PoolExtractor, poolPairPublisher domain.PairPublisher) *blockUpdatesIndexerBlockProcessStrategy {
+	return &blockUpdatesIndexerBlockProcessStrategy{
+		blockUpdateProcessUtils: blockUpdateProcessUtils,
+		client:                  client,
+		poolExtractor:           poolExtractor,
+		poolPairPublisher:       poolPairPublisher,
+	}
+}
+
+type BlockUpdatesIndexerBlockProcessStrategy = blockUpdatesIndexerBlockProcessStrategy
+
+func (s *blockUpdatesIndexerBlockProcessStrategy) PublishCreatedPools(ctx types.Context) error {
+	return s.publishCreatedPools(ctx)
+}
+
+func NewFullIndexerBlockProcessStrategy(client domain.Publisher, keepers domain.Keepers, poolExtractor commondomain.PoolExtractor, poolPairPublisher domain.PairPublisher) *fullIndexerBlockProcessStrategy {
+	return &fullIndexerBlockProcessStrategy{
+		client:            client,
+		keepers:           keepers,
+		poolExtractor:     poolExtractor,
+		poolPairPublisher: poolPairPublisher,
+	}
+}
+
+type FullIndexerBlockProcessStrategy = fullIndexerBlockProcessStrategy
+
+func (s *fullIndexerBlockProcessStrategy) PublishAllSupplies(ctx types.Context) {
+	s.publishAllSupplies(ctx)
+}
+
+func (s *fullIndexerBlockProcessStrategy) ProcessPools(ctx types.Context) error {
+	return s.processPools(ctx)
+}
