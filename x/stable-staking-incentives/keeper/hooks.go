c326a72e8ef4bee028cf00cd545bcb099ab1ab14
yuraolex <yurii.oleksyshyn@gmail.com>
2025-03-10 17:38:24 +0200

    Cleanup

--- x/stable-staking-incentives/keeper/hooks.go
+++ x/stable-staking-incentives/keeper/hooks.go
@@ -19,7 +19,6 @@ func (k Keeper) Hooks() Hooks { return Hooks{k} }
 
 // AfterDistributeMintedCoin coins after minter module allocate assets to incentives module.
 func (h Hooks) AfterDistributeMintedCoin(ctx sdk.Context) {
-	// @Sunny, @Tony, @Dev, what comments should we keep after modifying own BeginBlocker to hooks?
 
 	// WARNING: The order of how modules interact with the default distribution module matters if the distribution module is used in a similar way to:
 	// 1. mint module or 2. custom mint modules that uses the auth module’s feeCollector to mint new tokens that uses the default distribution module.
@@ -29,10 +28,6 @@ func (h Hooks) AfterDistributeMintedCoin(ctx sdk.Context) {
 	// Also, the pool-incentives module first takes the AllocationRatio from the total inflated amount, and the remainder is used by the distribution.
 	// So the amount is relative to each other. For example, if the AllocationRatio is 0.2(20%),
 	// the distribution uses the remaining 80% to calculate–which means if the community pool is set to receive 10% of newly minted OSMO, community pool is 8% of the total inflation.
-
-	// Calculate the AllocatableAsset using the AllocationRatio and the MintedDenom,
-	// then allocate the tokens to the registered pools’ gauges.
-	// If there is no record, inflation is not drained and the all amounts are used by the distribution module’s next BeginBlock.
 	err := h.k.AllocateAsset(ctx)
 	if err != nil {
 		panic(err)
