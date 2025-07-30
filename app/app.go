1a4bdfd07ffb1b85f9c6dbd43ccf8a106419c9e2
Alessandro Lo Manto <alessandro.lo.manto@gmail.com>
2025-07-30 16:29:19 +0200

    Added dd-profiler to test v29

--- app/app.go
+++ app/app.go
@@ -18,6 +18,8 @@ import (
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 
+	"gopkg.in/DataDog/dd-trace-go.v1/profiler"
+
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	"github.com/skip-mev/block-sdk/v2/block"
 	"github.com/skip-mev/block-sdk/v2/block/base"
@@ -281,6 +283,24 @@ func NewOsmosisApp(
 		if err != nil {
 			panic(err)
 		}
+	}
+
+	// Initialize DataDog profiler if enabled
+	err := profiler.Start(
+		profiler.WithService("osmosis-dd-profiler"),
+		// profiler.WithEnv("<ENVIRONMENT>"),
+		// profiler.WithVersion("<APPLICATION_VERSION>"),
+		// profiler.WithTags("<KEY1>:<VALUE1>", "<KEY2>:<VALUE2>"),
+		profiler.WithProfileTypes(
+		  profiler.CPUProfile,
+		  profiler.HeapProfile,
+		  profiler.BlockProfile,
+		  profiler.MutexProfile,
+		  profiler.GoroutineProfile,
+		),
+	)
+	if err != nil {
+		panic(err)
 	}
 
 	initReusablePackageInjections() // This should run before anything else to make sure the variables are properly initialized
