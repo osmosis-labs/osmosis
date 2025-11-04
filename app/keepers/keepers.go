dcaba175f3fd336c38d73d1eab3dc7b195e07f8b
JohnnyWyles <johnny.wyles@osmosis.team>
2025-11-04 13:22:06 +0000

    Reorder epoch hooks so swap routes from protorev are fresh

--- app/keepers/keepers.go
+++ app/keepers/keepers.go
@@ -882,12 +882,12 @@ func (appKeepers *AppKeepers) SetupHooks() {
 	appKeepers.EpochsKeeper.SetHooks(
 		epochstypes.NewMultiEpochHooks(
 			// insert epoch hooks receivers here
+			appKeepers.ProtoRevKeeper.EpochHooks(),
 			appKeepers.TxFeesKeeper.Hooks(),
 			appKeepers.TwapKeeper.EpochHooks(),
 			appKeepers.SuperfluidKeeper.Hooks(),
 			appKeepers.IncentivesKeeper.Hooks(),
 			appKeepers.MintKeeper.Hooks(),
-			appKeepers.ProtoRevKeeper.EpochHooks(),
 		),
 	)
 
