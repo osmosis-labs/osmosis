b9ef092fa09b4214271d02d4be58a7520099e389
Nicolas Lara <nicolaslara@gmail.com>
2026-01-19 16:07:07 +0100

    Update osmoutils IBC handling for v10

--- osmoutils/ibc.go
+++ osmoutils/ibc.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	transfertypes "github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
-	channeltypes "github.com/cosmos/ibc-go/v8/modules/core/04-channel/types"
-	ibcexported "github.com/cosmos/ibc-go/v8/modules/core/exported"
+	transfertypes "github.com/cosmos/ibc-go/v10/modules/apps/transfer/types"
+	channeltypes "github.com/cosmos/ibc-go/v10/modules/core/04-channel/types"
+	ibcexported "github.com/cosmos/ibc-go/v10/modules/core/exported"
 )
 
 const IbcAcknowledgementErrorType = "ibc-acknowledgement-error"
@@ -66,7 +66,7 @@ func MustExtractDenomFromPacketOnRecv(packet ibcexported.PacketI) string {
 		// The denomination used to send the coins is either the native denom or the hash of the path
 		// if the denomination is not native.
 		denomTrace := transfertypes.ParseDenomTrace(unprefixedDenom)
-		if denomTrace.Path != "" {
+		if !denomTrace.IsNative() {
 			denom = denomTrace.IBCDenom()
 		}
 	} else {
